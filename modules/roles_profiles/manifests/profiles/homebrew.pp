d12dfdce0922615028035c1135c3b3460ff7e8fb
Jake Watkins <jwatkins@mozilla.com>
2019-01-29 12:58:02 -0800

    Add initial draft of roles and profiles

---
+++ modules/roles_profiles/manifests/profiles/homebrew.pp
@@ -0,0 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class roles_profiles::profiles::homebrew {
+
+    class { 'homebrew':
+        user      => 'vagrant',
+        group     => 'staff',
+        multiuser => true,
+    }
+
+}
