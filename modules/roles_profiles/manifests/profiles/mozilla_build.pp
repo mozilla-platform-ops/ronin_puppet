2d7c1c6b0dc36a6e31b597124af56ac5b01b8577
markcor <mcornmesser@mozilla.com>
2019-02-13 17:06:35 -0800

    add mozilla build

---
+++ modules/roles_profiles/manifests/profiles/mozilla_build.pp
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class roles_profiles::profiles::mozilla_build {
+
+    case $::operatingsystem {
+        'Windows': {
+
+        $version      = '3.2'
+        $install_path = "${facts['custom_win_systemdrive']}\\mozilla-build"
+
+            class { 'win_mozilla_build':
+                version      => $version,
+                install_path => $install_path,
+            }
+            # Bug List
+            # https://bugzilla.mozilla.org/show_bug.cgi?id=1524440
+        }
+        default: {
+            fail("${::operatingsystem} not supported")
+        }
+    }
+}
