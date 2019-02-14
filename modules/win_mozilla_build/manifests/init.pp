de15a5b0c42f2f22efceb0d9d72602c69172cf92
markcor <mcornmesser@mozilla.com>
2019-02-13 16:48:43 -0800

    mozilla build base install

---
+++ modules/win_mozilla_build/manifests/init.pp
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_mozilla_build (
+    $version       = undef,
+    $install_path = undef
+){
+
+    if $::operatingsystem == 'Windows' {
+        include win_mozilla_build::install
+    } else {
+        fail("${module_name} does not support ${::operatingsystem}")
+    }
+}
+
+# Bug list
+# https://bugzilla.mozilla.org/show_bug.cgi?id=1520947
