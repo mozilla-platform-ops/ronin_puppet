17cbf5b64bce30f7a40fe91bc517169479c98f37
markcor <mcornmesser@mozilla.com>
2019-02-19 13:09:57 -0800

    grant sysmlink access

--- modules/win_mozilla_build/manifests/grant_symlnk_access.pp
+++ modules/win_mozilla_build/manifests/grant_symlnk_access.pp
@@ -5,10 +5,26 @@
 class win_mozilla_build::grant_symlnk_access {
 
     require win_mozilla_build::hg_install
+    $hg_exe     = "${win_mozilla_build::program_files}\\mercurial\\hg.exe"
+    $carbon_dir = "${win_mozilla_build::tempdir}\\carbon"
+    $system32   = $win_mozilla_build::system32
 
-    exec { 'rename-guest':
-        command   => file('win_mozilla_build/grant_symlnk_access.ps1'),
-        provider  => powershell,
-        logoutput => true,
+    exec { 'carbonclone':
+        command     => "${hg_exe} clone --insecure https://bitbucket.org/splatteredbits/carbon ${carbon_dir}",
+        subscribe   => Class['win_mozilla_build::hg_install'],
+        refreshonly => true,
     }
+    exec { 'carbonupdate':
+        command     => "${hg_exe} update 2.4.0 -R https://bitbucket.org/splatteredbits/carbon ${carbon_dir}",
+        subscribe   => Exec['carbonclone'],
+        refreshonly => true,
+    }
+    file { "${system32}\\WindowsPowerShell\\v1.0\\Modules\\Carbon":
+        source => "${carbon_dir}\\carbon",
+    }
+    #exec { 'rename-guest':
+        #command   => file('win_mozilla_build/grant_symlnk_access.ps1'),
+        #provider  => powershell,
+        #logoutput => true,
+    #}
 }
