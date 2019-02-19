563b48608b0a0e036bf41d817ff973c93a80e7c5
markcor <mcornmesser@mozilla.com>
2019-02-19 12:49:01 -0800

    grant sysmlink access

--- modules/win_mozilla_build/files/grant_symlnk_access.ps1
+++ modules/win_mozilla_build/files/grant_symlnk_access.ps1
@@ -2,9 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-C:\Program Files\Mercurial\hg.exe clone --insecure https://bitbucket.org/splatteredbits/carbon C:\Windows\Temp\carbon
+"C:\Program Files\Mercurial\hg.exe" clone --insecure https://bitbucket.org/splatteredbits/carbon C:\Windows\Temp\carbon
 
-C:\Program Files\Mercurial\hg.exe update 2.4.0 -R C:\Windows\Temp\carbon
+"C:\Program Files\Mercurial\hg.exe" update 2.4.0 -R C:\Windows\Temp\carbon
 
 xcopy C:\Windows\Temp\carbon\carbon C:\Windows\Temp\carbon C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Carbon /e /i /y
 
