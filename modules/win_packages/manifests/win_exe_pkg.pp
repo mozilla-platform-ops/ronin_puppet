5d94408b1598116b29f2867c1b06ff5a2023f896
markcor <mcornmesser@mozilla.com>
2019-02-26 11:35:57 -0800

    add moz maintenance service

--- modules/win_packages/manifests/win_exe_pkg.pp
+++ modules/win_packages/manifests/win_exe_pkg.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define win_packages::win_exe_pkg ($pkg, $creates=$undef, $unless=undef, $package=$title, $install_options_string=[]) {
+define win_packages::win_exe_pkg ($pkg, $creates=$undef, $unless=$undef, $package=$title, $install_options_string=[]) {
 
 $pkgdir       = $facts['custom_win_temp_dir']
 $srcloc       = lookup('win_ext_pkg_src')
