990109d2408e6eb8c32e8835f8909480b694206a
Jake Watkins <jwatkins@mozilla.com>
2019-03-06 10:37:20 -0800

    Include a site.pp and a place for provision generated node defs

---
+++ manifests/nodes/README.md
@@ -0,0 +1,3 @@
+### Node definitions
+
+Node definitions auto generated at time of provisioning.  If it becomes necessary to change the role of a node, the node should be completely reprovisioned.
---
+++ manifests/site.pp
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Default to 0:0, 0644 on POSIX, and root, 0644 on Windows
+case $::operatingsystem {
+    'Windows': {
+        File {
+            owner              => root,
+            backup             => false,
+            source_permissions => ignore,
+        }
+    }
+    default: {
+        File {
+            owner  => 0,
+            group  => 0,
+            mode   => 0644,
+            backup => false,
+        }
+    }
+}
+
+# Default node should always fail
+node default {
+  fail("Missing node classification for node ${networking['fqdn']}")
+}
