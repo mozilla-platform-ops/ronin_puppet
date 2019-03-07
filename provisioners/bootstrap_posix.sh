c1bfe27d9e01c60224ae9e1d0a252fc69f07c8e1
Jake Watkins <jwatkins@mozilla.com>
2019-03-07 14:12:56 -0800

    bootstrap_posix fixes and refactoring

--- provisioners/bootstrap_posix.sh
+++ provisioners/bootstrap_posix.sh
@@ -21,33 +21,50 @@
 # TODO: change this url to track master on the moz platform ops org relop
 PUPPET_REPO='https://github.com/dividehex/ronin_puppet/archive/bootstrap.tar.gz'
 
-# Hang for when a system is in no way of recovery and needs to be reprovisioned
-# eg. provisioner did not set a role
-function hang() {
-    echo "${@}"
+# If something fails hard, either exit for interactive or hang for non-interactive
+function fail {
     # TODO: emit an critical error event so provisioner knows this node needs to be handled
-    while true; do sleep 3600; done
-}
-
-# Exit non-zero so the init (systemd/launchd/etc) can try again with keepalive
-function fail() {
     echo "${@}"
+    # If this is a non-interactive session, hang indefinitely instead of exiting
+    if [ "$NONINTERACTIVE" = true ]; then
+        echo "Hanging..."
+        while true; do sleep 3600; done
+    fi
     exit 1
 }
 
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1
+
+# Parse options. Use of -l flags the script as non-interactive
+while getopts ":h?l:" opt; do
+    case "$opt" in
+        h|\?)
+            echo "Usage: ./bootstrap_posix.sh -h               - Show help"
+            echo "       ./bootstrap_posix.sh -l /path/logfile - Log output to file"
+            echo "       ./bootstrap_posix.sh                  - Interactive mode"
+            exit 0
+            ;;
+        l)
+            LOG_PATH=$OPTARG
+            NONINTERACTIVE=true
+            # touch log file to see if we have write access and fail otherwise
+            touch "$LOG_PATH" || fail "Can't write log to ${LOG_PATH}"
+            exec >"$LOG_PATH" 2>&1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument" >&2
+            exit 1
+            ;;
+    esac
+done
+
 # Determine OSTYPE so we can set OS specific paths and alter logic if need be
 case "${OSTYPE}" in
-  darwin*)  OS='darwin'
-            ROOT='/var/root' ;;
-  linux*)   OS='linux'
-            ROOT='/root';;
+  darwin*)  OS='darwin' ;;
+  linux*)   OS='linux' ;;
   *)        fail "OS either not detected or not supported!" ;;
 esac
-
-# Log to file in root home dir if non-interactive
-if [ -z "$PS1" ]; then
-    exec >"${ROOT}/puppetize.log" 2>&1
-fi
 
 # Linux and Darwin share some common paths
 if [ $OS == "linux" ] || [ $OS == "darwin" ]; then
@@ -64,58 +81,64 @@ fi
 if [ -f "${ROLE_FILE}" ]; then
     ROLE=$(<${ROLE_FILE})
 else
-    hang "Failed to find puppet role file ${ROLE_FILE}"
+    fail "Failed to find puppet role file ${ROLE_FILE}"
 fi
 
 # Check that we have the minimum requirements to run puppet
 # Since this is a bootstrap script we may actaully install minimum requirements here in the future
 if [ ! -x "${PUPPET_BIN}" ]; then
-    hang "${PUPPET_BIN} is missing or not executable"
+    fail "${PUPPET_BIN} is missing or not executable"
 fi
 
 if [ ! -x "${FACTER_BIN}" ]; then
-    hang "${FACTER_BIN} is missing or not executable"
+    fail "${FACTER_BIN} is missing or not executable"
 fi
 
 if [ ! -x "${R10K_BIN}" ]; then
-    hang "${R10K_BIN} is missing or not executable"
+    fail "${R10K_BIN} is missing or not executable"
 fi
 
-# Create the puppet dir and temp dir for downloading
-mkdir -p "puppet"
-[ -d "puppet" ] || fail "Failed to mkdir puppet dir"
+# Create a temp dir for executing puppet
+TMP_PUPPET_DIR=$(mktemp -d -t puppet_working)
+[ -d "${TMP_PUPPET_DIR}" ] || fail "Failed to mktemp puppet working dir"
 
-TMPDIR=$(mktemp -d)
-[ -d "${TMPDIR}" ] || fail "Failed to mktemp download dir"
+# Download puppet repository and extract
+function get_puppet_repo {
+    TMP_DL_DIR=$(mktemp -d -t puppet_download)
+    [ -d "${TMP_DL_DIR}" ] || fail "Failed to mktemp download dir"
 
-# Download the puppet repo tarball directly from github
-# We don't use git because some oses don't have git installed by default
-# In the future, we may publish master branch to s3 or some other highly available service since
-# Github has rate limits on downloads.
-curl -sL $PUPPET_REPO -o "${TMPDIR}/puppet.tar.gz" || fail "Failed to download puppet repo tar.gz"
-# Extract the puppet repo tarball
-tar -zxf "${TMPDIR}/puppet.tar.gz" --strip 1 -C "puppet" || fail "Failed to extract puppet tar.gz"
-# Clean up the download dir
-rm -rf "${TMPDIR}"
+    # Download the puppet repo tarball directly from github
+    # We don't use git because some oses don't have git installed by default
+    # In the future, we may publish master branch to s3 or some other highly available service since
+    # Github has rate limits on downloads.
+    curl -sL $PUPPET_REPO -o "${TMP_DL_DIR}/puppet.tar.gz" || fail "Failed to download puppet repo tar.gz"
+    # Extract the puppet repo tarball
+    tar -zxf "${TMP_DL_DIR}/puppet.tar.gz" --strip 1 -C "${TMP_PUPPET_DIR}" || fail "Failed to extract puppet tar.gz"
+    # Clean up the download dir
+    rm -rf "${TMP_DL_DIR}"
 
-# Change to puppet dir
-cd puppet || fail "Failed to change dir"
+    # Change to puppet dir
+    cd "$TMP_PUPPET_DIR" || fail "Failed to change dir"
 
-# Install R10k Modules
-$R10K_BIN puppetfile install || fail "Failed to install R10k modules"
+    # Install R10k Modules
+    $R10K_BIN puppetfile install || fail "Failed to install R10k modules"
 
-# Get fqdn from facter
-FQDN=$(${FACTER_BIN} networking.fqdn)
+    # Get fqdn from facter
+    FQDN=$(${FACTER_BIN} networking.fqdn)
 
-# Create a node definition for this host and write it to the manifests where puppet will pick it up
-cat <<EOF > manifests/nodes/nodes.pp
+    # Create a node definition for this host and write it to the manifests where puppet will pick it up
+    cat <<EOF > manifests/nodes/nodes.pp
 node '${FQDN}' {
     include ::roles_profiles::roles::${ROLE}
 }
 EOF
+}
 
 # Run puppet and return non-zero if errors are present
-run_puppet() {
+function run_puppet {
+    # Before running puppet, get puppet repo
+    get_puppet_repo
+
     echo "Running puppet apply"
     # this includes:
     # FACTER_PUPPETIZING so that the manifests know this is a first run of puppet
@@ -156,6 +179,9 @@ case "$OS" in
         rm /Library/LaunchDaemons/org.mozilla.boostrap_posix.plist*
         ;;
 esac
+
+# Remove the temp working puppet dir
+rm -rf "$TMP_PUPPET_DIR"
 
 # record the installation date (note that this won't appear anywhere on Darwin)
 echo "System Installed: $(date)" >> /etc/issue
