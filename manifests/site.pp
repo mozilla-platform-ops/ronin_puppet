72e3fc5f632ad76aa29e4034069229cf81a9862d
Jake Watkins <jwatkins@mozilla.com>
2019-03-08 14:10:04 -0800

    Fix comment related to File defaults in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Default to 0:0, 0644 on POSIX, and root, 0644 on Windows
 case $::operatingsystem {
     'Windows': {
         File {
@@ -11,6 +10,7 @@ case $::operatingsystem {
         }
     }
     default: {
+        # Default to 0:0, 0644 on POSIX
         File {
             owner  => 0,
             group  => 0,
