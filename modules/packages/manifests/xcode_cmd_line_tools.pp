da3ac633ebc4d90c607b17c5a73e9d6ffb7117a5
Jake Watkins <jwatkins@mozilla.com>
2019-03-13 09:13:02 -0700

    Method for installing macos packages from s3

---
+++ modules/packages/manifests/xcode_cmd_line_tools.pp
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class packages::xcode_cmd_line_tools {
+
+    packages::macos_package_from_s3 { 'Command_Line_Tools_macOS_10.14_for_Xcode_10.1.dmg':
+        private             => true,
+        os_version_specific => true
+    }
+}
