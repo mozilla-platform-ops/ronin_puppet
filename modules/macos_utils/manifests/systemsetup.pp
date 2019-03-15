426e1eeda4e73e671ca8c4f5610aa3cc019977d5
Jake Watkins <jwatkins@mozilla.com>
2019-03-15 10:19:16 -0700

    Refactor macos_utils::systemsetup

--- modules/macos_utils/manifests/systemsetup.pp
+++ modules/macos_utils/manifests/systemsetup.pp
@@ -1,16 +1,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-define macos_utils::systemsetup ($option = $title,$setting = '') {
+
+define macos_utils::systemsetup (
+    String $setting,
+    String $option = $title,
+) {
 
     $cmd = '/usr/sbin/systemsetup'
 
-    if ($option != undef) and ($setting != undef) {
-        exec {
-            "osx_systemsetup -set${title} ${setting}" :
-                command => "${cmd} -set${title} ${setting}",
-                unless  =>
-                    "${cmd} -get${title} | awk -F \": \" \'{print \$2}\' | grep -i ${setting}",
-        }
+    exec { "macos_systemsetup -set${title} ${setting}" :
+        command => "${cmd} -set${title} ${setting}",
+        unless  => "${cmd} -get${title} | awk -F \": \" \'{print \$2}\' | grep -i ${setting}",
     }
 }
