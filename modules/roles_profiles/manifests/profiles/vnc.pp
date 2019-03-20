af532d692f4573cc111d1b90f24f622d981659f6
markcor <mcornmesser@mozilla.com>
2019-03-20 15:45:49 -0700

    resolve merge conlift error

--- modules/roles_profiles/manifests/profiles/vnc.pp
+++ modules/roles_profiles/manifests/profiles/vnc.pp
@@ -40,6 +40,7 @@ class roles_profiles::profiles::vnc {
             # Bug List
             #
             # TODO Add 32 bit support
+        }
         'Darwin': {
             include macos_utils::enable_screensharing
         }
