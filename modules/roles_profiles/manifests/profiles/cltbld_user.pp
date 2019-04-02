1c06f22a05c5a514faf794ad0afa6aac4e5c56a1
Jake Watkins <jwatkins@mozilla.com>
2019-04-02 16:24:28 -0700

    Install multiple packages via homebrew

--- modules/roles_profiles/manifests/profiles/cltbld_user.pp
+++ modules/roles_profiles/manifests/profiles/cltbld_user.pp
@@ -46,6 +46,12 @@ class roles_profiles::profiles::cltbld_user {
                 group   => 'staff',
                 require => User['cltbld'],
             }
+
+            contain packages::python2
+            contain packages::python3
+            contain packages::mercurial
+            contain packages::nodejs
+            contain packages::wget
         }
         default: {
             fail("${::operatingsystem} not supported")
