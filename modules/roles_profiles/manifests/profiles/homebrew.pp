6d9bfc5f966fc7249be2c993b22f2548b00e87df
Jake Watkins <jwatkins@mozilla.com>
2019-04-02 16:18:30 -0700

    Install homebrew under cltbld

--- modules/roles_profiles/manifests/profiles/homebrew.pp
+++ modules/roles_profiles/manifests/profiles/homebrew.pp
@@ -5,10 +5,11 @@
 class roles_profiles::profiles::homebrew {
 
     require packages::xcode_cmd_line_tools
+    require roles_profiles::profiles::cltbld_user
+
     class { 'homebrew':
-        user      => 'vagrant',
+        user      => 'cltbld',
         group     => 'staff',
         multiuser => true,
     }
-
 }
