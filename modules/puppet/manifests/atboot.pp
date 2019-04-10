4e342988e88ebf1dbbccb9158e75922b31e49067
Jake Watkins <jwatkins@mozilla.com>
2019-04-10 15:47:15 -0700

    atboot env and erb vars

--- modules/puppet/manifests/atboot.pp
+++ modules/puppet/manifests/atboot.pp
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class puppet::atboot {
+class puppet::atboot (
+    String $puppet_repo   = 'https://github.com/mozilla-platform-ops/ronin_puppet.git',
+    String $puppet_branch = 'master',
+) {
 
     case $::operatingsystem {
         'Darwin': {
