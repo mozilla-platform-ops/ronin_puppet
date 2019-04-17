db4087973f9e4a4078d8b035e4089dfd5cdc2378
Dave House <dhouse@mozilla.com>
2019-04-17 13:20:08 -0600

    remove bootstrap user

--- modules/roles_profiles/manifests/roles/mac_v3_signing.pp
+++ modules/roles_profiles/manifests/roles/mac_v3_signing.pp
@@ -22,4 +22,5 @@ class roles_profiles::roles::mac_v3_signing {
     include ::roles_profiles::profiles::homebrew
     include ::roles_profiles::profiles::relops_users
     include ::roles_profiles::profiles::releng_users
+    include ::roles_profiles::profiles::remove_bootstrap_user
 }
