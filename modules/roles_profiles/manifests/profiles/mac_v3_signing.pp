1c432ba13bfdeeca30cfcd6e2daa69d92140cb2a
Dave House <dhouse@mozilla.com>
2019-04-24 14:53:05 -0600

    pin to notarization branch

--- modules/roles_profiles/manifests/profiles/mac_v3_signing.pp
+++ modules/roles_profiles/manifests/profiles/mac_v3_signing.pp
@@ -7,7 +7,13 @@ class roles_profiles::profiles::mac_v3_signing {
     case $::operatingsystem {
         'Darwin': {
 
-            include puppet::atboot
+            # "pinning"
+            # for the first setup of a node type, the provisioner script in the image must have a valid node
+            # then, this pinning will apply on the next run atboot
+            puppet::atboot { 'puppet_atboot':
+                puppet_repo   => 'https://github.com/davehouse/ronin_puppet.git',
+                puppet_branch => 'notarization',
+            }
 
             # we can add generic-worker setup here like in gecko_t_osx_1014_generic_worker.pp
 
