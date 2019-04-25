744ee71ab0bb9c80fc2ac01eee4aed054de268ac
Andrew Erickson <aerickson@mozilla.com>
2019-04-25 15:29:01 -0700

    spec helper: cleanup

--- test/integration/bitbar/serverspec/spec_helper.rb
+++ test/integration/bitbar/serverspec/spec_helper.rb
@@ -1,8 +1,5 @@
 require 'serverspec'
 
-# format kitchen-test spec output for Jenkins
-#require 'yarjuf'
-
 # avoid getting spammed with messages
 # see https://medium.com/@Joachim8675309/testkitchen-with-chef-and-serverspec-2ac0cd938e5
 set :backend, :exec
