7233da28d8a9f622b34c48baf77ed7cab358fadb
Andrew Erickson <aerickson@mozilla.com>
2019-04-26 13:34:56 -0700

    test manifest cleanup

--- manifests-kitchen/bitbar/z-bitbar.pp
+++ manifests-kitchen/bitbar/z-bitbar.pp
@@ -1,10 +1,1 @@
-##################################################
-#### MOCK CLASSES WHICH SHOULD NOT TESTED HERE
-# class kitchen_template2(
-#   Hash $config = {},
-# ) {
-#   notice( 'mocked class ==> kitchen_template::foobar' )
-# }
-
-# INCLUDE CLASSES HERE
 include roles_profiles::roles::bitbar_devicepool
