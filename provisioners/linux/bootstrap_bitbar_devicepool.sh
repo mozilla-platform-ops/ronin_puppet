3daac5f16de3a4816b7472943c5e19cdc6065224
Andrew Erickson <aerickson@mozilla.com>
2019-04-25 18:53:51 -0700

    bootstrap/run script is working

--- provisioners/linux/bootstrap_bitbar_devicepool.sh
+++ provisioners/linux/bootstrap_bitbar_devicepool.sh
@@ -1,52 +1,113 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
+
+PUPPET_BIN='/opt/puppetlabs/bin/puppet'
+PUPPET_ENV_DIR='/etc/puppetlabs/environments'
+FACTER_BIN='/opt/puppetlabs/bin/facter'
+R10K_BIN='/opt/puppetlabs/bin/r10k'
+R10K_DIR="/etc/puppetlabs/environments/production/r10k_modules"
+
+FQDN=$(${FACTER_BIN} networking.fqdn)
 
 ROLE="bitbar_devicepool"
-PROVISION_REPO="https://github.com/mozilla-platform-ops/ronin_puppet.git"
 
+# PUPPET_REPO="https://github.com/mozilla-platform-ops/ronin_puppet.git"
+# PUPPET_BRANCH="master"
+
+PUPPET_REPO="https://github.com/aerickson/ronin_puppet.git"
+PUPPET_BRANCH="bitbar_devicepool"
+
+ROLE_FILE='/etc/puppet_role'
+PUPPET_REPO_PATH="$PUPPET_ENV_DIR/production"
+# TODO: refactor this away
+WORKING_DIR="$PUPPET_REPO_PATH"
 
 ## FUNCTIONS
 
-# Download puppet repository
-function get_puppet_repo {
-    # clone or update repo
-    if [ ! -d "${PUPPET_ENV}/production" ]; then
-        # update
-        ( cd "$PUPPET_ENV/production" && git pull --rebase )
-    else
-        # clone
-        git clone "$PROVISION_REPO" "$PUPPET_ENV"
+function update_puppet {
+
+    mkdir -p "$WORKING_DIR"
+    cd "$WORKING_DIR"
+
+    # Initialize working dir if dir is not a git repo
+    if [ ! -d .git ]; then
+        git init || return 1
+        git remote add origin "${PUPPET_REPO}" || return 1
     fi
 
-    # Change to puppet dir
-    cd "${PUPPET_ENV}/production" || fail "Failed to change dir"
-    chmod 777 .
+    # Fetch and checkout production branch
+    git fetch --all --prune || return 1
+    git checkout --force origin/${PUPPET_BRANCH} || return 1
 
-    # Install R10k Modules
-    $R10K_BIN puppetfile install -v || fail "Failed to install R10k modules"
+    # Purge modules no longer managed by Puppetfile
+    R10K_PURGE_OPTIONS=("--moduledir=${R10K_DIR}" '-v')
+    $R10K_BIN puppetfile purge "${R10K_PURGE_OPTIONS[@]}"
 
-    # Get fqdn from facter
-    FQDN=$(${FACTER_BIN} networking.fqdn)
+    # Install r10k modules
+    R10K_INSTALL_OPTIONS=("--moduledir=${R10K_DIR}" '--force' '-v')
+    $R10K_BIN puppetfile install "${R10K_INSTALL_OPTIONS[@]}"
 
-    # Create a node definition for this host and write it to the manifests where puppet will pick it up
-    cat <<EOF > manifests/nodes/nodes.pp
+    # Copy secrets
+    # mkdir -p "${WORKING_DIR}/data/secrets"
+    # cp /var/root/vault.yaml "${WORKING_DIR}/data/secrets/vault.yaml"
+    # chmod 0600 "${WORKING_DIR}/data/secrets/vault.yaml"
+
+    cat <<EOF > "${WORKING_DIR}/manifests/nodes/nodes.pp"
     node '${FQDN}' {
-      include ::roles_profiles::roles::${ROLE}
+        include ::roles_profiles::roles::${ROLE}
     }
 EOF
+
+    return 0
 }
+
+# # Download puppet repository
+# function get_puppet_repo {
+#     # clone or update repo
+#     if [ -d "${PUPPET_REPO_PATH}" ]; then
+#         # update
+#         ( cd "$PUPPET_REPO_PATH" && \
+#           git reset --hard       && \
+#           git pull --rebase )
+#     else
+#         # clone
+#         git clone "$PROVISION_REPO" "$PUPPET_REPO_PATH"
+#     fi
+
+#     # Change to puppet dir
+#     cd "${PUPPET_REPO_PATH}" || fail "Failed to change dir"
+#     chmod 777 .
+
+#     # Install R10k Modules
+#     $R10K_BIN puppetfile install -v || fail "Failed to install R10k modules"
+
+#     # Get fqdn from facter
+#     FQDN=$(${FACTER_BIN} networking.fqdn)
+
+#     # Create a node definition for this host and write it to the manifests where puppet will pick it up
+#     cat <<EOF > manifests/nodes/nodes.pp
+#     node '${FQDN}' {
+#       include roles_profiles::roles::${ROLE}
+#     }
+# EOF
+# }
 
 # Run puppet and return non-zero if errors are present
 function run_puppet {
     # Before running puppet, get puppet repo
-    get_puppet_repo
+    # get_puppet_repo
+    update_puppet
 
     echo "Running puppet apply"
+
+    cd $PUPPET_REPO_PATH
     # this includes:
     # FACTER_PUPPETIZING so that the manifests know this is a first run of puppet
     # TODO: send logs to syslog? send a puppet report to puppetdb?
     PUPPET_OPTIONS=('--modulepath=./modules:./r10k_modules' '--hiera_config=./hiera.yaml' '--logdest=console' '--color=false' '--detailed-exitcodes' './manifests/')
+    # PUPPET_OPTIONS=('--modulepath=./modules:./r10k_modules' '--debug' '--hiera_config=./hiera.yaml' '--logdest=console' '--color=false' '--detailed-exitcodes' './manifests/')
 
     # check for 'Error:' in the output; this catches errors even
     # when the puppet exit status is incorrect.
@@ -77,18 +138,9 @@ fi
 
 # Determine OSTYPE so we can set OS specific paths and alter logic if need be
 case "${OSTYPE}" in
-  linux*)   OS='linux' ;;
+  linux*)   ;;
   *)        fail "OS either not detected or not supported!" ;;
 esac
-
-# Linux and Darwin share some common paths
-if [ $OS == "linux" ] || [ $OS == "darwin" ]; then
-    ROLE_FILE='/etc/puppet_role'
-    PUPPET_BIN='/opt/puppetlabs/bin/puppet'
-    PUPPET_ENV='/etc/puppetlabs/code/environments'
-    FACTER_BIN='/opt/puppetlabs/bin/facter'
-    R10K_BIN='/opt/puppetlabs/bin/r10k'
-fi
 
 # guard against running on hosts with other roles
 if [ -e "$ROLE_FILE" ]; then
@@ -135,7 +187,7 @@ fi
 wget -P /var/tmp/ "http://apt.puppetlabs.com/puppet6-release-$(lsb_release -c -s).deb"
 dpkg -i /var/tmp/*.deb
 apt-get update -y && apt-get install -y puppet-agent
-ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+ln -sf /opt/puppetlabs/bin/puppet /usr/bin/puppet
 # install r10k
 /opt/puppetlabs/puppet/bin/gem install r10k
 
