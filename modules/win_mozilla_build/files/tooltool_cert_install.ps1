e896dc00fd24ec12f0825f653207b3dd7882427a
markcor <mcornmesser@mozilla.com>
2019-05-01 17:50:21 -0700

    add tooltool cert install

--- modules/win_mozilla_build/files/tooltool_cert_install.ps1
+++ modules/win_mozilla_build/files/tooltool_cert_install.ps1
@@ -3,4 +3,6 @@ $webRequest = [Net.WebRequest]::Create("https://tooltool.mozilla-releng.net")
 try { $webRequest.GetResponse() } catch {}
 $cert = $webRequest.ServicePoint.Certificate
 $bytes = $cert.Export([Security.Cryptography.X509Certificates.X509ContentType]::Cert)
+set-content -value $bytes -encoding byte -path $tooltool_cer
+
 certutil -addstore root $tooltool_cer
