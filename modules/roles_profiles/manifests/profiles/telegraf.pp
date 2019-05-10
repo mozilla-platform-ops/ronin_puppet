7685cd3cf1e33660348b079f8e92bea2144efaed
Dave House <dhouse@mozilla.com>
2019-05-10 12:01:17 -0600

    fix var set

--- modules/roles_profiles/manifests/profiles/telegraf.pp
+++ modules/roles_profiles/manifests/profiles/telegraf.pp
@@ -7,16 +7,16 @@ class roles_profiles::profiles::telegraf (
     String $worker_type,
 ) {
 
-    influx_url             = 'https://telegraf.relops.mozops.net'
-    influx_database        = 'relops'
-    influx_username        = 'relops_wo'
-    influx_password        = lookup('influx_password')
-    interval               = '60s'
-    worker_group           = $worker_group
-    data_center            = $worker_group
-    worker_type            = $worker_type
+    $influx_url             = 'https://telegraf.relops.mozops.net'
+    $influx_database        = 'relops'
+    $influx_username        = 'relops_wo'
+    $influx_password        = lookup('influx_password')
+    $interval               = '60s'
+    $worker_group           = $worker_group
+    $data_center            = $worker_group
+    $worker_type            = $worker_type
 
-    telegraf_config_source = template('telegraf/telegraf.conf.erb')
+    $telegraf_config_source = template('telegraf/telegraf.conf.erb')
 
     case $::operatingsystem {
         'Windows': {
