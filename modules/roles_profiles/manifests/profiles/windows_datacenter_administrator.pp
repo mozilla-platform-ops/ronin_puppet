6f76245db91b934bf7d4dd16fe641666b5b9b45a
markcor <mcornmesser@mozilla.com>
2019-05-22 08:59:57 -0700

    Bug1545481 - Add support to manage admin password (#39)
    * Bug 1545481 - add dc administrator profile
    
    * Bug 1545481 - add dc administrator profile
    
    * Bug 1545481 - add dc administrator profile
    
    * add ref-hw role and bootsrap
    
    * remove pinning
    
    * remove pinning
    
    * add comment

---
+++ modules/roles_profiles/manifests/profiles/windows_datacenter_administrator.pp
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class roles_profiles::profiles::windows_datacenter_administrator {
+
+    case $::operatingsystem {
+        'Windows': {
+
+            $admin_password = lookup('win_adminpw')
+
+            class { 'win_users::administrator::account':
+                admin_password => $admin_password,
+            }
+
+        }
+        # Bug List
+        # https://bugzilla.mozilla.org/show_bug.cgi?id=1545481
+        default: {
+            fail("${::operatingsystem} not supported")
+        }
+    }
+}
