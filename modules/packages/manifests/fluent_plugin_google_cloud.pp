e125e90ff511c3e849e7c824538c3e8b1c643923
Dave House <dhouse@mozilla.com>
2019-05-24 12:46:57 -0600

    workaround, just re-run

--- modules/packages/manifests/fluent_plugin_google_cloud.pp
+++ modules/packages/manifests/fluent_plugin_google_cloud.pp
@@ -13,7 +13,7 @@ class packages::fluent_plugin_google_cloud {
     exec {
         'install plugin with agent ruby':
             path    => ['/bin', '/sbin', '/usr/sbin', '/usr/local/bin', '/usr/bin'],
-            command => "/usr/sbin/td-agent-gem install fluent-plugin-google-cloud --version ${version}",
+            command => "[ -e /usr/sbin/td-agent-gem ] && /usr/sbin/td-agent-gem install fluent-plugin-google-cloud --version ${version} || echo 'missing td-agent'",
             unless  => "test -f /opt/td-agent/embedded/lib/ruby/gems/2.4.0/gems/fluent-plugin-google-cloud-${version}",
             require => Class['packages::td_agent'];
     }
