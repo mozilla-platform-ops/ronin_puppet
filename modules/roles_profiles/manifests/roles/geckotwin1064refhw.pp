339fcc557fb393c620c15f26385f0f0fc55ae651
markcor <mcornmesser@mozilla.com>
2019-06-04 17:34:43 -0700

    remove vnc

--- modules/roles_profiles/manifests/roles/geckotwin1064refhw.pp
+++ modules/roles_profiles/manifests/roles/geckotwin1064refhw.pp
@@ -15,7 +15,7 @@ class roles_profiles::roles::geckotwin1064refhw {
     # Adminstration
     include roles_profiles::profiles::logging
     include roles_profiles::profiles::common_tools
-    include roles_profiles::profiles::vnc
+    # This role will use a different secrets file
     include roles_profiles::profiles::windows_datacenter_administrator
     # Openssh Fails when Puppet runs as a  schedule task
     # https://bugzilla.mozilla.org/show_bug.cgi?id=1544141
