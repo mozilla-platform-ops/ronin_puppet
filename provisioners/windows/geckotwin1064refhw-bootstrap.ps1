625adef4176ad3b1a1fdb185e2ae45f9c14d640e
markcor <mcornmesser@mozilla.com>
2019-07-01 17:19:54 -0700

    Download and create Bootstrap folder

--- provisioners/windows/geckotwin1064refhw-bootstrap.ps1
+++ provisioners/windows/geckotwin1064refhw-bootstrap.ps1
@@ -74,6 +74,7 @@ function Install-Prerequ {
   param (
     [string] $ext_src = "https://s3-us-west-2.amazonaws.com/ronin-puppet-package-repo/Windows/prerequisites",
     [string] $local_dir = "$env:systemdrive\BootStrap",
+    [string] $work_dir = "$env:systemdrive\scratch",
     [string] $git = "Git-2.18.0-64-bit.exe",
     [string] $puppet = "puppet-agent-6.0.0-x64.msi"
   )
@@ -81,9 +82,15 @@ function Install-Prerequ {
     Write-Log -message ('{0} :: begin - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
   }
   process {
-    Invoke-WebRequest -Uri  https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/Windows/prerequisites/BootStrap.zip  -UseBasicParsing
-    Expand-Archive -path BootStrap.zip -DestinationPath C:\
+
+    remove-item $local_dir   -Recurse  -force
+    New-Item -path $work_dir -ItemType "directory"
+    Set-location -path $work_dir
+    Invoke-WebRequest -Uri  $ext_src/BootStrap.zip  -UseBasicParsing -OutFile $work_dir\BootStrap.zip
+    Expand-Archive -path $work_dir\BootStrap.zip -DestinationPath $env:systemdrive\
     Read-Host "Enusre c:\bootstrap\secrets\vault.yam is present, and then press eneter to continue"
+    Set-location -path $local_dir
+    remove-item $work_dir   -Recurse  -force
 
     Start-Process $local_dir\$git /verysilent -wait
     Write-Log -message  ('{0} :: Git installed " {1}' -f $($MyInvocation.MyCommand.Name), ("$git")) -severity 'DEBUG'
