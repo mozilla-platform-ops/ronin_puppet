fa14300b003b8e9b04366d31bd99f9a618112c8f
markcor <mcornmesser@mozilla.com>
2019-08-19 09:00:21 -0700

    add gw 15 profile

--- modules/roles_profiles/manifests/roles/geckotwin1064ht.pp
+++ modules/roles_profiles/manifests/roles/geckotwin1064ht.pp
@@ -25,6 +25,7 @@ class roles_profiles::roles::geckotwin1064ht {
     # Worker
     include roles_profiles::profiles::mozilla_build
     include roles_profiles::profiles::mozilla_maintenance_service
-    include roles_profiles::profiles::windows_datacenter_generic_worker_14_1_2
+#    include roles_profiles::profiles::windows_datacenter_generic_worker_14_1_2
+    include roles_profiles::profiles::windows_datacenter_generic_worker_15_1_0
     include roles_profiles::profiles::microsoft_tools
 }
