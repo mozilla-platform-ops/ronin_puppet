d995bdd26d5cb957d66b1fc5178b84abb19b7725
markcor <mcornmesser@mozilla.com>
2019-08-21 15:21:18 -0700

    correct bad or statement

--- provisioners/windows/Bitbar/geckotwin1064ref18-bootstrap.ps1
+++ provisioners/windows/Bitbar/geckotwin1064ref18-bootstrap.ps1
@@ -116,8 +116,7 @@ If(!(test-path 'HKLM:\SOFTWARE\Mozilla\ronin_puppet')) {
   Bootstrap-schtasks
   shutdown @('-r', '-t', '0', '-c', 'Reboot; Prerequisites in place, logging setup, and registry setup', '-f', '-d', '4:5')
 }
-If ($stage -eq 'setup') or ($stage -eq 'inprogress'){
-
+If (($stage -eq 'setup') -or ($stage -eq 'inprogress')){
   Install-BootstrapModule -src_Organisation $src_Organisation -src_Repository $src_Repository -src_Revision $src_Revision
   Ronin-PreRun
   Bootstrap-Puppet
