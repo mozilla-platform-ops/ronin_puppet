6b41bccc57da71b59ccee8f65b03a843eb82d50e
davehouse <dhouse@mozilla.com>
2019-08-21 14:07:12 -0600

    check fingerprints

--- provisioners/macos/bootstrap_mojave_power.sh
+++ provisioners/macos/bootstrap_mojave_power.sh
@@ -2,13 +2,28 @@
 
 PUPPET_REPO=${PUPPET_REPO:-"https://github.com/mozilla-platform-ops/ronin_puppet.git"}
 PUPPET_BRANCH=${PUPPET_BRANCH:-"master"}
+PUPPET_ROLE=${PUPPET_ROLE:-"bitbar_mbp"}
+
+function fail {
+    echo "${@}"
+    date
+    echo "[sleep 120 ...]"
+    sleep 120
+    exit 1
+}
 
 host=$(scutil --get ComputerName)
 echo $host
 
+echo $PUPPET_ROLE > /etc/puppet_role
+cat /etc/puppet_role
+
 declare -i screenshotcounter=0
 function screenshot() {
-  screencapture -x ~/screenshot_bootstrap_${screenshotcounter}.jpg
+  filename="~relops/screenshot_bootstrap_${screenshotcounter}_$(date +"%Y-%m-%d-%H:%M:%S").jpg"
+  screencapture -x $filename
+  chown relops $filename
+  chmod ugo+r $filename
   screenshotcounter+=1
 }
 screenshot
@@ -16,19 +31,31 @@ screenshot
 ioreg -l | grep IOPlatformSerialNumber
 
 if csrutil status | grep -q "enabled"; then
-   echo "SIP is enabled!"
+   fail "SIP is enabled!"
 fi
 
 ifconfig|grep -v "127.0.0.1\|169."|grep -C4 "inet [0-9]\+\.[0-9]\+"
 
-echo "bitbar_mbp" > /etc/puppet_role
-
+# Enable ssh
 sudo systemsetup -setremotelogin on
 
+# Print config
 sudo systemsetup $(sudo systemsetup -help | grep -o '\-get[^ ]*')
+# Turn off power savings/etc
+sudo systemsetup \
+    -setsleep Never \
+    -setrestartpowerfailure on \
+    -setwakeonnetworkaccess on \
+    -setallowpowerbuttontosleepcomputer off \
+    -setrestartfreeze on
 
-sudo systemsetup -setsleep Never -setrestartpowerfailure on -setwakeonnetworkaccess on -setallowpowerbuttontosleepcomputer off -setrestartfreeze on
-
+# Ensure no fingerprints are stored for login
+fingerprints=$(declare -i count=0;\
+  while read -r line; do count+=$line; done \
+  <<<$(bioutil -c | grep -o '[0-9]\+ fingerprint' | cut -d\  -f1); echo $count)
+[[ $fingerprints == 0 ]] \
+  && echo "No fingerprints." \
+  || fail "Fingerprints stored!"
 
 function checksum() {
   sum=$(shasum -a 256 "$1" 2>/dev/null || openssl dgst -sha256 "$1" 2>/dev/null)
@@ -51,7 +78,6 @@ function install() {
   done < <(hdiutil mount -plist -nobrowse -readonly -noidme -mountrandom /tmp "$dmg" | grep '\(\/dev\/disk.s\|\/private\/tmp\)' | sed -e 's/.*>\([^<]*\)<.*/\1/' | tr $'\n' ' '; echo); 
 }
 
-echo "Checking Xcode CLI tools"
 # See http://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
 xcode-select -p &> /dev/null
 if [ $? -ne 0 ]; then
@@ -62,7 +88,8 @@ if [ $? -ne 0 ]; then
     head -n 1 | awk -F"*" '{print $2}' |
     sed -e 's/^ *//' |
     tr -d '\n')
-  softwareupdate -i "$PROD" --verbose;
+  softwareupdate -i "$PROD" --verbose \
+    || fail "Xcode CLI tools install failed"
 else
   echo "Xcode CLI tools OK"
 fi
@@ -70,24 +97,28 @@ fi
 [[ "2.21.0" == $(git --version | cut -d\  -f3) ]] \
   && git --version \
   || install "https://downloads.sourceforge.net/project/git-osx-installer/git-2.21.0-intel-universal-mavericks.dmg" \
-    7a828be2ea16ad48797157769d29028f2d1c0040c87525467fd5addbb11a2cac
-  
+    7a828be2ea16ad48797157769d29028f2d1c0040c87525467fd5addbb11a2cac \
+    || fail "git install failed"
+
 [[ "6.3.0" == $(puppet --version) ]] \
   && printf "puppet version %s\n" $(puppet --version) \
   || install "https://downloads.puppetlabs.com/mac/puppet/10.14/x86_64/puppet-agent-6.3.0-1.osx10.14.dmg" \
-    b8ffdb76613adac062dea5be6ccfab8d310fd4c203396a28614f6c62f56f4deb
+    b8ffdb76613adac062dea5be6ccfab8d310fd4c203396a28614f6c62f56f4deb \
+    || fail "puppet install failed"
 
 checksum "/Applications/Intel Power Gadget/PowerLog" \
   9210e37554afc4449dcd3896aa6c9a884b20f0788e75ed2dcfae79f294b2d151 \
-  || \
-  install "https://software.intel.com/sites/default/files/managed/34/fb/Intel%C2%AE%20Power%20Gadget.dmg" \
+  || install "https://software.intel.com/sites/default/files/managed/34/fb/Intel%C2%AE%20Power%20Gadget.dmg" \
     efd306800c28abda0d5543fbc5bf78eb142a43a96feb56fcf211e3bbc83a78d3 \
   && (
     sudo kextutil /Library/Extensions/EnergyDriver.kext
     sudo kextload /Library/Extensions/EnergyDriver.kext
     sudo spctl kext-consent status
     /Applications/Intel\ Power\ Gadget/PowerLog -resolution 1000 -file /dev/stdout -cmd "for I in {1..3}; do sleep 1; done"
-  )
+  ) \
+    || fail "intel power gadget install failed"
+
+screenshot
 
 PUPPET_FORK="${PUPPET_REPO%.git}"
 PUPPET_FORK="${PUPPET_FORK#*.com}"
@@ -96,9 +127,9 @@ curl -L -O "${PUPPET_REPO_BUNDLE}"
 
 export PUPPET_REPO
 export PUPPET_BRANCH
-
-screenshot
 sed -i.bak '/reboot/d' ./bootstrap_mojave.sh
-bash bootstrap_mojave.sh && echo Success
+bash bootstrap_mojave.sh \
+  && echo Success \
+  || fail "Puppet failed!?"
 
 screenshot
