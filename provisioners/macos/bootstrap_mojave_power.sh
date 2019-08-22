b7d44ab2afc987d86d5b4286d34e2019b89a4194
davehouse <dhouse@mozilla.com>
2019-08-22 16:16:38 -0600

    fix fmm var names

--- provisioners/macos/bootstrap_mojave_power.sh
+++ provisioners/macos/bootstrap_mojave_power.sh
@@ -57,11 +57,11 @@ if csrutil status | grep -q "enabled"; then
 fi
 
 # Check for find-my-mac
-fmm-computer-name=$(nvram fmm-computer-name 2>/dev/null | cut -d$'\t' -f2)
-fmm-mobileme-token-FMM=$(nvram fmm-mobileme-token-FMM 2>/dev/null | cut -d$'\t' -f2)
-if [[ -n "${fmm-mobileme-token-FMM}" ]]; then
-    echo "fmm-computer-name= ${fmm-computer-name}"
-    echo "fmm-mobileme-token-FMM= ${fmm-mobileme-token-FMM}"
+fmm_computer_name=$(nvram fmm-computer-name 2>/dev/null | cut -d$'\t' -f2)
+fmm_mobileme_token_FMM=$(nvram fmm-mobileme-token-FMM 2>/dev/null | cut -d$'\t' -f2)
+if [[ -n "${fmm_mobileme_token_FMM}" ]]; then
+    echo "fmm-computer-name= ${fmm_computer_name}"
+    echo "fmm-mobileme-token-FMM= ${fmm_mobileme_token_FMM}"
     fail "Find-my-mac is enabled!?"
 fi
 
