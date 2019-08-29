3744b97b3d0739781d50c21b4fb4e1bd39c92322
Andrew Erickson <aerickson@mozilla.com>
2019-08-29 12:36:08 -0700

    formatting

--- modules/bitbar_devicepool/manifests/telegraf.pp
+++ modules/bitbar_devicepool/manifests/telegraf.pp
@@ -28,8 +28,8 @@ class bitbar_devicepool::telegraf {
 
   # place configs
   file { '/etc/telegraf/telegraf.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/bitbar_devicepool/empty_telegraf.conf',
+    ensure => file,
+    source => 'puppet:///modules/bitbar_devicepool/empty_telegraf.conf',
   }
   # don't replace this file as we're going to do sed later in bootstrap
   file { '/etc/telegraf/telegraf.d/devicepool.conf':
