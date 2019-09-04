6cfbfb07720d8ab68c62e1b16924d85d227a4c02
Jake Watkins <jwatkins@mozilla.com>
2019-09-04 15:35:08 -0700

    Add appdmg capability and google chrome package
            This adds appdmg capability to macos_package_from_s3 in order to
            install app folders from within dmg files.  At the same time, we
            add the google chrome package with a default version string.

---
+++ modules/packages/manifests/google_chrome.pp
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class packages::google_chrome (
+    String $version = 'v76.0.3809.132'
+) {
+
+    packages::macos_package_from_s3 { "googlechrome_${version}.dmg":
+        private             => false,
+        os_version_specific => true,
+        type                => 'appdmg',
+    }
+}
