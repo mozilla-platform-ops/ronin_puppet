b726c8b67e2802c04f09777beaa4501933d233a6
markcor <mcornmesser@mozilla.com>
2019-09-09 14:32:58 -0700

    pull from S3

--- modules/win_packages/manifests/chrome.pp
+++ modules/win_packages/manifests/chrome.pp
@@ -13,8 +13,11 @@ class win_packages::chrome {
     # This will install and update
     # Ref: https://bugzilla.mozilla.org/show_bug.cgi?id=1570767#c6
     # Existing installs will update automaticly
+    # Check S3 tags for starting version
+    # https://s3.console.aws.amazon.com/s3/object/ronin-puppet-package-repo/Windows/googlechromestandaloneenterprise64.msi?region=us-east-2&tab=properties
     win_packages::win_msi_pkg { 'Google Chrome':
         pkg             => 'googlechromestandaloneenterprise64.msi',
+        install_options => ['/quiet'],
     }
 
     registry_key { 'HKLM\System\SOFTWARE\Policies\Google':
