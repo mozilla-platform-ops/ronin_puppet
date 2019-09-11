c1597c403d44c3fa0cd36e143eb8f4fbf2c8398d
markcor <mcornmesser@mozilla.com>
2019-09-11 12:41:52 -0700

    ensure sense key exists

--- modules/win_disable_services/manifests/disable_windows_defender.pp
+++ modules/win_disable_services/manifests/disable_windows_defender.pp
@@ -49,6 +49,13 @@ class win_disable_services::disable_windows_defender {
         # SecurityHealthService and sense actively watch the registry values of the other services,
         # and there start registry value needs to be changed and then the node needs rebooted
         # Also note this will fail on Windows 7
+
+        if $facts['custom_win_release_id'] == '1803' {
+            registry_key {"${win_defend_key}\\Sense":
+                ensure => present,
+            }
+        }
+
         registry_value { $diabled_start_value:
             ensure => present,
             type   => dword,
