78ba44b3457f94b67cce0714b6dead450a073399
davehouse <dhouse@mozilla.com>
2019-09-16 11:32:51 -0600

    lint

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
@@ -40,8 +40,8 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker {
             $bugzilla_api_key         = lookup('generic_worker.gecko_t_osx_1014.bugzilla_api_key')
 
             class { 'zstandard':
-                zstandard_version         => '1.3.8',
-                zstandard_sha256          => 'fa4c25da32000a6a6ab8135e72a4159e15b54bbda7f83c3947beb7a889ffd60a',
+                zstandard_version => '1.3.8',
+                zstandard_sha256  => 'fa4c25da32000a6a6ab8135e72a4159e15b54bbda7f83c3947beb7a889ffd60a',
             }
 
             class { 'generic_worker':
