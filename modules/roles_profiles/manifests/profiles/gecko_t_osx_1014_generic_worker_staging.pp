ebf004c156a8e439452760924bd8323f866ea96b
davehouse <dhouse@mozilla.com>
2019-09-16 12:27:23 -0600

    include both

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
@@ -104,6 +104,7 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging {
             contain packages::virtualenv
 
             contain packages::python2_zstandard
+            contain packages::python3_zstandard
 
             include mercurial::ext::robustcheckout
         }
