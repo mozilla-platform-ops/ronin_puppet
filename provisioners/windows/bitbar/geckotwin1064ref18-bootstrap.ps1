7dfbd17d8190f58dde98cf6ff3ad0f4784094ab7
markcor <mcornmesser@mozilla.com>
2019-09-19 10:20:11 -0700

    pin

--- provisioners/windows/bitbar/geckotwin1064ref18-bootstrap.ps1
+++ provisioners/windows/bitbar/geckotwin1064ref18-bootstrap.ps1
@@ -113,7 +113,7 @@ If(!(test-path 'HKLM:\SOFTWARE\Mozilla\ronin_puppet')) {
   Install-BootstrapModule -src_Organisation $src_Organisation -src_Repository $src_Repository -src_Revision $src_Revision
   Set-RoninRegOptions  -workerType $workerType -src_Organisation $src_Organisation -src_Repository $src_Repository -src_Revision $src_Revision -image_provisioner $image_provisioner
   Install-Prerequ
-  Bootstrap-schtasks
+#  Bootstrap-schtasks
   shutdown @('-r', '-t', '0', '-c', 'Reboot; Prerequisites in place, logging setup, and registry setup', '-f', '-d', '4:5')
 }
 If (($stage -eq 'setup') -or ($stage -eq 'inprogress')){
