d4d4af73f15c12f4ca9f7833ff90d0e157ef1c17
davehouse <dhouse@mozilla.com>
2019-09-20 07:38:52 -0600

    try other remote-syslog

--- modules/packages/manifests/fluent_plugin_remote_syslog.pp
+++ modules/packages/manifests/fluent_plugin_remote_syslog.pp
@@ -5,13 +5,13 @@
 class packages::fluent_plugin_remote_syslog {
     require packages::td_agent
 
-    $version = '1.0.0'
+    $version = '1.1'
 
     exec {
         'install remote syslog plugin with agent ruby':
             path    => ['/bin', '/sbin', '/usr/sbin', '/usr/local/bin', '/usr/bin'],
-            command => "/usr/sbin/td-agent-gem install fluent-plugin-remote_syslog --version ${version}",
-            unless  => "test -f /opt/td-agent/embedded/lib/ruby/gems/2.4.0/gems/fluent-plugin-papertrail-${version}",
+            command => "/usr/sbin/td-agent-gem install fluent-plugin-remote-syslog --version ${version}",
+            unless  => "test -f /opt/td-agent/embedded/lib/ruby/gems/2.4.0/gems/fluent-plugin-remote-syslog-${version}",
             require => Class['packages::td_agent'];
     }
 }
