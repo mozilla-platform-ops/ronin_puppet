f95771037e81f0cb34febc485ad58dded5e2ecf5
markcor <mcornmesser@mozilla.com>
2019-09-23 15:55:15 -0700

    remove disablke restore

--- modules/roles_profiles/manifests/profiles/disable_services.pp
+++ modules/roles_profiles/manifests/profiles/disable_services.pp
@@ -41,7 +41,7 @@ class roles_profiles::profiles::disable_services {
             include win_disable_services::disable_puppet
             include win_disable_services::disable_windows_defender
             include win_disable_services::disable_windows_update
-            include win_disable_services::disable_system_restore
+            #include win_disable_services::disable_system_restore
             if $facts['os']['release']['full'] == '10' {
                 include win_disable_services::disable_onedrive
             }
