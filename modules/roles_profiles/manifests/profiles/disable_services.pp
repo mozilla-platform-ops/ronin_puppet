0d896a710cf40e6adcdb079be958d8b7fc82133f
markcor <mcornmesser@mozilla.com>
2019-09-24 09:10:17 -0700

    remove disable vss

--- modules/roles_profiles/manifests/profiles/disable_services.pp
+++ modules/roles_profiles/manifests/profiles/disable_services.pp
@@ -37,7 +37,7 @@ class roles_profiles::profiles::disable_services {
         }
         'Windows': {
             include win_disable_services::disable_wsearch
-            include win_disable_services::disable_vss
+            #include win_disable_services::disable_vss
             include win_disable_services::disable_puppet
             include win_disable_services::disable_windows_defender
             include win_disable_services::disable_windows_update
