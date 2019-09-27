edfb424604cecdc46287adc704a564184b4b95c8
davehouse <dhouse@mozilla.com>
2019-09-27 14:35:50 -0600

    test purge false

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
@@ -68,7 +68,9 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging {
             class { 'packages::google_chrome':
                 version => 'v76.0.3809.132',
             }
-            include roles_profiles::profiles::disable_chrome_updater
+            class { 'roles_profiles::profiles::disable_chrome_updater':
+                purge => false,
+            }
 
             contain packages::nodejs
             contain packages::wget
