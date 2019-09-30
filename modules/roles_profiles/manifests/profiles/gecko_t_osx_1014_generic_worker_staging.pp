6eec819247f2e3e790aad6b6d9e9228367544eb1
davehouse <dhouse@mozilla.com>
2019-09-30 13:45:32 -0600

    purge updater

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
@@ -68,9 +68,7 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging {
             class { 'packages::google_chrome':
                 version => 'v76.0.3809.132',
             }
-            class { 'roles_profiles::profiles::disable_chrome_updater':
-                purge => false,
-            }
+            include roles_profiles::profiles::disable_chrome_updater'
 
             contain packages::nodejs
             contain packages::wget
