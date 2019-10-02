1500eefddf9f0b65c98d57fa9a4a2320bf4821c8
Dragos Crisan <dcrisan@mozilla.com>
2019-10-02 17:20:24 +0300

    Fix puppet syntax

--- modules/nrpe/manifests/check.pp
+++ modules/nrpe/manifests/check.pp
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # used from nrpe::check::*; do not use directly
-define nrpe::check($cfg=undef, $nsclient_cfg=undef) {
+define nrpe::check(
+    String $cfg          = undef,
+    String $nsclient_cfg = undef,
+    ) {
     include nrpe::base
     include nrpe::settings
     include users::root
