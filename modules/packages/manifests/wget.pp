1bbcbe15d2b2a741b674960ecbf81eca38e4f691
davehouse <dhouse@mozilla.com>
2019-10-03 11:25:10 -0600

    install wget from s3

--- modules/packages/manifests/wget.pp
+++ modules/packages/manifests/wget.pp
@@ -2,10 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class packages::wget {
+class packages::wget (
+    Pattern[/^\d+\.\d+\.\d+$/] $version = '1.20.3_1',
+) {
 
-    package { 'wget':
-        ensure   => present,
-        provider => brew,
+    packages::macos_package_from_s3 { "wget-${version}.dmg":
+        private             => false,
+        os_version_specific => false,
+        type                => 'dmg',
     }
 }
