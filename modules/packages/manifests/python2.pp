19de91f11920061bf44905a491ddc15bdfc5dbe3
davehouse <dhouse@mozilla.com>
2019-10-03 11:44:13 -0600

    python2 from s3

--- modules/packages/manifests/python2.pp
+++ modules/packages/manifests/python2.pp
@@ -2,10 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class packages::python2 {
+class packages::python2 (
+    Pattern[/^\d+\.\d+\.\d+$/] $version = '2.7.16_1',
+) {
 
-    package { 'python2':
-        ensure   => present,
-        provider => brew,
+    packages::macos_package_from_s3 { "python@2-${version}.dmg":
+        private             => false,
+        os_version_specific => false,
+        type                => 'dmg',
     }
 }
