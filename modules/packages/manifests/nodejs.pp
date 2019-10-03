0d8a935350e8beb1c58c34b497b97c6a2aaebca5
davehouse <dhouse@mozilla.com>
2019-10-03 11:53:02 -0600

    node from s3

--- modules/packages/manifests/nodejs.pp
+++ modules/packages/manifests/nodejs.pp
@@ -2,10 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class packages::nodejs {
+class packages::nodejs (
+    Pattern[/^\d+\.\d+\.\d+$/] $version = '12.10.0',
+) {
 
-    package { 'nodejs':
-        ensure   => present,
-        provider => brew,
+    packages::macos_package_from_s3 { "node-${version}.dmg":
+        private             => false,
+        os_version_specific => false,
+        type                => 'dmg',
     }
 }
