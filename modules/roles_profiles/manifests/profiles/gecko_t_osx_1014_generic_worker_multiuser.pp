0c3762897a32ee3731c9e59bc98cb0ca68419a04
Dragos Crisan <dcrisan@mozilla.com>
2019-10-08 15:10:42 +0300

    1561956 generic worker 15 (#115)
    * Update generic-worker to version 15.1.4 multiuser
    
    * Change working dir to /etc/generic-worker
    
    * Made changes required by reviewers
    
    * Delete application_default_credentials.json.erb
    
    * Made changes required by reviewers
    
    * Remove network-check block
    
    * Corrected livelog version and bug generation issue
    
    * Changed generic-worker plist file
    
    * Fixed plist file
    
    * Fix bug generation
    
    * Fixed generic-worker multiuser config file
    
    * Fixed generic-worker multiuser config file
    
    * Fix runTasksAsCurrentUser: false
    
    * Fixed generic-worker config url
    
    * remove cltbld user autologin for multiuser generic worker
    
    * remove cltbld user autologin for multiuser generic worker
    
    * remove cltbld user autologin for multiuser generic worker
    
    * Make task user owner fot /builds/tooltool_cache directory
    
    * Make task user owner for /builds/tooltool_cache directory
    
    * Make task user owner for /builds/tooltool_cache directory
    
    * Make task user owner for /builds/tooltool_cache directory
    
    * Set full access to builds subdirectories
    
    * Create generic worker multiuser profile and roles
    
    * Fix lint error
    
    * Update gecko_t_osx_1014_generic_worker_staging.pp

---
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_multiuser.pp
@@ -0,0 +1,110 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_multiuser {
+
+    $worker_type  = 'gecko-t-osx-1014-multiuser'
+    $worker_group = regsubst($facts['networking']['fqdn'], '.*\.releng\.(.+)\.mozilla\..*', '\1')
+
+    case $::operatingsystem {
+        'Darwin': {
+
+            class { 'puppet::atboot':
+                telegraf_user     => lookup('telegraf.user'),
+                telegraf_password => lookup('telegraf.password'),
+                puppet_branch     => 'master',
+                # Note the camelCase key names
+                meta_data         => {
+                    workerType    => $worker_type,
+                    workerGroup   => $worker_group,
+                    provisionerId => 'releng-hardware',
+                    workerId      => $facts['networking']['hostname'],
+                },
+            }
+
+            class { 'roles_profiles::profiles::logging':
+                worker_type => $worker_type,
+            }
+
+            class { 'talos':
+                user => 'cltbld',
+            }
+
+            $taskcluster_client_id    = lookup('generic_worker.gecko_t_osx_1014.taskcluster_client_id')
+            $taskcluster_access_token = lookup('generic_worker.gecko_t_osx_1014.taskcluster_access_token')
+            $livelog_secret           = lookup('generic_worker.gecko_t_osx_1014.livelog_secret')
+            $quarantine_client_id     = lookup('generic_worker.gecko_t_osx_1014.quarantine_client_id')
+            $quarantine_access_token  = lookup('generic_worker.gecko_t_osx_1014.quarantine_access_token')
+            $bugzilla_api_key         = lookup('generic_worker.gecko_t_osx_1014.bugzilla_api_key')
+
+            class { 'generic_worker::multiuser':
+                taskcluster_client_id     => $taskcluster_client_id,
+                taskcluster_access_token  => $taskcluster_access_token,
+                livelog_secret            => $livelog_secret,
+                worker_group              => $worker_group,
+                worker_type               => $worker_type,
+                quarantine_client_id      => $quarantine_client_id,
+                quarantine_access_token   => $quarantine_access_token,
+                bugzilla_api_key          => $bugzilla_api_key,
+                task_dir                  => '/Users',
+                generic_worker_version    => 'v15.1.4',
+                generic_worker_sha256     => 'caabc35ec26498e755863d08c4c8b79e8b041a1d11b1fc8be0909718fc81113d',
+                taskcluster_proxy_version => 'v5.1.0',
+                taskcluster_proxy_sha256  => '3faf524b9c6b9611339510797bf1013d4274e9f03e7c4bd47e9ab5ec8813d3ae',
+                quarantine_worker_version => 'v1.0.0',
+                quarantine_worker_sha256  => '60bb15fa912589fd8d94dbbff2e27c2718eadaf2533fc4bbefb887f469e22627',
+                livelog_version           => 'v1.1.0',
+                livelog_sha256            => 'caabc35ec26498e755863d08c4c8b79e8b041a1d11b1fc8be0909718fc81113d',
+                user                      => 'root',
+                user_homedir              => '/etc/generic-worker',
+            }
+
+            include dirs::tools
+
+            contain packages::nodejs
+            contain packages::wget
+            contain packages::tooltool
+            file { '/tools/tooltool.py':
+                ensure  => 'link',
+                target  => '/usr/local/bin/tooltool.py',
+                require => Class['packages::tooltool'],
+            }
+
+            contain packages::mercurial
+            contain mercurial::system_hgrc
+
+            contain packages::python2
+            python2::user_pip_conf { 'cltbld_user_pip_conf':
+                user  => 'cltbld',
+                group => 'staff',
+            }
+
+            file {
+                '/tools/python':
+                    ensure  => 'link',
+                    target  => '/usr/local/bin/python2',
+                    require => Class['packages::python2'];
+
+                '/tools/python2':
+                    ensure  => link,
+                    target  => '/usr/local/bin/python2',
+                    require => Class['packages::python2'];
+            }
+
+            contain packages::python3
+            file { '/tools/python3':
+                    ensure  => 'link',
+                    target  => '/usr/local/bin/python3',
+                    require => Class['packages::python3'],
+            }
+
+            contain packages::virtualenv
+
+            include mercurial::ext::robustcheckout
+        }
+        default: {
+            fail("${::operatingsystem} not supported")
+        }
+    }
+}
