6d50d71119245951fcf072ba3d95ed5bf7268d67
davehouse <dhouse@mozilla.com>
2019-10-08 17:20:21 -0600

    add telegraf for macos

---
+++ modules/telegraf/manifests/init.pp
@@ -0,0 +1,53 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class telegraf (
+    Hash global_tags = {},
+) {
+
+    include shared
+
+    case $facts['os']['name'] {
+        'Darwin': {
+            require packages::telegraf
+
+            $influxdb_url      = 'https://telegraf.relops.mozops.net'
+            $influxdb_username = lookup('telegraf.user', '')
+            $influxdb_password = lookup('telegraf.password', '')
+
+            file {
+                default: * => $::shared::file_defaults;
+
+                '/etc/telegraf':
+                    ensure => 'directory';
+
+                '/etc/telegraf/telegraf.d':
+                    ensure => 'directory';
+
+                '/etc/telegraf/telegraf.conf':
+                    ensure  => present,
+                    content => template('telegraf/telegraf.conf.erb'),
+                    mode    => '0600';
+
+                '/Library/LaunchDaemons/telegraf.plist':
+                    ensure  => present,
+                    content => template('telegraf/telegraf.plist.erb'),
+                    mode    => '0644';
+
+                '/var/log/telegraf':
+                    ensure => directory,
+                    mode   => '0755';
+            }
+
+            service { 'telegraf':
+                require => File['/Library/LaunchDaemons/telegraf.plist'],
+                enable  => true,
+            }
+
+        }
+        default: {
+            fail("${module_name} not supported under ${::operatingsystem}")
+        }
+    }
+}
