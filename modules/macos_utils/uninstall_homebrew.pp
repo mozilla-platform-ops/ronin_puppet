fc3cd107530d24cc3bb57df433975f4e452fa97f
davehouse <dhouse@mozilla.com>
2019-10-10 14:28:39 -0600

    pull homebrew uninstaller

---
+++ modules/macos_utils/uninstall_homebrew.pp
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class macos_utils::uninstall_homebrew {
+
+    exec { 'run_homebrew_uninstaller':
+        command => 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"',
+        onlyif  => '/usr/local/bin/brew --version',
+    }
+}
