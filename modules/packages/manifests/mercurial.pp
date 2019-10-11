70675564ae913805a1dfa5e8bf6848be84f0892a
davehouse <dhouse@mozilla.com>
2019-10-11 12:16:22 -0600

    allow without third number

--- modules/packages/manifests/mercurial.pp
+++ modules/packages/manifests/mercurial.pp
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::mercurial (
-    Pattern[/^\d+\.\d+\.\d+_?\d*$/] $version = '5.1',
+    Pattern[/^\d+\.\d+(\.\d+)?_?\d*$/] $version = '5.1',
 ) {
 
     packages::macos_package_from_s3 { "Mercurial-${version}-macosx10.14.pkg":
