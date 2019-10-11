68521cdd7bdca883d5900d6281450ce9c6083ccf
davehouse <dhouse@mozilla.com>
2019-10-11 11:22:15 -0600

    mercurial use vendor pkg

--- modules/packages/manifests/mercurial.pp
+++ modules/packages/manifests/mercurial.pp
@@ -3,12 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::mercurial (
-    Pattern[/^\d+\.\d+\.\d+_?\d*$/] $version = '5.1.1_2',
+    Pattern[/^\d+\.\d+\.\d+_?\d*$/] $version = '5.1',
 ) {
 
-    packages::macos_package_from_s3 { "mercurial-${version}.dmg":
+    packages::macos_package_from_s3 { "Mercurial-${version}-macosx10.14.pkg":
         private             => false,
         os_version_specific => false,
-        type                => 'dmg',
+        type                => 'pkg',
     }
 }
