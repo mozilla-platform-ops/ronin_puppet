8c5c49526cd30806e6d585045423dc375f9dcbe0
markcor <mcornmesser@mozilla.com>
2019-10-14 09:05:53 -0700

    unpin refht before

--- provisioners/windows/bitbar/geckotwin1064refht-bootstrap.ps1
+++ provisioners/windows/bitbar/geckotwin1064refht-bootstrap.ps1
@@ -97,9 +97,9 @@ function Install-BootstrapModule {
 }
 
 $workerType = 'gecko-t-win10-64-ref-ht'
-$src_Organisation = 'markcor'
+$src_Organisation = 'mozilla-platform-ops'
 $src_Repository = 'ronin_puppet'
-$src_Revision = 'bug1579893'
+$src_Revision = 'master'
 $image_provisioner = 'bitbar'
 
 # Ensuring scripts can run uninhibited
