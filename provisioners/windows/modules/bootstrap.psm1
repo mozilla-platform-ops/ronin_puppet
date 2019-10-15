76130eeabf6ed3128b4f751d4be33bce6be0f88f
markcor <mcornmesser@mozilla.com>
2019-10-15 08:14:30 -0700

    correct restore reg key

--- provisioners/windows/modules/bootstrap.psm1
+++ provisioners/windows/modules/bootstrap.psm1
@@ -385,6 +385,7 @@ Function Start-Restore {
 		Write-Log -message  ('{0} :: Removing Generic-worker directory .' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
 		Stop-process -name generic-worker -force
 		Remove-Item -Recurse -Force $env:systemdrive\generic-worker
+        Remove-Item -Recurse -Force $env:systemdrive\mozilla-build
 		Write-Log -message  ('{0} :: Initiating system restore from {1}.' -f $($MyInvocation.MyCommand.Name), ($checkpoint_date)) -severity 'DEBUG'
 		# set-restore_point delets all existing check points to ensure the one needed is "1"
 		Restore-Computer -RestorePoint 1
