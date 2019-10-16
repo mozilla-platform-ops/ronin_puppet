99629c4793c5a70bfb987abaa3ee23d9c16f54b3
davehouse <dhouse@mozilla.com>
2019-10-16 09:35:19 -0600

    pin to testing branch

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
@@ -15,7 +15,9 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging {
             class { 'puppet::atboot':
                 telegraf_user     => lookup('telegraf.user'),
                 telegraf_password => lookup('telegraf.password'),
-                puppet_branch     => 'staging',
+                puppet_repo       => 'https://github.com/davehouse/ronin_puppet.git',
+                puppet_branch     => 'bug1563799_remove-homebrew-testing',
+                puppet_notify_email => 'dhouse@mozilla.com',
                 # Note the camelCase key names
                 meta_data         => {
                     workerType    => $worker_type,
