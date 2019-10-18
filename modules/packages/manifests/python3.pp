162db63379c1b6c4a43f9d37c60466707855efc2
davehouse <dhouse@mozilla.com>
2019-10-18 10:51:22 -0600

    lint

--- modules/packages/manifests/python3.pp
+++ modules/packages/manifests/python3.pp
@@ -18,8 +18,8 @@ class packages::python3 (
     # Install certifi's set of CAs to override the system set
     exec {
         'install_python_certs':
-            command   => "'/Applications/Python ${version[0,3]}/Install Certificates.command'",
-            path      => ['/usr/bin', '/usr/sbin', '/bin'],
+            command => "'/Applications/Python ${version[0,3]}/Install Certificates.command'",
+            path    => ['/usr/bin', '/usr/sbin', '/bin'],
             unless  => "test -L /Library/Frameworks/Python.framework/Versions/${version[0,3]}/etc/openssl/cert.pem",
     }
 }
