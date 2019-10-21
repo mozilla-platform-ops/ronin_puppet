131220c9428462658b29638effdeeaed20d2aabc
markcor <mcornmesser@mozilla.com>
2019-10-21 09:33:00 -0700

    testing

--- provisioners/windows/modules/bootstrap.psm1
+++ provisioners/windows/modules/bootstrap.psm1
@@ -396,7 +396,7 @@ Function Start-Restore {
         sc delete ssh-agent
 		Write-Log -message  ('{0} :: Initiating system restore from {1}.' -f $($MyInvocation.MyCommand.Name), ($checkpoint_date)) -severity 'DEBUG'
 		# set-restore_point delets all existing check points to ensure the one needed is "1"
-		Restore-Computer -RestorePoint 1
+		# Restore-Computer -RestorePoint 1
 
 	} else {
         Write-Log -message  ('{0} :: Restore is not needed.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
