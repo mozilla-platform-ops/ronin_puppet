c98bc56fffc6bab4d74d3f06d32d5388c6065a70
markcor <mcornmesser@mozilla.com>
2019-10-22 12:25:10 -0700

    set reg value on continous gw error.

--- provisioners/windows/modules/bootstrap.psm1
+++ provisioners/windows/modules/bootstrap.psm1
@@ -392,6 +392,7 @@ Function Start-Restore {
 		Write-Log -message  ('{0} :: Removing Generic-worker directory .' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
 		Stop-process -name generic-worker -force
 		Remove-Item -Recurse -Force $env:systemdrive\generic-worker
+        Remove-Item -Recurse -Force $env:systemdrive\mozilla-build
         Remove-Item -Recurse -Force $env:ALLUSERSPROFILE\puppetlabs\ronin
         Remove-Item –Path -Force $env:windir\temp\*
         sc delete "generic-worker"
