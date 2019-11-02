e4b33df84fa0fbe003ef02c1a849a7ca0a803b0c
davehouse <dhouse@mozilla.com>
2019-11-02 13:44:15 -0600

    restore homebrew

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1014_staging.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1014_staging.pp
@@ -19,9 +19,8 @@ class roles_profiles::roles::gecko_t_osx_1014_staging {
     include ::roles_profiles::profiles::motd
     include ::roles_profiles::profiles::users
     include ::roles_profiles::profiles::cltbld_user
+    include ::roles_profiles::profiles::homebrew
     include ::roles_profiles::profiles::relops_users
     include ::roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging
     include ::fw::roles::osx_taskcluster_worker
-
-    include ::macos_utils::uninstall_homebrew
 }
