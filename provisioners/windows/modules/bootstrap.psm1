2b447bb933a0da1def233e76e7892948328c0a38
markcor <mcornmesser@mozilla.com>
2019-11-22 10:55:17 -0800

    test with lower reboot count

--- provisioners/windows/modules/bootstrap.psm1
+++ provisioners/windows/modules/bootstrap.psm1
@@ -354,7 +354,7 @@ function Bootstrap-Puppet {
         } elseif (($last_exit -ne 0) -or ($puppet_exit -ne 2)) {
           Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
           Write-Log -message  ('{0} Test-path "{1}\\max_boots"' -f $($MyInvocation.MyCommand.Name), ($ronnin_key)) -severity 'DEBUG'
-          if ( Test-path "$ronnin_key\\max_boots") {
+          if ( Test-path "$ronnin_key" -value "max_boots") {
             if ( $restore_needed -like "false") {
                 Set-ItemProperty -Path "$ronnin_key" -name  restore_needed -value "puppetize_failed"
             } else {
