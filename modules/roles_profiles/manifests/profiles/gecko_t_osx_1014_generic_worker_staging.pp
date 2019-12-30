90fc61c1652f04c8cab6e4e20b80e14b4fb170c1
davehouse <dhouse@mozilla.com>
2019-12-30 12:07:24 -0700

    typo

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
@@ -25,7 +25,7 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging {
                 puppet_env        => 'dev',
                 puppet_repo       => 'https://github.com/davehouse/ronin_puppet.git',
                 puppet_branch     => 'bug1571949_telegraf-monitor-sign',
-                puppet_notify_email = 'dhouse@mozilla.com',
+                puppet_notify_email => 'dhouse@mozilla.com',
                 # Note the camelCase key names
                 meta_data         => $meta_data,
             }
