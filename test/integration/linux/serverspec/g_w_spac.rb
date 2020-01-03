0de54525ffd8ad6ea735d6908cd8e4803a9b9b9c
Andrew Erickson <aerickson@mozilla.com>
2020-01-02 20:16:17 -0500

    work on specs

--- test/integration/linux/serverspec/g_w_spac.rb
+++ test/integration/linux/serverspec/g_w_spac.rb
@@ -2,5 +2,5 @@ require 'spec_helper.rb'
 
 describe command('generic-worker --version') do
   its(:exit_status) { should eq 0 }
-  # TODO: check version or something else
+  its(:stdout) { should match /generic-worker/ }
 end
