2a1c72ec9f5407946ca26e779151bb3b825c2b8e
markcor <mcornmesser@mozilla.com>
2020-01-16 09:21:27 -0800

    refactor all WIn hardware workerTypes

--- modules/roles_profiles/manifests/roles/geckotwin1064ref18.pp
+++ modules/roles_profiles/manifests/roles/geckotwin1064ref18.pp
@@ -4,23 +4,63 @@
 
 class roles_profiles::roles::geckotwin1064ref18 {
 
-    # System
-    include roles_profiles::profiles::disable_services
-    include roles_profiles::profiles::files_system_managment
-    include roles_profiles::profiles::firewall
-    include roles_profiles::profiles::ntp
-    include roles_profiles::profiles::power_management
-    include roles_profiles::profiles::scheduled_tasks
+  # Worker
+    ## Generic worker
+    include roles_profiles::profiles::windows_custom_config_generic_worker
 
-    # Adminstration
-    include roles_profiles::profiles::logging
-    include roles_profiles::profiles::common_tools
-    # This role will use a different secrets file
-    include roles_profiles::profiles::windows_datacenter_administrator
-
-    # Worker
+    ## Mozilla Build
     include roles_profiles::profiles::mozilla_build
+
+    ## Mozilla maintenance service
     include roles_profiles::profiles::mozilla_maintenance_service
-    include roles_profiles::profiles::microsoft_tools
-    include roles_profiles::profiles::windows_bitbar_generic_worker_16_2_0
+
+    ## Microsoft tools
+    include win_packages::vc_redist_x86
+    include win_packages::vc_redist_x64
+    include win_os_settings::powershell_profile
+
+  # Administration
+    ## Admin Password
+    class { 'win_users::administrator::account':
+        admin_password => lookup('win_adminpw'),
+    }
+
+    ## Logging
+    include roles_profiles::profiles::logging
+
+    ## Common tools
+    include win_packages::process_debug
+    include win_packages::jq
+    include win_packages::gpg4win
+    include win_packages::sevenzip
+    include win_packages::sublimetxt
+
+    # system
+    ## disabled services
+    include win_disable_services::disable_wsearch
+    include win_disable_services::disable_puppet
+    include win_disable_services::disable_windows_defender
+    include win_disable_services::disable_windows_update
+    include win_disable_services::disable_onedrive
+
+    ## File System management
+    include win_filesystem::disable8dot3
+    include win_filesystem::disablelastaccess
+
+    ## Ntp
+    $ntpserver = lookup('external.ntp')
+
+    class { 'windowstime':
+        servers  => { "${ntpserver}" => '0x08'},
+        timezone => 'Greenwich Standard Time',
+    }
+
+    ## Power management
+    class { 'windows::power_scheme':
+        ensure => 'High performance',
+    }
+
+    ## Schedule Tasks
+    include win_scheduled_tasks::maintain_system
+
 }
