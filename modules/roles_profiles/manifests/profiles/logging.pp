af8de432efa532040a5650afc8340c31c56f463e
markcor <mcornmesser@mozilla.com>
2020-01-22 11:01:05 -0800

    testing

--- modules/roles_profiles/manifests/profiles/logging.pp
+++ modules/roles_profiles/manifests/profiles/logging.pp
@@ -27,7 +27,8 @@ class roles_profiles::profiles::logging (
             $location        = $facts['custom_win_location']
             $programfilesx86 = $facts['custom_win_programfilesx86']
             if ($facts['custom_win_location'] == 'datacenter') {
-                $log_aggregator  = lookup('windows.datacenter.log_aggregator')
+#                $log_aggregator  = lookup('windows.datacenter.log_aggregator')
+                $log_aggregator  = lookup('windows.external.papertrail')
                 $conf_file = epp('win_nxlog/nxlog.conf.epp')
             } else {
                 # data will need to be added as could support builds out
