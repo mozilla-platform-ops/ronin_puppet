fafd586bf5cadce70d73323c8684109452f91311
markcor <mcornmesser@mozilla.com>
2020-01-23 11:35:16 -0800

    testing

--- modules/win_nxlog/manifests/configuration.pp
+++ modules/win_nxlog/manifests/configuration.pp
@@ -6,8 +6,7 @@ class win_nxlog::configuration {
 
     require win_nxlog::install
 
-#    $log_aggregator = $win_nxlog::log_aggregator
-    $log_aggregator = 'wth'
+    $log_aggregator = $win_nxlog::log_aggregator
 
     file { "${win_nxlog::nxlog_dir}\\conf\\nxlog.conf":
         content => $win_nxlog::conf_file,
