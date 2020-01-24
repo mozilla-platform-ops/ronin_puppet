5b4d9d369925a390e8342be960fe649b5632dea5
Andrew Erickson <aerickson@mozilla.com>
2020-01-24 15:11:23 -0800

    todos/cleanup

--- modules/linux_generic_worker/manifests/init.pp
+++ modules/linux_generic_worker/manifests/init.pp
@@ -1,10 +1,14 @@
-# TODO:
-# - generate config
-# - apache proxy setup?
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# TODO:
+# - generate config
+# - systemd config?
+# - generate config and runner
+# - switch to loading from s3
+# - apache proxy setup?
+# - control bug / bugzilla setup?
 
 class linux_generic_worker (
     String $taskcluster_client_id,
@@ -27,7 +31,7 @@ class linux_generic_worker (
 ) {
 
     # include httpd
-    # include shared
+    include shared
 
     class { 'packages::linux_generic_worker':
         generic_worker_version    => $generic_worker_version,
@@ -43,31 +47,32 @@ class linux_generic_worker (
     #     bugzilla_api_key => $bugzilla_api_key,
     # }
 
+
     # $livelog_certificate = "${user_homedir}/livelog.crt"
     # $livelog_key         = "${user_homedir}/livelog.key"
     # $task_dir            = "${user_homedir}/tasks"
     # $caches_dir          = "${user_homedir}/caches"
     # $downloads_dir       = "${user_homedir}/downloads"
-    # $opengpg_signing_key = "${user_homedir}/generic-worker.openpgp.signing.key"
-    # $ed25519_signing_key = "${user_homedir}/generic-worker.ed25519.signing.key"
+    $opengpg_signing_key = "${user_homedir}/generic-worker.openpgp.signing.key"
+    $ed25519_signing_key = "${user_homedir}/generic-worker.ed25519.signing.key"
 
-    # exec {
-    #     'create opengpg signing key':
-    #         path    => ['/bin', '/sbin', '/usr/local/bin', '/usr/bin'],
-    #         user    => $user,
-    #         cwd     => $user_homedir,
-    #         command => "generic-worker new-openpgp-keypair --file ${opengpg_signing_key}",
-    #         unless  => "test -f ${opengpg_signing_key}",
-    #         require => Class['packages::generic_worker'];
+    exec {
+        'create opengpg signing key':
+            path    => ['/bin', '/sbin', '/usr/local/bin', '/usr/bin'],
+            user    => $user,
+            cwd     => $user_homedir,
+            command => "generic-worker new-openpgp-keypair --file ${opengpg_signing_key}",
+            unless  => "test -f ${opengpg_signing_key}",
+            require => Class['packages::generic_worker'];
 
-    #     'create ed25519 signing key':
-    #         path    => ['/bin', '/sbin', '/usr/local/bin', '/usr/bin'],
-    #         user    => $user,
-    #         cwd     => $user_homedir,
-    #         command => "generic-worker new-ed25519-keypair --file ${ed25519_signing_key}",
-    #         unless  => "test -f ${ed25519_signing_key}",
-    #         require => Class['packages::generic_worker'];
-    # }
+        'create ed25519 signing key':
+            path    => ['/bin', '/sbin', '/usr/local/bin', '/usr/bin'],
+            user    => $user,
+            cwd     => $user_homedir,
+            command => "generic-worker new-ed25519-keypair --file ${ed25519_signing_key}",
+            unless  => "test -f ${ed25519_signing_key}",
+            require => Class['packages::generic_worker'];
+    }
 
     # case $::operatingsystem {
     #     'Darwin': {
