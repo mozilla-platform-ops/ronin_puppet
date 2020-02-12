8c91b61d2d985d98bf1f9298f10ca8d22ae89de0
markcor <mcornmesser@mozilla.com>
2020-02-12 11:18:19 -0800

    Bug 1614980  - Wait for MDT deployment to complete before bootstrapping datacenter hardware (#179)
    * pin
    
    * add sleep before restore
    
    * add log mention for sleep
    
    * ic c:\minint exists then wait
    
    * add Function Wait-On-MDT
    
    * add Function Wait-On-MDT
    
    * add Function Wait-On-MDT
    
    * unpin

--- provisioners/windows/modules/bootstrap.psm1
+++ provisioners/windows/modules/bootstrap.psm1
@@ -485,3 +485,20 @@ Function Bootstrap-CleanUp {
     Write-Log -message ('{0} :: end - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
   }
 }
+Function Wait-On-MDT {
+   param (
+   )
+   begin {
+     Write-Log -message ('{0} :: begin - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
+    }
+    process {
+       while ((Test-Path "$env:systemdrive:\MININT")) {
+         Write-Log -message  ('{0} ::Detecting MDT deployment has not completed. Waiting 10 seconds.'  -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+         Start-Sleep 10
+       }
+       Write-Log -message  ('{0} ::MDT deployment appears complete'  -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+    }
+    end {
+      Write-Log -message ('{0} :: end - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
+    }
+}
