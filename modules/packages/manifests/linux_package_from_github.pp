5ffe2974a25687ea145cc374f322ff6a23433250
Andrew Erickson <aerickson@mozilla.com>
2020-02-20 17:39:17 -0800

    remove comment about not working, it does now

--- modules/packages/manifests/linux_package_from_github.pp
+++ modules/packages/manifests/linux_package_from_github.pp
@@ -2,14 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This resource is broken because github releases use presigned S3 URIs which puppet can't handle.
-# In the meantime, copy releases from github to relops managed amazon s3 bucket and use macos_package_from_s3 resource
-#
-# https://tickets.puppetlabs.com/browse/PUP-8300
-# https://tickets.puppetlabs.com/browse/PUP-6380
-# https://github.com/puppetlabs/puppet/pull/5002
-# https://github.com/puppetlabs/puppet/pull/7051
-
 define packages::linux_package_from_github (
     String $github_repo_slug,
     String $version,
