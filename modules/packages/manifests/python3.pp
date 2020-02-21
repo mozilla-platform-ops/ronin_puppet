84b7c04caea409f880bd3368c85818178e73f11b
davehouse <dhouse@mozilla.com>
2020-02-21 12:40:03 -0700

    reduce diff

--- modules/packages/manifests/python3.pp
+++ modules/packages/manifests/python3.pp
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class packages::python3 {
+class packages::python3 (
+    Pattern[/^\d+\.\d+\.\d+_?\d*$/] $version = '3.7.4',
+) {
 
     # https://www.python.org/ftp/python/3.7.4/python-3.7.4-macosx10.9.pkg
     # 9c7771bc539c619e47aed34074d07d67abb80013610754a561bbc40d70eefe5b
