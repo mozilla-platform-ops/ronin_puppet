b62c350579765a1384c4cd355a4479ccf825c639
Andrew Erickson <aerickson@mozilla.com>
2020-02-26 15:09:01 -0800

    fix py3 zstandard installation on first run

--- modules/linux_packages/manifests/python3_zstandard.pp
+++ modules/linux_packages/manifests/python3_zstandard.pp
@@ -5,10 +5,18 @@
 class linux_packages::python3_zstandard {
     require linux_packages::py3
 
-    package { 'python3-zstandard':
-        ensure   => '0.11.1',
-        name     => 'zstandard',
-        provider => pip3,
-        require  => Class['linux_packages::py3'],
+    # for the first run, this tries to run pip because pip3 didn't exist
+    # at the start at the start of the run. ugh. puppet bug in 5.5.1.
+    # https://tickets.puppetlabs.com/browse/PUP-7644
+    # package { 'python3-zstandard':
+    #     ensure   => '0.11.1',
+    #     name     => 'zstandard',
+    #     provider => pip3,
+    #     require  => Class['linux_packages::py3'],
+    # }
+
+    exec { 'install python3-zstandard':
+      command => "/usr/bin/pip3 install zstandard==0.11.1",
+      unless  => "/usr/bin/pip3 list --format=columns | grep zstandard | grep 0.11.1",
     }
 }
