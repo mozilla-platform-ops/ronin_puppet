e360dc2039cc074b230512917d2f2071d1399cb0
markcor <mcornmesser@mozilla.com>
2020-03-04 13:48:49 -0800

    add taskcluster proxy

--- modules/roles_profiles/manifests/profiles/windows_standalone_worker_runner.pp
+++ modules/roles_profiles/manifests/profiles/windows_standalone_worker_runner.pp
@@ -7,20 +7,23 @@ class roles_profiles::profiles::windows_standalone_worker_runner {
     case $::operatingsystem {
         'Windows': {
 
-            $ext_pkg_src_loc     = lookup('windows.taskcluster.relops_s3')
+            $ext_pkg_src_loc       = lookup('windows.taskcluster.relops_s3')
 
-            $generic_worker_dir  = lookup('windows.dir.generic_worker')
-            $gw_name             = lookup('win-worker.generic_worker.name')
-            $desired_gw_version  = lookup('win-worker.generic_worker.exe_version')
+            $generic_worker_dir    = lookup('windows.dir.generic_worker')
+            $gw_name               = lookup('win-worker.generic_worker.name')
+            $desired_gw_version    = lookup('win-worker.generic_worker.exe_version')
 
-            $worker_runner_dir   = lookup('windows.dir.worker_runner')
-            $desired_rnr_version = lookup('win-worker.taskcluster.worker_runner_version')
+            $worker_runner_dir     = lookup('windows.dir.worker_runner')
+            $desired_rnr_version   = lookup('win-worker.taskcluster.worker_runner_version')
+
+            $desired_proxy_version = lookup('win-worker.taskcluster.proxy.version')
+            $proxy_name            = lookup('win-worker.taskcluster.proxy.name')
 
             class { 'win_taskcluster::generic_worker' :
                 generic_worker_dir => $generic_worker_dir,
                 desired_gw_version => $desired_gw_version,
                 current_gw_version => $facts['custom_win_genericworker_version'],
-                gw_exe_source      => "${ext_pkg_src_loc}/${gw_name}-${desired_gw_version}.exe"
+                gw_exe_source      => "${ext_pkg_src_loc}/${gw_name}-${desired_gw_version}.exe",
             }
             class { 'win_taskcluster::worker_runner':
                 worker_runner_dir      => $worker_runner_dir,
@@ -28,6 +31,12 @@ class roles_profiles::profiles::windows_standalone_worker_runner {
                 current_runner_version => $facts['custom_win_runner_version'],
                 runner_exe_source      => "${ext_pkg_src_loc}/start-worker-${desired_rnr_version}.exe",
             }
+            class { 'win_taskcluster::proxy':
+                generic_worker_dir    => $generic_worker_dir,
+                desired_proxy_version => $desired_proxy_version,
+                proxy_exe_source      => "${ext_pkg_src_loc}/${proxy_name}-${desired_proxy_version}.exe",
+            }
+
         }
         default: {
             fail("${::operatingsystem} not supported")
---
+++ modules/win_taskcluster/manifests/proxy.pp
@@ -0,0 +1,26 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_taskcluster::proxy (
+    String $generic_worker_dir,
+    String $desired_proxy_version,
+    #String $current_gw_version,
+    String $proxy_exe_source
+) {
+
+    require win_taskcluster::generic_worker
+
+    $proxy_exe_path = "${generic_worker_dir}\\taskcluster-proxy.exe"
+
+    #if ($current_gw_version != $desired_gw_version) {
+        #exec { 'purge_old_':
+            #command  => "Remove-Item –path ${gw_exe_path}",
+            #unless   => "Test-Path ${gw_exe_path}",
+            #provider => powershell,
+        #}
+    #}
+    file { $proxy_exe_path:
+        source  => $proxy_exe_source,
+    }
+}
