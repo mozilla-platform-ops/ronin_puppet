98d99c0922f11d0fcf5f514040d5902866b073f7
markcor <mcornmesser@mozilla.com>
2020-03-11 14:07:20 -0700

    pass parameters to custom_nssm class

--- modules/roles_profiles/manifests/profiles/windows_standalone_worker_runner.pp
+++ modules/roles_profiles/manifests/profiles/windows_standalone_worker_runner.pp
@@ -35,7 +35,6 @@ class roles_profiles::profiles::windows_standalone_worker_runner {
 
             class { 'win_packages::custom_nssm':
                 version  => $nssm_version,
-                nssm_dir => $nssm_dir,
                 nssm_exe => $nssm_exe,
             }
 
