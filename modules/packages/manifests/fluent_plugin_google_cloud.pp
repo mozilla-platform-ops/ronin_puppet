f187ddd96150c941a9aa4095eb48ec4347b9fecf
Jake Watkins <jwatkins@mozilla.com>
2020-03-11 13:09:35 -0700

    Use actual td-agent-gem  bin instead of link

--- modules/packages/manifests/fluent_plugin_google_cloud.pp
+++ modules/packages/manifests/fluent_plugin_google_cloud.pp
@@ -13,9 +13,8 @@ class packages::fluent_plugin_google_cloud {
     exec {
         'install plugin with agent ruby':
             path    => ['/bin', '/sbin', '/usr/sbin', '/usr/local/bin', '/usr/bin'],
-            command => "/usr/sbin/td-agent-gem install fluent-plugin-google-cloud --version ${version}",
+            command => "/opt/td-agent/usr/sbin/td-agent-gem install fluent-plugin-google-cloud --version ${version}",
             unless  => "test -e /opt/td-agent/embedded/lib/ruby/gems/2.4.0/gems/fluent-plugin-google-cloud-${version}",
             require => Class['packages::td_agent'];
     }
 }
-
