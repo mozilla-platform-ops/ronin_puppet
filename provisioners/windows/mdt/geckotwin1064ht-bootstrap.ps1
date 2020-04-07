bb482bbd5cb631573120dc38d5824b090635f9cc
markcor <mcornmesser@mozilla.com>
2020-04-07 15:30:46 -0700

    unpin

--- provisioners/windows/mdt/geckotwin1064ht-bootstrap.ps1
+++ provisioners/windows/mdt/geckotwin1064ht-bootstrap.ps1
@@ -102,9 +102,9 @@ function Install-BootstrapModule {
 }
 
 $workerType = 'gecko-t-win10-64-ht'
-$src_Organisation = 'markcor'
+$src_Organisation = 'mozilla-platform-ops'
 $src_Repository = 'ronin_puppet'
-$src_Revision = 'refactor_win_data'
+$src_Revision = 'master'
 $image_provisioner = 'mdt'
 $max_boots = 10
 
