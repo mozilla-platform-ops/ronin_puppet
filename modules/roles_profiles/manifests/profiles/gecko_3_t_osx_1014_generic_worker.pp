f104e78a8f6630faedc3b1cd74dceacaf926d3e0
davehouse <dhouse@mozilla.com>
2020-04-16 15:14:30 -0600

    workaround zstd not local

--- modules/roles_profiles/manifests/profiles/gecko_3_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_3_t_osx_1014_generic_worker.pp
@@ -115,7 +115,8 @@ class roles_profiles::profiles::gecko_3_t_osx_1014_generic_worker {
             contain mercurial::system_hgrc
 
             contain packages::python2
-            contain python2::system_pip_conf
+            # zstandard for py2 is not in moz pypi??
+            #contain python2::system_pip_conf
 
             file {
                 '/tools/python':
