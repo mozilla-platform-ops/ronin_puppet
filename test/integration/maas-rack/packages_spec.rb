d3c5d4251b9bbe1db444d968fdf252af7d93a1a4
Andrew Erickson <aerickson@mozilla.com>
2020-05-06 11:26:46 -0700

    test fixing

--- test/integration/maas-rack/packages_spec.rb
+++ test/integration/maas-rack/packages_spec.rb
@@ -1,3 +1,5 @@
+require_relative 'spec_helper'
+
 describe package('maas-rack-controller'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
 end
--- test/integration/maas-rack/spec_helper.rb
+++ test/integration/maas-rack/spec_helper.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require 'serverspec'
 
 # avoid getting spammed with messages
 # see https://medium.com/@Joachim8675309/testkitchen-with-chef-and-serverspec-2ac0cd938e5
