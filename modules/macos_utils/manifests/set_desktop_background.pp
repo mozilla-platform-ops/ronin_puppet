e2db1141f1b53ab3a938da456c544ef3eb962e63
davehouse <dhouse@mozilla.com>
2020-05-11 13:00:13 -0600

    make image a param

--- modules/macos_utils/manifests/set_desktop_background.pp
+++ modules/macos_utils/manifests/set_desktop_background.pp
@@ -2,9 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class macos_utils::set_desktop_background {
-
+class macos_utils::set_desktop_background (
+    Optional[String] $image = '/Library/Desktop\ Pictures/Solid\ Colors/Teal.png',
+) {
     $bg_script = '/usr/local/bin/mac_desktop_image.py'
+
     file { $bg_script:
         content => file('macos_utils/mac_desktop_image.py'),
     }
@@ -12,9 +14,9 @@ class macos_utils::set_desktop_background {
     if $::operatingsystem == 'Darwin' {
         # su to get user desktop environment. exec's user option cannot do this
         exec { 'execute python script to apply macos desktop background':
-            command => "/usr/bin/su - cltbld -c '/usr/bin/python ${bg_script} -v -s /Library/Desktop\ Pictures/Solid\ Colors/Teal.png'",
+            command => "/usr/bin/su - cltbld -c '/usr/bin/python ${bg_script} -v -s ${image}'",
             require => File[$bg_script],
-            unless  => "/usr/bin/su - cltbld -c '/usr/bin/python ${bg_script} -v -c /Library/Desktop\ Pictures/Solid\ Colors/Teal.png'",
+            unless  => "/usr/bin/su - cltbld -c '/usr/bin/python ${bg_script} -v -c ${image}'",
         }
     } else {
         fail("${module_name} does not support ${::operatingsystem}")
