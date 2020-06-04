c34e5ca3a83bc63714f9b003ec4fd47782475ff0
Jake Watkins <jwatkins@mozilla.com>
2020-06-04 10:36:22 -0700

    Fixs another erroneous curly brace

--- modules/win_packages/manifests/vc_redist_x86.pp
+++ modules/win_packages/manifests/vc_redist_x86.pp
@@ -8,7 +8,7 @@ class win_packages::vc_redist_x86 {
         win_packages::win_exe_pkg  { 'vc_redist_x86':
             pkg                    => 'vc_redist_x86.exe',
             install_options_string =>
-                "/install /passive  /norestart /log  ${facts['{custom_win_roninlogdir']}\\vcredist_vs2015_x86-install.log",
+                "/install /passive  /norestart /log  ${facts['custom_win_roninlogdir']}\\vcredist_vs2015_x86-install.log",
             creates                => "${facts['custom_win_systemdrive']}\\Windows\\System32\\vcruntime140.dll",
         }
     } else {
