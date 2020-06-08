36baef7b4ca2eb29e88e0259bdecaa2f4a515c02
Jake Watkins <jwatkins@mozilla.com>
2020-06-08 11:17:59 -0700

    Add path back to homebrew uninstall exec

--- modules/macos_utils/manifests/uninstall_homebrew.pp
+++ modules/macos_utils/manifests/uninstall_homebrew.pp
@@ -5,6 +5,7 @@
 class macos_utils::uninstall_homebrew {
 
     exec { 'run_homebrew_uninstaller':
+        path      => '/usr/bin:/usr/sbin:/bin',
         command   => '/bin/bash -c "set - -f $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"',
         onlyif    => 'test -e /usr/local/bin/brew',
         logoutput => true,
