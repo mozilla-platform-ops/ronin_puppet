7ce71930c127c97ac554907e79a5889391ea746e
markcor <mcornmesser@mozilla.com>
2020-06-11 11:11:00 -0700

    remove white spaces

--- modules/win_taskcluster/manifests/generic_worker.pp
+++ modules/win_taskcluster/manifests/generic_worker.pp
@@ -12,7 +12,7 @@ class win_taskcluster::generic_worker (
 
     if ($current_gw_version != $desired_gw_version) {
         exec { 'purge_old_nonservice_gw_exe':
-            command  => "Remove-Item  -path ${gw_exe_path}",
+            command  => "Remove-Item -path ${gw_exe_path}",
             unless   => "Test-Path ${gw_exe_path}",
             provider => powershell,
         }
