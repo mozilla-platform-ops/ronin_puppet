16b62525314cc80ebbc213a03115d461ccfc674d
markcor <mcornmesser@mozilla.com>
2020-06-15 15:28:00 -0700

    remove sysprep

--- provisioners/windows/modules/bootstrap.psm1
+++ provisioners/windows/modules/bootstrap.psm1
@@ -518,12 +518,12 @@ function Bootstrap-AzPuppet {
         }
       } elseif  (($puppet_exit -match 0) -or ($puppet_exit -match 2)) {
         Write-Log -message  ('{0} :: Puppet apply successful' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-        Write-Log -message  ('{0} :: Attempting to generalize image' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-        Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList ‘/generalize /quit /oobe /mode:vm’ -Wait
+        # Write-Log -message  ('{0} :: Attempting to generalize image' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        # Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList ‘/generalize /quit /oobe /mode:vm’ -Wait
         #C:\Windows\system32\sysprep\sysprep.exe /generalize /quit /oobe /mode:vm | Tee-Object -Variable cmdOutput
         # C:\Windows\system32\sysprep\sysprep.exe /generalize /quit /oobe | Tee-Object -Variable cmdOutput
-        Write-Log -message  ('{0} :: {1}' -f $($MyInvocation.MyCommand.Name), ($cmdOutput)) -severity 'DEBUG'
-        Write-Log -message  ('{0} :: Sysprep generalize command completeted' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        # Write-Log -message  ('{0} :: {1}' -f $($MyInvocation.MyCommand.Name), ($cmdOutput)) -severity 'DEBUG'
+        #Write-Log -message  ('{0} :: Sysprep generalize command completeted' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
         Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
         Set-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'complete'
         Write-Log -message  ('{0} :: Puppet apply successful. Waiting on Cloud-Image-Builder pickup' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
