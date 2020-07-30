ee80bd59d6c0f09f9ddca14bb450e9ba51d92493
markcor <mcornmesser@mozilla.com>
2020-07-30 10:16:23 -0700

    add hw-testpool support

---
+++ modules/win_packages/manifests/vbcable.pp
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_packages::vbcable (
+    String $directory,
+    String $version
+) {
+
+    $driver_name = "vbcable_driver_pack${version}"
+
+    win_packages::win_zip_pkg { $driver_name:
+            pkg         => '$driver_name',
+            creates     => "${directory}\\${driver_name}",
+            destination => $directory,
+    }
+
+    # Using puppetlabs-powershell
+    exec { "install_${$driver_name}":
+        command     => "${directory}\\VBCABLE_Setup_x64.exe -i -h",
+        subscribe   => Exec[$driver_name],
+        refreshonly => true,
+    }
+}
