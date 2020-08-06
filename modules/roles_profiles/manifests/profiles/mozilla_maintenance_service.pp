008a4ca11701b3dd03b9a37b33725687191cc2fd
markcor <mcornmesser@mozilla.com>
2020-08-06 14:10:51 -0700

    include moz mainentance service exe

--- modules/roles_profiles/manifests/profiles/mozilla_maintenance_service.pp
+++ modules/roles_profiles/manifests/profiles/mozilla_maintenance_service.pp
@@ -7,7 +7,9 @@ class roles_profiles::profiles::mozilla_maintenance_service {
     case $::operatingsystem {
         'Windows': {
         # Refrence https://support.mozilla.org/en-US/kb/what-mozilla-maintenance-service
-            include win_mozilla_maintenance_service
+            class { 'win_mozilla_maintenance_service':
+                source_location  => lookup('windows.s3.ext_pkg_src'),
+            }
 
             # Certs will need to be droped into win_mozilla_maintenance_service/files
             # The define type will install cert and create needed registry entries
--- modules/win_mozilla_maintenance_service/manifests/install.pp
+++ modules/win_mozilla_maintenance_service/manifests/install.pp
@@ -2,11 +2,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class win_mozilla_maintenance_service::install {
+class win_mozilla_maintenance_service::install (
+    String $source_location
+) {
 
+    $local_exe = "{facts['custom_win_temp_dir']}\\maintenanceservice.exe"
+
+    file { $local_exe:
+        source => "${source_location}/maintenanceservice.exe",
+    }
     win_packages::win_exe_pkg  { 'mozilla_maintenance_service':
         pkg                    => 'maintenanceservice_installer.exe',
         install_options_string =>  '/S',
-        creates                => "${facts['custom_win_programfilesx86']}\\Mozilla Maintenance Service\\uninstall.exe",
+        creates                => "${facts['custom_win_programfilesx86']}\\Mozilla Maintenance Service\\uninstall.exe.BAK",
+        require                => File[$local_exe],
+
     }
 }
