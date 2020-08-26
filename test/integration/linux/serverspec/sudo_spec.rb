f2c4623f53640721bb3d1afe4d7a8d1a476c18a5
Andrew Erickson <aerickson@mozilla.com>
2020-08-26 15:51:20 -0700

    spec testing: check sudoers

--- test/integration/linux/serverspec/sudo_spec.rb
+++ test/integration/linux/serverspec/sudo_spec.rb
@@ -1,6 +1,8 @@
 require_relative 'spec_helper'
 
-# TODO: check /etc/sudoers
+describe file('/etc/sudoers') do
+  its(:content) { should contain "%admin	ALL=(ALL)	NOPASSWD: ALL" }
+end
 
 describe file('/etc/group') do
   its(:content) { should match /admin:x:[\d]+:jwatkins,dhouse,klibby,qfortier,mcornmesser,aerickson,rthijssen/ }
