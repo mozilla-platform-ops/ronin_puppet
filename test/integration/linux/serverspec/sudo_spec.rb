446cc02065e84a43d05fd8571fde85e43bbfc51e
Andrew Erickson <aerickson@mozilla.com>
2020-08-26 15:13:23 -0700

    test fix

--- test/integration/linux/serverspec/sudo_spec.rb
+++ test/integration/linux/serverspec/sudo_spec.rb
@@ -3,5 +3,5 @@ require_relative 'spec_helper'
 # TODO: check /etc/sudoers
 
 describe file('/etc/group') do
-  its(:content) { should match /admin:x:[\n]+:jwatkins,dhouse,klibby,qfortier,mcornmesser,aerickson,rthijssen/ }
+  its(:content) { should match /admin:x:[\d]+:jwatkins,dhouse,klibby,qfortier,mcornmesser,aerickson,rthijssen/ }
 end
