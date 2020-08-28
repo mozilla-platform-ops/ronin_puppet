6e93256999fc0ea60ce742143ab153b059dee610
davehouse <dhouse@mozilla.com>
2020-08-28 16:58:44 -0600

    mac loaner setup

---
+++ modules/fw/manifests/roles/osx_taskcluster_worker_loaner.pp
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class fw::roles::osx_taskcluster_worker_loaner {
+
+    case $::fqdn {
+        /.*\.(mdc1|mdc2)\.mozilla\.com/: {
+            include ::fw::profiles::nrpe_from_nagios
+            include ::fw::profiles::vnc_from_anywhere_logging
+            include ::fw::profiles::ssh_from_anywhere_logging
+            include ::fw::profiles::ssh_from_roller
+        }
+        default:{
+            # Silently skip other DCs
+        }
+    }
+}
--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
@@ -2,11 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class roles_profiles::profiles::gecko_t_osx_1014_generic_worker {
+class roles_profiles::profiles::gecko_t_osx_1014_generic_worker (
+    String $worker_type = 'gecko-t-osx-1014',
+) {
 
     require roles_profiles::profiles::cltbld_user
 
-    $worker_type  = 'gecko-t-osx-1014'
     $worker_group = regsubst($facts['networking']['fqdn'], '.*\.releng\.(.+)\.mozilla\..*', '\1')
 
     $meta_data        = {
