aa9f410ad89600623b0bf6e3f5d60f1c036f2617
Andrew Erickson <aerickson@mozilla.com>
2020-09-23 12:33:05 -0700

    puppet spec note

--- test/integration/linux/serverspec/puppet6_spec.rb
+++ test/integration/linux/serverspec/puppet6_spec.rb
@@ -3,6 +3,7 @@ require_relative 'spec_helper'
 describe package('puppet-agent'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
   # TODO: check with with_version('/6.*/') when available
+  # - available with inspec (https://docs.chef.io/inspec/resources/package/#version-1)
 end
 
 describe package('puppet6-release'), :if => os[:family] == 'ubuntu' do
