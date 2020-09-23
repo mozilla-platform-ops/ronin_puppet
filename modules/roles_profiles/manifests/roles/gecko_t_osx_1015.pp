eea2949ac3466649c7e81c63ed065e9d6ea62c25
davehouse <dhouse@mozilla.com>
2020-09-22 22:34:19 -0600

    disable fw for now

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1015.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1015.pp
@@ -21,7 +21,7 @@ class roles_profiles::roles::gecko_t_osx_1015 {
     include ::roles_profiles::profiles::cltbld_user
     include ::roles_profiles::profiles::relops_users
     include ::roles_profiles::profiles::gecko_t_osx_1014_generic_worker
-    include ::fw::roles::osx_taskcluster_worker_loaner
+    #include ::fw::roles::osx_taskcluster_worker_loaner
 
     include ::macos_utils::uninstall_homebrew
 }
