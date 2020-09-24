7ce8ab63109aae8c61da1c1c2257d939d5ec7f0b
markcor <mcornmesser@mozilla.com>
2020-09-24 15:39:34 -0700

    add create for certi install

--- modules/win_mozilla_build/manifests/install_py3_certi.pp
+++ modules/win_mozilla_build/manifests/install_py3_certi.pp
@@ -11,5 +11,6 @@ class win_mozilla_build::install_py3_certi {
 
     exec { 'upgrade-py3_pip':
         command => "${mozbld}\\python3\\python3.exe -m pip install certifi",
+        creates => "${mozbld}\\python3\\Lib\\site-packages\\certifi\cacert.pem",
     }
 }
