77bd5b5c76089a34cfdc69ef8d092fd2acbe9639
markcor <mcornmesser@mozilla.com>
2020-09-24 15:13:54 -0700

    install py3 certi

---
+++ modules/win_mozilla_build/manifests/install_py3_certi.pp
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_mozilla_build::install_py3_certi {
+
+    require win_mozilla_build::install
+
+    $needed_py3_pip_ver = $win_mozilla_build::needed_py3_pip_ver
+    $mozbld = $win_mozilla_build::install_path
+
+    exec { 'upgrade-py3_pip':
+        command => "${mozbld}\\python3\\python3.exe -m pip install certifi",
+    }
+}
