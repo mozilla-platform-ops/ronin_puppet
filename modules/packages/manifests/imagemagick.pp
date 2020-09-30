b3357c24fc344b4cfa6e642bb250e63d274b91d4
davehouse <dhouse@mozilla.com>
2020-09-30 10:55:47 -0600

    add imagemagick pkg

---
+++ modules/packages/manifests/imagemagick.pp
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class packages::imagemagick (
+    Pattern[/^\d+\.\d+\.\d+$/] $version = '7.0.10-30',
+) {
+
+    packages::macos_package_from_s3 { "imagemagick-${version}.dmg":
+        private             => false,
+        os_version_specific => false,
+        type                => 'dmg',
+    }
+}
