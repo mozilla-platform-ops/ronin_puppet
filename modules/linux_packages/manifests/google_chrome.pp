710a2e8e18b95308592b08e25194e509d9a402e4
Andrew Erickson <aerickson@mozilla.com>
2020-10-01 16:01:29 -0700

    chrome: auto-update

--- modules/linux_packages/manifests/google_chrome.pp
+++ modules/linux_packages/manifests/google_chrome.pp
@@ -2,13 +2,31 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class linux_packages::google_chrome (
-  String $version = '80.0.3987.122-1'
-) {
+class linux_packages::google_chrome {
+    include apt
 
-  packages::linux_package_from_s3 { "google-chrome-stable_${version}_amd64.deb":
-    private             => false,
-    os_version_specific => false,
-    type                => 'deb',
-  }
+    # setup chrome source
+    file {
+        '/etc/apt/sources.list.d/google-chrome.list':
+            source => 'puppet:///modules/linux_packages/google-chrome.list',
+            notify => Exec['apt_update']
+    }
+
+    # configure auto-update
+    schedule { 'update-chrome-schedule':
+        period => weekly,
+        repeat => 1,
+    }
+    exec { 'update-chrome-action':
+        schedule => 'update-chrome-schedule',
+        command  => '/usr/bin/apt-get update -o \
+            Dir::Etc::sourcelist="sources.list.d/google-chrome.list" \
+            -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"',
+    }
+
+    # install chrome
+    package {
+        'google-chrome-stable':
+            ensure => latest;
+    }
 }
