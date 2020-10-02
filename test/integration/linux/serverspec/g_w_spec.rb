1745cbcf6a6c05cb7aee82a38bbfd3aa35230a44
Andrew Erickson <aerickson@mozilla.com>
2020-10-02 12:06:30 -0700

    enhance gw spec

--- test/integration/linux/serverspec/g_w_spec.rb
+++ test/integration/linux/serverspec/g_w_spec.rb
@@ -1,17 +1,40 @@
 require_relative 'spec_helper'
 
+# binaries
+
 describe command('generic-worker --version') do
   its(:exit_status) { should eq 0 }
-    # TODO: check version
+    # TODO: check version?
   its(:stdout) { should match /generic-worker/ }
 end
 
-describe file('/etc/generic-worker.config') do
+describe file('/usr/local/bin/generic-worker') do
   it { should exist }
+  it { should be_executable }
+end
+
+describe file('/usr/local/bin/livelog') do
+  it { should exist }
+  it { should be_executable }
+end
+
+describe file('/usr/local/bin/taskcluster-proxy') do
+  it { should exist }
+  it { should be_executable }
+end
+
+describe file('/usr/local/bin/quarantine-worker') do
+  it { should exist }
+  it { should be_executable }
 end
 
 describe file('/usr/local/share/generic-worker/bugzilla-utils.sh') do
   it { should exist }
+  it { should be_executable }
 end
 
-# TODO: check for tc-proxy, tc-w-r, liveproxy, etc
+# config
+
+describe file('/etc/generic-worker.config') do
+  it { should exist }
+end
