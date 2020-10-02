a919d6a7bc45e61d6d797ee8c4bbf6ca2f6ed1ab
Andrew Erickson <aerickson@mozilla.com>
2020-10-02 12:14:16 -0700

    add linux_gui spec

---
+++ test/integration/linux/serverspec/linux_gui_spec.rb
@@ -0,0 +1,37 @@
+require_relative 'spec_helper'
+
+# packages
+
+describe package('xrestop') do
+  it { should be_installed }
+end
+
+describe package('gnome-settings-daemon') do
+  it { should be_installed }
+end
+
+# files
+
+describe file('/etc/X11/xorg.conf') do
+  it { should exist }
+end
+
+describe file('/lib/systemd/system/x11.service') do
+  it { should exist }
+end
+
+describe file('/lib/systemd/system/Xsession.service') do
+  it { should exist }
+end
+
+describe file('/home/cltbld/.xsessionrc') do
+  it { should exist }
+end
+
+describe file('/etc/X11/Xwrapper.config') do
+  it { should exist }
+end
+
+describe file('/etc/X11/edid.bin') do
+  it { should exist }
+end
