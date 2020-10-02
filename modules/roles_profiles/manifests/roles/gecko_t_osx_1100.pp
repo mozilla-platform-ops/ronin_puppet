884ddd36e9df9299d09eaab6e61ef49c232552cc
davehouse <dhouse@mozilla.com>
2020-10-02 14:19:27 -0600

    do not disable wifi

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1100.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1100.pp
@@ -6,7 +6,7 @@ class roles_profiles::roles::gecko_t_osx_1100 {
 
     include ::roles_profiles::profiles::timezone
     include ::roles_profiles::profiles::ntp
-    include ::roles_profiles::profiles::network
+    # include ::roles_profiles::profiles::network  # disables wifi and bonjour
     include ::roles_profiles::profiles::disable_services
     include ::roles_profiles::profiles::ssh
     include ::roles_profiles::profiles::vnc
