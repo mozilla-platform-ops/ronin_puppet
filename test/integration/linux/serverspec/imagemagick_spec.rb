83088a40102a1be4872dfdf85c9a26767270fef1
Andrew Erickson <aerickson@mozilla.com>
2020-10-02 12:15:13 -0700

    add comment to imagemagick spec

--- test/integration/linux/serverspec/imagemagick_spec.rb
+++ test/integration/linux/serverspec/imagemagick_spec.rb
@@ -1,9 +1,11 @@
 require_relative 'spec_helper'
 
+# 6.x
 describe package('imagemagick'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
 end
 
+# 7.x
 describe command('/usr/local/bin/magick --version') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match /ImageMagick 7/ }
