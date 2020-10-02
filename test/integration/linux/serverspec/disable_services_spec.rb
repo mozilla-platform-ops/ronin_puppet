f1ada477034811abf24ae12b21c38ce71b9918a6
Andrew Erickson <aerickson@mozilla.com>
2020-10-02 14:10:37 -0700

    disable services spec: syntax fix

--- test/integration/linux/serverspec/disable_services_spec.rb
+++ test/integration/linux/serverspec/disable_services_spec.rb
@@ -3,38 +3,38 @@ require_relative 'spec_helper'
 # services
 
 describe service('acpid') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('anacron') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('apport') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('avahi-daemon') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('cups') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('modemmanager') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('network-manager') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 describe service('whoopsie') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
 
 # bluez packages, but bluetooth service
 describe service('bluetooth') do
-  it { should_not_be be_enabled }
+  it { should_not be_enabled }
 end
