aedba724da65f58a795a27a93b02c8b92f06d2c3
Andrew Erickson <aerickson@mozilla.com>
2020-10-02 12:51:38 -0700

    locale spec fix

--- test/integration/linux/serverspec/locale_spec.rb
+++ test/integration/linux/serverspec/locale_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 
-describe file('/etc/motd') do
+describe file('/etc/default/locale') do
   it { should contain 'LANG=en_US.UTF-8' }
   it { should contain 'LANGUAGE="en_US:en"' }
 end
