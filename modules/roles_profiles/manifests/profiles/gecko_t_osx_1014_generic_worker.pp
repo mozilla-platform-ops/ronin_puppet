b5b8e0e5a087c23954e495eaa5395150b8c2a78b
davehouse <dhouse@mozilla.com>
2020-10-05 15:55:06 -0600

    workerType bug1656963

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::gecko_t_osx_1014_generic_worker (
-    String $worker_type = 'gecko-t-osx-1014',
+    String $worker_type = 'gecko-t-osx-1014-bug1656963',
 ) {
 
     require roles_profiles::profiles::cltbld_user
