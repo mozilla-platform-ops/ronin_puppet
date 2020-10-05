b1c1d133ff064f1e9a90df4218d4c1d7619925a7
davehouse <dhouse@mozilla.com>
2020-10-05 15:58:11 -0600

    loosen ver regex

--- modules/packages/manifests/imagemagick.pp
+++ modules/packages/manifests/imagemagick.pp
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::imagemagick (
-    Pattern[/^\d+\.\d+\.\d+$/] $version = '7.0.10-30',
+    Pattern[/^\d+\.\d+.*$/] $version = '7.0.10-30',
 ) {
 
     packages::macos_package_from_s3 { "imagemagick-${version}.dmg":
