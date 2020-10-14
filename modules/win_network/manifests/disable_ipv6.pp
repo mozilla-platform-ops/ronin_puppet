c3136c6bdb9d2b15d3fcd2c4ed7578c1677e344c
markcor <mcornmesser@mozilla.com>
2020-10-14 08:09:33 -0700

    disable ipv6

--- modules/win_network/manifests/disable_ipv6.pp
+++ modules/win_network/manifests/disable_ipv6.pp
@@ -5,13 +5,9 @@
 class win_network::disable_ipv6 {
 
     # Using puppetlabs-registry
-    if $facts['custom_win_location'] == 'datacenter' {
-        registry_value { 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\DisabledComponents':
-            ensure => present,
-            type   => dword,
-            data   => '0xFF',
-        }
-    } else {
-        fail('IPv6 is to only be disabled for hardware nodes')
+    registry_value { 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\DisabledComponents':
+        ensure => present,
+        type   => dword,
+        data   => '0xFF',
     }
 }
