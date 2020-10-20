47942ecb0398bf6c67d2395a19fcf3801e163dde
Andrew Erickson <aerickson@mozilla.com>
2020-10-20 16:41:23 -0700

    linux_gui: remove unused mac files

--- modules/linux_gui/files/changebackground.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-/usr/bin/defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Library/Desktop\ Pictures/Solid\ Colors/Solid\ Aqua\ Blue.png"; }; }'
