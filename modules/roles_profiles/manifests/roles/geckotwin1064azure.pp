7e54c4c80e8538a8445d9e76cc14c4a0cf427393
markcor <mcornmesser@mozilla.com>
2020-11-12 08:37:51 -0800

    add VAC

--- modules/roles_profiles/manifests/roles/geckotwin1064azure.pp
+++ modules/roles_profiles/manifests/roles/geckotwin1064azure.pp
@@ -13,6 +13,7 @@ class roles_profiles::roles::geckotwin1064azure {
     include roles_profiles::profiles::power_management
     include roles_profiles::profiles::scheduled_tasks
     include roles_profiles::profiles::azure_vm_agent
+    include roles_profiles::profiles::virtual_drivers
 
     # Adminstration
     include roles_profiles::profiles::logging
