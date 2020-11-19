2329c57918a222f8a93371fc7c0537af5c0c4456
davehouse <dhouse@mozilla.com>
2020-11-19 15:15:16 -0700

    fix pin!

--- modules/roles_profiles/manifests/profiles/gecko_1_b_osx_1015_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_1_b_osx_1015_generic_worker.pp
@@ -26,7 +26,7 @@ class roles_profiles::profiles::gecko_1_b_osx_1015_generic_worker (
                 telegraf_password   => lookup('telegraf.password'),
                 puppet_env          => 'dev',
                 puppet_repo         => 'https://github.com/davehouse/ronin_puppet.git',
-                puppet_branch       => 'bug1665379_mac-builders-multi',
+                puppet_branch       => 'bug1665379_mac-builders',
                 puppet_notify_email => 'dhouse@mozilla.com',
                 meta_data           => $meta_data,
             }
