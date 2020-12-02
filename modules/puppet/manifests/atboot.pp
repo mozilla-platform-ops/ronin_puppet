2a2af12c3f408bb2c7cc542af3c8e37f20e03523
Andrew Erickson <aerickson@mozilla.com>
2020-12-01 16:39:37 -0800

    linux_talos_gw: atboot invocation points at my branch

--- modules/puppet/manifests/atboot.pp
+++ modules/puppet/manifests/atboot.pp
@@ -5,10 +5,10 @@
 class puppet::atboot (
     String $telegraf_user,
     String $telegraf_password,
-    String $puppet_env          = 'dev',
-    String $puppet_repo         = 'https://github.com/aerickson/ronin_puppet.git',
-    String $puppet_branch       = 'moonshot_1804',
-    String $puppet_notify_email = 'aerickson@mozilla.com',
+    String $puppet_env          = 'production',
+    String $puppet_repo         = 'https://github.com/mozilla-platform-ops/ronin_puppet.git',
+    String $puppet_branch       = 'master',
+    String $puppet_notify_email = 'puppet-ronin-reports@mozilla.com',
     String $smtp_relay_host     = 'localhost',
     Hash $meta_data             = {},
 ) {
--- modules/roles_profiles/manifests/profiles/gecko_t_linux_talos_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_linux_talos_generic_worker.pp
@@ -27,10 +27,15 @@ class roles_profiles::profiles::gecko_t_linux_talos_generic_worker {
             require linux_talos
 
             class { 'puppet::atboot':
-                telegraf_user     => lookup('telegraf.user'),
-                telegraf_password => lookup('telegraf.password'),
+                telegraf_user       => lookup('telegraf.user'),
+                telegraf_password   => lookup('telegraf.password'),
+                # TODO: change before prod test traffic
+                puppet_env          => 'dev',
+                puppet_repo         => 'https://github.com/aerickson/ronin_puppet.git',
+                puppet_branch       => 'moonshot_1804',
+                puppet_notify_email => 'aerickson@mozilla.com',
                 # Note the camelCase key names
-                meta_data         => {
+                meta_data           => {
                     workerType    => $worker_type,
                     workerGroup   => $worker_group,
                     provisionerId => 'releng-hardware',
