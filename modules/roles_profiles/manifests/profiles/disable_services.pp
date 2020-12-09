16703bc55831539b053a504d2e4507d0397fd8be
markcor <mcornmesser@mozilla.com>
2020-12-09 12:44:13 -0800

    disable updates

--- modules/roles_profiles/manifests/profiles/disable_services.pp
+++ modules/roles_profiles/manifests/profiles/disable_services.pp
@@ -38,11 +38,11 @@ class roles_profiles::profiles::disable_services {
         'Windows': {
             include win_disable_services::disable_wsearch
             include win_disable_services::disable_puppet
-            #include win_disable_services::disable_windows_defender
-            #include win_disable_services::disable_windows_update
-            #if $facts['os']['release']['full'] == '10' {
+            include win_disable_services::disable_windows_defender
+            include win_disable_services::disable_windows_update
+            if $facts['os']['release']['full'] == '10' {
                 #include win_disable_services::disable_onedrive
-            #}
+            }
             # May be needed for non-hardaware
             # Commented out because this will break the auto restore
             # include win_disable_services::disable_vss
