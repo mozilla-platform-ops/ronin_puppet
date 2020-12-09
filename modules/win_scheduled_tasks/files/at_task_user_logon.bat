fe37197f600adb40095be45719af7cf85a7ca20a
markcor <mcornmesser@mozilla.com>
2020-12-09 14:02:18 -0800

    supress fw warnings

--- modules/win_scheduled_tasks/files/at_task_user_logon.bat
+++ modules/win_scheduled_tasks/files/at_task_user_logon.bat
@@ -1,7 +1,6 @@
 :CheckForStuckRects3
 echo Wait for registry setting to exist before changing it...
 reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3 /ve
-if %ERRORLEVEL% EQU 0 goto HideTaskBar
 echo HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3 does not yet exist
 ping -n 2 127.0.0.1 1>/nul
 goto CheckForStuckRects3
--- modules/win_scheduled_tasks/manifests/at_task_user_logon.pp
+++ modules/win_scheduled_tasks/manifests/at_task_user_logon.pp
@@ -13,7 +13,7 @@ class win_scheduled_tasks::at_task_user_logon {
         # Resource from puppetlabs-scheduled_task
         scheduled_task { 'at_task_user_logon':
             ensure    => 'present',
-            command   => "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\cmd.exe",
+            command   => 'cmd.exe',
             arguments => "/c ${at_task_user_logon_bat}",
             enabled   => true,
             trigger   => [{
