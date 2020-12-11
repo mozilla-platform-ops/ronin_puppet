264300703005970b204184c09ddeaffb9aba6584
markcor <mcornmesser@mozilla.com>
2020-12-11 15:03:58 -0800

    correct bat file

--- modules/win_scheduled_tasks/files/at_task_user_logon.bat
+++ modules/win_scheduled_tasks/files/at_task_user_logon.bat
@@ -1,9 +1,11 @@
 :CheckForStuckRects3
 echo Wait for registry setting to exist before changing it...
-reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3 /ve
+reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ /ve
+if %ERRORLEVEL% EQU 0 goto netsh
 echo HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3 does not yet exist
 ping -n 2 127.0.0.1 1>/nul
 goto CheckForStuckRects3
 
+:netsh
 rem supress firewall warnings
 netsh firewall set notifications mode = disable profile = all
