60a3a5e91b6c9eda7b80e4e4dc9774827a2da5be
markcor <mcornmesser@mozilla.com>
2020-12-18 06:43:08 -0800

    add take own reg key

--- modules/win_shared/manifests/take_own_reg_key.pp
+++ modules/win_shared/manifests/take_own_reg_key.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class win_shared::take_own_reg_key (
+define win_shared::take_own_reg_key (
     String $regkey=$title
 ){
 
