48af603dd3baff24754ff5e96d37dc8f3e6fe3a0
markcor <mcornmesser@mozilla.com>
2020-12-22 09:34:04 -0800

    disable defender scripts

--- modules/win_disable_services/manifests/disable_windows_defender.pp
+++ modules/win_disable_services/manifests/disable_windows_defender.pp
@@ -11,26 +11,26 @@ class win_disable_services::disable_windows_defender {
     }
     file { "${script_dir}\\OwnRegistryKeys.bat":
         ensure  => present,
-        content => File['win_disable_services/OwnRegistryKeys.bat'],
+        content => file('win_disable_services/windows_defender/OwnRegistryKeys.bat'),
     }
     file { "${script_dir}\\OwnRegistryKeys.ps1":
         ensure  => present,
-        content => File['win_disable_services/OwnRegistryKeys.ps1'],
+        content => file('win_disable_services/windows_defender/OwnRegistryKeys.ps1'),
     }
     file { "${script_dir}\\DisableWindowsDefender.bat":
         ensure  => present,
-        content => File['win_disable_services/DisableWindowsDefender.bat'],
+        content => file('win_disable_services/windows_defender/DisableWindowsDefender.bat'),
     }
     file { "${script_dir}\\DisableWindowsDefenderfeatures.reg":
         ensure  => present,
-        content => File['win_disable_services/DisableWindowsDefenderfeatures.reg'],
+        content => file('win_disable_services/windows_defender/DisableWindowsDefenderfeatures.reg'),
     }
     file { "${script_dir}\\DisableWindowsDefenderobjects.reg":
         ensure  => present,
-        content => File['win_disable_services/DisableWindowsDefenderobjects.reg'],
+        content => file('win_disable_services/windows_defender/DisableWindowsDefenderobjects.reg'),
     }
     file { "${script_dir}\\DisableWindowsDefenderservices.reg":
         ensure  => present,
-        content => File['win_disable_services/DisableWindowsDefenderservices.reg'],
+        content => file('win_disable_services/windows_defender/DisableWindowsDefenderservices.reg'),
     }
 }
