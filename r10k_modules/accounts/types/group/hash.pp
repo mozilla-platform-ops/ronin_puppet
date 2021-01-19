50e8334f4b242fa7e5425a63f9b0f5a2df0dea2d
Andrew Erickson <aerickson@mozilla.com>
2021-01-19 12:06:45 -0800

    remove accounts puppet r10k module, not used

--- r10k_modules/accounts/types/group/hash.pp
+++
@@ -1,7 +0,0 @@
-# Group resoureces hash.
-# @summary A hash of group resources, keyed by group name.
-# Passed as the second parameter of the ensure_resources function.
-#
-type Accounts::Group::Hash = Hash[
-  Accounts::User::Name, Accounts::Group::Resource
-]
--- r10k_modules/accounts/types/user/hash.pp
+++
@@ -1,7 +0,0 @@
-# User resources hash.
-# @summary A hash of user resources, keyed by user name.
-# Passed as the second parameter of the ensure_resources function.
-#
-type Accounts::User::Hash = Hash[
-  Accounts::User::Name, Accounts::User::Resource
-]
