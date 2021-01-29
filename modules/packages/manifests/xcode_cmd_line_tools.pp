a71bf97f370e8e8b5a24914531bd76b6ed988fcc
Jake Watkins <jwatkins@mozilla.com>
2021-01-29 11:38:23 -0800

    Add versioning to xcode cli package

--- modules/packages/manifests/xcode_cmd_line_tools.pp
+++ modules/packages/manifests/xcode_cmd_line_tools.pp
@@ -2,9 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class packages::xcode_cmd_line_tools {
+class packages::xcode_cmd_line_tools (
+    Pattern[/^\d+\.\d+_?\d*$/] $version = '12.2',
+) {
 
-    packages::macos_package_from_s3 { 'Command_Line_Tools_for_Xcode_12.2.dmg':
+    packages::macos_package_from_s3 { "Command_Line_Tools_for_Xcode_${version}.dmg":
         private             => true,
         os_version_specific => true,
         type                => 'dmg',
