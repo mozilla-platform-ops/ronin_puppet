1c9c3d8db3fce1dc93cca98bf2509c4ceb9bcdce
Andrew Erickson <aerickson@mozilla.com>
2021-02-01 16:36:23 -0800

    test improvement

--- test/integration/bitbar/serverspec/run_puppet_spec.rb
+++ test/integration/bitbar/serverspec/run_puppet_spec.rb
@@ -2,5 +2,6 @@ require_relative 'spec_helper'
 
 describe file('/usr/local/bin/run-puppet.sh') do
   it { should exist }
-  # its(:content) { should match /bclary\:.*\:\/usr\/sbin\/nologin/ }
+  it { should be_file }
+  it { should be_executable }
 end
