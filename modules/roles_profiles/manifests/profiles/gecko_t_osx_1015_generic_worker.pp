57f9c8b2b0ef65870e4b4d1dd14dabb9c65016d1
Jake Watkins <jwatkins@mozilla.com>
2021-02-02 19:06:23 -0800

    Remove class parameters from gecko-t-osx-1015 gw profile

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1015_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1015_generic_worker.pp
@@ -2,9 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class roles_profiles::profiles::gecko_t_osx_1015_generic_worker (
-    String $worker_type = 'gecko-t-osx-1015',
-) {
+class roles_profiles::profiles::gecko_t_osx_1015_generic_worker {
+    $worker_type = 'gecko-t-osx-1015'
     require roles_profiles::profiles::cltbld_user
 
     $worker_group = regsubst($facts['networking']['fqdn'], '.*\.releng\.(.+)\.mozilla\..*', '\1')
