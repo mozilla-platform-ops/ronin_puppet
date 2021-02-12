ffb4701dcbe71c251d5d0d34f5be59bb31a91ee3
markcor <mcornmesser@mozilla.com>
2021-02-12 14:18:05 -0800

    add audit scripts

--- modules/roles_profiles/manifests/roles/geckotwin1064ht.pp
+++ modules/roles_profiles/manifests/roles/geckotwin1064ht.pp
@@ -29,4 +29,6 @@ class roles_profiles::roles::geckotwin1064ht {
     include roles_profiles::profiles::windows_custom_config_generic_worker
     include roles_profiles::profiles::microsoft_tools
     include roles_profiles::profiles::google_chrome
+
+    include roles_profiles::profiles::audit_and_recovery
 }
