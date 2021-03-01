51ae716ab45470685975e3c1cc72e95935a4037d
Jake Watkins <jwatkins@mozilla.com>
2021-03-01 12:07:24 -0800

    rearrange gecko-t-osx-1015-r8 roles and names to prod/staging model

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1015_r8.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1015_r8.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class roles_profiles::roles::gecko_t_osx_1015_r8_qa {
+class roles_profiles::roles::gecko_t_osx_1015_r8 {
 
     include ::roles_profiles::profiles::timezone
     include ::roles_profiles::profiles::ntp
