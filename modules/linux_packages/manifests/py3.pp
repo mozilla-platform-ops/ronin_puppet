a1179dc7b40b8110edff23ae6930e827f35b4c93
Andrew Erickson <aerickson@mozilla.com>
2021-03-09 14:53:08 -0800

    refactoring

--- modules/linux_packages/manifests/py3.pp
+++ modules/linux_packages/manifests/py3.pp
@@ -46,8 +46,7 @@ class linux_packages::py3 {
 
     # py3.9, from deadsnakes ppa
 
-    # TODO: rename to py39
-    file {'/opt/relops_py3/':
+    file {'/opt/relops_py39/':
         ensure => directory,
         group  => 'root',
         mode   => '0755',
@@ -55,11 +54,11 @@ class linux_packages::py3 {
     }
 
     $py39_urls = {
-        '/opt/relops_py3/libpython3.9-minimal_3.9.2-1+bionic2_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-minimal_3.9.2-1%2Bbionic2_amd64.deb' },
-        '/opt/relops_py3/libpython3.9-stdlib_3.9.2-1+bionic2_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-stdlib_3.9.2-1%2Bbionic2_amd64.deb' },
-        '/opt/relops_py3/python3-distutils_3.6.9-1~18.04_all.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/python3-distutils_3.6.9-1~18.04_all.deb' },
-        '/opt/relops_py3/python3-lib2to3_3.6.9-1~18.04_all.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/python3-lib2to3_3.6.9-1~18.04_all.deb' },
-        '/opt/relops_py3/python3.9-minimal_3.9.2-1+bionic2_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-minimal_3.9.2-1%2Bbionic2_amd64.deb' },
+        '/opt/relops_py39/libpython3.9-minimal_3.9.2-1+bionic2_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-minimal_3.9.2-1%2Bbionic2_amd64.deb' },
+        '/opt/relops_py39/libpython3.9-stdlib_3.9.2-1+bionic2_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-stdlib_3.9.2-1%2Bbionic2_amd64.deb' },
+        '/opt/relops_py39/python3-distutils_3.6.9-1~18.04_all.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/python3-distutils_3.6.9-1~18.04_all.deb' },
+        '/opt/relops_py39/python3-lib2to3_3.6.9-1~18.04_all.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/python3-lib2to3_3.6.9-1~18.04_all.deb' },
+        '/opt/relops_py39/python3.9-minimal_3.9.2-1+bionic2_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3-us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-minimal_3.9.2-1%2Bbionic2_amd64.deb' },
     }
     create_resources(file, $py39_urls, {
         owner => 'root',
@@ -70,9 +69,15 @@ class linux_packages::py3 {
     exec { 'install py39':
         command  => '/usr/bin/dpkg -i *.deb',
         path     => '/bin:/usr/bin/:/sbin:/usr/sbin',
-        cwd      => '/opt/relops_py3/',
+        cwd      => '/opt/relops_py39/',
         provider => shell,
         unless   => '/usr/bin/dpkg --list | /bin/grep python3.9',
+    }
+
+    # remove old /opt/py3
+
+    file {'/opt/relops_py3/':
+        ensure => absent,
     }
 
 }
