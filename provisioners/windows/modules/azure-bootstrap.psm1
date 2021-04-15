0d66002ca524bd90b102beb72aa4cfe7e384feae
markcor <mcornmesser@mozilla.com>
2021-04-15 15:08:49 -0700

    testing

--- provisioners/windows/modules/azure-bootstrap.psm1
+++ provisioners/windows/modules/azure-bootstrap.psm1
@@ -140,7 +140,7 @@ function AzBootstrap-Puppet {
       } elseif  (($puppet_exit -match 0) -or ($puppet_exit -match 2)) {
         Write-Log -message  ('{0} :: Puppet apply successful' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
         Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
-        #Set-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'complete'
+        Set-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'complete'
         Write-Log -message  ('{0} :: Puppet apply successful. Waiting on Cloud-Image-Builder pickup' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
         Move-StrapPuppetLogs
         exit 0
