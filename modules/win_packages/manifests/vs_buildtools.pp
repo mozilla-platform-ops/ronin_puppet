bcf6af8ebbc952eb4c58c7ee6c699b83cd440314
markcor <mcornmesser@mozilla.com>
2021-06-14 14:10:11 -0700

    debug vs tools

--- modules/win_packages/manifests/vs_buildtools.pp
+++ modules/win_packages/manifests/vs_buildtools.pp
@@ -4,15 +4,22 @@
 
 class win_packages::vs_buildtools {
 
-    $tools_dir = "${facts['custom_win_programfilesx86']}\\Microsoft Visual Studio\Installer"
+    $prog_dir = $facts['custom_win_programfilesx86']
+    $tools_dir = "${prog_dir}\\Microsoft Visual Studio\Installer"
     $vc_tools  = 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
-    $w10_sdk    = 'Microsoft.VisualStudio.Component.Windows10SDK.19041'
+    $sdk       = 'ExtensionSDKs\Microsoft.Midi.GmDls\10.0.19041.0'
+    $sdk_dir   = "${prog_dir}\\Microsoft SDKs\\Windows Kits\\10\\ExtensionSDKs\\${sdk}"
 
     if $::operatingsystem == 'Windows' {
         win_packages::win_exe_pkg  { 'vs_buildtools__1552942004.1623183462':
             pkg                    => 'vs_buildtools__1552942004.1623183462.exe',
-            install_options_string => "--add ${vc_tools} --add ${w10_sdk} --passive",
+            install_options_string => "--add ${vc_tools} --passive",
             creates                => "${tools_dir}\\NOTICE2.txt",
+        }
+        win_packages::win_exe_pkg  { 'winsdksetup':
+            pkg                    => 'winsdksetup.exe',
+            install_options_string => '/q /norestart',
+            creates                => "${sdk_dir}\\SDKManifest",
         }
     } else {
         fail("${module_name} does not support ${::operatingsystem}")
