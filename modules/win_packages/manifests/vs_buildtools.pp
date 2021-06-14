1ad9fda39770a10293442ed6a4762dd51d084a74
markcor <mcornmesser@mozilla.com>
2021-06-14 13:46:14 -0700

    debug vs tools

--- modules/win_packages/manifests/vs_buildtools.pp
+++ modules/win_packages/manifests/vs_buildtools.pp
@@ -5,12 +5,14 @@
 class win_packages::vs_buildtools {
 
     $tools_dir = "${facts['custom_win_programfilesx86']}\\Microsoft Visual Studio\Installer"
+    $vc_tools  = 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
+    $w10_sdk    = 'Microsoft.VisualStudio.Component.Windows10SDK.19041'
 
     if $::operatingsystem == 'Windows' {
         win_packages::win_exe_pkg  { 'vs_buildtools__1552942004.1623183462':
             pkg                    => 'vs_buildtools__1552942004.1623183462.exe',
-            install_options_string => '--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --passive',
-            creates                => "${tools_dir}\\NOTICE.txt",
+            install_options_string => "--add ${vc_tools} --add ${w10_sdk} --passive",
+            creates                => "${tools_dir}\\NOTICE2.txt",
         }
     } else {
         fail("${module_name} does not support ${::operatingsystem}")
