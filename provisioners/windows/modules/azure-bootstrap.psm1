084ec656926aef31acf2eb013530a837203d16e3
markcor <mcornmesser@mozilla.com>
2021-07-29 12:45:52 -0700

    move bootstrap logs

--- provisioners/windows/modules/azure-bootstrap.psm1
+++ provisioners/windows/modules/azure-bootstrap.psm1
@@ -159,8 +159,7 @@ function AzBootstrap-Puppet {
 }
 function Move-StrapPuppetLogs {
   param (
-    [string] $logdir = "$env:systemdrive\logs",
-    [string] $bootstraplogdir = "$logdir\bootstrap"
+    [string] $bootstraplogdir = "$env:programdata\PuppetLabs\ronin"
   )
   New-Item -ItemType Directory -Force -Path $bootstraplogdir
   Get-ChildItem -Path $logdir\*.log -Recurse | Move-Item -Destination $bootstraplogdir -ErrorAction SilentlyContinue
