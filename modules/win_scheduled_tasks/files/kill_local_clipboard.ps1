093284b24a5103ab5800500b18382b28e6966d73
markcor <mcornmesser@mozilla.com>
2021-08-18 20:54:29 -0700

    reduce noise

--- modules/win_scheduled_tasks/files/kill_local_clipboard.ps1
+++ modules/win_scheduled_tasks/files/kill_local_clipboard.ps1
@@ -37,15 +37,15 @@ function Write-Log {
   }
 }
 
-# logging below can be removed once thsi is working smoothly
+# Commented out logs to reduce news but leaving in place if future debbugging is needed
 while($true) {
 
 	$clip_service = (Get-Service | Where-Object {$_.name -Like "cbdhsvc_*"})
 	if ($clip_service.status -eq $null){
-		Write-Log -message  ('{0} :: Local Clip Board service not detected' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+		#Write-Log -message  ('{0} :: Local Clip Board service not detected' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
 	} else {
 		$start_type = ((Get-Service $clip_service.name).StartType)
-	    Write-Log -message  ('{0} ::  {1} service is {2} and start up is set to {3}' -f $($MyInvocation.MyCommand.Name), ($clip_service), ($clip_service.status), ($start_type)) -severity 'DEBUG'
+	    #Write-Log -message  ('{0} ::  {1} service is {2} and start up is set to {3}' -f $($MyInvocation.MyCommand.Name), ($clip_service), ($clip_service.status), ($start_type)) -severity 'DEBUG'
 	}
 	if ($clip_service.status -eq "running"){
 		Write-Log -message  ('{0} ::  Stopping {1} service' -f $($MyInvocation.MyCommand.Name), ($clip_service)) -severity 'DEBUG'
