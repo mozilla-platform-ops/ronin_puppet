c273a18401a0fbd0ed2bdd735b8c77f392a50121
Andrew Erickson <aerickson@mozilla.com>
2021-11-04 17:12:06 -0700

    deleting broken test that's covered

--- test/integration/linux/inspec/zstandard_spec.rb
+++ test/integration/linux/inspec/zstandard_spec.rb
@@ -10,11 +10,6 @@ describe bash('pip3 list | grep zstandard') do
   its('stdout') { should match /zstandard/ }
 end
 
-#describe bash('zstd') do
-  # its('exit_status') { should eq 1 }
-  # its('stdout') { should match /Usage/ }
-#end
-
 describe bash('zstd --version') do
   its('exit_status') { should eq 0 }
   its('stdout') { should match /zstd command line interface/ }
