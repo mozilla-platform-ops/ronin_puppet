53d8ec06d4ee7ee6581741d566ae8e61e2fd8485
markcor <mcornmesser@mozilla.com>
2021-12-15 13:56:49 -0800

    cleaning up Az boot strapping

--- provisioners/windows/azure/azure-bootstrap.ps1
+++ provisioners/windows/azure/azure-bootstrap.ps1
@@ -232,6 +232,15 @@ Function AzRonin-PreRun {
   }
 }
 
+function Move-StrapPuppetLogs {
+  param (
+    [string] $logdir = "$env:systemdrive\logs",
+    [string] $bootstraplogdir = "$logdir\bootstrap"
+  )
+  New-Item -ItemType Directory -Force -Path $bootstraplogdir
+  Get-ChildItem -Path $logdir\*.log -Recurse | Move-Item -Destination $bootstraplogdir -ErrorAction SilentlyContinue
+}
+
 function AzBootstrap-Puppet {
   param (
     [int] $exit,
