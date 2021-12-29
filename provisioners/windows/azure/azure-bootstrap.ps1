b53d4f6ef5f795e3d47931d0f7a06a80e62e4213
markcor <mcornmesser@mozilla.com>
2021-12-29 14:52:58 -0800

    supress git checkout niose

--- provisioners/windows/azure/azure-bootstrap.ps1
+++ provisioners/windows/azure/azure-bootstrap.ps1
@@ -112,7 +112,7 @@ function Set-RoninRegOptions {
   	}
 }
 
-function AzInstall-Prerequ {
+function Install-AzPrerequ {
   	param (
     	[string] $ext_src = "https://s3-us-west-2.amazonaws.com/ronin-puppet-package-repo/Windows/prerequisites",
     	[string] $local_dir = "$env:systemdrive\BootStrap",
@@ -225,66 +225,66 @@ function Apply-AzRoninPuppet {
   	}
   	process {
 
-    Set-Location $env:systemdrive\ronin
-    If(!(test-path $logdir\old))  {
-      	New-Item -ItemType Directory -Force -Path $logdir\old
-    }
-      Write-Log -message  ('{0} ::Ronin Puppet HEAD is set to {1} .' -f $($MyInvocation.MyCommand.Name), ($deploymentID)) -severity 'DEBUG'
-    }
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Mozilla\ronin_puppet" -Name 'bootstrap_stage' -Value 'inprogress'
-
-    # Setting Env variabes for PuppetFile install and Puppet run
-    # The ssl variables are needed for R10k
-    Write-Log -message  ('{0} :: Setting Puppet enviroment.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-    $env:path = $env:path = "$env:programfiles\Puppet Labs\Puppet\bin;$env:path"
-    $env:SSL_CERT_FILE = "$env:programfiles\Puppet Labs\Puppet\puppet\ssl\cert.pem"
-    $env:SSL_CERT_DIR = "$env:programfiles\Puppet Labs\Puppet\puppet\ssl"
-    $env:FACTER_env_windows_installdir = "$env:programfiles\Puppet Labs\Puppet"
-    $env:HOMEPATH = "\Users\Administrator"
-    $env:HOMEDRIVE = "C:"
-    $env:PL_BASEDIR = "$env:programfiles\Puppet Labs\Puppet"
-    $env:PUPPET_DIR = "$env:programfiles\Puppet Labs\Puppet"
-    $env:RUBYLIB = "$env:programfiles\Puppet Labs\Puppet\lib"
-    $env:USERNAME = "Administrator"
-    $env:USERPROFILE = "$env:systemdrive\Users\Administrator"
-
-    Write-Log -message  ('{0} :: Moving old logs.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-    Get-ChildItem -Path $logdir\*.log -Recurse | Move-Item -Destination $logdir\old -ErrorAction SilentlyContinue
-    Write-Log -message  ('{0} :: Running Puppet apply .' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-    puppet apply manifests\nodes.pp --onetime --verbose --no-daemonize --no-usecacheonfailure --detailed-exitcodes --no-splay --show_diff --modulepath=modules`;r10k_modules --hiera_config=win_hiera.yaml --logdest $logdir\$datetime-bootstrap-puppet.log
-    [int]$puppet_exit = $LastExitCode
-
-    if ($run_to_success -eq 'true') {
-      if (($puppet_exit -ne 0) -and ($puppet_exit -ne 2)) {
-        if (($last_exit -eq 0) -or ($puppet_exit -eq 2)) {
-          Write-Log -message  ('{0} :: Puppet apply failed 1st run.  ' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-          Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
-          Move-StrapPuppetLogs
-          exit 0
-        } elseif (($last_exit -ne 0) -or ($puppet_exit -ne 2)) {
-          Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
-          Write-Log -message  ('{0} :: Puppet apply failed multiple times. Waiting 5 minutes beofre Reboot' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-          Move-StrapPuppetLogs
-          exit 1
+        Set-Location $env:systemdrive\ronin
+        If(!(test-path $logdir\old))  {
+      	    New-Item -ItemType Directory -Force -Path $logdir\old
         }
-      } elseif  (($puppet_exit -match 0) -or ($puppet_exit -match 2)) {
-        Write-Log -message  ('{0} :: Puppet apply successful' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-        Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
-        Set-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'complete'
-        Write-Log -message  ('{0} :: Puppet apply successful. Waiting on Cloud-Image-Builder pickup' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-        Move-StrapPuppetLogs
-        exit 0
-      } else {
-        Write-Log -message  ('{0} :: Unable to detrimine state post Puppet apply' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-        Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $last_exit
-        Start-sleep -s 300
-        Move-StrapPuppetLogs
-        exit 1
-      }
+        Write-Log -message  ('{0} ::Ronin Puppet HEAD is set to {1} .' -f $($MyInvocation.MyCommand.Name), ($deploymentID)) -severity 'DEBUG'
+
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Mozilla\ronin_puppet" -Name 'bootstrap_stage' -Value 'inprogress'
+
+        # Setting Env variabes for PuppetFile install and Puppet run
+        # The ssl variables are needed for R10k
+        Write-Log -message  ('{0} :: Setting Puppet enviroment.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        $env:path = $env:path = "$env:programfiles\Puppet Labs\Puppet\bin;$env:path"
+        $env:SSL_CERT_FILE = "$env:programfiles\Puppet Labs\Puppet\puppet\ssl\cert.pem"
+        $env:SSL_CERT_DIR = "$env:programfiles\Puppet Labs\Puppet\puppet\ssl"
+        $env:FACTER_env_windows_installdir = "$env:programfiles\Puppet Labs\Puppet"
+        $env:HOMEPATH = "\Users\Administrator"
+        $env:HOMEDRIVE = "C:"
+        $env:PL_BASEDIR = "$env:programfiles\Puppet Labs\Puppet"
+        $env:PUPPET_DIR = "$env:programfiles\Puppet Labs\Puppet"
+        $env:RUBYLIB = "$env:programfiles\Puppet Labs\Puppet\lib"
+        $env:USERNAME = "Administrator"
+        $env:USERPROFILE = "$env:systemdrive\Users\Administrator"
+
+        Write-Log -message  ('{0} :: Moving old logs.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        Get-ChildItem -Path $logdir\*.log -Recurse | Move-Item -Destination $logdir\old -ErrorAction SilentlyContinue
+        Write-Log -message  ('{0} :: Running Puppet apply .' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        puppet apply manifests\nodes.pp --onetime --verbose --no-daemonize --no-usecacheonfailure --detailed-exitcodes --no-splay --show_diff --modulepath=modules`;r10k_modules --hiera_config=win_hiera.yaml --logdest $logdir\$datetime-bootstrap-puppet.log
+        [int]$puppet_exit = $LastExitCode
+
+        if ($run_to_success -eq 'true') {
+            if (($puppet_exit -ne 0) -and ($puppet_exit -ne 2)) {
+                if (($last_exit -eq 0) -or ($puppet_exit -eq 2)) {
+                    Write-Log -message  ('{0} :: Puppet apply failed 1st run.  ' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+                    Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
+                    Move-StrapPuppetLogs
+                    exit 0
+                } elseif (($last_exit -ne 0) -or ($puppet_exit -ne 2)) {
+                    Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
+                    Write-Log -message  ('{0} :: Puppet apply failed multiple times. Waiting 5 minutes beofre Reboot' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+                    Move-StrapPuppetLogs
+                exit 1
+                }
+            } elseif  (($puppet_exit -match 0) -or ($puppet_exit -match 2)) {
+                Write-Log -message  ('{0} :: Puppet apply successful' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+                Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $puppet_exit
+                Set-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'complete'
+                Write-Log -message  ('{0} :: Puppet apply successful. Waiting on Cloud-Image-Builder pickup' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+                Move-StrapPuppetLogs
+                exit 0
+            } else {
+                Write-Log -message  ('{0} :: Unable to detrimine state post Puppet apply' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+                Set-ItemProperty -Path "$ronnin_key" -name last_run_exit -value $last_exit
+                Start-sleep -s 300
+                Move-StrapPuppetLogs
+                exit 1
+            }
+        }
     }
-  }
-  end {
-    Write-Log -message ('{0} :: end - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
+    end {
+        Write-Log -message ('{0} :: end - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
   }
 }
 
@@ -304,7 +304,7 @@ If(test-path 'HKLM:\SOFTWARE\Mozilla\ronin_puppet') {
 If(!(test-path 'HKLM:\SOFTWARE\Mozilla\ronin_puppet')) {
     Setup-Logging -DisableNameChecking
     Set-RoninRegOptions -DisableNameChecking -workerType $workerType -src_Organisation $src_Organisation -src_Repository $src_Repository -src_Branch $src_Branch -image_provisioner $image_provisioner
-    AzInstall-Prerequ -DisableNameChecking
+    Install-AzPrerequ -DisableNameChecking
     exit 0
 }
 If (($stage -eq 'setup') -or ($stage -eq 'inprogress')){
