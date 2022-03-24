7b1371fb8032a260f10ab7cf744ffd4011033318
markcor <mcornmesser@mozilla.com>
2022-03-24 09:04:33 -0700

    force bootstrap registry changes

--- provisioners/windows/azure/azure-bootstrap.ps1
+++ provisioners/windows/azure/azure-bootstrap.ps1
@@ -94,22 +94,22 @@ function Set-RoninRegOptions {
     	}
 
     	New-Item -Path $ronnin_key -Name source -force
-    	New-ItemProperty -Path "$ronnin_key" -Name 'image_provisioner' -Value "$image_provisioner" -PropertyType String
+    	New-ItemProperty -Path "$ronnin_key" -Name 'image_provisioner' -Value "$image_provisioner" -PropertyType String  -force
     	#New-ItemProperty -Path "$ronnin_key" -Name 'workerType' -Value "$workerType" -PropertyType String
-        $WorkerPoolID = $worker_pool_id -replace '/','-'
-        New-ItemProperty -Path "$ronnin_key" -Name 'worker_pool_id' -Value "$WorkerPoolID" -PropertyType String
+        #$WorkerPoolID = $worker_pool_id -replace '/','-'
+        New-ItemProperty -Path "$ronnin_key" -Name 'worker_pool_id' -Value "$worker_pool_id" -PropertyType String -force
     	#$role = $workerType -replace '-',''
     	#New-ItemProperty -Path "$ronnin_key" -Name 'role' -Value "$role" -PropertyType String
-        New-ItemProperty -Path "$ronnin_key" -Name 'role' -Value "$base_image" -PropertyType String
+        New-ItemProperty -Path "$ronnin_key" -Name 'role' -Value "$base_image" -PropertyType String -force
     	Write-Log -message  ('{0} :: Node workerType set to {1}' -f $($MyInvocation.MyCommand.Name), ($workerType)) -severity 'DEBUG'
 
-    	New-ItemProperty -Path "$ronnin_key" -Name 'inmutable' -Value 'false' -PropertyType String
-    	New-ItemProperty -Path "$ronnin_key" -Name 'last_run_exit' -Value '0' -PropertyType Dword
-    	New-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'setup' -PropertyType String
+    	New-ItemProperty -Path "$ronnin_key" -Name 'inmutable' -Value 'false' -PropertyType String -force
+    	New-ItemProperty -Path "$ronnin_key" -Name 'last_run_exit' -Value '0' -PropertyType Dword -force
+    	New-ItemProperty -Path "$ronnin_key" -Name 'bootstrap_stage' -Value 'setup' -PropertyType String -force
 
-    	New-ItemProperty -Path "$source_key" -Name 'Organisation' -Value "$src_Organisation" -PropertyType String
-    	New-ItemProperty -Path "$source_key" -Name 'Repository' -Value "$src_Repository" -PropertyType String
-    	New-ItemProperty -Path "$source_key" -Name 'Branch' -Value "$src_Branch" -PropertyType String
+    	New-ItemProperty -Path "$source_key" -Name 'Organisation' -Value "$src_Organisation" -PropertyType String -force
+    	New-ItemProperty -Path "$source_key" -Name 'Repository' -Value "$src_Repository" -PropertyType String -force
+    	New-ItemProperty -Path "$source_key" -Name 'Branch' -Value "$src_Branch" -PropertyType String -force
   }
   	end {
     	Write-Log -message ('{0} :: end - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
