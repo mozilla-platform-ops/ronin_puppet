421f7e2df9e9ea70175938be8278e0a43df04e15
Heitor Neiva <hneiva@mozilla.com>
2022-04-01 10:22:31 -0700

    RELENG-502 Update mac signing certs
    Updates how keychain naming convention, this allows easier keychain rollover in the future by updating keychain path in user data.

--- modules/roles_profiles/manifests/profiles/mac_v3_signing.pp
+++ modules/roles_profiles/manifests/profiles/mac_v3_signing.pp
@@ -64,6 +64,7 @@ class roles_profiles::profiles::mac_v3_signing {
                     widevine_user       => $widevine_user,
                     widevine_key        => $widevine_key,
                     widevine_filename   => $user_data['widevine_filename'],
+                    keychain_filename   => $user_data['keychain_filename'],
                     worker_config       => $worker_config,
                     role_config         => $role_config,
                     notarization_users  => $user_data['notarization_users'],
