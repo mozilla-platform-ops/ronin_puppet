8facba86791afa2e6ef8d31fe34e88dd91a209ca
Mark Cornmesser <mcornmesser@mozilla.com>
2022-07-27 14:34:14 -0700

    initial commit

--- provisioners/windows/azure/azure-bootstrap.ps1
+++ provisioners/windows/azure/azure-bootstrap.ps1
@@ -121,12 +121,13 @@ function Install-AzPrerequ {
     	[string] $ext_src = "https://s3-us-west-2.amazonaws.com/ronin-puppet-package-repo/Windows/prerequisites",
     	[string] $local_dir = "$env:systemdrive\BootStrap",
     	[string] $work_dir = "$env:systemdrive\scratch",
-    	[string] $git = "Git-2.18.0-64-bit.exe",
+    	[string] $git = "Git-2.36.1-64-bit.exe",
     	[string] $puppet = "puppet-agent-6.0.0-x64.msi",
     	[string] $vault_file = "azure_vault_template.yaml",
     	[string] $rdagent = "rdagent",
     	[string] $azure_guest_agent = "WindowsAzureGuestAgent",
-    	[string] $azure_telemetry = "WindowsAzureTelemetryService"
+    	[string] $azure_telemetry = "WindowsAzureTelemetryService",
+        [string] $bootzip = "BootStrap_Azure_07-2022.zip"
   	)
   	begin {
     	Write-Log -message ('{0} :: begin - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
@@ -135,7 +136,7 @@ function Install-AzPrerequ {
 
     	New-Item -path $work_dir -ItemType "directory"
     	Set-location -path $work_dir
-    	Invoke-WebRequest -Uri  $ext_src/BootStrap.zip  -UseBasicParsing -OutFile $work_dir\BootStrap.zip
+    	Invoke-WebRequest -Uri  $ext_src/BootStrap.zip  -UseBasicParsing -OutFile $work_dir\$bootzip
     	Expand-Archive -path $work_dir\BootStrap.zip -DestinationPath $env:systemdrive\
     	Set-location -path $local_dir
     	remove-item $work_dir   -Recurse  -force
