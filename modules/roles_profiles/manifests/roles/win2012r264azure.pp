90ae9f396bfdfaed1de8df9395da5af2ec195ee9
Mark Cornmesser <mcornmesser@mozilla.com>
2022-07-29 15:45:01 -0700

    test filesystem managament profile

--- modules/roles_profiles/manifests/roles/win2012r264azure.pp
+++ modules/roles_profiles/manifests/roles/win2012r264azure.pp
@@ -7,7 +7,7 @@ class roles_profiles::roles::win2012r264azure {
     # System
     include roles_profiles::profiles::disable_services
     include roles_profiles::profiles::suppress_dialog_boxes
-    #include roles_profiles::profiles::files_system_managment
+    include roles_profiles::profiles::files_system_managment
     #include roles_profiles::profiles::firewall
     #include roles_profiles::profiles::network
     #include roles_profiles::profiles::ntp
