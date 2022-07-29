a2574d6f5911f57359a4642eaef597c416a8c0bf
Mark Cornmesser <mcornmesser@mozilla.com>
2022-07-29 14:45:17 -0700

    no wsearch on 2012

--- modules/roles_profiles/manifests/profiles/disable_services.pp
+++ modules/roles_profiles/manifests/profiles/disable_services.pp
@@ -39,6 +39,9 @@ class roles_profiles::profiles::disable_services {
             include win_disable_services::disable_wsearch
             include win_disable_services::disable_puppet
             include win_disable_services::disable_windows_update
+            if $facts['os']['release']['full'] == '2012 R2' {
+                include include win_disable_services::disable_wsearch
+            }
             if $facts['os']['release']['full'] == '10' {
                 include win_disable_services::disable_onedrive
             }
