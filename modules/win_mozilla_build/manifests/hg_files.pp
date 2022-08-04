4c56d2fbf81dfe447e49e184f8bbcff878019e7b
Mark Cornmesser <mcornmesser@mozilla.com>
2022-08-04 11:57:48 -0700

    use msys2 dir for mozillabuild 4.0.1

--- modules/win_mozilla_build/manifests/hg_files.pp
+++ modules/win_mozilla_build/manifests/hg_files.pp
@@ -9,12 +9,18 @@ class win_mozilla_build::hg_files {
 
     $mozbld = $win_mozilla_build::install_path
 
+    if $win_mozilla_build::needed_mozbld_ver == '4.0.1' {
+        $msys_dir = "${win_mozilla_build::install_path}\\msys2"
+    } else {
+        $msys_dir = "${win_mozilla_build::install_path}\\msys"
+    }
+
     # Original Source https://github.com/mozilla/version-control-tools/blob/master/hgext/robustcheckout/__init__.py
     # Reference  https://bugzilla.mozilla.org/show_bug.cgi?id=1305485#c5
     file { "${mozbld}\\robustcheckout.py":
         content => file('win_mozilla_build/robustcheckout.py'),
     }
-    file { "${win_mozilla_build::install_path}\\msys\\etc\\cacert.pem":
+    file { "${win_mozilla_build::install_path}\\${msys_dir}\\etc\\cacert.pem":
         content => file('win_mozilla_build/cacert.pem'),
     }
     file { "${win_mozilla_build::program_files}\\mercurial\\mercurial.ini":
