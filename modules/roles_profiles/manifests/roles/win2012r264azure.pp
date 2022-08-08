023d10f853af3c6c3332c5490751b9d1409a25f3
Mark Cornmesser <mcornmesser@mozilla.com>
2022-08-08 10:00:47 -0700

    add error dump reg

--- modules/roles_profiles/manifests/roles/win2012r264azure.pp
+++ modules/roles_profiles/manifests/roles/win2012r264azure.pp
@@ -9,6 +9,7 @@ class roles_profiles::roles::win2012r264azure {
 
     # System
     include roles_profiles::profiles::disable_services
+    include roles_profiles::profiles::error_reporting
     include roles_profiles::profiles::suppress_dialog_boxes
     include roles_profiles::profiles::files_system_managment
     include roles_profiles::profiles::firewall
