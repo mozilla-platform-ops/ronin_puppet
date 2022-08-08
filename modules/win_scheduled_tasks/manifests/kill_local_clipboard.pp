864249bd6a1e957a1eb68d3036507c84c29339fa
Jonathan Moss <jwmoss88+github@gmail.com>
2022-08-08 14:38:47 -0400

    Re-work disabling clipboard  Set to disabled in registry

--- modules/win_scheduled_tasks/manifests/kill_local_clipboard.pp
+++ modules/win_scheduled_tasks/manifests/kill_local_clipboard.pp
@@ -3,27 +3,26 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_scheduled_tasks::kill_local_clipboard {
+  $kill_localclip = "${facts['custom_win_roninprogramdata']}\\kill_local_clipboard.ps1"
 
-    $kill_localclip = "${facts['custom_win_roninprogramdata']}\\kill_local_clipboard.ps1"
-
-    if $::operatingsystem == 'Windows' {
-        file { $kill_localclip:
-            content => file('win_scheduled_tasks/kill_local_clipboard.ps1'),
-        }
-        # Resource from puppetlabs-scheduled_task
-        scheduled_task { 'kill_remote_clipboard':
-            ensure    => 'present',
-            command   => "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\powershell.exe",
-            arguments => "-executionpolicy bypass -File ${kill_localclip}",
-            enabled   => true,
-            trigger   => [{
-                'schedule'         => 'boot',
-                'minutes_interval' => '0',
-                'minutes_duration' => '0'
-            }],
-            user      => 'system',
-        }
-    } else {
-        fail("${module_name} does not support ${::operatingsystem}")
+  if $facts['os']['name'] == 'Windows' {
+    file { $kill_localclip:
+      content => file('win_scheduled_tasks/kill_local_clipboard.ps1'),
     }
+    # Resource from puppetlabs-scheduled_task
+    scheduled_task { 'kill_remote_clipboard':
+      ensure    => 'present',
+      command   => "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\powershell.exe",
+      arguments => "-executionpolicy bypass -File ${kill_localclip}",
+      enabled   => true,
+      trigger   => [{
+          'schedule'         => 'boot',
+          'minutes_interval' => '0',
+          'minutes_duration' => '0'
+      }],
+      user      => 'system',
+    }
+  } else {
+    fail("${module_name} does not support ${facts['facts['os']['name']']}")
+  }
 }
