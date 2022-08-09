173a8f4336c1a0c6cd5691e5519aba1286d79523
Jonathan Moss <jwmoss88+github@gmail.com>
2022-08-09 16:37:22 -0400

    Add firewall puppet support

--- modules/roles_profiles/manifests/profiles/firewall.pp
+++ modules/roles_profiles/manifests/profiles/firewall.pp
@@ -3,23 +3,22 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::firewall {
-
-    case $::operatingsystem {
-        'Windows': {
-            include win_firewall::allow_ping
-            if $facts['custom_win_location'] == 'aws' {
-                if $facts['custom_win_firewall_status'] == 'running' {
-                    include win_firewall::disable_firewall
-                }
-            }
-            # Bug List
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1510834
-            # AWS disable firewall
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1562038
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1358301
+  case $facts['os']['name'] {
+    'Windows': {
+      include win_firewall::allow_ping
+      if $facts['custom_win_location'] == 'aws' {
+        if $facts['custom_win_firewall_status'] == 'running' {
+          include win_firewall::disable_firewall
         }
-        default: {
-            fail("${::operatingsystem} not supported")
-        }
+      }
+      # Bug List
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1510834
+      # AWS disable firewall
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1562038
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1358301
     }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
