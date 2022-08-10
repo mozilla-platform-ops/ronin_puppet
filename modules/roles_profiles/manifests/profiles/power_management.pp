c23e2e84386e76906f400e73b9c8e69ec2db65c1
Jonathan Moss <jwmoss88+github@gmail.com>
2022-08-10 15:44:44 -0400

    Add power management puppet

--- modules/roles_profiles/manifests/profiles/power_management.pp
+++ modules/roles_profiles/manifests/profiles/power_management.pp
@@ -3,20 +3,19 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::power_management {
-
-    case $::operatingsystem {
-        'Darwin': {
-            include macos_mobileconfig_profiles::power_management
-        }
-        'Windows': {
-            class { 'win_os_settings::power_scheme':
-                ensure => 'High performance',
-            }
-            # Bug List
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1524436
-        }
-        default: {
-            fail("${::operatingsystem} not supported")
-        }
+  case $facts['os']['name'] {
+    'Darwin': {
+      include macos_mobileconfig_profiles::power_management
     }
+    'Windows': {
+      class { 'win_os_settings::power_scheme':
+        ensure => 'High performance',
+      }
+      # Bug List
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1524436
+    }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
--- modules/roles_profiles/manifests/roles/win116421h2azure.pp
+++ modules/roles_profiles/manifests/roles/win116421h2azure.pp
@@ -10,7 +10,7 @@ class roles_profiles::roles::win116421h2azure {
   include roles_profiles::profiles::firewall
   include roles_profiles::profiles::network
   include roles_profiles::profiles::ntp
-  #include roles_profiles::profiles::power_management
+  include roles_profiles::profiles::power_management
   #include roles_profiles::profiles::scheduled_tasks
   #include roles_profiles::profiles::azure_vm_agent
   #include roles_profiles::profiles::virtual_drivers
