8354dec52d83ffcc4bc36ae36c15b8d47cbdfd9f
Jonathan Moss <jwmoss88+github@gmail.com>
2022-08-10 23:24:24 -0400

    Add mozilla maint certs

--- modules/roles_profiles/manifests/profiles/mozilla_maintenance_service.pp
+++ modules/roles_profiles/manifests/profiles/mozilla_maintenance_service.pp
@@ -3,41 +3,40 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::mozilla_maintenance_service {
+  case $facts['os']['name'] {
+    'Windows': {
+      # Refrence https://support.mozilla.org/en-US/kb/what-mozilla-maintenance-service
 
-    case $::operatingsystem {
-        'Windows': {
-        # Refrence https://support.mozilla.org/en-US/kb/what-mozilla-maintenance-service
+      $source_location = lookup('windows.s3.ext_pkg_src')
 
-            $source_location = lookup('windows.s3.ext_pkg_src')
+      class { 'win_mozilla_maintenance_service':
+        source_location  => $source_location,
+      }
 
-            class { 'win_mozilla_maintenance_service':
-                source_location  => $source_location,
-            }
-
-            # Certs will need to be droped into win_mozilla_maintenance_service/files
-            # The define type will install cert and create needed registry entries
-            win_mozilla_maintenance_service::certificate_install { 'MozFakeCA':
-                cert_key        => '0',
-                registry_name   => 'Mozilla Corporation',
-                registry_issuer => 'Thawte Code Signing CA - G2',
-            }
-            win_mozilla_maintenance_service::certificate_install { 'MozFakeCA_2017-10-13':
-                cert_key        => '1',
-                registry_name   => 'Mozilla Fake SPC',
-                registry_issuer => 'Mozilla Fake CA',
-            }
-            win_mozilla_maintenance_service::certificate_install { 'MozRoot':
-                cert_key        => '2',
-                registry_name   => 'Mozilla Corporation',
-                registry_issuer => 'DigiCert SHA2 Assured ID Code Signing CA'
-            }
-            # Bug List
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1529631
-            # Registry key access
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1353889
-        }
-        default: {
-            fail("${::operatingsystem} not supported")
-        }
+      # Certs will need to be droped into win_mozilla_maintenance_service/files
+      # The define type will install cert and create needed registry entries
+      win_mozilla_maintenance_service::certificate_install { 'MozFakeCA':
+        cert_key        => '0',
+        registry_name   => 'Mozilla Corporation',
+        registry_issuer => 'Thawte Code Signing CA - G2',
+      }
+      win_mozilla_maintenance_service::certificate_install { 'MozFakeCA_2017-10-13':
+        cert_key        => '1',
+        registry_name   => 'Mozilla Fake SPC',
+        registry_issuer => 'Mozilla Fake CA',
+      }
+      win_mozilla_maintenance_service::certificate_install { 'MozRoot':
+        cert_key        => '2',
+        registry_name   => 'Mozilla Corporation',
+        registry_issuer => 'DigiCert SHA2 Assured ID Code Signing CA',
+      }
+      # Bug List
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1529631
+      # Registry key access
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1353889
     }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
