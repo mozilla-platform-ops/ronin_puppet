14116c39fd141c512fcff8faa7a1c7e01d3b506d
Andrew Erickson <aerickson@mozilla.com>
2022-09-09 13:27:59 -0700

    spec: ensure python is python3

--- test/integration/bitbar/inspec/bitbar_devicepool_spec.rb
+++ test/integration/bitbar/inspec/bitbar_devicepool_spec.rb
@@ -51,6 +51,7 @@ end
 
 describe command('/home/bitbar/mozilla-bitbar-devicepool/venv/bin/python --version') do
   its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /Python 3/ }
 end
 
 # last_started_alert stuff
