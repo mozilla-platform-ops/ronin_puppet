84187d623fde39ebddca90731d380726cc8a9f68
Jonathan Moss <jwmoss88+github@gmail.com>
2022-09-15 16:37:13 -0400

    Rename windows 10 build

--- modules/roles_profiles/manifests/roles/win10642009azure.pp
+++ modules/roles_profiles/manifests/roles/win10642009azure.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-class roles_profiles::roles::win106421h2azure {
+class roles_profiles::roles::win10642009azure {
   # System
   include roles_profiles::profiles::disable_services
   include roles_profiles::profiles::suppress_dialog_boxes
