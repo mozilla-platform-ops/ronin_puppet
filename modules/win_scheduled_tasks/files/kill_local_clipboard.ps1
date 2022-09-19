8b235e514cd8dc5d359eee2e38f89349aa7e8199
Jonathan Moss <jwmoss88+github@gmail.com>
2022-09-19 13:14:37 -0400

    Silence output

--- modules/win_scheduled_tasks/files/kill_local_clipboard.ps1
+++ modules/win_scheduled_tasks/files/kill_local_clipboard.ps1
@@ -43,14 +43,14 @@ while ($true) {
   $clip_service = (Get-Service | Where-Object { $_.name -Like "cbdhsvc_*" })
   Foreach ($c in $clip_service) {
     if ($null -eq $c.Status) {
-      Write-Log -message  ('{0} :: Local Clip Board service not detected' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+      #Write-Log -message  ('{0} :: Local Clip Board service not detected' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
     }
     else {
       $start_type = ((Get-Service $c.name).StartType)
-      Write-Log -message  ('{0} :: {1} is {2} and start up is set to {3}' -f $($MyInvocation.MyCommand.Name), $c.DisplayName, ($c.status), ($start_type)) -severity 'DEBUG'
+      #Write-Log -message  ('{0} :: {1} is {2} and start up is set to {3}' -f $($MyInvocation.MyCommand.Name), $c.DisplayName, ($c.status), ($start_type)) -severity 'DEBUG'
     }
     if ($c.status -eq "running") {
-      Write-Log -message  ('{0} :: Stopping {1} service' -f $($MyInvocation.MyCommand.Name), $c.DisplayName) -severity 'DEBUG'
+      #Write-Log -message  ('{0} :: Stopping {1} service' -f $($MyInvocation.MyCommand.Name), $c.DisplayName) -severity 'DEBUG'
       Stop-Service -Name $c.name
       start-sleep -s 3
       Set-Service -name $c.name -StartupType Disabled -force
