2c50c5f45ff117c8ff2bd1e24c0e7262862435bc
Andrew Erickson <aerickson@mozilla.com>
2022-10-13 12:20:09 -0700

    r8 1015: enable safaridriver (#417)

---
+++ modules/macos_safaridriver/manifests/init.pp
@@ -0,0 +1,36 @@
+class macos_safaridriver {
+  case $facts['os']['name'] {
+    'Darwin': {
+      $the_script = '/usr/local/bin/add-safari-permissions.sh'
+
+      file { $the_script:
+        content => file('macos_safaridriver/add-safari-permissions.sh'),
+        mode    => '0755',
+      }
+
+      exec { 'execute script':
+        command => $the_script,
+        require => File[$the_script],
+      }
+
+      # non-admin users need to be in _webdeveloper group for safaridriver to work
+      #   - https://developer.apple.com/forums/thread/124461
+      #   - if not, `safaridriver --diagnose` will complain with:
+      #       'ERROR: safaridriver could not launch because it is not configured'
+      #
+      # dseditgroup preferred to dscl
+      #   - https://superuser.com/questions/214004/how-to-add-user-to-a-group-from-mac-os-x-command-line
+      #
+      # TODO: pull out user as param, i.e. don't hardcode cltbld user
+      $group = '_webdeveloper'
+      exec { "cltbld_group_${group}":
+        command => "/usr/sbin/dseditgroup -o edit -a cltbld -t user ${group}",
+        unless  => "/usr/bin/groups cltbld | /usr/bin/grep -q -w ${group}",
+        require => User['cltbld'],
+      }
+    }
+    default: {
+      fail("${facts['os']['name']} not supported")
+    }
+  }
+}
