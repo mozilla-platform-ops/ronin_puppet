c6a8766561d16b431d81ac59d20655a290d247f1
Mark Cornmesser <mcornmesser@mozilla.com>
2022-10-17 13:06:57 -0700

    add 2022 builder support

--- modules/roles_profiles/manifests/roles/win202264azure.pp
+++ modules/roles_profiles/manifests/roles/win202264azure.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-class roles_profiles::roles::win2012compare {
+class roles_profiles::roles::win202264azure {
 
     # Needs to be applied before other Win Update is disabled
     include roles_profiles::profiles::microsoft_tools
