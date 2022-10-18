32380c6d5eec41024376fcdc58821894a768eb77
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2022-10-17 20:07:04 -0400

    Attempt at replacing user logon task from batch to powershell (#433)
    * Attempt at replacing user logon task from batch to powershell
    
    * suppress debug messages

--- modules/win_scheduled_tasks/manifests/at_task_user_logon.pp
+++ modules/win_scheduled_tasks/manifests/at_task_user_logon.pp
@@ -3,27 +3,26 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_scheduled_tasks::at_task_user_logon {
+  $at_task_user_logon_ps = "${facts['custom_win_roninprogramdata']}\\at_task_user_logon.ps1"
 
-    $at_task_user_logon_bat = "${facts['custom_win_roninprogramdata']}\\at_task_user_logon.bat"
-
-    if $facts['os']['name'] == 'Windows' {
-        file { $at_task_user_logon_bat:
-            content => file('win_scheduled_tasks/at_task_user_logon.bat'),
-        }
-        # Resource from puppetlabs-scheduled_task
-        scheduled_task { 'at_task_user_logon':
-            ensure    => 'present',
-            command   => "${facts['custom_win_system32']}\\cmd.exe",
-            arguments => "/c ${at_task_user_logon_bat}",
-            enabled   => true,
-            trigger   => [{
-                'schedule'         => 'boot',
-                'minutes_interval' => '0',
-                'minutes_duration' => '0'
-            }],
-            user      => 'system',
-        }
-    } else {
-        fail("${module_name} does not support ${$facts['os']['name']}")
+  if $facts['os']['name'] == 'Windows' {
+    file { $at_task_user_logon_ps:
+      content => file('win_scheduled_tasks/at_task_user_logon.ps1'),
     }
+    # Resource from puppetlabs-scheduled_task
+    scheduled_task { 'at_task_user_logon':
+      ensure    => 'present',
+      command   => "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\powershell.exe",
+      arguments => "-executionpolicy bypass -File ${at_task_user_logon_ps}",
+      enabled   => true,
+      trigger   => [{
+          'schedule'         => 'boot',
+          'minutes_interval' => '0',
+          'minutes_duration' => '0'
+      }],
+      user      => 'system',
+    }
+  } else {
+    fail("${module_name} does not support ${$facts['os']['name']}")
+  }
 }
