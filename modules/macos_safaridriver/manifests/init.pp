570f047cbea4efc8651576ae837c3d99a9d8d7a7
Andrew Erickson <aerickson@mozilla.com>
2022-10-18 18:11:21 -0700

    refactoring, check os x major version

--- modules/macos_safaridriver/manifests/init.pp
+++ modules/macos_safaridriver/manifests/init.pp
@@ -3,56 +3,62 @@ class macos_safaridriver (
 ) {
   case $facts['os']['name'] {
     'Darwin': {
-      # TODO: version check? tcc db entries probably differ per OS release
+      case $facts['os']['release']['major'] {
+        # 19 == OS X 10.15
+        '19': {
+          $perm_script = '/usr/local/bin/add_tcc_perms.sh'
+          $enable_script = '/usr/local/bin/safari-enable-remote-automation.sh'
+          $tcc_script = '/usr/local/bin/tccutil.py'
 
-      $perm_script = '/usr/local/bin/add_tcc_perms.sh'
-      $enable_script = '/usr/local/bin/safari-enable-remote-automation.sh'
-      $tcc_script = '/usr/local/bin/tccutil.py'
+          file { $perm_script:
+            content => file('macos_safaridriver/add_tcc_perms.sh'),
+            mode    => '0755',
+          }
 
-      file { $perm_script:
-        content => file('macos_safaridriver/add_tcc_perms.sh'),
-        mode    => '0755',
-      }
+          file { $enable_script:
+            content => file('macos_safaridriver/safari-enable-remote-automation.sh'),
+            mode    => '0755',
+          }
 
-      file { $enable_script:
-        content => file('macos_safaridriver/safari-enable-remote-automation.sh'),
-        mode    => '0755',
-      }
+          exec { 'execute perms script':
+            command => $perm_script,
+            require => File[$perm_script],
+            user    => 'root',
+            # logoutput => true,
+          }
 
-      exec { 'execute perms script':
-        command => $perm_script,
-        require => File[$perm_script],
-        user    => 'root',
-        # logoutput => true,
-      }
+          # needs to run as cltbld via launchctl or won't work
+          exec { 'execute enable remote automation script':
+            # TODO: don't hardcode user id of cltbld
+            #   - make a driver script that gets id of cltbld on each system?
+            command => "/bin/launchctl asuser 36 sudo -u ${user_running_safari} ${enable_script}",
+            require => File[$enable_script],
+            # logoutput => true,
+          }
 
-      # needs to run as cltbld via launchctl or won't work
-      exec { 'execute enable remote automation script':
-        # TODO: don't hardcode user id of cltbld
-        #   - make a driver script that gets id of cltbld on each system?
-        command => "/bin/launchctl asuser 36 sudo -u ${user_running_safari} ${enable_script}",
-        require => File[$enable_script],
-        # logoutput => true,
-      }
+          exec { 'enable safari driver':
+            command => '/usr/bin/safaridriver --enable',
+          }
 
-      exec { 'enable safari driver':
-        command => '/usr/bin/safaridriver --enable',
-      }
-
-      # non-admin users need to be in _webdeveloper group for safaridriver to work
-      #   - https://developer.apple.com/forums/thread/124461
-      #   - if not, `safaridriver --diagnose` will complain with:
-      #       'ERROR: safaridriver could not launch because it is not configured'
-      #
-      # dseditgroup preferred to dscl
-      #   - https://superuser.com/questions/214004/how-to-add-user-to-a-group-from-mac-os-x-command-line
-      #
-      # TODO: pull out user as param, i.e. don't hardcode cltbld user
-      $group = '_webdeveloper'
-      exec { "${user_running_safari}_group_${group}":
-        command => "/usr/sbin/dseditgroup -o edit -a ${user_running_safari} -t user ${group}",
-        unless  => "/usr/bin/groups ${user_running_safari} | /usr/bin/grep -q -w ${group}",
-        require => User[$user_running_safari],
+          # non-admin users need to be in _webdeveloper group for safaridriver to work
+          #   - https://developer.apple.com/forums/thread/124461
+          #   - if not, `safaridriver --diagnose` will complain with:
+          #       'ERROR: safaridriver could not launch because it is not configured'
+          #
+          # dseditgroup preferred to dscl
+          #   - https://superuser.com/questions/214004/how-to-add-user-to-a-group-from-mac-os-x-command-line
+          #
+          # TODO: pull out user as param, i.e. don't hardcode cltbld user
+          $group = '_webdeveloper'
+          exec { "${user_running_safari}_group_${group}":
+            command => "/usr/sbin/dseditgroup -o edit -a ${user_running_safari} -t user ${group}",
+            unless  => "/usr/bin/groups ${user_running_safari} | /usr/bin/grep -q -w ${group}",
+            require => User[$user_running_safari],
+          }
+        }
+        default: {
+          fail("${facts['os']['release']} not supported")
+        }
       }
     }
     default: {
