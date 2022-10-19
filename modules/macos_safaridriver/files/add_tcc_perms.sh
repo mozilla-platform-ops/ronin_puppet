f0db88155e50520fa8da4a5765469926c6463368
Andrew Erickson <aerickson@mozilla.com>
2022-10-18 17:26:47 -0700

    replace tccutil with bare sqlite

--- modules/macos_safaridriver/files/add_tcc_perms.sh
+++ modules/macos_safaridriver/files/add_tcc_perms.sh
@@ -8,12 +8,17 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-tool_path="/usr/local/bin"
+# tool_path="/usr/local/bin"
 
-# accessibility, in system TCC DB
-python ${tool_path}/tccutil.py -i /usr/libexec/sshd-keygen-wrapper
+# kTCCServiceAccessibility, in system TCC DB
+# python ${tool_path}/tccutil.py -i /usr/libexec/sshd-keygen-wrapper
+query="REPLACE INTO access VALUES('kTCCServiceAccessibility','/usr/libexec/sshd-keygen-wrapper',1,1,1,NULL,NULL,NULL,'UNUSED',NULL,0,0);"
+sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" "$query"
+# needed?
+# query="REPLACE INTO access VALUES('kTCCServiceSystemPolicyAllFiles','/usr/libexec/sshd-keygen-wrapper',1,1,1,X'fade0c000000003c0000000100000006000000020000001d636f6d2e6170706c652e737368642d6b657967656e2d7772617070657200000000000003',NULL,NULL,'UNUSED',NULL,0,1618013664);"
+# sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" "$query"
 
-# applescript, in user TCC DB
+# kTCCServiceAppleEvents, in user TCC DB
 #   - not using tccutil due to funky binary data
 query="REPLACE INTO access VALUES('kTCCServiceAppleEvents','/usr/libexec/sshd-keygen-wrapper',1,1,1,X'fade0c000000003c0000000100000006000000020000001d636f6d2e6170706c652e737368642d6b657967656e2d7772617070657200000000000003',NULL,0,'com.apple.systemevents',X'fade0c000000003400000001000000060000000200000016636f6d2e6170706c652e73797374656d6576656e7473000000000003',NULL,1666043382);"
 sudo sqlite3 "/Users/cltbld/Library/Application Support/com.apple.TCC/TCC.db" "$query"
