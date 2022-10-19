a97ac83b5f6549dfeb4e1761b6ff192967d3c4c7
Mark Cornmesser <mcornmesser@mozilla.com>
2022-10-19 15:14:05 -0700

    add build tools

--- modules/roles_profiles/manifests/profiles/microsoft_tools.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_tools.pp
@@ -38,8 +38,7 @@ class roles_profiles::profiles::microsoft_tools {
                     include win_packages::vc_redist_x64
                 }
                 default: {
-                            include win_packages::vs_buildtools
-                    }
+                        include win_packages::vs_buildtools
                 }
             }
             # Bug List
