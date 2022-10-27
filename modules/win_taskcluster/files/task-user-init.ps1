b5897ca69d7b7e578dbc0c10a1d30e99619aa91c
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2022-10-27 15:08:38 -0400

    Add win11 task user init + formatting

---
+++ modules/win_taskcluster/files/task-user-init.ps1
@@ -0,0 +1,65 @@
+# From time to time we need to have the different releases of the same OS version
+$release_key = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion')
+$release_id = $release_key.ReleaseId
+$win_os_build = [System.Environment]::OSVersion.Version.build
+
+# OS caption
+# Used to determine which KMS license for cloud workers
+$caption = ((Get-WmiObject Win32_OperatingSystem).caption)
+$caption = $caption.ToLower()
+$os_caption = $caption -replace ' ', '_'
+
+if ($os_caption -like "*windows_10*") {
+    $os_version = ( -join ( "win_10_", $release_id))
+}
+elseif ($os_caption -like "*windows_11*") {
+    $os_version = ( -join ( "win_11_", $release_id))
+}
+elseif ($os_caption -like "*2012*") {
+    $os_version = "win_2012"
+}
+else {
+    $os_version = $null
+}
+
+## For now, run all of the commands below on win10 or win11
+
+## Disable windows security and maintenance notifications
+$Path1 = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance'
+if (-not (Test-Path $Path1)) {
+    New-Item -Path $path1 -Force
+}
+Set-ItemProperty -Path $Path1 -Name Enabled -Value 0
+
+## Set Windows VisualFX
+
+<#
+## Doesn't work https://www.reddit.com/r/PowerShell/comments/u3y4xd/change_visualfx_on_windows_11/
+## Stackoverflow: https://stackoverflow.com/questions/9897310/can-you-change-the-visual-effects-performance-settings-from-an-application
+## https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfow
+#>
+
+## Enable Scrollbars to always show
+if ($Null -eq (Get-ItemProperty -Path 'HKCU:\Control Panel\Accessibility').DynamicScrollbars) { 
+    New-ItemProperty -Path 'HKCU:\Control Panel\Accessibility' -Name 'DynamicScrollbars' -Value 0 
+}
+
+## Prepare Chrome Profile
+## Not needed due to only being required for hardware gpu testers
+if (test-path "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe") {
+    Start-Process chrome
+    Start-Sleep -s 30
+    taskkill /F /IM chrome.exe /T
+}
+
+## After all hkcu items are changed, restart explorer
+Stop-Process -f -ProcessName explorer
+
+# To-do: Add support for additional OS'
+<#
+switch ($os_caption) {
+    "win_11_2009" {}
+    "win_10_2004" {  }
+    Default {}
+}
+#>
--- modules/win_taskcluster/manifests/generic_worker.pp
+++ modules/win_taskcluster/manifests/generic_worker.pp
@@ -3,46 +3,41 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_taskcluster::generic_worker (
-    String $generic_worker_dir,
-    String $desired_gw_version,
-    String $current_gw_version,
-    String $gw_exe_source,
-    String $gw_exe_path,
-    # HERE
-    String $init_file
-    # HERE
+  String $generic_worker_dir,
+  String $desired_gw_version,
+  String $current_gw_version,
+  String $gw_exe_source,
+  String $gw_exe_path,
+  String $init_file
 ) {
+  $ed25519private = "${generic_worker_dir}\\ed25519-private.key"
 
-    $ed25519private = "${generic_worker_dir}\\ed25519-private.key"
-
-    if ($current_gw_version != $desired_gw_version) {
-        exec { 'purge_old_nonservice_gw_exe':
-            command  => "Remove-Item -path ${gw_exe_path}",
-            unless   => "Test-Path ${gw_exe_path}",
-            provider => powershell,
-        }
+  if ($current_gw_version != $desired_gw_version) {
+    exec { 'purge_old_nonservice_gw_exe':
+      command  => "Remove-Item -path ${gw_exe_path}",
+      unless   => "Test-Path ${gw_exe_path}",
+      provider => powershell,
     }
-    file { $generic_worker_dir:
-        ensure => directory,
+  }
+  file { $generic_worker_dir:
+    ensure => directory,
+  }
+  file { $gw_exe_path:
+    source  => $gw_exe_source,
+  }
+  exec { 'generate_ed25519_keypair':
+    command => "${gw_exe_path} new-ed25519-keypair --file ${ed25519private}",
+    creates => $ed25519private,
+  }
+  # TODO: Add conditional language to profile based on OS version
+  # To pass the correct source file name instead of hard code
+  file { "${generic_worker_dir}\\task-user-init.cmd":
+    content   => file($init_file),
+  }
+  if $init_file == 'task-user-init-win11.cmd' {
+    # C:\generic-worker\task-user-init.ps1
+    file { "${generic_worker_dir}\\task-user-init.ps1":
+      content   => file('task-user-init.ps1'),
     }
-    file { $gw_exe_path:
-        source  => $gw_exe_source,
-    }
-    exec { 'generate_ed25519_keypair':
-        command => "${gw_exe_path} new-ed25519-keypair --file ${ed25519private}",
-        creates => $ed25519private,
-    }
-    # TODO: Add conditional language to profile based on OS version
-    # To pass the correct source file name instead of hard code
-    file { "${generic_worker_dir}\\task-user-init.cmd":
-        # HERE
-        content   => file($init_file),
-    }
-    if $init_file == 'task-user-init-win11.cmd' {
-        # C:\generic-worker\task-user-init.ps1
-        file { "${generic_worker_dir}\\task-user-init.ps1":
-            content   => file('task-user-init-win11.cmd'),
-        }
-    }
-    # HERE
+  }
 }
