aa85fe1a6ed51328ad8e63aa73ff9a65f36f85af
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2022-10-31 14:29:42 -0400

    Add win11 nvidia grid driver support

--- modules/win_packages/manifests/drivers/nvidia_grid.pp
+++ modules/win_packages/manifests/drivers/nvidia_grid.pp
@@ -3,37 +3,36 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_packages::drivers::nvidia_grid (
-    String $driver_name,
-    String $srcloc
+  String $driver_name,
+  String $srcloc
 ) {
+  require win_packages::sevenzip
 
-    require win_packages::sevenzip
+  $setup_exe   = "${facts['custom_win_systemdrive']}\\${driver_name}\\setup.exe"
+  $working_dir = "${facts['custom_win_systemdrive']}\\${driver_name}"
+  $seven_zip   = "\"${facts['custom_win_programfiles']}\\7-Zip\\7z.exe\""
+  $zip_name    = "${driver_name}.zip"
+  $pkgdir      = $facts['custom_win_temp_dir']
+  $src_file    = "\"${pkgdir}\\${zip_name}\""
 
-    $setup_exe   = "${facts['custom_win_systemdrive']}\\${driver_name}\\setup.exe"
-    $working_dir = "${facts['custom_win_systemdrive']}\\${driver_name}"
-    $seven_zip   = "\"${facts['custom_win_programfiles']}\\7-Zip\\7z.exe\""
-    $zip_name    = "${driver_name}.zip"
-    $pkgdir      = $facts['custom_win_temp_dir']
-    $src_file    = "\"${pkgdir}\\${zip_name}\""
+  # copy the installtion file during image build
+  # only install if it is a gpu worker with gpu in the pool name
 
-    # copy the installtion file during image build
-    # only install if it is a gpu worker with gpu in the pool name
-
-    file { $working_dir:
-        ensure => directory,
+  file { $working_dir:
+    ensure => directory,
+  }
+  file { "${pkgdir}\\${zip_name}":
+    source => "${srcloc}/${zip_name}",
+  }
+  if $facts['custom_win_gpu'] == 'yes' {
+    exec { 'grid_unzip':
+      command => "${seven_zip} x ${src_file} -o${working_dir} -y",
+      creates => $setup_exe,
     }
-    file { "${pkgdir}\\${zip_name}":
-        source => "${srcloc}/${zip_name}"
+    exec { 'grid_install':
+      command     => "${facts['custom_win_system32']}\\cmd.exe /c ${setup_exe} -s -noreboot",
+      subscribe   => Exec['grid_unzip'],
+      refreshonly => true,
     }
-    if $facts['custom_win_gpu'] == 'yes' {
-        exec { 'grid_unzip':
-            command => "${seven_zip} x ${src_file} -o${working_dir} -y",
-            creates => $setup_exe,
-        }
-        exec { 'grid_install':
-            command     => "${facts['custom_win_system32']}\\cmd.exe /c ${setup_exe} -s -noreboot",
-            subscribe   => Exec['grid_unzip'],
-            refreshonly => true,
-        }
-    }
+  }
 }
