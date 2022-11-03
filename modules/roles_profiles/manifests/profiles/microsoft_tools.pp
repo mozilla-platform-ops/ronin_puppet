421b80c4b5fb42e7563345cc755eea5587bd5e09
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2022-11-03 19:14:37 -0400

    Fix python gpu win11 issues (#450)
    * * fix case statement
    * vsbuild2022 win11 support
    
    * fix case statement lines
    
    * change to warning

--- modules/roles_profiles/manifests/profiles/microsoft_tools.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_tools.pp
@@ -3,56 +3,72 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::microsoft_tools {
+  case $facts['os']['name'] {
+    'Windows': {
+      include win_os_settings::powershell_profile
+      class { 'win_packages::performance_tool_kit':
+        moz_profile_source => lookup('win-worker.mozilla_profile.source'),
+        moz_profile_file   => lookup('win-worker.mozilla_profile.local'),
+      }
 
-    case $facts['os']['name'] {
-        'Windows': {
-
-            # These may chnage for diffrent versions of Windows
-            # TODO: Research why we are using a pinned version and add comment why.
-            # Original source 'https://hg.mozilla.org/mozilla-central/raw-file/360ab7771e27/toolkit/components/startup/mozprofilerprobe.mof'
-            # When pulling from a HG repo Puppet see it the file as a new file one ach run
-            # In this case triggers the exec and adds to the local WMI repo each time
-            # For now pulling from S3
-
-            include win_os_settings::powershell_profile
-
-            class { 'win_packages::performance_tool_kit':
-                moz_profile_source => lookup('win-worker.mozilla_profile.source'),
-                moz_profile_file   => lookup('win-worker.mozilla_profile.local'),
-            }
-
-            # This fact comes up as 2016 for 2022
-            if $facts['os']['release']['full'] == (('2012 R2') or ('2016')) {
-                $purpose = 'builder'
-            } else {
-                $purpose = 'tester'
-            }
-
-            case $purpose {
-                'builder': {
-                    case $facts['custom_win_os_version'] {
-                        'win_2022_2009': {
-                            include win_packages::vs_buildtools_2022
-                        }
-                        default: {
-                            include win_packages::vs_buildtools
-                            include win_packages::dxsdk_jun10
-                            include win_packages::binscope
-                            # Required by rustc (tooltool artefact)
-                            include win_packages::vc_redist_x86
-                            include win_packages::vc_redist_x64
-                        }
-                    }
-                }
-                default: {
-                    include win_packages::vs_buildtools
-                }
-            }
-            # Bug List
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1510837
+      case $facts['custom_win_os_version'] {
+        'win_10_2004','win_11_2009': {
+          $purpose = 'tester'
+        }
+        'win_2012','win_2022_2009': {
+          $purpose = 'builder'
         }
         default: {
-            fail("${$facts['os']['name']} not supported")
+          warning("${$facts['os']['name']} not supported")
         }
+      }
+
+      case $purpose {
+        'builder': {
+          case $facts['custom_win_os_version'] {
+            'win_2022_2009': {
+              include win_packages::vs_buildtools_2022
+            }
+            'win_2012':{
+              include win_packages::vs_buildtools
+              include win_packages::dxsdk_jun10
+              include win_packages::binscope
+              # Required by rustc (tooltool artefact)
+              include win_packages::vc_redist_x86
+              include win_packages::vc_redist_x64
+            }
+            default: {
+              include win_packages::vs_buildtools
+              include win_packages::dxsdk_jun10
+              include win_packages::binscope
+              # Required by rustc (tooltool artefact)
+              include win_packages::vc_redist_x86
+              include win_packages::vc_redist_x64
+            }
+          }
+        }
+        'tester': {
+          case $facts['custom_win_os_version'] {
+            'win_11_2009': {
+              include win_packages::vs_buildtools_2022
+            }
+            'win_10_2004': {
+              include win_packages::vs_buildtools
+            }
+            default: {
+              include win_packages::vs_buildtools
+            }
+          }
+        }
+        default: {
+          include win_packages::vs_buildtools
+        }
+      }
+      # Bug List
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1510837
     }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
