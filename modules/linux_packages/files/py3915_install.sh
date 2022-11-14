b09a33eb182b975b4565818913b2f5e975537602
Andrew Erickson <aerickson@mozilla.com>
2022-11-14 14:53:29 -0800

    backport changes from testing branch

---
+++ modules/linux_packages/files/py3915_install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+# set -x
+
+# TODO: check that this is run as root
+
+apt remove libpython3.9-minimal libpython3.9-stdlib -y
+
+# order matters
+#   created with https://github.com/starkandwayne/install-debs-in-order
+dpkg -i libpython3.9-minimal_3.9.15-1+bionic1_amd64.deb
+dpkg -i python3.9-lib2to3_3.9.15-1+bionic1_all.deb
+dpkg -i libpython3.9-stdlib_3.9.15-1+bionic1_amd64.deb
+dpkg -i python3.9-distutils_3.9.15-1+bionic1_all.deb
+dpkg -i python3.9-minimal_3.9.15-1+bionic1_amd64.deb
+dpkg -i libpython3.9_3.9.15-1+bionic1_amd64.deb
+dpkg -i python3.9_3.9.15-1+bionic1_amd64.deb
+dpkg -i libpython3.9-dev_3.9.15-1+bionic1_amd64.deb
+dpkg -i python3.9-venv_3.9.15-1+bionic1_amd64.deb
+dpkg -i python3.9-dev_3.9.15-1+bionic1_amd64.deb
+
+# changing /usr/bin/python3 makes `apt update` unhappy, fix.
+#
+# Traceback (most recent call last):
+#   File "/usr/lib/cnf-update-db", line 8, in <module>
+#     from CommandNotFound.db.creator import DbCreator
+#   File "/usr/lib/python3/dist-packages/CommandNotFound/db/creator.py", line 11, in <module>
+#     import apt_pkg
+# ModuleNotFoundError: No module named 'apt_pkg'
+#
+cd /usr/lib/python3/dist-packages
+cp apt_pkg.cpython-36m-x86_64-linux-gnu.so apt_pkg.so
+cd -
+
+apt install --fix-broken -y
--- modules/linux_packages/manifests/py3.pp
+++ modules/linux_packages/manifests/py3.pp
@@ -17,27 +17,35 @@ class linux_packages::py3 {
 
           # py3.9, from deadsnakes ppa
 
-          file { '/opt/relops_py39/':
+          file { '/opt/relops_py3915/':
             ensure => directory,
             group  => 'root',
             mode   => '0755',
             owner  => 'root',
           }
 
+          file { '/opt/relops_py3915/py3915_install.sh':
+            ensure => present,
+            group  => 'root',
+            mode   => '0755',
+            owner  => 'root',
+            source => "puppet:///modules/${module_name}/py3915_install.sh",
+          }
+
           $py39_urls = {
             # base
-            '/opt/relops_py39/python3.9_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9_3.9.15-1%2Bbionic1_amd64.deb' },
-            '/opt/relops_py39/libpython3.9_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9_3.9.15-1%2Bbionic1_amd64.deb' },
-            '/opt/relops_py39/libpython3.9-stdlib_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-stdlib_3.9.15-1%2Bbionic1_amd64.deb' },
-            '/opt/relops_py39/python3.9-minimal_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-minimal_3.9.15-1%2Bbionic1_amd64.deb' },
-            '/opt/relops_py39/libpython3.9-minimal_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-minimal_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/python3.9_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/libpython3.9_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/libpython3.9-stdlib_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-stdlib_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/python3.9-minimal_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-minimal_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/libpython3.9-minimal_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-minimal_3.9.15-1%2Bbionic1_amd64.deb' },
             # optional
-            '/opt/relops_py39/python3.9-distutils_3.9.15-1+bionic1_all.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-distutils_3.9.15-1%2Bbionic1_all.deb' },
-            '/opt/relops_py39/python3.9-lib2to3_3.9.15-1+bionic1_all.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-lib2to3_3.9.15-1%2Bbionic1_all.deb' },
-            '/opt/relops_py39/python3.9-venv_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-venv_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/python3.9-distutils_3.9.15-1+bionic1_all.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-distutils_3.9.15-1%2Bbionic1_all.deb' },
+            '/opt/relops_py3915/python3.9-lib2to3_3.9.15-1+bionic1_all.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-lib2to3_3.9.15-1%2Bbionic1_all.deb' },
+            '/opt/relops_py3915/python3.9-venv_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-venv_3.9.15-1%2Bbionic1_amd64.deb' },
             # dev
-            '/opt/relops_py39/python3.9-dev_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-dev_3.9.15-1%2Bbionic1_amd64.deb' },
-            '/opt/relops_py39/libpython3.9-dev_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-dev_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/python3.9-dev_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/python3.9-dev_3.9.15-1%2Bbionic1_amd64.deb' },
+            '/opt/relops_py3915/libpython3.9-dev_3.9.15-1+bionic1_amd64.deb' => { source => 'https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/linux/public/common/py39/1804/libpython3.9-dev_3.9.15-1%2Bbionic1_amd64.deb' },
           }
           # need python3-dev for psutil and zstandard compilation
 
@@ -48,13 +56,12 @@ class linux_packages::py3 {
           })
 
           exec { 'install py39':
-            # note the version string
-            command  => '/usr/bin/dpkg -i *3.9.15*.deb',
+            command  => '/opt/relops_py3915/py3915_install.sh',
             path     => '/bin:/usr/bin/:/sbin:/usr/sbin',
-            cwd      => '/opt/relops_py39/',
+            cwd      => '/opt/relops_py3915/',
             provider => shell,
-            # note the version string
-            unless   => '/usr/bin/dpkg --list | /bin/grep python3.9 | /bin/grep 3.9.15 && /usr/bin/python3.9 -c "import distutils"',
+            unless   => '/usr/bin/dpkg --list | /bin/grep -v rc | /bin/grep "python3.9 " | /bin/grep "3.9.15" \
+              && /usr/bin/python3 -c "import distutils"',
           }
 
           # configure alternatives
@@ -125,6 +132,7 @@ class linux_packages::py3 {
             ensure => absent,
             force  => true,
           }
+          # TODO: cleanup relops_py39
         }
         default: {
           fail("${facts['os']['release']['major']} not supported")
