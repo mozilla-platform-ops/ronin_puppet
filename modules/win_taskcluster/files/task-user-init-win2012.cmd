82a98569deff8a4ba5d1dd566beb762ff07ac4c5
Mark Cornmesser <mcornmesser@mozilla.com>
2022-11-22 20:51:35 -0800

    look for bash hack

--- modules/win_taskcluster/files/task-user-init-win2012.cmd
+++ modules/win_taskcluster/files/task-user-init-win2012.cmd
@@ -1,24 +1,21 @@
 :: Task User initialisation script - this script runs as task user, not as administrator.
 :: It runs after task user has logged in, but before worker claims a task.
 
-@echo off
+:WaitForExplorerKey
+echo Wait for Explorer registry key to exist before allowing a task to run...
+reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer /ve
+if %ERRORLEVEL% EQU 0 goto Completed
+echo HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer does not yet exist
+:: Cannot use timeout command from non-interactive process
+:: (try it yourself with e.g. `echo hello | timeout /t 1`)
+ping -n 2 127.0.0.1 1>/nul
+goto WaitForExplorerKey
 
-:HideTaskBar
-echo Hiding taskbar...
-powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -ProcessName explorer}"
+:Completed
 
-echo Setting visual effects
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v "VisualFXSetting" /t REG_DWORD /d 1 /f
-
-:: Holding off including this here for now, as we will likely be doing this in preflight
-:: scripts in future. See: https://bugzilla.mozilla.org/show_bug.cgi?id=1396168#c13
-::
-:: :: Task user firewall exceptions
-:: netsh advfirewall firewall add rule name="ssltunnel-%USERNAME%" dir=in action=allow program="%USERPROFILE%\build\tests\bin\ssltunnel.exe" enable=yes
-:: netsh advfirewall firewall add rule name="ssltunnel-%USERNAME%" dir=out action=allow program="%USERPROFILE%\build\tests\bin\ssltunnel.exe" enable=yes
-:: netsh advfirewall firewall add rule name="python-%USERNAME%" dir=in action=allow program="%USERPROFILE%\build\venv\scripts\python.exe" enable=yes
-:: netsh advfirewall firewall add rule name="python-%USERNAME%" dir=out action=allow program="%USERPROFILE%\build\venv\scripts\python.exe" enable=yes
-
+echo Looking for bash
 which bash
+echo Testing bash
+bash -c "hg clone -h"
 
 echo Completed task user initialisation.
