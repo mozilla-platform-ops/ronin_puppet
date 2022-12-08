7ac5260d4ff0d387be5daa003eb26d936cd1b837
Mark Cornmesser <mcornmesser@mozilla.com>
2022-12-07 17:45:15 -0800

    set z git repos safe in init script

--- modules/win_taskcluster/files/task-user-init.ps1
+++ modules/win_taskcluster/files/task-user-init.ps1
@@ -22,51 +22,48 @@ else {
     $os_version = $null
 }
 
-## For now, run all of the commands below on win10 or win11
-
-## Disable windows security and maintenance notifications
-$Path1 = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance'
-if (-not (Test-Path $Path1)) {
-    New-Item -Path $path1 -Force
-}
-Set-ItemProperty -Path $Path1 -Name Enabled -Value 0
-
-## Set Windows VisualFX
-
-<#
-## Doesn't work https://www.reddit.com/r/PowerShell/comments/u3y4xd/change_visualfx_on_windows_11/
-## Stackoverflow: https://stackoverflow.com/questions/9897310/can-you-change-the-visual-effects-performance-settings-from-an-application
-## https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfow
-#>
-
-## Enable Scrollbars to always show
-if ($Null -eq (Get-ItemProperty -Path 'HKCU:\Control Panel\Accessibility').DynamicScrollbars) {
-    New-ItemProperty -Path 'HKCU:\Control Panel\Accessibility' -Name 'DynamicScrollbars' -Value 0
-}
-
-## Prepare Chrome Profile
-## Not needed due to only being required for hardware gpu testers
-if (test-path "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe") {
-    Start-Process chrome
-    Start-Sleep -s 30
-    taskkill /F /IM chrome.exe /T
-}
-
-## After all hkcu items are changed, restart explorer
-Stop-Process -f -ProcessName explorer
-
-# To-do: Add support for additional OS'
-<#
 switch ($os_caption) {
-    "win_11_2009" {}
-    "win_10_2004" {  }
+    "win_11_2009" {
+		## Disable windows security and maintenance notifications
+		$Path1 = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance'
+		if (-not (Test-Path $Path1)) {
+    		New-Item -Path $path1 -Force
+		}
+		Set-ItemProperty -Path $Path1 -Name Enabled -Value 0
+
+		## Set Windows VisualFX
+
+		<#
+		## Doesn't work https://www.reddit.com/r/PowerShell/comments/u3y4xd/change_visualfx_on_windows_11/
+		## Stackoverflow: https://stackoverflow.com/questions/9897310/can-you-change-the-visual-effects-performance-settings-from-an-application
+		## https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfow
+		#>
+
+		## Enable Scrollbars to always show
+		if ($Null -eq (Get-ItemProperty -Path 'HKCU:\Control Panel\Accessibility').DynamicScrollbars) {
+    		New-ItemProperty -Path 'HKCU:\Control Panel\Accessibility' -Name 'DynamicScrollbars' -Value 0
+		}
+
+		## Prepare Chrome Profile
+		## Not needed due to only being required for hardware gpu testers
+		if (test-path "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe") {
+    		Start-Process chrome
+    		Start-Sleep -s 30
+    		taskkill /F /IM chrome.exe /T
+		}
+
+		## After all hkcu items are changed, restart explorer
+		Stop-Process -f -ProcessName explorer
+	}
     "win_2012" {
+		## Mark Git repos under Z $taskuser safe
         $user = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username)
         $task_user = $user.split('\')[1]
         $git_repo = ("Z:/{0}/*" -f ($task_user))
         git config --global --add safe.directory $git_repo
+
+		## Ensure strong encryption
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     }
     Default {}
 }
-#>
