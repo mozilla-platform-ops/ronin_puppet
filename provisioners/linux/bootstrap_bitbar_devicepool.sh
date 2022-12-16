c7d6d0d443c8f0016861d07c678bb03f61540264
Andrew Erickson <aerickson@mozilla.com>
2022-12-16 08:57:43 -0800

    bitbar bootstrap: rm -f to avoid errors if no files present (#457)

--- provisioners/linux/bootstrap_bitbar_devicepool.sh
+++ provisioners/linux/bootstrap_bitbar_devicepool.sh
@@ -133,7 +133,7 @@ else
 fi
 
 # install puppet
-rm /var/tmp/puppet*.deb*
+rm -f /var/tmp/puppet*.deb*
 wget -P /var/tmp/ "http://apt.puppetlabs.com/puppet7-release-$(lsb_release -c -s).deb"
 dpkg -i /var/tmp/*.deb
 apt-get update -y && apt-get install -y puppet-agent
