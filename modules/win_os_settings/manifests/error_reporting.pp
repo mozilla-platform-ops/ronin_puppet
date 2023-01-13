0dca0580f05002404d172d56ea822be9bbb5b9b2
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-01-13 08:51:22 -0500

    Add support for error reports to win11

--- modules/win_os_settings/manifests/error_reporting.pp
+++ modules/win_os_settings/manifests/error_reporting.pp
@@ -3,37 +3,36 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_os_settings::error_reporting {
+  $error_report_key = "HKLM\\SOFTWARE\\Microsoft\\Windows\\Windows\\Error\\Reporting"
+  if $facts['custom_win_z_drive'] == 'exists' {
+    $dump_drive = 'Z:'
+  } else {
+    $dump_drive = $facts['custom_win_systemdrive']
+  }
+  $dump_dir = "${dump_drive}\\error-dumps"
 
-    $error_report_key = "HKLM\\SOFTWARE\\Microsoft\\Windows\\Windows\\Error\\Reporting"
-    if $facts['custom_win_z_drive'] == 'exists'{
-        $dump_drive = 'Z:'
-    } else {
-        $dump_drive = $facts['custom_win_systemdrive']
-    }
-    $dump_dir = "${dump_drive}\\error-dumps"
+  file { $dump_dir :
+    ensure => directory,
+  }
 
-
-    file { $dump_dir :
-        ensure => directory,
-    }
-
-    # Using puppetlabs-registry
-    registry::value { 'DumpFolder' :
-        key  => $error_report_key,
-        type => string,
-        data => $dump_dir,
-    }
-    registry::value { 'LocalDumps' :
-        key  => $error_report_key,
-        type => dword,
-        data => '1',
-    }
-    registry::value { 'DontShowUI' :
-        key  => $error_report_key,
-        type => dword,
-        data => '1',
-    }
+  # Using puppetlabs-registry
+  registry::value { 'DumpFolder' :
+    key  => $error_report_key,
+    type => string,
+    data => $dump_dir,
+  }
+  registry::value { 'LocalDumps' :
+    key  => $error_report_key,
+    type => dword,
+    data => '1',
+  }
+  registry::value { 'DontShowUI' :
+    key  => $error_report_key,
+    type => dword,
+    data => '1',
+  }
 }
 
 # Bug list
 # https://bugzilla.mozilla.org/show_bug.cgi?id=1562024
+# https://bugzilla.mozilla.org/show_bug.cgi?id=1694584
