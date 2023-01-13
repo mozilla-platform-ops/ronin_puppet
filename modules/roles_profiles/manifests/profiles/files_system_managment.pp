14161185b5117b2caf3c47df69bf70c5af1f5e2b
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-01-13 12:01:39 -0500

    Add support for error reporting for windows 11 (#464)
    * Set UAC to never notify on Windows 11
    
    * Add support for error reports to win11

--- modules/roles_profiles/manifests/profiles/files_system_managment.pp
+++ modules/roles_profiles/manifests/profiles/files_system_managment.pp
@@ -3,30 +3,29 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::files_system_managment {
-
-    case $facts['os']['name'] {
-        'Windows': {
-            include win_filesystem::disable8dot3
-            include win_filesystem::disablelastaccess
-            if $facts['custom_win_location'] == 'azure' {
-                if $facts['custom_win_y_drive'] == 'exists'{
-                    win_filesystem::set_paging_file { 'azure_paging_file':
-                        location =>  'y:\pagefile.sys',
-                        min_size => 8192,
-                        max_size => 8192,
-                    }
-                }
-                if $facts['custom_win_z_drive'] == 'exists'{
-                    include win_filesystem::grant_z_access
-                }
-            }
-            # Bug List
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1515779
-            # Paging file
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1562974
+  case $facts['os']['name'] {
+    'Windows': {
+      include win_filesystem::disable8dot3
+      include win_filesystem::disablelastaccess
+      if $facts['custom_win_location'] == 'azure' {
+        if $facts['custom_win_y_drive'] == 'exists' {
+          win_filesystem::set_paging_file { 'azure_paging_file':
+            location => 'y:\pagefile.sys',
+            min_size => 8192,
+            max_size => 8192,
+          }
         }
-        default: {
-            fail("${$facts['os']['name']} not supported")
+        if $facts['custom_win_z_drive'] == 'exists' {
+          include win_filesystem::grant_z_access
         }
+      }
+      # Bug List
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1515779
+      # Paging file
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1562974
     }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
--- modules/win_filesystem/manifests/grant_z_access.pp
+++ modules/win_filesystem/manifests/grant_z_access.pp
@@ -3,16 +3,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_filesystem::grant_z_access {
-
-    if $facts['custom_win_z_drive'] == 'exists'{
-        acl { 'Z:':
-            permissions                =>   {
-                                            identity    => 'everyone',
-                                            rights      => ['full'],
-                                            perm_type   => 'allow',
-                                            child_types => 'all',
-                                            affects     => 'all'
-                                        }
-        }
+  if $facts['custom_win_z_drive'] == 'exists' {
+    acl { 'Z:':
+      permissions                => {
+        identity    => 'everyone',
+        rights      => ['full'],
+        perm_type   => 'allow',
+        child_types => 'all',
+        affects     => 'all',
+      },
     }
+  }
 }
