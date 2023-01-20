e92e67bfa1200752e237c123d90f811acb3894e0
Mark Cornmesser <mcornmesser@mozilla.com>
2023-01-20 12:25:46 -0800

    don't delete python hg for 2012 with  older mozilla build version

--- modules/win_mozilla_build/manifests/modifications.pp
+++ modules/win_mozilla_build/manifests/modifications.pp
@@ -31,10 +31,14 @@ class win_mozilla_build::modifications {
             }
         }
     }
-    file { "${mozbld}\\python\\Scripts\\hg":
-        ensure => absent,
-        purge  => true,
-        force  => true,
+    if $facts['os']['release']['full'] == '2012 R2' {
+        if $win_mozilla_build::needed_mozbld_ver == '3.2' {
+            file { "${mozbld}\\python\\Scripts\\hg":
+                ensure => absent,
+                purge  => true,
+                force  => true,
+            }
+        }
     }
     # File resource fails when create symlink
     # https://bugzilla.mozilla.org/show_bug.cgi?id=1544140
