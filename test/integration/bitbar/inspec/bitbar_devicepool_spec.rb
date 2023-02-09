21d42b119f4c44579bd46b9185fcbad66896fe95
Michelle Goossens <michelle@masterwayz.nl>
2023-02-09 22:16:52 +0100

    add mgoossens to bitbar

--- test/integration/bitbar/inspec/bitbar_devicepool_spec.rb
+++ test/integration/bitbar/inspec/bitbar_devicepool_spec.rb
@@ -8,6 +8,13 @@ describe 'users' do
     end
   end
 
+  describe user('mgoossens') do
+    it { should exist }
+    %w(mgoossens wheel).each do |group|
+      its('groups') { should include group }
+    end
+  end
+
   describe user('bitbar') do
     it { should exist }
   end
