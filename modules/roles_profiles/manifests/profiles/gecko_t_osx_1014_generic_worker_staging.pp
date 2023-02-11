d3c2100408d41e6e68bf6a767d585b32717e6f3f
Michelle Goossens <michelle@masterwayz.nl>
2023-02-11 19:33:36 +0100

    Adjust references

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker_staging.pp
@@ -25,9 +25,9 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker_staging {
                 telegraf_user       => lookup('telegraf.user'),
                 telegraf_password   => lookup('telegraf.password'),
                 puppet_env          => 'dev',
-                puppet_repo         => 'https://github.com/davehouse/ronin_puppet.git',
+                puppet_repo         => 'https://github.com/mozilla-platform-ops/ronin_puppet.git',
                 puppet_branch       => '1561956_generic-worker_15-recover',
-                puppet_notify_email => 'dhouse@mozilla.com',
+                puppet_notify_email => 'relops-puppet-alerts@mozilla.com',
                 meta_data           => $meta_data,
             }
 
