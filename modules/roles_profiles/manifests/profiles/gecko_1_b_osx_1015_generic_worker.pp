dbdea1281a1e2f5621b8826a41881be1c3d0a1c4
Michelle Goossens <michelle@masterwayz.nl>
2023-02-11 14:42:03 +0100

    fix gecko_1_b_osx_1015 pin

--- modules/roles_profiles/manifests/profiles/gecko_1_b_osx_1015_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_1_b_osx_1015_generic_worker.pp
@@ -21,9 +21,9 @@ class roles_profiles::profiles::gecko_1_b_osx_1015_generic_worker {
                 telegraf_user       => lookup('telegraf.user'),
                 telegraf_password   => lookup('telegraf.password'),
                 puppet_env          => 'dev',
-                puppet_repo         => 'https://github.com/davehouse/ronin_puppet.git',
+                puppet_repo         => 'https://github.com/mozilla-platform-ops/ronin_puppet.git',
                 puppet_branch       => 'bug1665379_mac-builders',
-                puppet_notify_email => 'dhouse@mozilla.com',
+                puppet_notify_email => 'relops@mozilla.com',
                 meta_data           => $meta_data,
             }
 
