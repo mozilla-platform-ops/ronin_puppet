e35ce5d0ecf7c46456f39912dd1ad5bb617d2c48
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-02-22 11:56:07 -0500

    add test

---
+++ modules/roles_profiles/manifests/roles/win11642009azure_test.pp
@@ -0,0 +1,7 @@
+# Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+class roles_profiles::roles::win11642009azure_test {
+  include roles_profiles::profiles::mozilla_build_tester
+}
