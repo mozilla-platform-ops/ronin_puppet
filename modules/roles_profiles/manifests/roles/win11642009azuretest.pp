43efa02118bb73030696ef97c2b3975bcb45c0d5
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-02-22 11:58:51 -0500

    fix

--- modules/roles_profiles/manifests/roles/win11642009azuretest.pp
+++ modules/roles_profiles/manifests/roles/win11642009azuretest.pp
@@ -2,6 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-class roles_profiles::roles::win11642009azure_test {
+class roles_profiles::roles::win11642009azuretest {
   include roles_profiles::profiles::mozilla_build_tester
 }
