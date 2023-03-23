55ff45e97fa3d0a95397996179df0221dc03af43
Mark Cornmesser <mcornmesser@mozilla.com>
2023-03-23 16:46:32 -0700

    add worker-runner support

--- modules/roles_profiles/manifests/profiles/windows_worker_runner.pp
+++ modules/roles_profiles/manifests/profiles/windows_worker_runner.pp
@@ -62,7 +62,7 @@ class roles_profiles::profiles::windows_worker_runner {
                     $downloads_dir         = "${facts['custom_win_systemdrive']}\\\\downloads"
                     $ed25519signingkey     = "${facts['custom_win_systemdrive']}\\\\generic-worker\\\\ed25519-private.key"
                     $idle_timeout          = lookup('win-worker.generic_worker.idle_timeout')
-                    $ip                    = "\"facts['networking']['ip']\""
+                    $ip                    = facts['networking']['ip']
                     $livelog_exe           = "${facts['custom_win_systemdrive']}\\\\generic-worker\\\\livelog.exe"
                     $root_url              = lookup('windows.taskcluster.root_url')
                     $task_dir              = "${facts['custom_win_systemdrive']}\\\\Users"
