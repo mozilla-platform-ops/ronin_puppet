c78c0dcfeb5f3c43fa359725ff577c62e3277b3e
Mark Cornmesser <mcornmesser@mozilla.com>
2023-03-29 11:29:23 -0700

    wait on first reboot

--- provisioners/windows/MAAS/bootstrap.ps1
+++ provisioners/windows/MAAS/bootstrap.ps1
@@ -150,7 +150,7 @@ Function Set-DCRoninRepo {
         [string] $nodes_def = "$env:systemdrive\ronin\manifests\nodes.pp",
         [string] $bootstrap_dir = "$env:systemdrive\BootStrap\",
         [string] $secret_src = "$env:systemdrive\programdata\secrets\",
-        [string] $secrets = "$env:systemdrive\ronin\data\",
+        [string] $secrets = "$env:systemdrive\ronin\data\secrets",
         [String] $sentry_reg = "HKLM:SYSTEM\CurrentControlSet\Services",
         [string] $workerType = (Get-ItemProperty "HKLM:\SOFTWARE\Mozilla\ronin_puppet").workerType,
         [string] $role = (Get-ItemProperty "HKLM:\SOFTWARE\Mozilla\ronin_puppet").role,
@@ -184,7 +184,8 @@ Function Set-DCRoninRepo {
             (Get-Content -path $nodes_def) -replace 'roles::role', "roles::$role" | Set-Content $nodes_def
         }
         if (!(Test-path $secrets)) {
-            Copy-item -path $secret_src -destination $secrets -recurse -force
+            mkdir $secrets
+            Copy-item -path "$secret_src\*" -destination $secrets -recurse -force
         }
         # Start to disable Windows defender here
         $caption = ((Get-WmiObject Win32_OperatingSystem).caption)
