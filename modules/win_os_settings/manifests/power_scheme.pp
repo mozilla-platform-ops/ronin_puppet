16ebd56d5ad67367560bb0c57273c4f98d0b0e19
Mark Cornmesser <mcornmesser@mozilla.com>
2023-04-03 10:05:10 -0700

    add ulitmate power setting for win 11

--- modules/win_os_settings/manifests/power_scheme.pp
+++ modules/win_os_settings/manifests/power_scheme.pp
@@ -20,7 +20,7 @@ class win_os_settings::power_scheme(
     'Balanced'             => '381b4222-f694-41f0-9685-ff5bb260df2e',
     'High performance'     => '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c',
     'Power saver'          => 'a1841308-3541-4fab-bc81-f71556f20b4a',
-    'ultimate performance' => 'e9a42b02-d5df-448d-aa00-03f14749eb61',
+    'Ultimate Performance' => 'e9a42b02-d5df-448d-aa00-03f14749eb61',
   }
 ){
 
