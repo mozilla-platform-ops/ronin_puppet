ba743862c627a2c1736a1234913b78d15cfb1e5b
Mark Cornmesser <mcornmesser@mozilla.com>
2023-04-03 10:20:23 -0700

    add ulitmate power setting for win 11

--- modules/win_os_settings/manifests/power_scheme.pp
+++ modules/win_os_settings/manifests/power_scheme.pp
@@ -36,4 +36,14 @@ class win_os_settings::power_scheme(
     unless   => template('windows/powercfg_check.ps1.erb'),
     provider => 'powershell',
   }
+  # Using puppetlabs-registry
+  if $guid == 'e9a42b02-d5df-448d-aa00-03f14749eb61' {
+    $power_key = "HKLM\\\SYSTEM\\CurrentControlSet\\Control\\Power"
+
+    registry::value { 'HibernateEnabled' :
+      key  => $power_key,
+      type => dword,
+      data => '0',
+    }
+  }
 }
