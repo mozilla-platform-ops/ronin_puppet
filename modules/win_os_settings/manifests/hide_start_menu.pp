6cdb0086dae4ab266f7e63921b80b5a3fa56b0dc
Mark Cornmesser <mcornmesser@mozilla.com>
2023-04-18 14:04:37 -0700

    hide start menu

--- modules/win_os_settings/manifests/hide_start_menu.pp
+++ modules/win_os_settings/manifests/hide_start_menu.pp
@@ -1,9 +1,9 @@
 class win_os_settings::hide_start_menu {
 
-    $explorer_key ="HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced"
+    $explorer_key ="HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartMenu"
     $start_pg_key ="${explorer_key}\\StartPage"
 
-    registry::value { 'HideRecommendedSect':
+    registry::value { 'NoSimpleStartMenu':
         key  => $explorer_key,
         type => dword,
         data => '1',
