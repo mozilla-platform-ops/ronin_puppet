a2addb47673db2545fe57e0035758b25b17a9986
Mark Cornmesser <mcornmesser@mozilla.com>
2023-04-18 13:12:28 -0700

    disable start menu

---
+++ modules/win_os_settings/manifests/hide_start_menu.pp
@@ -0,0 +1,8 @@
+class win_os_settings::hide_start_menu {
+
+    registry::value { 'Start_ShowClassicMode' :
+        key  => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
+        type => dword,
+        data => '1',
+    }
+}
