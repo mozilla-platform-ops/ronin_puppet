6620d6f222784ab5f4426f33515ce627401be39c
Mark Cornmesser <mcornmesser@mozilla.com>
2023-04-18 15:59:42 -0700

    hide start menu

--- modules/win_scheduled_tasks/files/maintainsystem.ps1
+++ modules/win_scheduled_tasks/files/maintainsystem.ps1
@@ -271,6 +271,11 @@ If ($bootstrap_stage -eq 'complete') {
   Run-MaintainSystem
   Puppet-Run
   Write-Log -message  ('{0} :: Puppet exited with {1}' -f $($MyInvocation.MyCommand.Name), ($LastExitCode)) -severity 'DEBUG'
+  ## Last catch if Puppet failed
+  if (($puppet_exit -ne 0) -and ($puppet_exit -ne 2)) {
+    shutdown @('-r', '-t', '0', '-c', 'Reboot; Puppet apply failed', '-f', '-d', '4:5')
+  }
+  Write-Log -message  ('{0} :: Disabling Start Menu' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
   ## Disable start menu. If shown can interfere with tests.
   while ($true) {
     $processname = "StartMenuExperienceHost"
