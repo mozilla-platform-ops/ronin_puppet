2c628a8abee4d5a35f62f9f2f778b729ace70b87
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-05-22 15:26:49 -0400

    rename

--- modules/roles_profiles/manifests/profiles/win_disable_dec22patch.pp
+++ modules/roles_profiles/manifests/profiles/win_disable_dec22patch.pp
@@ -3,6 +3,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Source: https://support.microsoft.com/en-gb/topic/kb5022083-change-in-how-wpf-based-applications-render-xps-documents-a4ae4fa4-bc58-4c37-acdd-5eebc4e34556
-class roles_profiles::profiles::disable_dec22patch {
+class roles_profiles::profiles::win_disable_dec22patch {
   include win_disable_dec22patch::disable
 }
