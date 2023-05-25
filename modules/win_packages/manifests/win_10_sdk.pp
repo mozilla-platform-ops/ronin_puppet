ac4a5a419930c0675c5e9c6127b89bb35884229d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-05-25 09:26:47 -0400

    Increase timeout to 600

--- modules/win_packages/manifests/win_10_sdk.pp
+++ modules/win_packages/manifests/win_10_sdk.pp
@@ -3,26 +3,26 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_packages::win_10_sdk {
+  $prog_dir = $facts['custom_win_programfilesx86']
+  $tools_dir = "${prog_dir}\\Microsoft Visual Studio\\Installer"
+  $vc_tools  = 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
+  $sdk       = 'ExtensionSDKs\Microsoft.Midi.GmDls\10.0.19041.0'
+  $sdk_dir   = "${prog_dir}\\Microsoft SDKs\\Windows Kits\\10\\ExtensionSDKs\\Microsoft.UniversalCRT.Debug\\10.0.19041.0"
 
-    $prog_dir = $facts['custom_win_programfilesx86']
-    $tools_dir = "${prog_dir}\\Microsoft Visual Studio\\Installer"
-    $vc_tools  = 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
-    $sdk       = 'ExtensionSDKs\Microsoft.Midi.GmDls\10.0.19041.0'
-    $sdk_dir   = "${prog_dir}\\Microsoft SDKs\\Windows Kits\\10\\ExtensionSDKs\\Microsoft.UniversalCRT.Debug\\10.0.19041.0"
-
-    if $facts['os']['name'] == 'Windows' {
-        #win_packages::win_exe_pkg  { 'vs_buildtools__1552942004.1623183462':
-            #pkg                    => 'vs_buildtools__1552942004.1623183462.exe',
-            #install_options_string => "--add ${vc_tools} --passive",
-            #creates                => "${tools_dir}\\NOTICE.txt",
-        #}
-        # This is the win 10 SDK. poor exe naming.
-        win_packages::win_exe_pkg  { 'winsdksetup':
-            pkg                    => 'winsdksetup.exe',
-            install_options_string => '/q /norestart',
-            creates                => "${sdk_dir}\\SDKManifest.xml",
-        }
-    } else {
-        fail("${module_name} does not support ${$facts['os']['name']}")
+  if $facts['os']['name'] == 'Windows' {
+    #win_packages::win_exe_pkg  { 'vs_buildtools__1552942004.1623183462':
+    #pkg                    => 'vs_buildtools__1552942004.1623183462.exe',
+    #install_options_string => "--add ${vc_tools} --passive",
+    #creates                => "${tools_dir}\\NOTICE.txt",
+    #}
+    # This is the win 10 SDK. poor exe naming.
+    win_packages::win_exe_pkg { 'winsdksetup':
+      pkg                    => 'winsdksetup.exe',
+      install_options_string => '/q /norestart',
+      creates                => "${sdk_dir}\\SDKManifest.xml",
+      timeout                => 600,
     }
+  } else {
+    fail("${module_name} does not support ${$facts['os']['name']}")
+  }
 }
