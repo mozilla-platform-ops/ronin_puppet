559eacc313f6a43e6d84aa39ac1623a8b76aef6d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-05-25 01:02:31 -0400

    fix path to script

--- modules/win_10_sdk/manifests/install.pp
+++ modules/win_10_sdk/manifests/install.pp
@@ -4,8 +4,8 @@
 
 class win_10_sdk::install {
   exec { 'win10-sdk':
-    command   => file('files/install.ps1'),
-    onlyif    => file('files/validate.ps1'),
+    command   => file('win_10_sdk/install.ps1'),
+    onlyif    => file('win_10_sdk/validate.ps1'),
     provider  => powershell,
     logoutput => true,
   }
