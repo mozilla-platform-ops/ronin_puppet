b115276b85324ceeabf0695bc55244fca3b72c32
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-06-27 13:32:39 -0400

    No longer needed

--- test/integration/windows11/disable_dec22patch.tests.ps1
+++
@@ -1,8 +0,0 @@
-Describe "Disable Dec22 Patch" {
-    It "XPSAllowedTypes should exist" {
-        Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\.NETFramework\Windows Presentation Foundation\XPSAllowedTypes\'| Should -Not -Be $null
-    }
-    It "DisableDec2022Patch should exist" {
-        Get-ItemPropertyValue "HKLM:\SOFTWARE\Microsoft\.NETFramework\Windows Presentation Foundation\XPSAllowedTypes" -Name "DisableDec2022Patch" | Should -Be '*'
-    }
-}
