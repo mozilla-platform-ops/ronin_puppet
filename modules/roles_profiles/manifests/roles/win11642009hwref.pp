2e7a600820ebdda9b0e34d31a0622e6a4514dbed
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-07-16 21:57:34 -0400

    add tc config

--- modules/roles_profiles/manifests/roles/win11642009hwref.pp
+++ modules/roles_profiles/manifests/roles/win11642009hwref.pp
@@ -30,5 +30,5 @@ class roles_profiles::roles::win11642009hwref {
   include roles_profiles::profiles::mozilla_build_tester
   #include roles_profiles::profiles::mozilla_build
   include roles_profiles::profiles::mozilla_maintenance_service
-  #include roles_profiles::profiles::windows_worker_runner
+  include roles_profiles::profiles::windows_worker_runner
 }
