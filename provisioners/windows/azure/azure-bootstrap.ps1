403d4c61922bc3178489d01b739ff664af13542f
Mark Cornmesser <mcornmesser@mozilla.com>
2023-07-19 16:49:11 -0700

    revert update

--- provisioners/windows/azure/azure-bootstrap.ps1
+++ provisioners/windows/azure/azure-bootstrap.ps1
@@ -117,7 +117,6 @@ function Install-AzPrerequ {
         [string] $local_dir = "$env:systemdrive\BootStrap",
         [string] $work_dir = "$env:systemdrive\scratch",
         [string] $git = "Git-2.36.1-64-bit.exe",
-#        [string] $puppet = "puppet-agent-7.24.0-x64.msi",
         [string] $puppet = "puppet-agent-6.0.0-x64.msi",
         [string] $vault_file = "azure_vault_template.yaml",
         [string] $rdagent = "rdagent",
@@ -127,7 +126,7 @@ function Install-AzPrerequ {
         [string] $ps_ver_min = $PSVersionTable.PSVersion.Minor,
         [string] $ps_ver = ('{0}.{1}' -f $ps_ver_maj,$ps_ver_min),
         [string] $wmf_5_1 ="Win8.1AndW2K12R2-KB3191564-x64.msu",
-        [string] $bootzip = "BootStrap_Azure_07-2023.zip"
+        [string] $bootzip = "BootStrap_Azure_07-2022.zip"
     )
     begin {
         Write-Log -message ('{0} :: begin - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
