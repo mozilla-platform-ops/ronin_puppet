b6c89351bc625fd11238f32ba34cdd18b2f11427
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-07-19 21:57:39 -0400

    Checking for git bombs out when not installed, check for path instead

--- modules/win_shared/facts.d/facts_win_other_apps.ps1
+++ modules/win_shared/facts.d/facts_win_other_apps.ps1
@@ -2,9 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-$git = Get-Command "git.exe"
+$git = Test-Path "$ENV:ProgramFiles\Git\bin\git.exe"
 if ($git) {
-	$git_ver = "{0}.{1}.{2}" -f $git.Version.Major,$git.Version.Minor,$git.Version.Build
+    $git_check = Get-Command "git.exe"
+	$git_ver = "{0}.{1}.{2}" -f $git_check.Version.Major,$git_check.Version.Minor,$git_check.Version.Build
 }
 else {
 	$git_ver = 0.0.0
