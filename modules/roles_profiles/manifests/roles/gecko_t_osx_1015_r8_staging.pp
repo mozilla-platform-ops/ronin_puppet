5252e757f1bbcf7a9822215d134cc30d3f3f3f64
Ryan Curran <rcurran@mozilla.com>
2023-07-27 14:26:09 -0400

    Testing fix for bluetooth dialog

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1015_r8_staging.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1015_r8_staging.pp
@@ -24,4 +24,5 @@ class roles_profiles::roles::gecko_t_osx_1015_r8_staging {
     include ::roles_profiles::profiles::metrics
     include ::roles_profiles::profiles::worker
     include ::roles_profiles::profiles::safaridriver
+    include macos_utils::disable_bluetooth_setup
 }
