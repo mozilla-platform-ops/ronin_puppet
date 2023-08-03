3d320f6368193eed29a853707c5e254c5b8c459b
Mark Cornmesser <mcornmesser@mozilla.com>
2023-08-03 14:18:01 -0700

    set up ram disk on workers

--- modules/win_scheduled_tasks/files/2022-azure-maintainsystem.ps1
+++ modules/win_scheduled_tasks/files/2022-azure-maintainsystem.ps1
@@ -265,51 +265,13 @@ function LinkZY2D {
         if ((Test-VolumeExists -DriveLetter 'D') -and (-not (Test-VolumeExists -DriveLetter 'Y'))) {
             subst Y: D:\
         }
-        if ((Test-VolumeExists -DriveLetter 'D') -and (-not (Test-VolumeExists -DriveLetter 'Z'))) {
-            subst Z: D:\
-        }
+        #if ((Test-VolumeExists -DriveLetter 'D') -and (-not (Test-VolumeExists -DriveLetter 'Z'))) {
+        #    subst Z: D:\
+        #}
     }
     end {
         Write-Log -message ('{0} :: end - {1:o}' -f $($MyInvocation.MyCommand.Name), (Get-Date).ToUniversalTime()) -severity 'DEBUG'
     }
-}
-function Write-Log {
-  param (
-    [string] $message,
-    [string] $severity = 'INFO',
-    [string] $source = 'MaintainSystem',
-    [string] $logName = 'Application'
-  )
-  if (!([Diagnostics.EventLog]::Exists($logName)) -or !([Diagnostics.EventLog]::SourceExists($source))) {
-    New-EventLog -LogName $logName -Source $source
-  }
-  switch ($severity) {
-    'DEBUG' {
-      $entryType = 'SuccessAudit'
-      $eventId = 2
-      break
-    }
-    'WARN' {
-      $entryType = 'Warning'
-      $eventId = 3
-      break
-    }
-    'ERROR' {
-      $entryType = 'Error'
-      $eventId = 4
-      break
-    }
-    default {
-      $entryType = 'Information'
-      $eventId = 1
-      break
-    }
-  }
-  Write-EventLog -LogName $logName -Source $source -EntryType $entryType -Category 0 -EventID $eventId -Message $message
-  if ([Environment]::UserInteractive) {
-    $fc = @{ 'Information' = 'White'; 'Error' = 'Red'; 'Warning' = 'DarkYellow'; 'SuccessAudit' = 'DarkGray' }[$entryType]
-    Write-Host -object $message -ForegroundColor $fc
-  }
 }
 function Create-RAMDisk {
     param (
