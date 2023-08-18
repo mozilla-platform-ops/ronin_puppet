10ef49d9a78daf9b086384ac09091cf83c0f2a69
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-08-18 13:21:36 -0400

    Formatting

--- modules/roles_profiles/manifests/roles/win10642009azure.pp
+++ modules/roles_profiles/manifests/roles/win10642009azure.pp
@@ -20,7 +20,7 @@ class roles_profiles::roles::win10642009azure {
   ## *_drivers depends on 7zip which isn't installed until common_tools
   include roles_profiles::profiles::virtual_drivers
   include roles_profiles::profiles::gpu_drivers
-  
+
   # Adminstration
   include roles_profiles::profiles::logging
   include roles_profiles::profiles::common_tools
--- test/integration/windows11/mozilla_build.tests.ps1
+++ test/integration/windows11/mozilla_build.tests.ps1
@@ -34,23 +34,23 @@ Describe "Mozilla Build" {
     }
     Context "Pip" {
         It "Certifi is installed" {
-            $certifi = ($pip_packages | Where-Object {$psitem -Match "Certifi"}) -split "==" 
+            $certifi = ($pip_packages | Where-Object {$psitem -Match "Certifi"}) -split "=="
             $certifi | Should -Not -Be $null
         }
         It "PSUtil is installed" {
-            $PSUtil = ($pip_packages | Where-Object {$psitem -Match "PSUtil"}) -split "==" 
+            $PSUtil = ($pip_packages | Where-Object {$psitem -Match "PSUtil"}) -split "=="
             $PSUtil | Should -Not -Be $null
         }
         It "PSUtil version 5.9.4" {
-            $PSUtil = ($pip_packages | Where-Object {$psitem -Match "PSUtil"}) -split "==" 
+            $PSUtil = ($pip_packages | Where-Object {$psitem -Match "PSUtil"}) -split "=="
             $PSUtil[1] | Should -Not -Be $null
         }
         It "ZStandard is installed" {
-            $ZStandard = ($pip_packages | Where-Object {$psitem -Match "zstandard"}) -split "==" 
+            $ZStandard = ($pip_packages | Where-Object {$psitem -Match "zstandard"}) -split "=="
             $ZStandard | Should -Not -Be $null
         }
         It "ZStandard version 0.15.2" {
-            $ZStandard = ($pip_packages | Where-Object {$psitem -Match "zstandard"}) -split "==" 
+            $ZStandard = ($pip_packages | Where-Object {$psitem -Match "zstandard"}) -split "=="
             $ZStandard[1] | Should -Be "0.15.2"
         }
     }
