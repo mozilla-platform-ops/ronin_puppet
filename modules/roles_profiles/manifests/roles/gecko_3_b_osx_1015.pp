2fc59fe5359aa68d0641ec48bd21f50bfb3bdd2a
Michelle <MasterWayZ@users.noreply.github.com>
2023-10-25 12:58:21 +0200

    Move level 1 and level 3 builders to the same module as testers for easier management (#572)

--- modules/roles_profiles/manifests/roles/gecko_3_b_osx_1015.pp
+++ modules/roles_profiles/manifests/roles/gecko_3_b_osx_1015.pp
@@ -7,22 +7,21 @@ class roles_profiles::roles::gecko_3_b_osx_1015 {
     include ::roles_profiles::profiles::timezone
     include ::roles_profiles::profiles::ntp
     include ::roles_profiles::profiles::network
-    include ::roles_profiles::profiles::disable_services
+    # include ::roles_profiles::profiles::disable_services
     include ::roles_profiles::profiles::vnc
-    include ::roles_profiles::profiles::suppress_dialog_boxes
-    include ::roles_profiles::profiles::power_management
-    include ::roles_profiles::profiles::screensaver
-    include ::roles_profiles::profiles::gui
+    # include ::roles_profiles::profiles::suppress_dialog_boxes
+    # include ::roles_profiles::profiles::power_management
+    # include ::roles_profiles::profiles::screensaver
+    # include ::roles_profiles::profiles::gui
     include ::roles_profiles::profiles::sudo
-    include ::roles_profiles::profiles::software_updates
-    include ::roles_profiles::profiles::hardware
+    # include ::roles_profiles::profiles::software_updates
+    # include ::roles_profiles::profiles::hardware
     include ::roles_profiles::profiles::motd
     include ::roles_profiles::profiles::users
-    #include ::roles_profiles::profiles::cltbld_user
+    include ::roles_profiles::profiles::cltbld_user
     include ::roles_profiles::profiles::relops_users
-    include ::roles_profiles::profiles::homebrew
 
-    include ::roles_profiles::profiles::gecko_3_b_osx_1015_generic_worker
+    include ::roles_profiles::profiles::worker
     #include ::fw::roles::osx_taskcluster_worker_loaner
 
     #include ::macos_utils::uninstall_homebrew
