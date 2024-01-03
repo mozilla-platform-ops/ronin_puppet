38b2e995e3d46592ee8275e7dfcbb7b0933af472
Mark Cornmesser <mcornmesser@mozilla.com>
2024-01-03 10:48:25 -0800

    initial MDC1Windows commit

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -1,1 +1,2 @@
-write-host New-PSDrive -Name Z -PSProvider FileSystem -Root $sharePath -Credential $cred -Persist
+$credential = New-Object System.Management.Automation.PSCredential($env:deployuser, $env:deploymentaccess)
+write-host New-PSDrive -Name Z -PSProvider FileSystem -Root //mdt2022.ad.mozilla.com/deployments  -Credential -Credential $credential -Persist
