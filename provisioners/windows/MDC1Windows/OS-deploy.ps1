4be9c1f3a6d886a12811aeb84bb492463bd2e44b
Mark Cornmesser <mcornmesser@mozilla.com>
2024-01-03 16:23:53 -0800

    Building OS deploy script

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -13,6 +13,7 @@ $credential = New-Object System.Management.Automation.PSCredential($deployuser, 
 net use Z: \\mdt2022.ad.mozilla.com\deployments /user:$deployuser $deploymentaccess /persistent:yes
 
 ## Get node name
+Import-Module "X:\Windows\System32\WindowsPowerShell\v1.0\Modules\DnsClient"
 $Ethernet = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | Where-Object {$_.name -match "ethernet"}
 $IPAddress = ($Ethernet.GetIPProperties().UnicastAddresses.Address | Where-object {$_.AddressFamily -eq "InterNetwork"}).IPAddressToString
 $ResolvedName = (Resolve-DnsName -Name $IPAddress -Server "10.48.75.120").NameHost
