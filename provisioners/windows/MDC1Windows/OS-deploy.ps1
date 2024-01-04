769427f40d57f94e607341328c509a4780619ac9
Mark Cornmesser <mcornmesser@mozilla.com>
2024-01-03 16:20:41 -0800

    Building OS deploy script

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -3,8 +3,19 @@ param(
     [string]$deploymentaccess
 )
 
+## Mount Deployment share
+## PSDrive is will unmount when the Powershell sessions ends. Ultimately maybe OK.
+## net use will presist
 $deploypw = ConvertTo-SecureString -String $deploymentaccess -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($deployuser, $deploypw)
 #New-PSDrive -Name Z -PSProvider FileSystem -Root \\mdt2022.ad.mozilla.com\deployments  -Credential $credential -Persist
 
 net use Z: \\mdt2022.ad.mozilla.com\deployments /user:$deployuser $deploymentaccess /persistent:yes
+
+## Get node name
+$Ethernet = [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | Where-Object {$_.name -match "ethernet"}
+$IPAddress = ($Ethernet.GetIPProperties().UnicastAddresses.Address | Where-object {$_.AddressFamily -eq "InterNetwork"}).IPAddressToString
+$ResolvedName = (Resolve-DnsName -Name $IPAddress -Server "10.48.75.120").NameHost
+
+Write-Host $IPAddress
+Write-Host $ResolvedName
