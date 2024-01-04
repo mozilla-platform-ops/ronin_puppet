d8ca0820a6367c8986c6e283237fa50dcca42c03
Mark Cornmesser <mcornmesser@mozilla.com>
2024-01-03 16:07:54 -0800

    initial MDC1Windows commit

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -5,4 +5,6 @@ param(
 
 $deploypw = ConvertTo-SecureString -String $deploymentaccess -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($deployuser, $deploypw)
-New-PSDrive -Name Z -PSProvider FileSystem -Root \\mdt2022.ad.mozilla.com\deployments  -Credential $credential -Persist
+#New-PSDrive -Name Z -PSProvider FileSystem -Root \\mdt2022.ad.mozilla.com\deployments  -Credential $credential -Persist
+
+net use Z: \\mdt2022.ad.mozilla.com\deployments /user:$deployuser $deploypw /persistent:yes
