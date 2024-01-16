216934d5385d6eebe3650fe3093023cf629748a2
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-01-16 11:34:52 -0500

    restore

--- modules/roles_profiles/manifests/profiles/microsoft_store.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_store.pp
@@ -1,3 +1,11 @@
 class roles_profiles::profiles::microsoft_store {
-  include microsoft_store::av1
+  case $facts['os']['name'] {
+    'Windows': {
+      include microsoft_store::init
+      include microsoft_store::av1
+    }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
