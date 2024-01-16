e9da8ad99d8fa052bcc99815edb5fdead50f54d1
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-01-16 11:33:29 -0500

    Move stuff around

--- modules/roles_profiles/manifests/profiles/microsoft_store.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_store.pp
@@ -1,11 +1,3 @@
 class roles_profiles::profiles::microsoft_store {
-  case $facts['os']['name'] {
-    'Windows': {
-      include microsoft_store::init
-      include microsoft_store::av1
-    }
-    default: {
-      fail("${$facts['os']['name']} not supported")
-    }
-  }
+  include microsoft_store::av1
 }
