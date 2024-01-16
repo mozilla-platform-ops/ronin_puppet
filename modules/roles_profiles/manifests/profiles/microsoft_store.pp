f477a00c83c504b5c2319a252cccf0ea80b666aa
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-01-16 15:52:17 -0500

    Add support for av1 extension (#605)
    * Install av1 store extension
    
    * Skip nuc dns name
    
    * Add init title
    
    * Add to role
    
    * Move stuff around
    
    * restore
    
    * Updates
    
    * Fix name
    
    * fix
    
    * Add support for exiting
    
    * Swap exit codes
    
    * Restore to original contents
    
    * Remove alpha role

---
+++ modules/roles_profiles/manifests/profiles/microsoft_store.pp
@@ -0,0 +1,11 @@
+class roles_profiles::profiles::microsoft_store {
+  case $facts['os']['name'] {
+    'Windows': {
+      #include microsoft_store::init
+      include microsoft_store::av1
+    }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
+}
