928c5996c614e02c965a8ba65d13835b27e9b40e
Mark Cornmesser <mcornmesser@mozilla.com>
2024-01-23 12:20:44 -0800

    build deploy script

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -26,18 +26,19 @@ if ($partitions.Count -eq 0) {
     Write-Host "Local Install Partition is $local_files_size MB"
 
     $diskPartScript = @"
-        select disk 0,
-        clean,
-        convert gpt,
-        create partition efi size=100,
-        format fs=fat32 label=EFI,
-        assign letter=S,
-        create partition msr size=16,
-        create partition primary size=$primary_size,
-        format fs=ntfs quick,
-        assign letter=C,
-        create partition primary size=20480,
+        select disk 0
+        clean
+        convert gpt
+        create partition efi size=100
+        format fs=fat32 label=EFI
+        assign letter=S
+        create partition msr size=16
+        create partition primary size=$primary_size
+        format fs=ntfs quick
+        assign letter=C
+        create partition primary size=20480
         format fs=ntfs quick label=Y
+        exit
 "@
 
     $diskPartScript | Out-File -FilePath "$env:TEMP\diskpart_script.txt" -Encoding ASCII
