fb179c562797a9564f2bfa9e645c04dcb30419cc
Mark Cornmesser <mcornmesser@mozilla.com>
2024-01-24 14:53:17 -0800

    build deploy script

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -156,4 +156,4 @@ if (!(Test-Path $setup)) {
 dir $local_install
 Set-Location -Path $OS_files
 write-host Start-Process -FilePath $setup -ArgumentList "/unattend:autounattend.xml"
-Start-Process -FilePath $setup -ArgumentList "/unattend:autounattend.xml", "/imageindex:5"
+Start-Process -FilePath $setup -ArgumentList "/unattend:autounattend.xml", "/imageindex 5"
