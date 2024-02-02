c07c29b70d6627f3ad5e2947edf3a382d89c67e9
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-02-02 10:56:27 -0500

    Disable wu again

--- modules/win_disable_services/files/disable_wu.ps1
+++ modules/win_disable_services/files/disable_wu.ps1
@@ -11,6 +11,9 @@ $servicesToDisable.WaitForStatus('Stopped', "00:02:00")
 $servicesToDisable | Set-Service -StartupType Disabled
 
 ## check if wuauserv is disabled, and if not, disable it again
-if ((Get-Service wuauserv).StartType -ne "Disabled") {
+if ((Get-Service "wuauserv").StartType -ne "Disabled") {
+    if ((Get-Service "wuauserv").Status -ne "Stopped") {
+        Stop-Service "wuauserv" -Force
+    }
     Get-Service wuauserv | Set-Service -StartupType Disabled
 }
