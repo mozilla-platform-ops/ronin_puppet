e4128a86afc740085b38ce6d9e7555dd281e6f8f
Mark Cornmesser <mcornmesser@mozilla.com>
2024-02-09 00:13:25 -0800

    build deploy

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -211,7 +211,7 @@ if (!(Test-Path $setup)) {
         $content2 = $content2 -replace $replacement.OldString, $replacement.NewString
     }
 
-    Set-Content -Path $unattend -Value $content
+    Set-Content -Path $unattend -Value $content2
 
 }
 
