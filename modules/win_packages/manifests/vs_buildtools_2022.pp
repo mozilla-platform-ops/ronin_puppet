713f14a2edb126670eaf37709b10c31e8570c71a
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-02-09 10:56:12 -0500

    Fix case sensitivity

--- modules/win_packages/manifests/vs_buildtools_2022.pp
+++ modules/win_packages/manifests/vs_buildtools_2022.pp
@@ -3,17 +3,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_packages::vs_buildtools_2022 {
+  $prog_dir = $facts['custom_win_programfilesx86']
+  $tools_dir = "${prog_dir}\\Microsoft Visual Studio\\Installer"
 
-    $prog_dir = $facts['custom_win_programfilesx86']
-    $tools_dir = "${prog_dir}\\Microsoft Visual Studio\\Installer"
-
-    if $facts['os']['name'] == 'Windows' {
-        win_packages::win_exe_pkg  { 'vs_buildtools_2022':
-            pkg                    => 'vs_buildtools_2022.exe',
-            install_options_string => '--all --passive --norestart',
-            creates                => "${tools_dir}\\NOTICE.txt",
-        }
-    } else {
-        fail("${module_name} does not support ${$facts['os']['name']}")
+  if $facts['os']['name'] == 'Windows' {
+    win_packages::win_exe_pkg { 'vs_BuildTools_2022':
+      pkg                    => 'vs_BuildTools_2022.exe',
+      install_options_string => '--all --passive --norestart',
+      creates                => "${tools_dir}\\NOTICE.txt",
     }
+  } else {
+    fail("${module_name} does not support ${$facts['os']['name']}")
+  }
 }
