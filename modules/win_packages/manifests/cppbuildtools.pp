a3f6b660bc50b869e5143973e3ebe3a532bd6153
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-02-27 17:11:19 -0500

    Fix legacy fact operatingsystem in known places

--- modules/win_packages/manifests/cppbuildtools.pp
+++ modules/win_packages/manifests/cppbuildtools.pp
@@ -3,18 +3,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_packages::cppbuildtools {
+  # reduce path length so it may pass linting
+  $sdk_dir = "${facts['custom_win_programfilesx86']}\\Microsoft SDKs"
 
-    # reduce path length so it may pass linting
-    $sdk_dir = "${facts['custom_win_programfilesx86']}\\Microsoft SDKs"
-
-    if $::operatingsystem == 'Windows' {
-        win_packages::win_exe_pkg  { 'visualcppbuildtools_full2015':
-            pkg                    => 'visualcppbuildtools_full.exe',
-            install_options_string => '/q',
-            creates                =>
-                "${sdk_dir}\\Windows Kits\\10\\ExtensionSDKs\\Microsoft.UniversalCRT.Debug\\10.0.10240.0\\SDKManifest.xml",
-        }
-    } else {
-        fail("${module_name} does not support ${::operatingsystem}")
+  if $facts['os']['name'] == 'Windows' {
+    win_packages::win_exe_pkg { 'visualcppbuildtools_full2015':
+      pkg                    => 'visualcppbuildtools_full.exe',
+      install_options_string => '/q',
+      creates                => "${sdk_dir}\\Windows Kits\\10\\ExtensionSDKs\\Microsoft.UniversalCRT.Debug\\10.0.10240.0\\SDKManifest.xml",
     }
+  } else {
+    fail("${module_name} does not support ${facts['os']['name']}")
+  }
 }
