18841fbe661aa4112841b75277370f0b1cf864ce
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2024-02-28 14:17:20 -0500

    Revamp cltbld user creation process for macOS 13 (#619)
    * Stage 1
    
    * Removed pre-commit 140 char check
    
    * auto login for cltbld is still broken
    
    * Commiting before cleanup
    
    * Post cleanup
    
    * fixed bug
    
    * Resolved aericksons comments
    
    * fixed typo
    
    * add unhashedpassword to int test
    
    * add unhashedpassword value for ci tests
    
    * fix integration tests
    
    * Another one
    
    * fix failed integration test
    
    * rolling back commit
    
    * rolling back commit
    
    * revert commit
    
    * reverting commit

--- modules/roles_profiles/manifests/profiles/cltbld_user.pp
+++ modules/roles_profiles/manifests/profiles/cltbld_user.pp
@@ -3,107 +3,132 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::cltbld_user {
-    case $::operatingsystem {
-        'Darwin': {
-            $password     = lookup('cltbld_user.password')
-            $salt         = lookup('cltbld_user.salt')
-            $iterations   = lookup('cltbld_user.iterations')
-            $kcpassword   = lookup('cltbld_user.kcpassword')
+  case $facts['os']['name'] {
+    'Darwin': {
+      $account_username = 'cltbld'
+      $password     = lookup('cltbld_user.password')
+      $password_unhashed    = lookup('cltbld_user.unhashedpassword')
+      $salt         = lookup('cltbld_user.salt')
+      $iterations   = lookup('cltbld_user.iterations')
+      $kcpassword   = lookup('cltbld_user.kcpassword')
+      $password_hash = inline_template("<%= IO.popen(['openssl', 'passwd', '-6', '-salt', '${salt}', '-6', '-rounds', '${iterations}', '${password}']).read.chomp %>")
 
-            # Create the cltbld user
-            users::single_user { 'cltbld':
-                # Bug 1122875 - cltld needs to be in this group for debug tests
-                password   => $password,
-                salt       => $salt,
-                iterations => $iterations,
-            }
-
-            # Monkey patching directoryservice.rb in order to create users also breaks group merging
-            # So we directly add the user to the group(s)
-            $groups = ['_developer','com.apple.access_screensharing','com.apple.access_ssh']
-            $groups.each |String $group| {
-                exec { "cltbld_group_${group}":
-                    command => "/usr/bin/dscl . -append /Groups/${group} GroupMembership cltbld",
-                    unless  => "/usr/bin/groups cltbld | /usr/bin/grep -q -w ${group}",
-                    require => User['cltbld'],
-                }
-            }
-
-            # Set user to autologin
-            class { 'macos_utils::autologin_user':
-                user       => 'cltbld',
-                kcpassword => $kcpassword,
-            }
-
-            # Ensure the AuthenticationAuthority hash is set.  Puppet does not set this when creating a user.  Needed for GUI/VNC access.
-            $auth_key_hash = '\';ShadowHash;HASHLIST:<SALTED-SHA512-PBKDF2,SRP-RFC5054-4096-SHA512-PBKDF2,SMB-NT>\''
-            exec { 'cltbld_auth_key':
-                command =>  "/usr/bin/dscl . -create /Users/cltbld AuthenticationAuthority ${auth_key_hash}",
-                unless  =>  "/usr/bin/dscl . -read /Users/cltbld AuthenticationAuthority| grep -q -w ${auth_key_hash}",
-                require => User['cltbld'],
-            }
-
-            # Enable DevToolsSecurity
-            include macos_utils::enable_dev_tools_security
-
-            macos_utils::clean_appstate { 'cltbld':
-                user  => 'cltbld',
-                group => 'staff',
-            }
-
-            mercurial::hgrc { '/Users/cltbld/.hgrc':
-                user    => 'cltbld',
-                group   => 'staff',
-                require => User['cltbld'],
-            }
-
-            file { '/Users/cltbld/Library/LaunchAgents':
-                ensure => directory,
-            }
-
-            $sudo_commands = ['/sbin/reboot']
-            $sudo_commands.each |String $command| {
-                sudo::custom { "allow_cltbld_${command}":
-                    user    => 'cltbld',
-                    command => $command,
-                }
-            }
+      # Create the cltbld user
+      case $facts['os']['release']['major'] {
+        '19', '20': {
+          users::single_user { 'cltbld':
+            # Bug 1122875 - cltld needs to be in this group for debug tests
+            password   => $password,
+            salt       => $salt,
+            iterations => $iterations,
+          }
+          # Set user to autologin
+          class { 'macos_utils::autologin_user':
+            user       => 'cltbld',
+            kcpassword => $kcpassword,
+          }
+          macos_utils::clean_appstate { 'cltbld':
+            user  => 'cltbld',
+            group => 'staff',
+          }
+          file { '/Users/cltbld/Library/LaunchAgents':
+            ensure => directory,
+          }
         }
-        'Ubuntu': {
-            $password     = lookup('cltbld_user.password')
-            $salt         = lookup('cltbld_user.salt')
-            $iterations   = lookup('cltbld_user.iterations')
-
-            $username     = 'cltbld'
-            $group        = 'cltbld'
-            $homedir      = '/home/cltbld'
-
-            group { 'cltbld':
-                name      => 'cltbld'
-            }
-
-            # Create the cltbld user
-            users::single_user { 'cltbld':
-                # Bug 1122875 - cltld needs to be in this group for debug tests
-                password   => $password,
-                salt       => $salt,
-                iterations => $iterations,
-                groups     => ['audio','video']
-            }
-
-            # conflicts with /etc/mercurial/hgrc
-            file {
-                '/home/cltbld/.hgrc':
-                    ensure => absent;
-            }
-
-            sudo::custom { 'allow_cltbld_reboot':
-                user    => 'cltbld',
-                command => '/sbin/reboot',
-            }
+        '22', '23': {
+          # Account creation on macOS 13+
+          exec { 'create_home_directory':
+            command => "/bin/mkdir -p /Users/${account_username}",
+            unless  => "/bin/test -d /Users/${account_username}",
+          }
+          exec { 'create_macos_user':
+            command => "/usr/sbin/sysadminctl -addUser ${account_username} -fullName '${account_username}' -UID 35 -password '${password_hash}' -home /Users/${account_username}",
+            unless  => '/usr/bin/dscl . -read /Users/cltbld',
+          }
+          class { 'macos_utils::autologin_user':
+            user       => $account_username,
+            kcpassword => $password_unhashed,
+          }
+          macos_utils::clean_appstate2_13_plus { 'cltbld':
+            user  => 'cltbld',
+            group => 'staff',
+          }
         }
         default: {
-            fail("${::operatingsystem} not supported")
+          fail("${facts['os']['release']} not supported")
         }
+      }
+
+      #   Monkey patching directoryservice.rb in order to create users also breaks group merging
+      #   So we directly add the user to the group(s)
+      $groups = ['_developer','com.apple.access_screensharing','com.apple.access_ssh']
+      $groups.each |String $group| {
+        exec { "cltbld_group_${group}":
+          command => "/usr/bin/dscl . -append /Groups/${group} GroupMembership cltbld",
+          unless  => "/usr/bin/groups cltbld | /usr/bin/grep -q -w ${group}",
+          #require => User['cltbld'],
+        }
+      }
+      # Ensure the AuthenticationAuthority hash is set.  Puppet does not set this when creating a user.  Needed for GUI/VNC access.
+      $auth_key_hash = '\';ShadowHash;HASHLIST:<SALTED-SHA512-PBKDF2,SRP-RFC5054-4096-SHA512-PBKDF2,SMB-NT>\''
+      exec { 'cltbld_auth_key':
+        command => "/usr/bin/dscl . -create /Users/cltbld AuthenticationAuthority ${auth_key_hash}",
+        unless  => "/usr/bin/dscl . -read /Users/cltbld AuthenticationAuthority| grep -q -w ${auth_key_hash}",
+        #require => User['cltbld'],
+      }
+
+      # Enable DevToolsSecurity
+      include macos_utils::enable_dev_tools_security
+
+      mercurial::hgrc { '/Users/cltbld/.hgrc':
+        user  => 'cltbld',
+        group => 'staff',
+        # require => User['cltbld'],
+      }
+
+      $sudo_commands = ['/sbin/reboot']
+      $sudo_commands.each |String $command| {
+        sudo::custom { "allow_cltbld_${command}":
+          user    => 'cltbld',
+          command => $command,
+        }
+      }
     }
+    'Ubuntu': {
+      $password     = lookup('cltbld_user.password')
+      $salt         = lookup('cltbld_user.salt')
+      $iterations   = lookup('cltbld_user.iterations')
+
+      $username     = 'cltbld'
+      $group        = 'cltbld'
+      $homedir      = '/home/cltbld'
+
+      group { 'cltbld':
+        name      => 'cltbld',
+      }
+
+      # Create the cltbld user
+      users::single_user { 'cltbld':
+        # Bug 1122875 - cltld needs to be in this group for debug tests
+        password   => $password,
+        salt       => $salt,
+        iterations => $iterations,
+        groups     => ['audio','video'],
+      }
+
+      # conflicts with /etc/mercurial/hgrc
+      file {
+        '/home/cltbld/.hgrc':
+          ensure => absent;
+      }
+
+      sudo::custom { 'allow_cltbld_reboot':
+        user    => 'cltbld',
+        command => '/sbin/reboot',
+      }
+    }
+    default: {
+      fail("${facts['os']['name']} not supported")
+    }
+  }
 }
