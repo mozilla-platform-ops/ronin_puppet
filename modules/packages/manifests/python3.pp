fb77e698665a90e0749dfd59e68732a082457352
Ryan Curran <rcurran@mozilla.com>
2024-02-29 14:57:05 -0500

    revert python change

--- modules/packages/manifests/python3.pp
+++ modules/packages/manifests/python3.pp
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::python3 (
-  $version = '3.11.0',
+  Pattern[/^\d+\.\d+\.\d+_?\d*$/] $version = '3.7.4',
 ) {
 #    # As of puppet 7.0.0, facts.os.architecture still reports the M1 arm64 hardware as x86_64
 #    # therfore, we check the mac model instead
@@ -12,8 +12,7 @@ class packages::python3 (
 #    } else {
 #        $pkg_name = "python-${version}-macosx10.9.pkg"
 #    }
-  # Unsure why this works
-  $short_version = String($version)[0,4]
+
   $pkg_name = "python-${version}-macosx10.9.pkg"
   packages::macos_package_from_s3 { $pkg_name:
     private             => false,
@@ -24,9 +23,9 @@ class packages::python3 (
   # Install certifi's set of CAs to override the system set
   exec {
     'install python3 certs':
-      command => "\"/Applications/Python ${short_version}/Install Certificates.command\"",
+      command => "\"/Applications/Python ${version[0,3]}/Install Certificates.command\"",
       path    => ['/usr/bin', '/usr/sbin', '/bin'],
-      unless  => "test -L /Library/Frameworks/Python.framework/Versions/${short_version}/etc/openssl/cert.pem",
+      unless  => "test -L /Library/Frameworks/Python.framework/Versions/${version[0,3]}/etc/openssl/cert.pem",
       require => Packages::Macos_package_from_s3[$pkg_name],
   }
 }
