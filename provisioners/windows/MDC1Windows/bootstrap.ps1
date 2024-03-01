2dce0e9542512f70129c39bc2ab556084decf007
Mark Cornmesser <mcornmesser@mozilla.com>
2024-03-01 13:55:09 -0800

    build bootstrap

--- provisioners/windows/MDC1Windows/bootstrap.ps1
+++ provisioners/windows/MDC1Windows/bootstrap.ps1
@@ -124,7 +124,7 @@ function Get-PSModules {
         $nugetProvider = Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue
         if ($nugetProvider -eq $null) {
            Write-Log -message  ('{0} :: Installing NuGet Package Provider' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-           Install-PackageProvider -Name NuGet -Force
+           Install-PackageProvider -Name NuGet -Force -Confirm:$false
         } else {
            Write-Log -message  ('{0} :: NuGet is present.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
         }
