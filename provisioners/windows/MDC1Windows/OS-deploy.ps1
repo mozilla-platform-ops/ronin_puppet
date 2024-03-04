704ea0089b35eecca07e683bfb42cee297d50bc9
Mark Cornmesser <mcornmesser@mozilla.com>
2024-03-04 15:22:47 -0800

    set up kms key

--- provisioners/windows/MDC1Windows/OS-deploy.ps1
+++ provisioners/windows/MDC1Windows/OS-deploy.ps1
@@ -206,7 +206,7 @@ if (!(Test-Path $setup)) {
     Write-Host "Updating autounattend.xml."
 
     $replacetheses = @(
-        @{ OldString = "THIS-IS-A-NAME"; NewString = $shortname },
+        @{ OldString = "THIS-IS-A-NAME"; NewString = $ResolvedName },
         @{ OldString = "NotARealPassword"; NewString = $secret_YAML.win_adminpw }
 )
 
