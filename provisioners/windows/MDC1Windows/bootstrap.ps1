7da40a8dae8da145f383ea3233d2d066f1091df6
Mark Cornmesser <mcornmesser@mozilla.com>
2024-03-06 11:05:55 -0800

    set up kms key

--- provisioners/windows/MDC1Windows/bootstrap.ps1
+++ provisioners/windows/MDC1Windows/bootstrap.ps1
@@ -477,7 +477,7 @@ function Set-PXE {
 			bcdedit /set "{fwbootmgr}" bootsequence "$GUID"
 
 			Write-Log -message  ('{0} :: Device will PXE boot. Restarting' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-			Restart-Computer -Comment "Bootstrapping failed. Redeploying OS." -Force
+			Restart-Computer -Force
 		}
 		Catch {
 			Write-Log -message  ('{0} :: Unable to set next boot to PXE. Exiting!' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
