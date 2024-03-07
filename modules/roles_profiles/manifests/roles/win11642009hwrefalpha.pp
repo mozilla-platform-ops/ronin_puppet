88c96cce9db74aaa8dbb8aaee12c01497b656230
Mark Cornmesser <mcornmesser@mozilla.com>
2024-03-06 16:02:08 -0800

    bootstrap

--- modules/roles_profiles/manifests/roles/win11642009hwrefalpha.pp
+++ modules/roles_profiles/manifests/roles/win11642009hwrefalpha.pp
@@ -6,7 +6,7 @@ class roles_profiles::roles::win11642009hwrefalpha {
   # Install MS tools earlier
   include roles_profiles::profiles::microsoft_tools
   # System
-  include roles_profiles::profiles::disable_services1
+  include roles_profiles::profiles::disable_services
   include roles_profiles::profiles::error_reporting
   include roles_profiles::profiles::suppress_dialog_boxes
   include roles_profiles::profiles::files_system_managment
