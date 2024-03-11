9b364886f088e79e4e43cfcdbffbda9539afb0d9
Mark Cornmesser <mcornmesser@mozilla.com>
2024-03-11 12:42:02 -0700

    testing Azcopy

--- modules/win_packages/manifests/azcopy_script.pp
+++ modules/win_packages/manifests/azcopy_script.pp
@@ -2,11 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define win_packages::azcopy (
-    String $pkg,
-    String $package=$title,
-    String $pkgdir = $facts['custom_win_temp_dir']
-){
+class win_packages::azcopy_script {
 
     $srcloc = lookup('windows.ext_pkg_src')
     $app_id = lookup('azcopy_app_id')
@@ -25,9 +21,10 @@ define win_packages::azcopy (
         value => $tenant,
     }
 
-    exec { "azcopy_${pkg}":
-        command  => "${azcopy} copy ${srcloc}/${pkg} ${pkgdir}\\${pkg}",
-        provider => powershell,
-        creates  => "${pkgdir}\\${pkg}",
+    windows::environment { 'AZCOPY_SPA_APPLICATION_ID':
+        value => $app_id,
+    }
+    file { 'D:\applications\azcopy.ps1':
+        content => epp('win_packages/azcopy_pkg.ps1.epp'),
     }
 }
