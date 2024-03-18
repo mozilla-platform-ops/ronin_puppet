85eebaee5db52f0bfdcd8d2ba938c913fc344b2a
Mark Cornmesser <mcornmesser@mozilla.com>
2024-03-18 14:40:31 -0700

    test win 11 pro

--- provisioners/windows/MDC1Windows/bootstrap.ps1
+++ provisioners/windows/MDC1Windows/bootstrap.ps1
@@ -531,7 +531,7 @@ If ($stage -ne 'complete') {
     Setup-Logging
     Set-SCHTask
     Get-PSModules
-    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    #Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
     Start-Service sshd
     Set-Service -Name sshd -StartupType 'Automatic'
     New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
