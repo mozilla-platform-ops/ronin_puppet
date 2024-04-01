003b608704072725d31191acfd139794a7264f92
Jonathan Moss <jwmoss88@gmail.com>
2024-04-01 16:27:31 -0400

    Try custom package

--- modules/win_packages/manifests/win_msi_pkg.pp
+++ modules/win_packages/manifests/win_msi_pkg.pp
@@ -10,7 +10,7 @@ define win_packages::win_msi_pkg (
   $pkgdir = $facts['custom_win_temp_dir']
   $srcloc = lookup('windows.ext_pkg_src')
 
-  download_file { $package :
+  win_download { $package :
     url                   => "${$srcloc}/${pkg}",
     destination_directory => $pkgdir,
     destination_file      => $pkg,
