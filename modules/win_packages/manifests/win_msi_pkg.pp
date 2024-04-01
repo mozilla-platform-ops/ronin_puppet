3a2e5266d4da6c7e75064a29d54b920e926dab02
Jonathan Moss <jwmoss88@gmail.com>
2024-04-01 14:17:40 -0400

    Update winpackages to use download_file

--- modules/win_packages/manifests/win_msi_pkg.pp
+++ modules/win_packages/manifests/win_msi_pkg.pp
@@ -3,22 +3,23 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 define win_packages::win_msi_pkg (
-    String $pkg,
-    Array $install_options,
-    String $package=$title
-){
+  String $pkg,
+  Array $install_options,
+  String $package=$title
+) {
+  $pkgdir = $facts['custom_win_temp_dir']
+  $srcloc = lookup('windows.ext_pkg_src')
 
-    $pkgdir = $facts['custom_win_temp_dir']
-    $srcloc = lookup('windows.ext_pkg_src')
-
-    file { "${pkgdir}\\${pkg}" :
-        source => "${srcloc}/${pkg}",
-    }
-    package { $title :
-        ensure  => installed,
-        source  => "${pkgdir}\\${pkg}",
-        require => File["${pkgdir}\\${pkg}"],
-    }
+  download_file { $pkg :
+    url                   => "${$srcloc}/${pkg}",
+    destination_directory => $pkgdir,
+    destination_file      => $pkg,
+  }
+  package { $title :
+    ensure  => installed,
+    source  => "${pkgdir}\\${pkg}",
+    require => File["${pkgdir}\\${pkg}"],
+  }
 }
 
 # Bug list
--- modules/win_packages/manifests/win_zip_pkg.pp
+++ modules/win_packages/manifests/win_zip_pkg.pp
@@ -3,33 +3,33 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 define win_packages::win_zip_pkg (
-    String $pkg,
-    String $destination,
-    String $creates,
-    String $package=$title
-){
+  String $pkg,
+  String $destination,
+  String $creates,
+  String $package=$title
+) {
+  require win_packages::sevenzip
 
-    require win_packages::sevenzip
+  $pkgdir      = $facts['custom_win_temp_dir']
+  $srcloc      = lookup('windows.ext_pkg_src')
+  $seven_zip   = "\"${facts['custom_win_programfiles']}\\7-Zip\\7z.exe\""
+  $source      = "\"${pkgdir}\\${pkg}\""
 
-    $pkgdir      = $facts['custom_win_temp_dir']
-    $srcloc      = lookup('windows.ext_pkg_src')
-    $seven_zip   = "\"${facts['custom_win_programfiles']}\\7-Zip\\7z.exe\""
-    $source      = "\"${pkgdir}\\${pkg}\""
-
-    file { "${pkgdir}\\${pkg}":
-        source => "${srcloc}/${pkg}",
-    }
-    file { $destination:
-        ensure => directory,
-    }
-    # Unzip resources from Forge will fail when Puppet is ran
-    # as system  in a schedule task. This is because of the context
-    # powershell is ran in.
-    exec { $pkg:
-        command => "${seven_zip} x ${source} -o${destination} -y",
-        creates => $creates,
-    }
-
+  download_file { $pkg :
+    url                   => "${$srcloc}/${pkg}",
+    destination_directory => $pkgdir,
+    destination_file      => $pkg,
+  }
+  file { $destination:
+    ensure => directory,
+  }
+  # Unzip resources from Forge will fail when Puppet is ran
+  # as system  in a schedule task. This is because of the context
+  # powershell is ran in.
+  exec { $pkg:
+    command => "${seven_zip} x ${source} -o${destination} -y",
+    creates => $creates,
+  }
 }
 
 # Bug list
