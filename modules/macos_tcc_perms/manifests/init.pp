42e78e9bf5b961488abad999d52a259e43163537
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2024-04-19 08:10:26 -0400

    Add 5 m2s + macOS14 fixes (#652)

--- modules/macos_tcc_perms/manifests/init.pp
+++ modules/macos_tcc_perms/manifests/init.pp
@@ -34,6 +34,20 @@ class macos_tcc_perms (
           user    => 'root',
         }
       }
+      '23': {
+        $tcc_script = '/usr/local/bin/tcc_perms3.sh'
+
+        file { $tcc_script:
+          content => file('macos_tcc_perms/tcc_perms3.sh'),
+          mode    => '0755',
+        }
+
+        exec { 'execute tcc perms3 script':
+          command => $tcc_script,
+          require => File[$tcc_script],
+          user    => 'root',
+        }
+      }
       default: {
             fail("${facts['os']['release']} not supported")
           }
