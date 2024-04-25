ce7432e9f601ff078bf5b3be1665af4dce2d3db3
Andrew Erickson <aerickson@mozilla.com>
2024-04-25 13:15:18 -0700

    add linux base role profile

---
+++ modules/roles_profiles/manifests/roles/gecko_t_linux_base.pp
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class roles_profiles::roles::gecko_t_linux_base {
+  include roles_profiles::profiles::linux_base
+  include roles_profiles::profiles::cltbld_user
+}
