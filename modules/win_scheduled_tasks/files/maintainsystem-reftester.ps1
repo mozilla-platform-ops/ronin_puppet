e1ef2fbdfc8a3fbfc1a0740ba91d6ab035417d98
Mark Cornmesser <mcornmesser@mozilla.com>
2024-04-25 13:12:42 -0700

    additional checks for corrupt user profile

--- modules/win_scheduled_tasks/files/maintainsystem-reftester.ps1
+++ modules/win_scheduled_tasks/files/maintainsystem-reftester.ps1
@@ -306,6 +306,22 @@ If ($bootstrap_stage -eq 'complete') {
     StartWorkerRunner
     start-sleep -s 30
     while ($true) {
+
+        $lastBootTime = Get-WinEvent -LogName "System" -FilterXPath "<QueryList><Query Id='0' Path='System'><Select Path='System'>*[System[EventID=12]]</Select></Query></QueryList>" |
+            Select-Object -First 1 |
+            ForEach-Object { $_.TimeCreated }
+        $eventIDs = @(1511, 1515)
+
+        $events = Get-WinEvent -LogName "Application" |
+            Where-Object { $_.ID -in $eventIDs -and $_.TimeCreated -gt $lastBootTime } |
+            Sort-Object TimeCreated -Descending | Select-Object -First 1
+
+        if ($events) {
+            Write-Log -message  ('{0} :: Possible User Profile Corruption After Worker Runner Start Restarting' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+            Restart-Computer -Force
+            exit
+        }
+
         $gw = (Get-process -name generic-worker -ErrorAction SilentlyContinue )
         $processname = "StartMenuExperienceHost"
         $process = Get-Process -Name StartMenuExperienceHost -ErrorAction SilentlyContinue
