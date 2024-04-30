891d78dceb8082886e6aff57fd6c4932d69e2899
Jonathan Moss <jwmoss88@gmail.com>
2024-04-30 09:57:20 -0400

    Fix legacy puppet fact

--- modules/roles_profiles/manifests/profiles/microsoft_kms.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_kms.pp
@@ -3,22 +3,21 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::microsoft_kms {
+  case $facts['os']['name'] {
+    'Windows': {
+      include win_kms
+      if $facts['custom_win_kms_activated'] != 'activated' {
+        $server = lookup('windows.datacenter.mdc1.kms.ip')
+        $key = lookup("windows.kms.key.${facts['custom_win_os_caption']}")
 
-    case $::operatingsystem {
-        'Windows': {
-            include win_kms
-            if $facts['custom_win_kms_activated'] != 'activated' {
-                $server = lookup('windows.datacenter.mdc1.kms.ip')
-                $key = lookup("windows.kms.key.${facts['custom_win_os_caption']}")
-
-                class { 'win_kms::force_activation':
-                    server => $server,
-                    key    => $key,
-                }
-            }
+        class { 'win_kms::force_activation':
+          server => $server,
+          key    => $key,
         }
-        default: {
-            fail("${::operatingsystem} not supported")
-        }
+      }
     }
+    default: {
+      fail("${facts['os']['name']} not supported")
+    }
+  }
 }
