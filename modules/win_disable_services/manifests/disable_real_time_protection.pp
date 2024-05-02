275920d5919d1b837233133cdf2ede86f7c1ec9d
Mark Cornmesser <mcornmesser@mozilla.com>
2024-05-02 15:08:50 -0700

    update perf tester config hash

--- modules/win_disable_services/manifests/disable_real_time_protection.pp
+++ modules/win_disable_services/manifests/disable_real_time_protection.pp
@@ -3,25 +3,25 @@ class win_disable_services::disable_real_time_protection {
     $win_defend_key = "HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows Defender"
 
     registry::value { 'DisableRealtimeMonitoring' :
-        key  => $win_defend_key,
+        key  => "${win_defend_key}\\Real-Time Protection",
         type => dword,
         data => '1',
     }
     ## Disable Cloud Delivery
     registry::value { 'SpynetReporting' :
-        key  => "${win_defend_key}//Spynet",
+        key  => "${win_defend_key}\\Spynet",
         type => dword,
         data => '0',
     }
     ## Disable Sample submission
     registry::value { 'SubmitSamplesConsent' :
-        key  => "${win_defend_key}//Spynet",
+        key  => "${win_defend_key}\\Spynet",
         type => dword,
         data => '0',
     }
     ## Disable TamperProtection
     registry::value { 'TamperProtection' :
-        key  => "${win_defend_key}//Features",
+        key  => "${win_defend_key}\\Features",
         type => dword,
         data => '0',
     }
