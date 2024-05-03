efe9f42f7808c6c236b0837f15fc4877bfcf8cec
Mark Cornmesser <mcornmesser@mozilla.com>
2024-05-03 12:10:59 -0700

    update perf tester config hash

--- modules/win_disable_services/manifests/disable_real_time_protection.pp
+++ modules/win_disable_services/manifests/disable_real_time_protection.pp
@@ -1,7 +1,7 @@
 class win_disable_services::disable_real_time_protection {
 
-    $win_defend_policy_key = "HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows Defender"
-    $win_defend_key = "HKLM\\SOFTWARE\\Microsoft\\Windows Defender"
+    $win_defend_policy_key = 'HKLM\SOFTWARE\\Policies\Microsoft\Windows Defender'
+    $win_defend_key = 'HKLM\SOFTWARE\Microsoft\Windows Defender'
 
     registry::value { 'DisableRealtimeMonitoring' :
         key  => "${win_defend_key}\\Real-Time Protection",
