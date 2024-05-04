dd8301c689e208f47100a1baeea8f15f211152aa
Mark Cornmesser <mcornmesser@mozilla.com>
2024-05-03 17:28:46 -0700

    update perf tester config hash

--- modules/win_disable_services/manifests/disable_real_time_protection.pp
+++ modules/win_disable_services/manifests/disable_real_time_protection.pp
@@ -4,9 +4,9 @@ class win_disable_services::disable_real_time_protection {
     $win_defend_key = 'HKLM\SOFTWARE\Policies\Microsoft\Windows Defender'
     #$win_defend_key = 'HKLM\SOFTWARE\Microsoft\Windows Defender'
 
-    registry_key { $win_defend_policy_key:
-        ensure => present,
-    }
+#    registry_key { $win_defend_policy_key:
+#        ensure => present,
+#    }
     registry_key { $win_defend_key:
         ensure => present,
     }
