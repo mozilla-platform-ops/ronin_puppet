a66e6f39681d1c8705dd5dab65bdef20a9d63108
Mark Cornmesser <mcornmesser@mozilla.com>
2024-05-08 16:37:09 -0700

    bustage temp maintainsystem script for 2012

---
+++ modules/win_scheduled_tasks/files/2012-azure-maintainsystem.ps1
@@ -0,0 +1,11 @@
+--2024-05-08 16:34:35--  https://raw.githubusercontent.com/mozilla-platform-ops/ronin_puppet/bff70ed0bf29c6d65d10587ec6f7a1937b787b43/modules/win_scheduled_tasks/files/azure-maintainsystem.ps1
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.108.133, 185.199.111.133, 185.199.110.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.108.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 25924 (25K) [text/plain]
+Saving to: ‘azure-maintainsystem.ps1.1’
+
+     0K .......... .......... .....                           100% 8.78M=0.003s
+
+2024-05-08 16:34:36 (8.78 MB/s) - ‘azure-maintainsystem.ps1.1’ saved [25924/25924]
+
