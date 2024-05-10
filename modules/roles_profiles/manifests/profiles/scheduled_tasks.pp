8136bac36c310cb206ec769cb90b55f128368f72
Jonathan Moss <jwmoss88@gmail.com>
2024-05-10 08:32:40 -0400

    Configure 2012 builder to use z: and y: instead of d:

--- modules/roles_profiles/manifests/profiles/scheduled_tasks.pp
+++ modules/roles_profiles/manifests/profiles/scheduled_tasks.pp
@@ -6,15 +6,15 @@ class roles_profiles::profiles::scheduled_tasks {
   case $facts['os']['name'] {
     'Windows': {
       if ($facts['custom_win_location'] == 'azure') {
-        #case $facts['custom_win_os_version'] {
-        #  'win_2022_2009': {
-        #    $startup_script = '2022-azure-maintainsystem.ps1'
-        #  }
-        #  default: {
-        #    $startup_script = 'azure-maintainsystem.ps1'
-        #  }
-        #}
-        $startup_script = 'azure-maintainsystem.ps1'
+        case $facts['custom_win_os_version'] {
+          'win_2012': {
+            $startup_script = '2012-azure-maintainsystem.ps1'
+          }
+          default: {
+            $startup_script = 'azure-maintainsystem.ps1'
+          }
+        }
+        #$startup_script = 'azure-maintainsystem.ps1'
         ## Temp change for new 2012 image.
         #$startup_script = '2012-azure-maintainsystem.ps1'
         include win_scheduled_tasks::at_task_user_logon
