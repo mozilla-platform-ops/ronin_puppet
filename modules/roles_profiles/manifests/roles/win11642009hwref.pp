14e8e95640cc9ea61c6c5412e3890e1d6a2a68e6
Jonathan Moss <jwmoss88@gmail.com>
2024-05-13 14:26:46 -0400

    Add KMS to win11 ref testers

--- modules/roles_profiles/manifests/roles/win11642009hwref.pp
+++ modules/roles_profiles/manifests/roles/win11642009hwref.pp
@@ -21,6 +21,7 @@ class roles_profiles::roles::win11642009hwref {
   include roles_profiles::profiles::virtual_drivers
   #include roles_profiles::profiles::microsoft_store
   include roles_profiles::profiles::windows_datacenter_administrator
+  include roles_profiles::profiles::microsoft_kms
 
   # Adminstration
   include roles_profiles::profiles::logging
