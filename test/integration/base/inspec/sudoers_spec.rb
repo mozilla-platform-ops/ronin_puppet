91f3124e2f77563c6c4680933fbce5a6a8bbe461
Andrew Erickson <aerickson@mozilla.com>
2024-05-15 13:43:53 -0700

    remove sudoers spec

--- test/integration/base/inspec/sudoers_spec.rb
+++
@@ -1,5 +0,0 @@
-describe file('/etc/sudoers') do
-  its(:content) { should include 'cltbld ALL=(root) NOPASSWD: /bin/hahah2222' }
-  its(:content) { should include 'cltbld ALL=(root) NOPASSWD: /sbin/reboot' }
-  its(:content) { should include 'cltbld ALL=(root) NOPASSWD: /bin/chacha' }
-end
