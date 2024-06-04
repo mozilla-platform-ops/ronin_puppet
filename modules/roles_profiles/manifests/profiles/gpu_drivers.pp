c3907d497f058e5ea1c5d135f3730c517b35a073
Jonathan Moss <jwmoss88@gmail.com>
2024-06-04 15:11:14 -0400

    Remove custom_win_gpu from profile

--- modules/roles_profiles/manifests/profiles/gpu_drivers.pp
+++ modules/roles_profiles/manifests/profiles/gpu_drivers.pp
@@ -13,19 +13,10 @@ class roles_profiles::profiles::gpu_drivers {
       }
     }
     'win_11_2009':{
-      if $facts['custom_win_gpu'] == 'yes' {
-        class { 'win_packages::drivers::nvidia_grid':
-          display_name => lookup('win-worker.gpu-latest.display_name'),
-          driver_name  => lookup('win-worker.gpu-latest.name'),
-          srcloc       => lookup('windows.ext_pkg_src'),
-        }
-      }
-      else {
-        class { 'win_packages::drivers::nvidia_grid':
-          display_name => lookup('win-worker.gpu.display_name'),
-          driver_name  => lookup('win-worker.gpu.name'),
-          srcloc       => lookup('windows.ext_pkg_src'),
-        }
+      class { 'win_packages::drivers::nvidia_grid':
+        display_name => lookup('win-worker.gpu-latest.display_name'),
+        driver_name  => lookup('win-worker.gpu-latest.name'),
+        srcloc       => lookup('windows.ext_pkg_src'),
       }
     }
     'win_10_2004':{
