d3a8c9bd89ed0f70f99c7783d8a8c9171cb633f9
Andrew Erickson <aerickson@mozilla.com>
2024-06-12 10:13:11 -0700

    set netperf latest tc versions, support insecure versions of g-w

--- modules/packages/manifests/linux_generic_worker.pp
+++ modules/packages/manifests/linux_generic_worker.pp
@@ -4,55 +4,65 @@
 
 # TODO: move this to linux_packages::generic_worker
 class packages::linux_generic_worker (
-    Pattern[/^v\d+\.\d+\.\d+$/] $generic_worker_version,
-    String                      $generic_worker_sha256,
-    Pattern[/^v\d+\.\d+\.\d+$/] $taskcluster_proxy_version,
-    String                      $taskcluster_proxy_sha256,
-    Pattern[/^v\d+\.\d+\.\d+$/] $livelog_version,
-    String                      $livelog_sha256,
-    Pattern[/^v\d+\.\d+\.\d+$/] $start_worker_version,
-    String                      $start_worker_sha256,
-    Pattern[/^v\d+\.\d+\.\d+$/] $quarantine_worker_version,
-    String                      $quarantine_worker_sha256,
+  Pattern[/^v\d+\.\d+\.\d+$/] $generic_worker_version,
+  String                      $generic_worker_sha256,
+  Pattern[/^v\d+\.\d+\.\d+$/] $taskcluster_proxy_version,
+  String                      $taskcluster_proxy_sha256,
+  Pattern[/^v\d+\.\d+\.\d+$/] $livelog_version,
+  String                      $livelog_sha256,
+  Pattern[/^v\d+\.\d+\.\d+$/] $start_worker_version,
+  String                      $start_worker_sha256,
+  Pattern[/^v\d+\.\d+\.\d+$/] $quarantine_worker_version,
+  String                      $quarantine_worker_sha256,
 ) {
 
+  if versioncmp($generic_worker_version, '63') < 0 {
     packages::linux_package_from_s3 { "generic-worker-simple-linux-amd64-${generic_worker_version}":
-        private             => false,
-        os_version_specific => false,
-        type                => 'bin',
-        file_destination    => '/usr/local/bin/generic-worker',
-        checksum            => $generic_worker_sha256,
+      private             => false,
+      os_version_specific => false,
+      type                => 'bin',
+      file_destination    => '/usr/local/bin/generic-worker',
+      checksum            => $generic_worker_sha256,
     }
+  } else {
+    packages::linux_package_from_s3 { "generic-worker-insecure-linux-amd64-${generic_worker_version}":
+      private             => false,
+      os_version_specific => false,
+      type                => 'bin',
+      file_destination    => '/usr/local/bin/generic-worker',
+      checksum            => $generic_worker_sha256,
+    }
+  }
 
-    packages::linux_package_from_s3 { "taskcluster-proxy-linux-amd64-${taskcluster_proxy_version}":
-        private             => false,
-        os_version_specific => false,
-        type                => 'bin',
-        file_destination    => '/usr/local/bin/taskcluster-proxy',
-        checksum            => $taskcluster_proxy_sha256,
-    }
+  packages::linux_package_from_s3 { "taskcluster-proxy-linux-amd64-${taskcluster_proxy_version}":
+    private             => false,
+    os_version_specific => false,
+    type                => 'bin',
+    file_destination    => '/usr/local/bin/taskcluster-proxy',
+    checksum            => $taskcluster_proxy_sha256,
+  }
 
-    packages::linux_package_from_s3 { "livelog-linux-amd64-${livelog_version}":
-        private             => false,
-        os_version_specific => false,
-        type                => 'bin',
-        file_destination    => '/usr/local/bin/livelog',
-        checksum            => $livelog_sha256,
-    }
+  packages::linux_package_from_s3 { "livelog-linux-amd64-${livelog_version}":
+    private             => false,
+    os_version_specific => false,
+    type                => 'bin',
+    file_destination    => '/usr/local/bin/livelog',
+    checksum            => $livelog_sha256,
+  }
 
-    packages::linux_package_from_s3 { "start-worker-linux-amd64-${start_worker_version}":
-        private             => false,
-        os_version_specific => false,
-        type                => 'bin',
-        file_destination    => '/usr/local/bin/start-worker',
-        checksum            => $start_worker_sha256,
-    }
+  packages::linux_package_from_s3 { "start-worker-linux-amd64-${start_worker_version}":
+    private             => false,
+    os_version_specific => false,
+    type                => 'bin',
+    file_destination    => '/usr/local/bin/start-worker',
+    checksum            => $start_worker_sha256,
+  }
 
-    packages::linux_package_from_s3 { "quarantine-worker-linux-amd64-${quarantine_worker_version}":
-        private             => false,
-        os_version_specific => false,
-        type                => 'bin',
-        file_destination    => '/usr/local/bin/quarantine-worker',
-        checksum            => $quarantine_worker_sha256,
-    }
+  packages::linux_package_from_s3 { "quarantine-worker-linux-amd64-${quarantine_worker_version}":
+    private             => false,
+    os_version_specific => false,
+    type                => 'bin',
+    file_destination    => '/usr/local/bin/quarantine-worker',
+    checksum            => $quarantine_worker_sha256,
+  }
 }
