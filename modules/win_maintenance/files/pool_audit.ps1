a6a0c4667134db7c99b331bd255c71743e4b1e85
Mark Cornmesser <mcornmesser@mozilla.com>
2024-06-25 14:53:15 -0700

    add delay before pxe reboot

--- modules/win_maintenance/files/pool_audit.ps1
+++ modules/win_maintenance/files/pool_audit.ps1
@@ -63,7 +63,8 @@ function Set-PXE {
             $GUID = '{' + $FullLine.split('{')[1]
             bcdedit /set "{fwbootmgr}" bootsequence "$GUID"
             Write-Log -message ('{0} :: Device will PXE boot. Restarting' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-            Restart-Computer -Force
+            Restart-Computer -Force -Wait -For PowerShell -Timeout 5
+            exit 66
         }
         Catch {
             Write-Log -message ('{0} :: Unable to set next boot to PXE. Exiting!' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
