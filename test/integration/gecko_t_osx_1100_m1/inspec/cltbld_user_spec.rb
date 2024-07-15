1920fcd53d4ecaa34045038f87699f0fef63b3f0
Andrew Erickson <aerickson@mozilla.com>
2024-07-15 15:08:04 -0700

    kitchen.circleci: switch to inspec for mac

--- test/integration/gecko_t_osx_1100_m1/inspec/cltbld_user_spec.rb
+++ test/integration/gecko_t_osx_1100_m1/inspec/cltbld_user_spec.rb
@@ -1,8 +1,23 @@
+# serverspec
+
+# describe 'users' do
+#   describe user('cltbld') do
+#     it { should exist }
+#     it { should belong_to_group '_developer' }
+#     it { should belong_to_group 'com.apple.access_screensharing' }
+#     it { should belong_to_group 'com.apple.access_ssh' }
+#   end
+# end
+
+
+# inspec
+
 describe 'users' do
   describe user('cltbld') do
     it { should exist }
-    it { should belong_to_group '_developer' }
-    it { should belong_to_group 'com.apple.access_screensharing' }
-    it { should belong_to_group 'com.apple.access_ssh' }
+
+    %w(cltbld _developer com.apple.access_screensharing com.apple.access_ssh).each do |group|
+      its('groups') { should include group }
+    end
   end
 end
