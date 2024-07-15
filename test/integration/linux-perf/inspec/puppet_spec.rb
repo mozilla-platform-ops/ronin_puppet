15d66d79d55d220a6d80ac1ed2fe1a83a1cb0f97
Andrew Erickson <aerickson@mozilla.com>
2024-07-15 13:33:31 -0700

    improve test

--- test/integration/linux-perf/inspec/puppet_spec.rb
+++ test/integration/linux-perf/inspec/puppet_spec.rb
@@ -31,3 +31,9 @@ end
 describe file('/etc/puppet/ronin_puppet.example') do
   it { should exist }
 end
+
+# /usr/local/bin/change_worker_id_and_type.py
+describe file('/usr/local/bin/change_worker_id_and_type.py') do
+  it { should exist }
+  it { should be_executable }
+end
