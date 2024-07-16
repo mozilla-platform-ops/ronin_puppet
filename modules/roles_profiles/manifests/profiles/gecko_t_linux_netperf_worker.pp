45c4d1297bbeb5a0aacbd54a7c3f73cd885906fa
Andrew Erickson <aerickson@mozilla.com>
2024-07-16 11:00:52 -0700

    cltbld can run sudo caddy, add test

--- modules/roles_profiles/manifests/profiles/gecko_t_linux_netperf_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_linux_netperf_worker.pp
@@ -83,10 +83,15 @@ class roles_profiles::profiles::gecko_t_linux_netperf_worker {
       # install caddy
       require linux_packages::caddy
 
-      # cltbld needs to be able to run tc
+      # cltbld needs to be able to run tc and caddy
       sudo::custom { 'allow cltbld to run tc':
         user    => 'cltbld',
         command => '/sbin/tc',
+        runas   => 'ALL',
+      }
+      sudo::custom { 'allow cltbld to run caddy':
+        user    => 'cltbld',
+        command => '/usr/bin/caddy',
         runas   => 'ALL',
       }
     }
