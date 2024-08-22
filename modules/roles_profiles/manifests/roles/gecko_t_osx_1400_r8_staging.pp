74313b7d9ac976d161a2e8534a0fab72038a23e8
Ryan Curran <rcurran@mozilla.com>
2024-08-22 11:39:49 -0400

    expanded scope

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1400_r8_staging.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1400_r8_staging.pp
@@ -5,6 +5,7 @@
 class roles_profiles::roles::gecko_t_osx_1400_r8_staging {
   include macos_utils::disable_bluetooth_setup
   include roles_profiles::profiles::cltbld_user
+  include roles_profiles::profiles::macos_gw_checker
   include roles_profiles::profiles::macos_people_remover
   include roles_profiles::profiles::macos_tcc_perms
   include roles_profiles::profiles::motd
