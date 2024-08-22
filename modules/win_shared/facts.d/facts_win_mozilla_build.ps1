ee742c56fd7ac613f8f5dc587d879d8157bda1c8
Mark Cornmesser <mcornmesser@mozilla.com>
2024-08-22 14:01:05 -0700

    fix facts_win_mozilla_build.ps1

--- modules/win_shared/facts.d/facts_win_mozilla_build.ps1
+++ modules/win_shared/facts.d/facts_win_mozilla_build.ps1
@@ -33,7 +33,7 @@ if (Test-Path $hg_file) {
 # Python 3 Pip
 if (Test-Path $python3_file) {
     $pip_version = (C:\mozilla-build\python3\python3.exe -m pip --version)
-    if ($pip_version -ne $null) {
+    if (-not [string]::IsNullOrEmpty($pip_version)) {
         $py3_pip_version = [regex]::Matches($pip_version, "(\d+\.\d+\.\d+)").value
     } else {
         $py3_pip_version = 0.0.0
