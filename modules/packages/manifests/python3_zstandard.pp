34831aba2e9367afffd9a93bac9c22b4ff059d41
Ryan Curran <rcurran@mozilla.com>
2024-08-23 08:57:38 -0400

    Automate Xcode CLI tools instal

--- modules/packages/manifests/python3_zstandard.pp
+++ modules/packages/manifests/python3_zstandard.pp
@@ -3,14 +3,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::python3_zstandard (
-    String $version = '0.11.1',
+  String $version = '0.11.1',
 ) {
-    require packages::python3
+  require packages::python3
+  require macos_xcode_tools
 
-    package { 'python3-zstandard':
-        ensure   => $version,
-        name     => 'zstandard',
-        provider => pip3,
-        require  => Class['packages::python3', 'packages::xcode_cmd_line_tools'],
-    }
+  package { 'python3-zstandard':
+    ensure   => $version,
+    name     => 'zstandard',
+    provider => pip3,
+    require  => Class['packages::python3', 'macos_xcode_tools'],
+  }
 }
