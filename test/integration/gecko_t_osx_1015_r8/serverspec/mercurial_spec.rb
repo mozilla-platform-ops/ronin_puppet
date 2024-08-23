233698102c429459633c34a04252317961d1e69a
Ryan Curran <rcurran@mozilla.com>
2024-08-23 12:43:09 -0400

    add mercurial check

--- test/integration/gecko_t_osx_1015_r8/serverspec/mercurial_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/mercurial_spec.rb
@@ -2,6 +2,6 @@ require_relative 'spec_helper'
 
 # This test suite checks for the presence and properties of the Mercurial binary.
 # Tests hard code the binary path so if it changes tests will fail
-describe file('/usr/local/bin/hg') do
+describe file('/Library/Frameworks/Python.framework/Versions/3.11/bin/hg') do
   it { should exist }
 end
