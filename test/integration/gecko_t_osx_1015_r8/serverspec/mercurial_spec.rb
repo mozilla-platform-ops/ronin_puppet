cad52bf1edf86de2a2b2dee2e56ec6aa906b62ac
Ryan Curran <rcurran@mozilla.com>
2024-08-26 11:00:23 -0400

    testing

--- test/integration/gecko_t_osx_1015_r8/serverspec/mercurial_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/mercurial_spec.rb
@@ -1,7 +1,7 @@
-require_relative 'spec_helper'
+# require_relative 'spec_helper'
 
-# This test suite checks for the presence and properties of the Mercurial binary.
-# Tests hard code the binary path so if it changes tests will fail
-describe file('/Library/Frameworks/Python.framework/Versions/3.11/bin/hg') do
-  it { should exist }
-end
+# # This test suite checks for the presence and properties of the Mercurial binary.
+# # Tests hard code the binary path so if it changes tests will fail
+# describe file('/Library/Frameworks/Python.framework/Versions/3.11/bin/hg') do
+#   it { should exist }
+# end
---
+++ test/integration/gecko_t_osx_1015_r8/serverspec/pipconf_spec.rb
@@ -0,0 +1,7 @@
+require_relative 'spec_helper'
+
+# This test suite checks for the presence and properties of the Mercurial binary.
+# Tests hard code the binary path so if it changes tests will fail
+describe file('/Library/Application\ Support/pip/pip.conf') do
+  it { should exist }
+end
