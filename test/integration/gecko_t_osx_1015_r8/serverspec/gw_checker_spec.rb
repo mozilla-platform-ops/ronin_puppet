83203469877fd3377c7cf74c02b4b0f2caf8690f
Ryan Curran <rcurran@mozilla.com>
2024-08-26 12:08:22 -0400

    test

--- test/integration/gecko_t_osx_1015_r8/serverspec/gw_checker_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/gw_checker_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
 
-describe crontab('root') do
-  its(:content) { should match(%r{^*/30 \* \* \* \* /usr/local/bin/gw_checker.sh$}) }
+describe command('crontab -l -u root') do
+  its(:stdout) { should match(%r{^*/30 \* \* \* \* /usr/local/bin/gw_checker.sh$}) }
 end
