4cfe86de91397051c3b17e7b66986f06a5076235
Mark Cornmesser <mcornmesser@mozilla.com>
2024-08-27 12:44:09 -0700

    corrections

--- modules/roles_profiles/manifests/roles/win11a6424h2azuretester.pp
+++ modules/roles_profiles/manifests/roles/win11a6424h2azuretester.pp
@@ -40,7 +40,6 @@ class roles_profiles::roles::win11a6424h2azuretester {
       include roles_profiles::profiles::power_management
       include roles_profiles::profiles::scheduled_tasks
       include roles_profiles::profiles::azure_vm_agent
-      include roles_profiles::profiles::virtual_drivers
       include roles_profiles::profiles::gpu_drivers
 
       # Adminstration
