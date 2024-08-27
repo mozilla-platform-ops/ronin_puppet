bfd90c265bfe94b1babbdcdccfd8916213a46c6e
Mark Cornmesser <mcornmesser@mozilla.com>
2024-08-27 12:13:07 -0700

    add win 11 a64 builder and tester roles.

--- modules/roles_profiles/manifests/roles/win11a6424h2azurebuilder.pp
+++ modules/roles_profiles/manifests/roles/win11a6424h2azurebuilder.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-class roles_profiles::roles::win11a6424h2azure {
+class roles_profiles::roles::win11a6424h2azurebuilder {
 
     case $facts['custom_win_bootstrap_stage'] {
         ## Include only what is needed in the catalog post bootstrap
