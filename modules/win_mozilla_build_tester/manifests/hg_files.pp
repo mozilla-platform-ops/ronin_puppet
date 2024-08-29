249105f835e0aec6c4e597b2cb96c40cd67258af
Mark Cornmesser <mcornmesser@mozilla.com>
2024-08-29 11:36:18 -0700

    use D drive for cache if present

--- modules/win_mozilla_build_tester/manifests/hg_files.pp
+++ modules/win_mozilla_build_tester/manifests/hg_files.pp
@@ -8,20 +8,10 @@ class win_mozilla_build_tester::hg_files {
   $mozbld      = "${facts['custom_win_systemdrive']}\\mozilla-build"
   $msys_dir = "${facts['custom_win_systemdrive']}\\mozilla-build\\msys2"
 
-  ## If Azure, then cache drive is either Y or D
-  if ($facts['custom_win_location'] == 'azure') {
-    case $facts['custom_win_os_version'] {
-      'win_2012': {
-        $cache_drive = 'y:'
-      }
-      default: {
-        $cache_drive = 'd:'
-      }
-    }
-  }
-  ## If Datacenter, then cache drive is C
-  if ($facts['custom_win_location'] == 'datacenter') {
-    $cache_drive = 'c:'
+  if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_Ddrive_present'] == 'true') {
+    $cache_drive = 'd:'
+  } else {
+    $cache_drive = $facts['custom_win_systemdrive']
   }
 
   file { "${cache_drive}\\hg-shared":
--- modules/win_mozilla_build_tester/manifests/pip.pp
+++ modules/win_mozilla_build_tester/manifests/pip.pp
@@ -3,20 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_mozilla_build_tester::pip {
-  ## If Azure, then cache drive is either Y or D
-  if ($facts['custom_win_location'] == 'azure') {
-    case $facts['custom_win_os_version'] {
-      'win_2012': {
-        $cache_drive = 'y:'
-      }
-      default: {
-        $cache_drive = 'd:'
-      }
-    }
-  }
-  ## If Datacenter, then cache drive is C
-  if ($facts['custom_win_location'] == 'datacenter') {
-    $cache_drive = 'c:'
+
+  if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_Ddrive_present'] == 'true') {
+    $cache_drive = 'd:'
+  } else {
+    $cache_drive = $facts['custom_win_systemdrive']
   }
 
   file { "${$facts['custom_win_programdata']}\\pip":
