260224c6ca5e893148bccc5bdb0fdf38f656830a
Ryan Curran <rcurran@mozilla.com>
2024-08-30 09:36:05 -0400

    gw_checker spec test

--- test/integration/gecko_t_osx_1015_r8/serverspec/gw_checker_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/gw_checker_spec.rb
@@ -3,4 +3,13 @@ require_relative 'spec_helper'
 # This test suite checks for the presence of the generic worker checker script
 describe file('/usr/local/bin/gw_checker.sh') do
   it { should exist }
+  it { should be_executable.by('owner') }
+  it { should be_executable.by('group') }
+  it { should be_executable.by('others') }
+end
+
+# Experimental
+# This test checks if the root crontab contains the gw_checker.sh entry
+describe command('crontab -l -u root') do
+  its(:stdout) { should match(%r{^\*/30 \* \* \* \* /usr/local/bin/gw_checker.sh}) }
 end
