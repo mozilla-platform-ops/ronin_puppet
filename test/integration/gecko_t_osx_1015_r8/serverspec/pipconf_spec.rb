9b8b4e5f907ca7b7d7e292ec3b053a14bfd715b8
Ryan Curran <rcurran@mozilla.com>
2024-08-30 08:54:24 -0400

    expanded test

--- test/integration/gecko_t_osx_1015_r8/serverspec/pipconf_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/pipconf_spec.rb
@@ -1,7 +1,17 @@
 require_relative 'spec_helper'
 
-# This test suite checks for the presence and properties of the Mercurial binary.
-# Tests hard code the binary path so if it changes tests will fail
+# This test suite checks for the presence, properties, and contents of the pip.conf file.
+# It ensures that the file exists, that each user has read access, and that the file contains
+# the expected configuration settings.
 describe file('/Library/Application Support/pip/pip.conf') do
   it { should exist }
+
+  # Check that the file is readable by all users
+  it { should be_readable.by('others') }
+  it { should be_readable.by('group') }
+  it { should be_readable.by('owner') }
+
+  # Check the contents of the file
+  its(:content) { should match /\[install\]\nno-index = true\ndisable-pip-version-check = true\nfind-links =\n    https:\/\/pypi\.pub\.build\.mozilla\.org\/pub\/\ntrusted-host =\n    pypi\.pub\.build\.mozilla\.org\n/ }
+  its(:content) { should match /\[global\]\ndisable-pip-version-check = true/ }
 end
