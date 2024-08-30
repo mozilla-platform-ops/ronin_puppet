ef87192883b54ac7dc1d556d9e90f0a4cff3e0c4
Ryan Curran <rcurran@mozilla.com>
2024-08-30 11:55:26 -0400

    possible fix

--- test/integration/gecko_t_osx_1015_r8/serverspec/bonjour_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/bonjour_spec.rb
@@ -2,18 +2,18 @@ require_relative 'spec_helper'
 
 # This test checks if Bonjour multicast advertisements are disabled
 describe 'Bonjour multicast advertisements disabled' do
-  # Check the macOS version and file settings
+  # Check if the NoMulticastAdvertisements key is set correctly
   describe command('defaults read /Library/Preferences/com.apple.mDNSResponder NoMulticastAdvertisements') do
     its(:stdout) { should match(/^1$/) }
   end
 
-  # Check if the mDNSResponder service is running
-  describe command('launchctl list | grep com.apple.mDNSResponder') do
-    its(:stdout) { should match(/com.apple.mDNSResponder/) }
+  # Check if the mDNSResponder service is listed in any domain
+  describe command('launchctl list | grep -E "(com.apple.mDNSResponder|mdnsresponder)"') do
+    its(:stdout) { should match(/(com.apple.mDNSResponder|mdnsresponder)/) }
   end
 
-  # Alternatively, use a command to check if the service is active
-  describe command('launchctl print system/com.apple.mDNSResponder | grep -E "state = running|state = active"') do
-    its(:exit_status) { should eq 0 }
+  # Check if the mDNSResponder service is running or active
+  describe command('launchctl print system/com.apple.mDNSResponder || launchctl print user/$(id -u)/com.apple.mDNSResponder || launchctl print gui/$(id -u)/com.apple.mDNSResponder') do
+    its(:stdout) { should match(/state = running|state = active/) }
   end
 end
