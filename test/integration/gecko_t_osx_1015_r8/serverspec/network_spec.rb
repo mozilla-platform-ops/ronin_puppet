84572bf3a30759ea2f1756f59f0121c1b70dfabf
Ryan Curran <rcurran@mozilla.com>
2024-08-30 10:47:56 -0400

    possible fix

--- test/integration/gecko_t_osx_1015_r8/serverspec/network_spec.rb
+++ test/integration/gecko_t_osx_1015_r8/serverspec/network_spec.rb
@@ -2,15 +2,23 @@ require_relative 'spec_helper'
 
 # This test checks if Wi-Fi is disabled
 describe 'Wi-Fi disabled check' do
-  wifi_interface = if os[:family] == 'darwin'
-                     command('networksetup -listallhardwareports | grep -A 1 "Wi-Fi" | awk \'/Device/ {print $2}\'').stdout.strip
-                   else
-                     fail('This test is only supported on macOS')
-                   end
+  wifi_interface = nil
 
-  it 'disables Wi-Fi on the correct interface' do
-    expect(wifi_interface).not_to be_empty
-    result = command("/usr/sbin/networksetup -getairportpower #{wifi_interface}")
-    expect(result.stdout).to match(/Off/)
+  describe 'Detect Wi-Fi interface' do
+    it 'identifies the correct Wi-Fi interface' do
+      output = command("networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}'").stdout.strip
+      wifi_interface = output
+      puts "Detected Wi-Fi interface: #{wifi_interface}"
+      expect(wifi_interface).not_to be_empty
+    end
+  end
+
+  if wifi_interface
+    describe "Check if Wi-Fi is disabled on interface #{wifi_interface}" do
+      it "should be turned off" do
+        result = command("/usr/sbin/networksetup -getairportpower #{wifi_interface}")
+        expect(result.stdout).to match(/Off/)
+      end
+    end
   end
 end
