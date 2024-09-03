660d925568e2a13c601c2d2cd352a8bf9057ae24
Ryan Curran <rcurran@mozilla.com>
2024-09-03 12:36:07 -0400

    directory_cleaner implementation

---
+++ modules/macos_directory_cleaner/files/clean_before_reboot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set environment variables explicitly
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/puppetlabs/bin:/Library/Frameworks/Python.framework/Versions/3.11/bin:/usr/local/munki
+export HOME=/Users/relops
+export SHELL=/bin/bash
+export LANG=en_US.UTF-8
+
+if command -v directory_cleaner &> /dev/null
+then
+    directory_cleaner -c /opt/directory_cleaner/configs/config.toml /opt/worker/downloads
+    directory_cleaner -c /opt/directory_cleaner/configs/config.toml /opt/worker/cache
+fi
