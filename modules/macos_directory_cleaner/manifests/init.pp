3c5b39bbf82e99c71972b696cd71d63df85e659e
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2024-09-04 16:01:01 +0100

    directory_cleaner implementation (#738)

--- modules/macos_directory_cleaner/manifests/init.pp
+++ modules/macos_directory_cleaner/manifests/init.pp
@@ -4,7 +4,6 @@ class macos_directory_cleaner (
   # Install the directory_cleaner package using pip3
   exec { 'install_directory_cleaner':
     command => '/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install directory_cleaner',
-    # require => Package['python3-pip'],
     unless  => '/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 show directory_cleaner',
   }
 
@@ -39,5 +38,31 @@ EOF
     owner   => 'root',
     group   => 'wheel',
     require => File['/opt/directory_cleaner/configs'],
+  }
+
+  # Deploy the clean_before_reboot.sh script
+  file { '/usr/local/bin/clean_before_reboot.sh':
+    ensure => file,
+    source => 'puppet:///modules/macos_directory_cleaner/clean_before_reboot.sh',
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'wheel',
+  }
+
+  # Deploy the org.mozilla.cleanbeforereboot.plist file
+  file { '/Library/LaunchDaemons/org.mozilla.cleanbeforereboot.plist':
+    ensure  => file,
+    source  => 'puppet:///modules/macos_directory_cleaner/org.mozilla.cleanbeforereboot.plist',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'wheel',
+    require => File['/usr/local/bin/clean_before_reboot.sh'],
+  }
+
+  # Ensure the plist is loaded
+  exec { 'load_cleanbeforereboot_plist':
+    command     => '/bin/launchctl load /Library/LaunchDaemons/org.mozilla.cleanbeforereboot.plist',
+    refreshonly => true,
+    subscribe   => File['/Library/LaunchDaemons/org.mozilla.cleanbeforereboot.plist'],
   }
 }
