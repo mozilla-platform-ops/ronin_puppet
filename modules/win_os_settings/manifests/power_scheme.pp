49a894845e6b2a74c150c0c5afa7e4fdc8cf264d
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-09-19 16:14:43 -0400

    perf: :memo: Update power plan to account for hardware vs cloud power plan options

--- modules/win_os_settings/manifests/power_scheme.pp
+++ modules/win_os_settings/manifests/power_scheme.pp
@@ -1,34 +1,15 @@
-# == Class: windows::power_scheme
-#
-# Configures the power scheme used by Windows.
-#
-# === Parameters
-#
-# [*ensure*]
-#  The power scheme to use, defaults to 'Balanced'.  Must correspond to a key in the
-#  `$guids` parameter.
-#
-# [*guids*]
-#  A mapping of power scheme names to their GUID.  The default schemes supported are:
-#  'Balanced', 'High performance', and 'Power saver'.
-#
-# Originally part of the forge Windows modules
-
-class win_os_settings::power_scheme(
-  $ensure = 'Balanced',
-  $guids  = {
-    'Balanced'             => '381b4222-f694-41f0-9685-ff5bb260df2e',
-    'High performance'     => '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c',
-    'Power saver'          => 'a1841308-3541-4fab-bc81-f71556f20b4a',
-    'Ultimate Performance' => 'e9a42b02-d5df-448d-aa00-03f14749eb61',
+class win_os_settings::power_scheme {
+  ## It seems that the hardware workers have ultimate performance
+  ## While the cloud workers only have high performance.
+  ## Let's hardcode the guid based on the location
+  case $facts['custom_win_location'] {
+    'datacenter': {
+      $guid = 'e9a42b02-d5df-448d-aa00-03f14749eb61' # Ultimate Performance
+    }
+    default: {
+      $guid = '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c' # High Performance
+    }
   }
-){
-
-  # Get the GUID for the desired power scheme.
-  if ! has_key($guids, $ensure) {
-    fail('There is no GUID for the power scheme.')
-  }
-  $guid = $guids[$ensure]
 
   # Use POWERCFG.EXE to set the desired power scheme.
   exec { 'windows-powercfg':
