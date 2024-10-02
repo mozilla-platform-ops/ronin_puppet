cb9317cd973e6a8e05c9cc09e785d542d7afbb52
Ryan Curran <rcurran@mozilla.com>
2024-10-02 11:23:55 -0400

    initial commit

--- modules/macos_directory_cleaner/files/clean_before_reboot.sh
+++ modules/macos_directory_cleaner/files/clean_before_reboot.sh
@@ -10,4 +10,5 @@ if command -v directory_cleaner &> /dev/null
 then
     directory_cleaner -c /opt/directory_cleaner/configs/config.toml --remove-empty-directories /opt/worker/downloads
     directory_cleaner -c /opt/directory_cleaner/configs/config.toml --remove-empty-directories /Users/cltbld/Library/Caches/Mozilla/updates/opt/worker/tasks/
+    directory_cleaner -c /opt/directory_cleaner/configs/config.toml --remove-empty-directories /opt/worker/tasks/cltbld
 fi
