4b3b9268faa856ed9b1916894cde6a83161fc786
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-10-07 18:32:32 -0400

    Install webview2 using chocolatey

--- modules/roles_profiles/manifests/roles/win11642009azure.pp
+++ modules/roles_profiles/manifests/roles/win11642009azure.pp
@@ -43,6 +43,9 @@ class roles_profiles::roles::win11642009azure {
       include roles_profiles::profiles::virtual_drivers
       include roles_profiles::profiles::gpu_drivers
 
+      # WebView
+      include roles_profiles::profiles::webview2
+
       # Adminstration
       include roles_profiles::profiles::logging
       include roles_profiles::profiles::common_tools
