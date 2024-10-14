1ea404c6bae8fa9d2297ac15dcda7a0b06c1529c
Andrew Erickson <aerickson@mozilla.com>
2024-10-14 16:24:55 -0500

    fix pip installation, check $enabled

--- modules/linux_directory_cleaner/manifests/init.pp
+++ modules/linux_directory_cleaner/manifests/init.pp
@@ -1,57 +1,58 @@
 class linux_directory_cleaner (
   Boolean $enabled = true,
 ) {
-  # Install the directory_cleaner package using pip3 as cltbld user
-  exec { 'install_directory_cleaner_linux':
-    command => '/usr/local/bin/pip3 install --prefix /usr/local directory_cleaner==0.2.0',
-    unless  => '/usr/local/bin/pip3 show directory_cleaner | grep "Version: 0.2.0"',
-    user    => 'cltbld',
-  }
+  # use $enabled to conditionally install the package and enable the service
+  if $enabled {
+    # Install the directory_cleaner package using pip3
+    package { 'python3-directory_cleaner':
+      ensure   => '0.2.0',
+      name     => 'directory_cleaner',
+      provider => pip3,
+      require  => Class['linux_packages::py3', 'linux_python'],
+    }
 
-  # Create necessary directories if they do not exist
-  file { '/opt/directory_cleaner':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'admin',
-    mode   => '0755',
-  }
+    # Create necessary directories if they do not exist
+    file { '/opt/directory_cleaner':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'admin',
+      mode   => '0755',
+    }
 
-  file { '/opt/directory_cleaner/configs':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'admin',
-    mode    => '0755',
-    require => File['/opt/directory_cleaner'],
-  }
+    file { '/opt/directory_cleaner/configs':
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'admin',
+      mode    => '0755',
+      require => File['/opt/directory_cleaner'],
+    }
 
-  # Define the content of the file to be created
-  $config_content = @("EOF")
-        exclusion_patterns = [
-
-               ]
+    # Define the content of the file to be created
+    $config_content = @("EOF")
+  exclusion_patterns = []
 EOF
 
-  # Create the configuration file with the specified content
-  file { '/opt/directory_cleaner/configs/config.toml':
-    ensure  => file,
-    content => $config_content,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'admin',
-    require => File['/opt/directory_cleaner/configs'],
-  }
+    # Create the configuration file with the specified content
+    file { '/opt/directory_cleaner/configs/config.toml':
+      ensure  => file,
+      content => $config_content,
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'admin',
+      require => File['/opt/directory_cleaner/configs'],
+    }
 
-  # Deploy the clean_before_reboot.sh script
-  file { '/usr/local/bin/clean_before_reboot.sh':
-    ensure => file,
-    source => 'puppet:///modules/linux_directory_cleaner/clean_before_reboot.sh',
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'admin',
-  }
+    # Deploy the clean_before_reboot.sh script
+    file { '/usr/local/bin/clean_before_reboot.sh':
+      ensure => file,
+      source => 'puppet:///modules/linux_directory_cleaner/clean_before_reboot.sh',
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'admin',
+    }
 
-  # Define the systemd service content
-  $systemd_service_content = @("EOF")
+    # Define the systemd service content
+    $systemd_service_content = @("EOF")
   [Unit]
   Description=Run cleanup script before shutdown/reboot
   DefaultDependencies=no
@@ -66,27 +67,27 @@ EOF
   WantedBy=halt.target reboot.target shutdown.target
 EOF
 
-  # Create the systemd service file
-  file { '/etc/systemd/system/clean_before_reboot.service':
-    ensure  => file,
-    content => $systemd_service_content,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => File['/usr/local/bin/clean_before_reboot.sh'],
-  }
+    # Create the systemd service file
+    file { '/etc/systemd/system/clean_before_reboot.service':
+      ensure  => file,
+      content => $systemd_service_content,
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      require => File['/usr/local/bin/clean_before_reboot.sh'],
+    }
 
-  # Ensure systemd reloads the service files
-  exec { 'systemd-reload':
-    command     => '/bin/systemctl daemon-reload',
-    refreshonly => true,
-    subscribe   => File['/etc/systemd/system/clean_before_reboot.service'],
-  }
+    # Ensure systemd reloads the service files
+    exec { 'systemd-reload':
+      command     => '/bin/systemctl daemon-reload',
+      refreshonly => true,
+      subscribe   => File['/etc/systemd/system/clean_before_reboot.service'],
+    }
 
-  # Enable the service to run on shutdown/reboot
-  service { 'clean_before_reboot':
-    ensure  => 'running',
-    enable  => true,
-    require => Exec['systemd-reload'],
-  }
-}
+    # Enable the service to run on shutdown/reboot
+    service { 'clean_before_reboot':
+      ensure  => 'running',
+      enable  => true,
+      require => Exec['systemd-reload'],
+    }
+  }}
