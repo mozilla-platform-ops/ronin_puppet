38dbbed33fb1916e69bfc61140a26bd540e8350a
Andrew Erickson <aerickson@mozilla.com>
2024-10-14 14:24:27 -0500

    cleanup

--- modules/linux_directory_cleaner/manifests/init.pp
+++ modules/linux_directory_cleaner/manifests/init.pp
@@ -1,29 +1,12 @@
 class linux_directory_cleaner (
   Boolean $enabled = true,
 ) {
-  # # Temporarily change ownership of /usr/local/lib/python3.9/dist-packages/
-  # exec { 'change_ownership_before_install':
-  #   command => 'chown cltbld:wheel /usr/local/lib/python3.9/dist-packages/',
-  #   onlyif  => 'test ! -w /usr/local/lib/python3.9/dist-packages/',
-  #   user    => 'root',
-  #   path    => ['/usr/bin', '/bin'],  # Add the path for 'test' and other common binaries
-  # }
-
   # Install the directory_cleaner package using pip3 as cltbld user
   exec { 'install_directory_cleaner_linux':
     command => '/usr/local/bin/pip3 install --prefix /usr/local directory_cleaner==0.2.0',
     unless  => '/usr/local/bin/pip3 show directory_cleaner | grep "Version: 0.2.0"',
     user    => 'cltbld',
   }
-
-  # # Revert ownership of /usr/local/lib/python3.9/dist-packages/ after install
-  # exec { 'revert_ownership_after_install':
-  #   command => 'chown root:wheel /usr/local/lib/python3.9/dist-packages/',
-  #   onlyif  => 'test -w /usr/local/lib/python3.9/dist-packages/',
-  #   user    => 'root',
-  #   require => Exec['install_directory_cleaner_linux'],
-  #   path    => ['/usr/bin', '/bin'],  # Add the path for 'test' and other common binaries
-  # }
 
   # Create necessary directories if they do not exist
   file { '/opt/directory_cleaner':
