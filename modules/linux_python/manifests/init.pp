b13ae2e225bf2b7931e91fa3d4254ae6a2f3c5ec
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2024-10-18 13:31:03 -0400

    [RELOPS-1104] talos linux 1804 hw: deploy directory cleaner (#758)
    * initial commit
    
    * Fix service bug
    
    * Fix install as user, add bin to path,  and add spec tests
    
    * Fix install landing
    
    * fix ownership issue
    
    * add full path to test binary
    
    * wip
    
    * typo fix
    
    * fix formatting
    
    * remove require
    
    * cleanup
    
    * fix group
    
    * fix pip installation, check $enabled
    
    * fix run before reboot bug
    
    * fix run before reboot bug
    
    * fix run before reboot bug
    
    * bug fix
    
    * bug fix
    
    * bf
    
    * bf
    
    * bf
    
    * bf
    
    * bf
    
    * bf
    
    * bf
    
    * bf
    
    * bf
    
    ---------
    
    Co-authored-by: Andrew Erickson <aerickson@mozilla.com>

---
+++ modules/linux_python/manifests/init.pp
@@ -0,0 +1,31 @@
+# TODO: set up pip.conf for root user
+
+class linux_python () {
+  case $facts['os']['name'] {
+    'Ubuntu': {
+      case $facts['os']['release']['full'] {
+        '18.04' : {
+          file {
+            '/root/.pip':
+              ensure => directory,
+              group  => 'root',
+              mode   => '0755',
+              owner  => 'root';
+
+            '/root/.pip/pip.conf':
+              owner  => 'root',
+              group  => 'root',
+              mode   => '0644',
+              source => "puppet:///modules/${module_name}/pip.conf";
+          }
+        }
+        default: {
+          fail("Unrecognized Ubuntu version ${facts['os']['release']['full']}")
+        }
+      }
+    }
+    default: {
+      fail("Not supported on ${facts['os']['name']}")
+    }
+  }
+}
