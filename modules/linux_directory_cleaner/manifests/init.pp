a2d5c3a0a7e2ae42a9c991a501e5b7d660ddd6c1
Andrew Erickson <aerickson@mozilla.com>
2024-10-18 12:41:23 -0500

    try waiting for puppet, don't start on systemd unit installation

--- modules/linux_directory_cleaner/manifests/init.pp
+++ modules/linux_directory_cleaner/manifests/init.pp
@@ -54,6 +54,7 @@ EOF
     $systemd_service_content = @("EOF")
 [Unit]
 Description=Clean directory at startup
+Before=run-puppet.service
 
 [Service]
 ExecStart=/usr/local/bin/clean_at_startup.sh
@@ -81,7 +82,7 @@ EOF
 
     # Enable the service to run at startup
     service { 'clean_at_startup':
-      ensure  => 'running',
+      # ensure  => 'running',
       enable  => true,
       require => Exec['systemd-reload'],
     }
