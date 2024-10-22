7af0d0b38db7a84e3f4de951124e5fb4dc1ef5e2
Jonathan Moss <jwmoss88@gmail.com>
2024-10-22 17:19:40 -0400

    Fix local git conflict

--- provisioners/windows/MDC1Windows/Get-Bootstrap.ps1
+++ provisioners/windows/MDC1Windows/Get-Bootstrap.ps1
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SRCOrganisation/SRCRepository/SRCBranch/provisioners/windows/ImageProvisioner/authorized_keys -OutFile C:\users\administrator\authorized_keys
 
->>>>>>> 619c23f53994f6093d2e5e8873e166ef68d56b05
 $local_bootstrap = "C:\bootstrap\bootstrap.ps1"
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/SRCOrganisation/SRCRepository/SRCBranch/provisioners/windows/ImageProvisioner/bootstrap.ps1 -OutFile $local_bootstrap
