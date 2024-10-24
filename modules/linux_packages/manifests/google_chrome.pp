ce8bd5cd9f7ff6ea0ef57a7c88b4665e7ccccac9
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2024-10-24 14:53:47 -0400

    [RELOPS-1122] Moonshot linux: switch back to latest google chrome (#769)
    * Set Chrome to auto update
    
    * bug fix
    
    * fix spec test

--- modules/linux_packages/manifests/google_chrome.pp
+++ modules/linux_packages/manifests/google_chrome.pp
@@ -2,70 +2,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This class installs Google Chrome on Ubuntu.
+# This class installs the latest Google Chrome on Ubuntu.
 class linux_packages::google_chrome () {
   case $facts['os']['name'] {
     'Ubuntu': {
       case $facts['os']['release']['full'] {
-        '18.04': {
-          #
-          # installation via apt is not working (latest chrome is too new, so install via s3 deb)
-          #
-
-          # include apt
-
-          # Exec['apt_update'] -> Package['google-chrome-stable']
-
-          # # setup chrome source
-          # apt::source { 'google_repo':
-          #   location => '[arch=amd64] https://dl.google.com/linux/chrome/deb/',
-          #   release  => 'stable',
-          #   key      => {
-          #     id     => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
-          #     source => 'https://dl.google.com/linux/linux_signing_key.pub',
-          #   },
-          #   repos    => 'main',
-          #   include  => {
-          #     'src' => false,
-          #   },
-          #   notify   => Exec['apt_update'],
-          # }
-
-          # # configure auto-update
-          # schedule { 'update-chrome-schedule':
-          #   period => weekly,
-          #   repeat => 1,
-          # }
-          # exec { 'update-chrome-action':
-          #   schedule => 'update-chrome-schedule',
-          #   command  => '/usr/bin/apt-get update -o \
-          #   Dir::Etc::sourcelist="sources.list.d/google-chrome.list" \
-          #   -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"',
-          # }
-          #
-          # install chrome
-          # package {
-          #   'google-chrome-stable':
-          #     # on 1804, latest no longer works missing packages (won't be created for deprecated release)
-          #     #  `google-chrome-stable : Depends: libgcc-s1 (>= 4.2) but it is not installable`
-          #     # need to upgrade to newer os version
-          #     ensure => '127.0.6533.119-1';
-          # }
-
-          packages::linux_package_from_s3 { 'google-chrome-stable_127.0.6533.119-1_amd64.deb':
-            private             => false,
-            os_version_specific => false,
-            type                => 'deb',
-            file_destination    => '/usr/bin/google-chrome',
-            checksum            => '93daec10b02d38574b4a2d5d3935782ebec4d94bb9b11d7f18e2fd0560ea665e',  # sha256
-          }
-        }
-        '22.04','24.04': {
+        '18.04', '22.04', '24.04': {
+          # Ensure apt is included
           include apt
 
           Exec['apt_update'] -> Package['google-chrome-stable']
 
-          # setup chrome source
+          # Setup Google Chrome apt repository
           apt::source { 'google_repo':
             location => '[arch=amd64] https://dl.google.com/linux/chrome/deb/',
             release  => 'stable',
@@ -77,14 +25,15 @@ class linux_packages::google_chrome () {
             include  => {
               'src' => false,
             },
-            notify   => Exec['apt_update'],
+            notify   => Exec['apt_update'],  # This ensures apt_update is triggered after the source is added
           }
 
-          # configure auto-update
+          # Schedule for Chrome auto-updates
           schedule { 'update-chrome-schedule':
             period => weekly,
             repeat => 1,
           }
+
           exec { 'update-chrome-action':
             schedule => 'update-chrome-schedule',
             command  => '/usr/bin/apt-get update -o \
@@ -92,22 +41,18 @@ class linux_packages::google_chrome () {
             -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"',
           }
 
-          # install chrome
-          package {
-            'google-chrome-stable':
-              # on 1804, latest no longer works missing packages (won't be created for deprecated release)
-              #  `google-chrome-stable : Depends: libgcc-s1 (>= 4.2) but it is not installable`
-              # need to upgrade to newer os version
-              ensure => 'latest';
+          # Install Google Chrome stable version
+          package { 'google-chrome-stable':
+            ensure => 'latest',
           }
         }
         default: {
-          fail("cannot install on ${facts['os']['release']['full']}")
+          fail("Cannot install Google Chrome on ${facts['os']['release']['full']}")
         }
       }
     }
     default: {
-      fail("cannot install on ${facts['os']['name']}")
+      fail("Cannot install Google Chrome on ${facts['os']['name']}")
     }
   }
 }
