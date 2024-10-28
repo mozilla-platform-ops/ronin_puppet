35ac786c98aac3f85708ff2999c79eb425e9b43b
Jonathan Moss <jwmoss88@gmail.com>
2024-10-28 14:05:33 -0400

    fix: :bug: Update profiles to work with win-worker.function hiera value

--- modules/roles_profiles/manifests/profiles/common_tools.pp
+++ modules/roles_profiles/manifests/profiles/common_tools.pp
@@ -2,29 +2,47 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class roles_profiles::profiles::common_tools {
+# Installs additional software based on whether it's a tester or builder
+class roles_profiles::profiles::microsoft_tools {
+  case $facts['os']['name'] {
+    'Windows': {
+      ## May not be needed. Start pahasing out with 2022
+      if $facts['os']['release']['full'] != '2016' {
+          include win_os_settings::powershell_profile
+      }
+      include win_shared::win_ronin_dirs
+      class { 'win_packages::performance_tool_kit':
+        moz_profile_source => lookup('win-worker.mozilla_profile.source'),
+        moz_profile_file   => lookup('win-worker.mozilla_profile.local'),
+      }
 
-    case $facts['os']['name']  {
-        'Windows': {
-            include win_packages::process_debug
-            #include win_packages::jq
-            include win_packages::gpg4win
-            include win_packages::sevenzip
-            include win_packages::sublimetxt
-            ## Is managed in the mozilla build module for builders
-            if $facts['custom_win_purpose'] != builder {
-                include win_packages::mercurial
-            }
-            if $facts['custom_win_location'] == 'aws' {
-                include win_packages::nircmd
-            }
-            # Bug List
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1510837
-            # nircmd
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1562042
+      $func = lookup('win-worker.function')
+
+      case $func {
+        'builder':{
+          ## This class seems to timeout on the first run of a new VM
+          ## For now don't look for it after bootstrap.
+          if $facts['custom_win_bootstrap_stage'] != 'complete' {
+            include win_packages::dxsdk_jun10
+            include win_packages::win_10_sdk
+            #include win_packages::win_11_sdk
+          }
+          include win_packages::binscope
+          # Required by rustc (tooltool artefact)
+          include win_packages::vc_redist_x86
+          include win_packages::vc_redist_x64
+        }
+        'tester':{
+          include win_packages::win_10_sdk
         }
         default: {
-            fail("${$facts['os']['name']} not supported")
+          fail("${$facts['custom_win_purpose']} not supported")
         }
+      }
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1510837
     }
+    default: {
+      fail("${$facts['os']['name']} not supported")
+    }
+  }
 }
--- modules/roles_profiles/manifests/profiles/mozilla_build.pp
+++ modules/roles_profiles/manifests/profiles/mozilla_build.pp
@@ -10,33 +10,12 @@ class roles_profiles::profiles::mozilla_build {
             # C++ libraries need to be in place before Python bits
             require roles_profiles::profiles::microsoft_tools
 
-            # Current versions Determined in /modules/win_shared/facts.d/facts_win_mozilla_build.ps1
-
-            case $facts['custom_win_os_version'] {
-                'win_2022_2009': {
-                    if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_bootstrap_stage'] == 'complete') {
-                        $cache_drive  = 'd:'
-                    } else {
-                        $cache_drive  = $facts['custom_win_systemdrive']
-                    }
-                }
-                default: {
-                    if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_bootstrap_stage'] == 'complete') {
-                        $cache_drive  = 'y:'
-                    } else {
-                        $cache_drive  = $facts['custom_win_systemdrive']
-                    }
-                }
+            if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_Ddrive_present'] == 'true') {
+                $cache_drive = 'd:'
+            } else {
+                $cache_drive = $facts['custom_win_systemdrive']
             }
 
-            #$cache_drive  = $facts['custom_win_location'] ? {
-                #'azure'   => 'y:',
-                #default => $facts['custom_win_systemdrive'],
-            #}
-            # tooltool token is not needed with worker-runner
-            # https://bugzilla.mozilla.org/show_bug.cgi?id=1624900#c1
-            # As worker-runner support is expanded this conditional will expand as well
-            # Once worker-runner is fully implemented then this support can be removed
             $tooltool_tok = $facts['custom_win_location'] ? {
                 'azure' => undef,
                 default => lookup('tooltool_tok')
