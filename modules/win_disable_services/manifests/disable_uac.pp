0bf64b9715f0da22ef21554650ddf72ca28ac1e7
Jonathan Moss <jwmoss88@gmail.com>
2024-10-28 14:10:16 -0400

    fix: :ambulance: Add purpose string to disable uac puppet class

--- modules/win_disable_services/manifests/disable_uac.pp
+++ modules/win_disable_services/manifests/disable_uac.pp
@@ -4,7 +4,9 @@
 
 # https://woshub.com/user-account-control-slider-and-group-policy-settings/
 
-class win_disable_services::disable_uac {
+class win_disable_services::disable_uac (
+  String $purpose
+) {
   case $facts['custom_win_os_version'] {
     'win_10_2009':{
       registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\ConsentPromptBehaviorAdmin':
@@ -53,9 +55,19 @@ class win_disable_services::disable_uac {
         type => dword,
         data => '1',
       }
-      registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableLUA':
-        type => dword,
-        data => '1',
+      case $purpose {
+        'builder':{
+          registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableLUA':
+            type => dword,
+            data => '0',
+          }
+        }
+        default: {
+          registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableLUA':
+            type => dword,
+            data => '1',
+          }
+        }
       }
       registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableVirtualization':
         type => dword,
