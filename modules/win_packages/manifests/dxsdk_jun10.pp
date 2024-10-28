0cba1673a2fb7693eef9b7708583ca6ea9d71a5b
Jonathan Moss <jwmoss88@gmail.com>
2024-10-28 14:10:43 -0400

    Add timeout + switch on metadata provider from azure vm

--- modules/win_packages/manifests/dxsdk_jun10.pp
+++ modules/win_packages/manifests/dxsdk_jun10.pp
@@ -3,21 +3,34 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_packages::dxsdk_jun10 {
+  $prog86x = $facts['custom_win_programfilesx86']
+  $sdk_dir = 'Microsoft DirectX SDK (June 2010)'
+  $file    = 'DXSDK_Jun10.exe'
 
-    $prog86x = $facts['custom_win_programfilesx86']
-    $sdk_dir = 'Microsoft DirectX SDK (June 2010)'
-    $file    = 'DXSDK_Jun10.exe'
-
-    windowsfeature { 'NET-Framework-Core':
+  case $facts['az_metadata']['compute']['publisher'] {
+    'microsoftwindowsdesktop': {
+      exec { 'install_net_framework3.5':
+        command   => 'Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All',
+        provider  => powershell,
+        tries     => 3,
+        try_sleep => 10,
+        timeout   => 1200,
+      }
+    }
+    default: {
+      windowsfeature { 'NET-Framework-Core':
         ensure => present,
+      }
     }
+  }
 
-    win_packages::win_exe_pkg  { 'dxsdk_jun10':
-        pkg                    => 'DXSDK_Jun10.exe',
-        install_options_string => '/U',
-        creates                => "${prog86x}\\${sdk_dir}\\Include\\audiodefs.h",
-    }
-    windows::environment { 'DXSDK_DIR':
-        value => "${facts['custom_win_programfilesx86']}\\${sdk_dir}"
-    }
+  win_packages::win_exe_pkg { 'dxsdk_jun10':
+    pkg                    => 'DXSDK_Jun10.exe',
+    install_options_string => '/U',
+    creates                => "${prog86x}\\${sdk_dir}\\Include\\audiodefs.h",
+    returns                => [0, 1023],
+  }
+  windows::environment { 'DXSDK_DIR':
+    value => "${facts['custom_win_programfilesx86']}\\${sdk_dir}",
+  }
 }
