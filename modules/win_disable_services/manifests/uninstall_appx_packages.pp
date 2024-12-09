c6a8043ec7cb46b7097a627240876c3c42490596
Jonathan Moss <jwmoss88@gmail.com>
2024-12-09 15:19:50 -0500

    Add missing parent directory in command path

--- modules/win_disable_services/manifests/uninstall_appx_packages.pp
+++ modules/win_disable_services/manifests/uninstall_appx_packages.pp
@@ -1,7 +1,7 @@
 # This class is responsible for disabling AppX packages on Windows.
 class win_disable_services::uninstall_appx_packages {
   exec { 'disable_appx_packages':
-    command  => file('appxpackages/uninstall.ps1'),
+    command  => file('win_disable_services/appxpackages/uninstall.ps1'),
     provider => powershell,
     timeout  => 300,
   }
