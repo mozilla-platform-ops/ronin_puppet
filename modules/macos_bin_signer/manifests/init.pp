8ee49d5f526c0cf8b9254d2d4679a7f70fbf6bfb
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2024-12-12 08:52:47 -0500

    fixed bug (#781)

--- modules/macos_bin_signer/manifests/init.pp
+++ modules/macos_bin_signer/manifests/init.pp
@@ -1,16 +1,8 @@
 class macos_bin_signer (
   Boolean $enabled = true,
 ) {
-  # Ensure the start-worker script is present and executable
-  file { '/usr/local/bin/start-worker':
-    ensure => file,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'wheel',
-    source => 'puppet:///modules/macos_bin_signer/start-worker',
-  }
-
-  # Ensure the start-worker script is signed
+  # Addresses: https://bugzilla.mozilla.org/show_bug.cgi?id=1932140
+  # Ensure the start-worker binary is signed
   exec { 'codesign_start_worker':
     command => 'codesign --force --sign - /usr/local/bin/start-worker',
     path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
