1b895b9a814c66c902428f9fb8be86831829d611
Jonathan Moss <jwmoss88@gmail.com>
2025-01-06 12:25:57 -0500

    Add missing \

--- modules/win_disable_services/manifests/disable_permissions_prompt.pp
+++ modules/win_disable_services/manifests/disable_permissions_prompt.pp
@@ -1,7 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 # Class: win_disable_services::disable_permissions_prompt
 # 
 # This class disables the permissions prompt for the microphone in Windows.
@@ -14,7 +10,7 @@ class win_disable_services::disable_permissions_prompt {
   }
 
   ## Required for Mochitest browser-chrome run from msix packages 
-  registry_value { 'HKLM\SOFTWARE\MicrosoftWindows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\Mozilla.Firefox.MSIX':
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\Mozilla.Firefox.MSIX':
     ensure => present,
     type   => string,
     data   => 'Allow',
