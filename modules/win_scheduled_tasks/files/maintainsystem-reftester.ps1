ae9a96b48aa9fab8430a8faae0785678adb89d65
Mark Cornmesser <mcornmesser@mozilla.com>
2025-01-23 16:18:40 -0800

    set ssh and winrm in maintain system

--- modules/win_scheduled_tasks/files/maintainsystem-reftester.ps1
+++ modules/win_scheduled_tasks/files/maintainsystem-reftester.ps1
@@ -415,6 +415,96 @@ if (!($osVersion -like "10.*")) {
     }
 }
 
+
+function Set-SSH {
+    [CmdletBinding()]
+    param (
+        [Switch]
+        $DownloadKeys
+    )
+
+    ## OpenSSH
+    $sshdService = Get-Service -Name sshd -ErrorAction SilentlyContinue
+    if ($null -eq $sshdService) {
+        Write-Log -message ('{0} :: Enabling OpenSSH.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        switch (Get-WinDisplayVersion) {
+            "24H2" {
+                ## running this manually on 24h2 didn't need the trailing ~~~~0.0.1.0
+                Add-WindowsCapability -Online -Name OpenSSH.Server
+                ## When adding the open.ssh server capability, it doesn't start the service
+                $destinationDirectory = "C:\users\administrator\.ssh"
+                ## This is the path where the authorized_keys file will be saved
+                $authorized_keys = Join-Path $destinationDirectory -ChildPath "authorized_keys"
+                ## Create the hidden ssh directory
+                New-Item -ItemType Directory -Path $destinationDirectory -Force
+                Invoke-DownloadWithRetry "https://raw.githubusercontent.com/mozilla-platform-ops/worker-images/refs/heads/main/provisioners/windows/MDC1Windows/ssh/authorized_keys" -Path $authorized_keys
+                Invoke-DownloadWithRetry "https://raw.githubusercontent.com/mozilla-platform-ops/worker-images/refs/heads/main/provisioners/windows/MDC1Windows/ssh/sshd_config" -Path "C:\programdata\ssh\sshd_config"
+                $sshdService = Get-Service -Name sshd -ErrorAction SilentlyContinue
+                if ($sshdService.status -ne "Running") {
+                    Start-Service sshd
+                    Set-Service -Name sshd -StartupType Automatic
+                }
+                ## Is sshdservice set to autmatically start?
+                if ((Get-Service -Name sshd -ErrorAction SilentlyContinue).StartType -ne "Automatic") {
+                    Set-Service -Name sshd -StartupType Automatic
+                }
+                $sshfw = @{
+                    Name        = "AllowSSH"
+                    DisplayName = "Allow SSH"
+                    Description = "Allow SSH traffic on port 22"
+                    Profile     = "Any"
+                    Direction   = "Inbound"
+                    Action      = "Allow"
+                    Protocol    = "TCP"
+                    LocalPort   = 22
+                }
+                New-NetFirewallRule @sshfw
+            }
+            default {
+                Write-Log -message ('{0} :: Enabling OpenSSH.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+                $destinationDirectory = "C:\users\administrator\.ssh"
+                $authorized_keys = $destinationDirectory + "authorized_keys"
+                New-Item -ItemType Directory -Path $destinationDirectory -Force
+                Invoke-DownloadWithRetry https://raw.githubusercontent.com/SRCOrganisation/SRCRepository/SRCBranch/provisioners/windows/ImageProvisioner/ssh/authorized_keys -Path $authorized_keys
+                Invoke-DownloadWithRetry https://raw.githubusercontent.com/SRCOrganisation/SRCRepository/SRCBranch/provisioners/windows/ImageProvisioner/ssh/sshd_config -Path C:\programdata\ssh\sshd_config
+                Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+                Start-Service sshd
+                Set-Service -Name sshd -StartupType Automatic
+                New-NetFirewallRule -Name "AllowSSH" -DisplayName "Allow SSH" -Description "Allow SSH traffic on port 22" -Profile Any -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22
+            }
+        }
+    }
+    else {
+        Write-Log -message ('{0} :: SSHd is running.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        if ($sshdService.Status -ne 'Running') {
+            Start-Service sshd
+            Set-Service -Name sshd -StartupType Automatic
+        }
+        else {
+            Write-Log -message ('{0} :: SSHD service is already running.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+        }
+    }
+}
+
+function Set-WinRM {
+    [CmdletBinding()]
+    param (
+
+    )
+    ## WinRM
+    Write-Log -message ('{0} :: Enabling WinRM.' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
+    $adapter = Get-NetAdapter | Where-Object { $psitem.name -match "Ethernet" }
+    $network_category = Get-NetConnectionProfile -InterfaceAlias $adapter.Name
+    ## WinRM only works on the the active network interface if it is set to private
+    if ($network_category.NetworkCategory -ne "Private") {
+        Set-NetConnectionProfile -InterfaceAlias $adapter.name -NetworkCategory "Private"
+        Enable-PSRemoting -Force
+    }
+}
+
+Set-WinRM
+Set-SSH
+
 $bootstrap_stage = (Get-ItemProperty -path "HKLM:\SOFTWARE\Mozilla\ronin_puppet").bootstrap_stage
 If ($bootstrap_stage -eq 'complete') {
     Run-MaintainSystem
