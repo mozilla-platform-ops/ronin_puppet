3d00ffc05613d118b051fa3f9f9c569e798e5fe7
Mark Cornmesser <mcornmesser@mozilla.com>
2025-01-28 13:15:48 -0800

    set ssh and winrm in maintain system

--- modules/win_scheduled_tasks/files/maintainsystem-reftester.ps1
+++ modules/win_scheduled_tasks/files/maintainsystem-reftester.ps1
@@ -406,10 +406,14 @@ function Test-ConnectionUntilOnline {
 ## Bug https://bugzilla.mozilla.org/show_bug.cgi?id=1910123
 ## The bug tracks when we reimaged a machine and the machine had a different refresh rate (64hz vs 60hz)
 ## This next line will check if the refresh rate is not 60hz and trigger a reimage if so
+$hardware = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property Manufacturer, Model
+$model = $hardware.Model
 $refresh_rate = (Get-WmiObject win32_videocontroller).CurrentRefreshRate
-if ($refresh_rate -ne "60") {
-    Write-Log -message ('{0} :: Refresh rate is {1}. Reimaging {2}' -f $($MyInvocation.MyCommand.Name), $refresh_rate, $ENV:COMPUTERNAME) -severity 'DEBUG'
-    Set-PXE
+if ($model -ne "ProLiant m710x Server Cartridge") {
+    if ($refresh_rate -ne "60") {
+        Write-Log -message ('{0} :: Refresh rate is {1}. Reimaging {2}' -f $($MyInvocation.MyCommand.Name), $refresh_rate, $ENV:COMPUTERNAME) -severity 'DEBUG'
+        Set-PXE
+    }
 }
 
 $bootstrap_stage = (Get-ItemProperty -path "HKLM:\SOFTWARE\Mozilla\ronin_puppet").bootstrap_stage
