8a499d3781474a48b6dcbee0d93e2eb58bcb9128
Mark Cornmesser <mcornmesser@mozilla.com>
2025-01-31 15:06:00 -0800

    add win10-2009 alpha role

--- modules/roles_profiles/manifests/roles/win10642009hwalpha.pp
+++ modules/roles_profiles/manifests/roles/win10642009hwalpha.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-class roles_profiles::roles::win10642009hw {
+class roles_profiles::roles::win10642009hwalpha {
   include roles_profiles::profiles::chocolatey
   ## Install before Widnows Updates is disabled.
   include roles_profiles::profiles::microsoft_tools
