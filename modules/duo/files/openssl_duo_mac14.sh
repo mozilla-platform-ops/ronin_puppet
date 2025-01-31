c0f4201d72f4648a4e91965a57080ceb130266be
Ryan Curran <rcurran@mozilla.com>
2025-01-31 12:52:01 -0500

    added script to install openssl/duo

---
+++ modules/duo/files/openssl_duo_mac14.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Determine the currently logged-in user (excluding root)
+CURRENT_USER=$(stat -f%Su /dev/console)
+
+if [[ "$CURRENT_USER" == "root" || -z "$CURRENT_USER" ]]; then
+    echo "No non-root user is currently logged in. Exiting."
+    exit 1
+fi
+
+echo "Detected logged-in user: $CURRENT_USER"
+
+# Get the user's home directory
+USER_HOME=$(eval echo "~$CURRENT_USER")
+DOWNLOADS_DIR="$USER_HOME/Downloads"
+
+# Ensure the Downloads directory exists
+if [[ ! -d "$DOWNLOADS_DIR" ]]; then
+    echo "Creating Downloads directory for $CURRENT_USER"
+    mkdir -p "$DOWNLOADS_DIR"
+    chown "$CURRENT_USER" "$DOWNLOADS_DIR"
+fi
+
+# Change to the user's Downloads directory
+cd "$DOWNLOADS_DIR"
+
+######
+# Install OpenSSL 3.4.0
+######
+
+OPENSSL_URL="https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/macos/public/14/openssl-3.4.0.tar.gz"
+
+echo "Downloading OpenSSL 3.4.0..."
+sudo -u "$CURRENT_USER" curl -O "$OPENSSL_URL"
+
+echo "Extracting OpenSSL 3.4.0..."
+sudo -u "$CURRENT_USER" tar -xzf openssl-3.4.0.tar.gz
+
+cd openssl-3.4.0
+
+# Set compiler environment variables for ARM64
+export CC="clang"
+export CFLAGS="-arch arm64"
+export LDFLAGS="-arch arm64"
+
+# Configure, make, and install OpenSSL
+echo "Building OpenSSL..."
+sudo -u "$CURRENT_USER" ./Configure darwin64-arm64-cc
+sudo -u "$CURRENT_USER" make
+
+echo "Installing OpenSSL..."
+sudo make install
+
+echo "OpenSSL installation complete."
+
+# Verify OpenSSL Installation
+if [[ ! -f "/usr/local/bin/openssl" ]]; then
+    echo "Error: OpenSSL binary not found in /usr/local/bin!"
+    exit 1
+fi
+
+OPENSSL_VERSION=$(/usr/local/bin/openssl version)
+echo "Installed OpenSSL version: $OPENSSL_VERSION"
+
+######
+# Install Duo Unix 2.0.4
+######
+
+DUO_UNIX_URL="https://ronin-puppet-package-repo.s3.us-west-2.amazonaws.com/macos/public/14/duo_unix-2.0.4.tar.gz"
+
+cd "$DOWNLOADS_DIR"
+
+echo "Downloading Duo Unix 2.0.4..."
+sudo -u "$CURRENT_USER" curl -O "$DUO_UNIX_URL"
+
+echo "Extracting Duo Unix 2.0.4..."
+sudo -u "$CURRENT_USER" tar -xzf duo_unix-2.0.4.tar.gz
+
+cd duo_unix-2.0.4
+
+echo "Configuring Duo Unix with OpenSSL and PAM..."
+sudo -u "$CURRENT_USER" ./configure --with-openssl=/usr/local --with-pam=/usr/local/lib/pam
+
+echo "Building Duo Unix..."
+sudo -u "$CURRENT_USER" make
+
+echo "Installing Duo Unix..."
+sudo make install
+
+echo "Duo Unix installation complete."
--- modules/duo/manifests/duo_unix.pp
+++ modules/duo/manifests/duo_unix.pp
@@ -3,74 +3,99 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class duo::duo_unix (
-    Boolean $enabled          = false,
-    String $ikey              = '',
-    String $skey              = '',
-    String $host              = '',
-    String $group             = '',
-    String $http_proxy        = '',
-    String $fallback_local_ip = 'no',
-    String $failmode          = 'safe',
-    String $pushinfo          = 'no',
-    String $autopush          = 'no',
-    String $prompts           = '3',
-    String $accept_env_factor = 'no',
+  Boolean $enabled          = false,
+  String $ikey              = '',
+  String $skey              = '',
+  String $host              = '',
+  String $group             = '',
+  String $http_proxy        = '',
+  String $fallback_local_ip = 'no',
+  String $failmode          = 'safe',
+  String $pushinfo          = 'no',
+  String $autopush          = 'no',
+  String $prompts           = '3',
+  String $accept_env_factor = 'no',
 ) {
-    if $enabled {
-        # Sanity Check
-        if $ikey == '' or $skey == '' or $host == '' {
-            fail('ikey, skey, and host must all be defined.')
-        }
-    }
+  # Determine macOS version
+  $mac_version = $facts['os']['release']['major']
 
+  if $enabled {
+    # Sanity Check
+    if $ikey == '' or $skey == '' or $host == '' {
+      fail('ikey, skey, and host must all be defined.')
+    }
+  }
+
+  if $mac_version == '10.15' {
+    # Install Duo Unix via Package if macOS 10.15
     include packages::openssl
     include packages::duo_unix
+  } elsif $mac_version == '14' {
+    # Install OpenSSL & Duo Unix via Script for macOS 14
 
-    # Do not leave duo config around if disabled
-    $conf_present = $enabled ? {
-        true => 'present',
-        default => 'absent',
+    file { '/usr/local/bin/openssl_duo_mac14.sh':
+      ensure => file,
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0755',
+      source => 'puppet:///modules/duo/openssl_duo_mac14.sh',
     }
 
-    file { '/etc/duo':
-        ensure => directory,
+    exec { 'install_duo_unix_mac14':
+      command     => '/usr/local/bin/openssl_duo_mac14.sh',
+      path        => ['/usr/local/bin', '/usr/bin', '/bin'],
+      refreshonly => true,
+      subscribe   => File['/usr/local/bin/openssl_duo_mac14.sh'],
     }
+  } else {
+    fail("Unsupported macOS version: ${mac_version}")
+  }
 
-    file { '/etc/duo/pam_duo.conf':
-        ensure    => $conf_present,
-        owner     => 'root',
-        group     => 'wheel',
-        mode      => '0600',
-        show_diff => false,
-        content   => template('duo/duo.conf.erb'),
-        require   => Class['packages::duo_unix'],
-    }
+  # Do not leave duo config around if disabled
+  $conf_present = $enabled ? {
+    true => 'present',
+    default => 'absent',
+  }
 
-    file { '/etc/duo/login_duo.conf':
-        ensure    => $conf_present,
-        owner     => '_sshd',
-        group     => 'wheel',
-        mode      => '0600',
-        show_diff => false,
-        content   => template('duo/duo.conf.erb'),
-        require   => Class['packages::duo_unix'],
-    }
+  file { '/etc/duo':
+    ensure => directory,
+  }
 
-    file { '/etc/ssh/sshd_config':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'wheel',
-        mode    => '0644',
-        source  => 'puppet:///modules/duo/sshd_config',
-        require => Class['packages::duo_unix'],
-    }
+  file { '/etc/duo/pam_duo.conf':
+    ensure    => $conf_present,
+    owner     => 'root',
+    group     => 'wheel',
+    mode      => '0600',
+    show_diff => false,
+    content   => template('duo/duo.conf.erb'),
+    require   => Class['packages::duo_unix'],
+  }
 
-    file { '/etc/pam.d/sshd':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'wheel',
-        mode    => '0444',
-        source  => 'puppet:///modules/duo/pam_sshd',
-        require => Class['packages::duo_unix'],
-    }
+  file { '/etc/duo/login_duo.conf':
+    ensure    => $conf_present,
+    owner     => '_sshd',
+    group     => 'wheel',
+    mode      => '0600',
+    show_diff => false,
+    content   => template('duo/duo.conf.erb'),
+    require   => Class['packages::duo_unix'],
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0644',
+    source  => 'puppet:///modules/duo/sshd_config',
+    require => Class['packages::duo_unix'],
+  }
+
+  file { '/etc/pam.d/sshd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0444',
+    source  => 'puppet:///modules/duo/pam_sshd',
+    require => Class['packages::duo_unix'],
+  }
 }
