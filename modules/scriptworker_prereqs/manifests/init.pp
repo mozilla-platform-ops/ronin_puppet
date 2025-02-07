dd6ddb3c9f21ce4339bb42773be8a90d72cc8dc2
Ryan Curran <rcurran@mozilla.com>
2025-02-07 13:19:55 -0500

    reverted license back

--- modules/scriptworker_prereqs/manifests/init.pp
+++ modules/scriptworker_prereqs/manifests/init.pp
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class scriptworker_prereqs {
   class { 'packages::python3':
     version => '3.8.3',
