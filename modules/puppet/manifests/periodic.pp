f0e1fcc587f61c7a22f64f653f6a570a61c42988
Ryan Curran <rcurran@mozilla.com>
2025-02-07 13:35:37 -0500

    revert periodic.pp format change

--- modules/puppet/manifests/periodic.pp
+++ modules/puppet/manifests/periodic.pp
@@ -3,59 +3,61 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class puppet::periodic (
-  String $telegraf_user,
-  String $telegraf_password,
-  String $puppet_env          = 'production',
-  String $puppet_repo         = 'https://github.com/mozilla-platform-ops/ronin_puppet.git',
-  String $puppet_branch       = 'master',
-  String $puppet_notify_email = 'puppet-ronin-reports@mozilla.com',
-  String $smtp_relay_host     = lookup({ 'name' => 'smtp_relay_host', 'default_value' => 'localhost' }),
-  Hash $meta_data             = {},
+    String $telegraf_user,
+    String $telegraf_password,
+    String $puppet_env          = 'production',
+    String $puppet_repo         = 'https://github.com/mozilla-platform-ops/ronin_puppet.git',
+    String $puppet_branch       = 'master',
+    String $puppet_notify_email = 'puppet-ronin-reports@mozilla.com',
+    String $smtp_relay_host     = lookup({'name' => 'smtp_relay_host', 'default_value' => 'localhost'}),
+    Hash $meta_data             = {},
 ) {
-  include puppet::setup
 
-  case $facts['os']['name'] {
-    'Darwin': {
-      file {
-        '/Library/LaunchDaemons/com.mozilla.atboot_puppet.plist':
-          ensure => absent;
+    include puppet::setup
 
-        '/usr/local/bin/run-puppet.sh':
-          owner   => 'root',
-          group   => 'wheel',
-          mode    => '0755',
-          content => template('puppet/puppet-darwin-run-puppet.sh.erb');
+    case $facts['os']['name'] {
+        'Darwin': {
+            file {
+                '/Library/LaunchDaemons/com.mozilla.atboot_puppet.plist':
+                    ensure => absent;
 
-        '/usr/local/bin/periodic-puppet.sh':
-          owner   => 'root',
-          group   => 'wheel',
-          mode    => '0755',
-          content => template('puppet/puppet-darwin-periodic-puppet.sh.erb');
+                '/usr/local/bin/run-puppet.sh':
+                    owner   => 'root',
+                    group   => 'wheel',
+                    mode    => '0755',
+                    content => template('puppet/puppet-darwin-run-puppet.sh.erb');
 
-        '/usr/local/bin/periodic_launchctl_wrapper.sh':
-          owner  => 'root',
-          group  => 'wheel',
-          mode   => '0755',
-          source => 'puppet:///modules/puppet/periodic_launchctl_wrapper.sh';
+                '/usr/local/bin/periodic-puppet.sh':
+                    owner   => 'root',
+                    group   => 'wheel',
+                    mode    => '0755',
+                    content => template('puppet/puppet-darwin-periodic-puppet.sh.erb');
 
-        '/Library/LaunchDaemons/com.mozilla.periodic.plist':
-          owner  => 'root',
-          group  => 'wheel',
-          mode   => '0755',
-          source => 'puppet:///modules/puppet/com.mozilla.periodic_puppet.plist';
-      }
+                '/usr/local/bin/periodic_launchctl_wrapper.sh':
+                    owner  => 'root',
+                    group  => 'wheel',
+                    mode   => '0755',
+                    source => 'puppet:///modules/puppet/periodic_launchctl_wrapper.sh';
 
-      exec { 'periodic_puppet_launchctl_load':
-        command     => '/bin/bash /usr/local/bin/periodic_launchctl_wrapper.sh',
-        refreshonly => true,
-        subscribe   => [
-          File['/usr/local/bin/periodic_launchctl_wrapper.sh'],
-          File['/Library/LaunchDaemons/com.mozilla.periodic.plist'],
-        ],
-      }
+                '/Library/LaunchDaemons/com.mozilla.periodic.plist':
+                    owner  => 'root',
+                    group  => 'wheel',
+                    mode   => '0755',
+                    source => 'puppet:///modules/puppet/com.mozilla.periodic_puppet.plist';
+            }
+
+            exec { 'periodic_puppet_launchctl_load':
+                command     => '/bin/bash /usr/local/bin/periodic_launchctl_wrapper.sh',
+                refreshonly => true,
+                subscribe   => [
+                    File['/usr/local/bin/periodic_launchctl_wrapper.sh'],
+                    File['/Library/LaunchDaemons/com.mozilla.periodic.plist'],
+                ],
+            }
+        }
+        default: {
+            fail("${module_name} does not support ${facts['os']['name']}")
+        }
     }
-    default: {
-      fail("${module_name} does not support ${facts['os']['name']}")
-    }
-  }
+
 }
