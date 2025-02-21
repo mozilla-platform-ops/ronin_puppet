625cc1b349a568a682ac8c45b0ff7ee4a3f4a70d
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2025-02-21 14:41:00 -0500

    deploy pyobjc to mac15 pool (#814)

---
+++ modules/packages/manifests/python3_pyobjc.pp
@@ -0,0 +1,17 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class packages::python3_pyobjc (
+  String $version = '11.0',
+) {
+  require packages::python3
+  require macos_xcode_tools
+
+  exec { 'install_pyobjc':
+    command => "/Library/Frameworks/Python.framework/Versions/3.11/bin/pip3 install pyobjc==${version}",
+    unless  => "/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -c 'import objc; print(objc.__version__)' | grep -q ${version}",
+    path    => ['/Library/Frameworks/Python.framework/Versions/3.11/bin', '/usr/local/bin', '/usr/bin'],
+    require => Class['packages::python3', 'macos_xcode_tools'],
+  }
+}
