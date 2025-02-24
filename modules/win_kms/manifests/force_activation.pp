d01047bbab76948bd1251798b81cd95d1714fdcb
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-02-24 09:59:59 -0500

    feat: Update KMS Server for Windows Hardware (#815)
    * feat: :memo: Update KMS Server for Windows Hardware
    
    Format force_activation.pp & update windows hiera to new kms server ip
    
    * update both win10/win11 kms key per product keys

--- modules/win_kms/manifests/force_activation.pp
+++ modules/win_kms/manifests/force_activation.pp
@@ -3,19 +3,18 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_kms::force_activation (
-    String $server,
-    String $key,
-    Integer $port = 1688
+  String $server,
+  String $key,
+  Integer $port = 1688
 ) {
+  $force_kms_script = "${facts['custom_win_roninprogramdata']}\\force_kms_activation.ps1"
 
-    $force_kms_script = "${facts['custom_win_roninprogramdata']}\\force_kms_activation.ps1"
+  file { $force_kms_script:
+    content => epp('win_kms/force_kms_activation.ps1.epp'),
+  }
 
-    file{ $force_kms_script:
-        content => epp('win_kms/force_kms_activation.ps1.epp'),
-    }
-
-    exec { 'kms_activation':
-        command  => $force_kms_script,
-        provider => powershell,
-    }
+  exec { 'kms_activation':
+    command  => $force_kms_script,
+    provider => powershell,
+  }
 }
