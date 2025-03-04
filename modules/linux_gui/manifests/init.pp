590baf53c7c8521674a975b4724b29fe7ac40109
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-03-04 15:38:41 -0500

    [RELOPS-1318] Remove pulse_client.conf which may be causing pactl issues (#826)
    * Remove pulse_client.conf which may be causing pactl issues
    
    * remove pulse_client.conf entirely
    
    * add quick note about removing client.conf

--- modules/linux_gui/manifests/init.pp
+++ modules/linux_gui/manifests/init.pp
@@ -94,17 +94,13 @@ class linux_gui(
                 # from 1804 docker image
                 # silence pip version warnings
                 # TODO: should be in linux base
+                # RELOPS-1318: We used to disable autospawn in client.conf, but may be causing issues. Removed this in relops-1318.
                 ["${builder_home}/.pip",
                   "${builder_home}/.config/pulse"  ]:
                     ensure => directory,
                     group  => $builder_group,
                     mode   => '0755',
                     owner  => $builder_user;
-                "${builder_home}/.config/pulse/client.conf":
-                    owner  => $builder_user,
-                    group  => $builder_group,
-                    mode   => '0644',
-                    source => "puppet:///modules/${module_name}/pulse_client.conf";
                 "${builder_home}/.pip/pip.conf":
                     owner  => $builder_user,
                     group  => $builder_group,
