6d72e0cd2bbdc42bf12f88ad783d47bea86e40ec
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-03-05 07:46:48 -0500

    [RELOPS-1346] Remove win10sdk from win11 aarch64 builder (#827)
    * Add custom fact for x64 vs aarch64 and do not install win10sdk on win aarch64 builder
    
    * Use get-ciminstance instead to pull the os arch

--- modules/win_shared/facts.d/facts_win_custom_os.ps1
+++ modules/win_shared/facts.d/facts_win_custom_os.ps1
@@ -87,6 +87,14 @@ if (Test-Path -Path "D:\") {
     $DDriveExists = $false
 }
 
+$os_arch = (Get-CimInstance Win32_OperatingSystem).OSArchitecture
+if ($os_arch -like "*ARM*") {
+	$custom_win_os_arch = "aarch64"
+}
+else {
+	$custom_win_os_arch = "x64"
+}
+
 write-host "custom_win_display_version=$display_version"
 write-host "custom_win_release_id=$release_id"
 write-host "custom_win_os_caption=$os_caption"
@@ -101,3 +109,4 @@ write-host "custom_win_worker_pool_id=$worker_pool_id"
 write-host "custom_win_gpu=$gpu"
 write-host "custom_win_purpose=$purpose"
 write-host "custom_win_Ddrive_present=$DDriveExists"
+write-host "custom_win_os_arch=$custom_win_os_arch"
