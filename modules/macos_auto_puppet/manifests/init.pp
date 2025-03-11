3e438575b5de9ad4bc6ef8164a35b3f353b8835f
Ryan Curran <rcurran@mozilla.com>
2025-03-11 15:16:39 -0400

    add blurb to retrigger ci failure

--- modules/macos_auto_puppet/manifests/init.pp
+++ modules/macos_auto_puppet/manifests/init.pp
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class macos_auto_puppet (
   Boolean $enabled = true,
   String $puppet_role = $facts['puppet_role'], # Dynamically pull from Facter
