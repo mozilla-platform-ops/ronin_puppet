f70b7334177efad3f1b05c2d3123e1144bb5aa9b
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-03-13 13:14:39 -0400

    [RELOPS-1352] deploy hg 6.9 to win azure testers (#829)
    * deploy hg 6.9 to win azure testers
    
    * Try install tortoisehg installation for hg 6.9
    
    * fix install options
    
    * try using brackets and 2 backslashes
    
    * fix mercurial path

--- modules/win_packages/manifests/mercurial.pp
+++ modules/win_packages/manifests/mercurial.pp
@@ -16,6 +16,10 @@ class win_packages::mercurial {
 
   win_packages::win_msi_pkg { "Mercurial ${needed_hg_ver}" :
     pkg             => "mercurial-${needed_hg_ver}-x64.msi",
-    install_options => ['/quiet'],
+    install_options => [
+      { 'INSTALLDIR' => 'C:\\Program Files\\Mercurial' },
+      { 'ADDLOCAL' => 'MainProgram' },
+      '/quiet',
+    ],
   }
 }
