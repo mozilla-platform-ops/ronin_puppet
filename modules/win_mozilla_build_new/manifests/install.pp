3ab9df020e62028efd00f346e3e72cd0e432ef22
Jonathan Moss <jwmoss88@gmail.com>
2025-03-18 10:31:49 -0400

    rename resource

--- modules/win_mozilla_build_new/manifests/install.pp
+++ modules/win_mozilla_build_new/manifests/install.pp
@@ -5,7 +5,7 @@
 class win_mozilla_build_new::install {
   $mozbld_version = lookup('win-worker.mozilla_build.version')
 
-  win_packages::win_exe_pkg { 'mozilla_build_new':
+  win_packages::win_exe_pkg { 'mozilla_build_new2':
     pkg                    => "MozillaBuildSetup-${mozbld_version}.exe",
     install_options_string => '/S',
     creates                => "C:\\mozilla-build\\msys2\\usr\\bin\\sh.exe",
