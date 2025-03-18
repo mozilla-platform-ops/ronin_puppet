85b582537f2d1f0d01d20e90a86002fd24b5438b
Ryan Curran <rcurran@mozilla.com>
2025-03-18 18:13:18 -0400

    possible hiera adjustment

--- modules/macos_auto_puppet/files/auto-puppet.sh
+++ modules/macos_auto_puppet/files/auto-puppet.sh
@@ -91,32 +91,32 @@ get_puppet_repo() {
     cd "$LOCAL_PUPPET_REPO" || fail "Failed to change directory to $LOCAL_PUPPET_REPO"
 
     # Inject Hiera Configuration
-    sudo tee "$LOCAL_PUPPET_REPO/hiera.yaml" > /dev/null << 'EOF'
----
-version: 5
-defaults:
-  data_hash: yaml_data
-  datadir: data
+#     sudo tee "$LOCAL_PUPPET_REPO/hiera.yaml" > /dev/null << 'EOF'
+# ---
+# version: 5
+# defaults:
+#   data_hash: yaml_data
+#   datadir: data
 
-hierarchy:
-  - name: "local"
-    path: "/var/root/vault.yaml"
+# hierarchy:
+#   - name: "local"
+#     path: "/var/root/vault.yaml"
 
-  - name: "Per-role data"
-    path: "roles/%%{facts.puppet_role}.yaml"
+#   - name: "Per-role data"
+#     path: "roles/%%{facts.puppet_role}.yaml"
 
-  - name: "Per-role Windows"
-    path: "roles/%%{facts.custom_win_role}.yaml"
+#   - name: "Per-role Windows"
+#     path: "roles/%%{facts.custom_win_role}.yaml"
 
-  - name: "Per-OS defaults"
-    path: "os/%%{facts.os.family}.yaml"
+#   - name: "Per-OS defaults"
+#     path: "os/%%{facts.os.family}.yaml"
 
-  - name: "Secrets generated from Vault"
-    path: "secrets/vault.yaml"
+#   - name: "Secrets generated from Vault"
+#     path: "secrets/vault.yaml"
 
-  - name: "Common data to all"
-    path: "common.yaml"
-EOF
+#   - name: "Common data to all"
+#     path: "common.yaml"
+# EOF
 
     # Inject Hiera Secrets
     mkdir -p ./data/secrets
