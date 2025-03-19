09ef505f708e232e5f73d56b09adf667630494bb
Jonathan Moss <jwmoss88@gmail.com>
2025-03-19 10:04:43 -0400

    add src location

--- modules/roles_profiles/manifests/profiles/mozbuild_post_boostrap_new.pp
+++ modules/roles_profiles/manifests/profiles/mozbuild_post_boostrap_new.pp
@@ -6,6 +6,7 @@ class roles_profiles::profiles::mozbuild_post_boostrap_new {
   $mozbld = "C:\\mozilla-build"
   case $facts['custom_win_location'] {
     'azure': {
+      $srcloc = lookup('windows.ext_pkg_src')
       if $facts['custom_win_os_arch'] != 'aarch64' {
         $cache_drive = 'd:'
       }
@@ -15,6 +16,7 @@ class roles_profiles::profiles::mozbuild_post_boostrap_new {
     }
     'datacenter': {
       $cache_drive = 'c:'
+      $srcloc       = lookup('windows.s3.ext_pkg_src')
     }
     default: {
       fail('custom_win_location not supported')
