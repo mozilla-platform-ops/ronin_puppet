c6af1327d81cea1aa1cda089714712e8119fb12a
Jonathan Moss <jwmoss88@gmail.com>
2025-03-25 12:44:33 -0400

    deploy mercurial 6.9 to windows hardware

--- modules/roles_profiles/manifests/roles/win10642009hwalpha.pp
+++ modules/roles_profiles/manifests/roles/win10642009hwalpha.pp
@@ -24,7 +24,7 @@ class roles_profiles::roles::win10642009hwalpha {
 
   # Adminstration
   include roles_profiles::profiles::logging
-  include roles_profiles::profiles::common_tools
+  include roles_profiles::profiles::mercurial
   include roles_profiles::profiles::nuc_management
   include roles_profiles::profiles::vnc
 
--- modules/roles_profiles/manifests/roles/win116424h2hw.pp
+++ modules/roles_profiles/manifests/roles/win116424h2hw.pp
@@ -21,7 +21,7 @@ class roles_profiles::roles::win116424h2hw {
 
   # Adminstration
   include roles_profiles::profiles::logging
-  include roles_profiles::profiles::common_tools
+  include roles_profiles::profiles::mercurial
   include roles_profiles::profiles::nuc_management
   #include roles_profiles::profiles::vnc
 
