23185462f9e2ab5a9e3f688274d8f8460ac2d998
Ryan Curran <rcurran@mozilla.com>
2025-03-27 15:37:19 -0400

    add new signer roles

--- modules/roles_profiles/manifests/roles/mac_v4_signing_vpn.pp
+++ modules/roles_profiles/manifests/roles/mac_v4_signing_vpn.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class roles_profiles::roles::mac_v4_signing_dep {
+class roles_profiles::roles::mac_v4_signing_vpn {
   include roles_profiles::profiles::duo
   include roles_profiles::profiles::gui
   include roles_profiles::profiles::hardware
