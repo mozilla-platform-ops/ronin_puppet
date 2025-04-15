f6ad05c4e71eda33873a79019eea1a04901a6438
Mark Cornmesser <mcornmesser@mozilla.com>
2025-04-15 16:16:21 -0700

    revert gw profile

--- modules/roles_profiles/manifests/profiles/windows_generic_worker_standalone.pp
+++ modules/roles_profiles/manifests/profiles/windows_generic_worker_standalone.pp
@@ -17,10 +17,10 @@ class roles_profiles::profiles::windows_generic_worker_standalone {
 
             $generic_worker_dir    = lookup('windows.dir.generic_worker')
             $gw_name               = lookup('win-worker.generic_worker.name')
-            $gw_exe_path           = "${generic_worker_dir}\generic-worker.exe"
+            $gw_exe_path           = "${generic_worker_dir}\\generic-worker.exe"
             $desired_gw_version    = lookup('win-worker.generic_worker.version')
             $worker_pool_id        = $facts['custom_win_worker_pool_id']
-            $gw_config_path        = "${generic_worker_dir}\generic-worker.config"
+            $gw_config_path        = "${generic_worker_dir}\\generic-worker.config"
 
 
             $desired_proxy_version = lookup('win-worker.taskcluster.proxy.version')
@@ -50,17 +50,17 @@ class roles_profiles::profiles::windows_generic_worker_standalone {
             }
 
             class { 'win_generic_worker::generic_worker':
-                cache_dir                => "${facts['custom_win_systemdrive']}\\cache",
+                cache_dir                => "${facts['custom_win_systemdrive']}\\\\cache",
                 client_id                => lookup('win-worker.generic_worker.client_id'),
                 current_gw_version       => $facts['custom_win_genericworker_version'],
                 current_proxy_version    => $facts['custom_win_taskcluster_proxy_version'],
                 desired_gw_version       => $desired_gw_version,
                 desired_proxy_version    => lookup('win-worker.taskcluster.proxy.version'),
-                downloads_dir            => "${facts['custom_win_systemdrive']}\\downloads",
-                ed25519signingkey        => "${facts['custom_win_systemdrive']}\\generic-worker\\ed25519-private.key",
+                downloads_dir            => "${facts['custom_win_systemdrive']}\\\\downloads",
+                ed25519signingkey        => "${facts['custom_win_systemdrive']}\\\\generic-worker\\\\ed25519-private.key",
                 idle_timeout             => lookup('win-worker.generic_worker.idle_timeout'),
                 init_file                => $init,
-                init_path                => "${facts['custom_win_systemdrive']}\\generic-worker\\${init}",
+                init_path                => "${facts['custom_win_systemdrive']}\\\\generic-worker\\\\${init}",
                 generic_worker_dir       => $generic_worker_dir,
                 gw_config_path           => $gw_config_path,
                 gw_exe_path              => $gw_exe_path,
@@ -68,11 +68,11 @@ class roles_profiles::profiles::windows_generic_worker_standalone {
                 gw_install_command       =>
                     "${gw_exe_path} install service --nssm ${nssm_command} --config ${gw_config_path}",
                 gw_status                => $facts['custom_win_genericworker_service'],
-                livelog_exe              => "${facts['custom_win_systemdrive']}\\generic-worker\\livelog.exe",
+                livelog_exe              => "${facts['custom_win_systemdrive']}\\\\generic-worker\\\\livelog.exe",
                 livelog_exe_source       => "${ext_pkg_src_loc}/${livelog_version}/${livelog_name}",
-                task_dir                 => "${facts['custom_win_systemdrive']}\\",
+                task_dir                 => "${facts['custom_win_systemdrive']}\\\\",
                 taskcluster_access_token => lookup('taskcluster_access_token'),
-                taskcluster_proxy_exe    => "${facts['custom_win_systemdrive']}\\generic-worker\\taskcluster-proxy.exe",
+                taskcluster_proxy_exe    => "${facts['custom_win_systemdrive']}\\\\generic-worker\\\\taskcluster-proxy.exe",
                 taskcluster_proxy_source => "${ext_pkg_src_loc}/${desired_proxy_version}/${proxy_name}",
                 taskcluster_root         => lookup('windows.taskcluster.root_url'),
                 #task_user_init_cmd      => $init,
