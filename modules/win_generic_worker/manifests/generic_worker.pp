34424ff05f3e32816cfa22365632d3f22bdd8f77
Mark Cornmesser <mcornmesser@mozilla.com>
2025-04-17 12:21:09 -0700

    wait for GW to exit in maintain system

--- modules/win_generic_worker/manifests/generic_worker.pp
+++ modules/win_generic_worker/manifests/generic_worker.pp
@@ -75,7 +75,8 @@ class  win_generic_worker::generic_worker (
     }
     if $gw_status != 'present' {
         exec { 'install_generic_worker_service':
-            command => $gw_install_command,
+            #command => $gw_install_command,
+            command => '%SystemRoot%\System32\cmd.exe /c exit 0',
             require => File[$gw_exe_path],
         }
     }
