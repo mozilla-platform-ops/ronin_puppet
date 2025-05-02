b8f622681a90fa42a7a38be3802b9069cd7e16ad
Ryan Curran <rcurran@mozilla.com>
2025-05-02 08:28:57 -0400

    scriptworker bug fix

--- modules/scriptworker_prereqs/manifests/init.pp
+++ modules/scriptworker_prereqs/manifests/init.pp
@@ -17,7 +17,7 @@ class scriptworker_prereqs {
     version => $python_version,
   }
 
-  # Install virtualenv only for macOS 14+ using raw path to avoid SIP issues
+  # Install virtualenv and setup tools symlink for macOS 14+ (mac_version 21 or 23)
   if $mac_version in ['21', '23'] {
     exec { 'install python3 virtualenv':
       command => '/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install virtualenv',
@@ -27,14 +27,21 @@ class scriptworker_prereqs {
       require => Class['packages::python3'],
     }
 
+    file { '/usr/local/tools':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0755',
+    }
+
     file { '/usr/local/tools/python3':
       ensure  => 'link',
       target  => '/usr/local/bin/python3',
-      require => Class['packages::python3'],
+      require => [Class['packages::python3'], File['/usr/local/tools']],
     }
   }
 
-  # Legacy dirs and symlink for older macOS
+  # Legacy dirs and symlink for older macOS (macOS 10.14 and 10.15)
   if $mac_version in ['18', '19'] {
     file { '/tools':
       ensure => 'directory',
