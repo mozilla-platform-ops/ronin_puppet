188b1bd4ae3358d843fa735a89338e68248b5513
Andrew Erickson <aerickson@mozilla.com>
2025-05-20 18:47:40 -0700

    check for tool

--- modules/linux_packages/files/google_chrome/extract.sh
+++ modules/linux_packages/files/google_chrome/extract.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+# check that shfmt is installed
+if ! command -v shfmt &> /dev/null
+then
+    echo "shfmt could not be found"
+    echo "  on os x, you can install it with brew install shfmt"
+    exit
+fi
+
 # convert script to ast
 #
 # cat postinst | shfmt -tojson > ast.json
