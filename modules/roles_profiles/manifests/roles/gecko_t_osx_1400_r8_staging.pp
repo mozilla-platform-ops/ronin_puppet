96f1816d8e47de73765ec2d9903f3dbd6854b10b
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2025-06-05 15:39:26 -0400

    extend local puppet run scope (#872)

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1400_r8_staging.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1400_r8_staging.pp
@@ -5,13 +5,13 @@
 class roles_profiles::roles::gecko_t_osx_1400_r8_staging {
   include macos_utils::disable_bluetooth_setup
   include roles_profiles::profiles::cltbld_user
-  include roles_profiles::profiles::macos_run_puppet
   include roles_profiles::profiles::macos_bin_signer
   include roles_profiles::profiles::macos_directory_cleaner
   include roles_profiles::profiles::macos_fsmonitor
   include roles_profiles::profiles::macos_gw_checker
   include roles_profiles::profiles::macos_notification_disabler
   include roles_profiles::profiles::macos_people_remover
+  include roles_profiles::profiles::macos_run_puppet
   include roles_profiles::profiles::macos_tcc_perms
   include roles_profiles::profiles::macos_xcode_tools
   include roles_profiles::profiles::motd
--- modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4_staging.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4_staging.pp
@@ -5,11 +5,11 @@
 class roles_profiles::roles::gecko_t_osx_1500_m4_staging {
   include macos_utils::disable_bluetooth_setup
   include roles_profiles::profiles::cltbld_user
-  include roles_profiles::profiles::macos_run_puppet
   include roles_profiles::profiles::macos_bin_signer
   include roles_profiles::profiles::macos_directory_cleaner
   include roles_profiles::profiles::macos_gw_checker
   include roles_profiles::profiles::macos_people_remover
+  include roles_profiles::profiles::macos_run_puppet
   include roles_profiles::profiles::macos_tcc_perms
   include roles_profiles::profiles::macos_xcode_tools
   include roles_profiles::profiles::motd
