1a6ebd74b8c9abb1d1bbff83384f0896608ca7b5
Ryan Curran <rcurran@mozilla.com>
2025-06-05 14:56:04 -0400

    extend local puppet run scope

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4.pp
@@ -9,6 +9,7 @@ class roles_profiles::roles::gecko_t_osx_1500_m4 {
   include roles_profiles::profiles::macos_directory_cleaner
   include roles_profiles::profiles::macos_gw_checker
   include roles_profiles::profiles::macos_people_remover
+  include roles_profiles::profiles::macos_run_puppet
   include roles_profiles::profiles::macos_tcc_perms
   include roles_profiles::profiles::macos_xcode_tools
   include roles_profiles::profiles::motd
