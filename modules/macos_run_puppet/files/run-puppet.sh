f452ac370b8c1417382eaee271be00be0af04839
Ryan Curran <rcurran@mozilla.com>
2025-06-16 13:56:50 -0400

    make email messages more verbose

--- modules/macos_run_puppet/files/run-puppet.sh
+++ modules/macos_run_puppet/files/run-puppet.sh
@@ -150,6 +150,11 @@ run_puppet() {
         retval=1
     fi
 
+    if [[ "$retval" -ne 0 ]]; then
+        LOG_SUMMARY=$(tail -n 50 "$TMP_LOG")
+        email_report "Puppet apply failed on $(hostname -f)" "Puppet apply failed on host $(hostname -f). Last 50 lines of log:\n\n$LOG_SUMMARY"
+    fi
+
     rm "$TMP_LOG"
 
     case $retval in
