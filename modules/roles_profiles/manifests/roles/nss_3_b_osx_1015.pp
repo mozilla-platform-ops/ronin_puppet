c2fa49693a7cd27bd49eb560949ad99e52cd0080
Ryan Curran <rcurran@mozilla.com>
2025-06-30 13:20:23 -0400

    extend local puppet run to nss/gecko b

--- modules/roles_profiles/manifests/roles/nss_3_b_osx_1015.pp
+++ modules/roles_profiles/manifests/roles/nss_3_b_osx_1015.pp
@@ -5,6 +5,7 @@
 class roles_profiles::roles::nss_3_b_osx_1015 {
   include macos_utils::disable_bluetooth_setup
   include roles_profiles::profiles::macos_people_remover
+  include roles_profiles::profiles::macos_run_puppet
   include roles_profiles::profiles::macos_xcode_tools
   include roles_profiles::profiles::motd
   include roles_profiles::profiles::network
