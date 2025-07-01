03893e81569eddf25f68fd37f4ac35e3892417bc
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2025-07-01 11:50:16 -0400

    remove talos (#887)

--- modules/roles_profiles/manifests/roles/gecko_3_b_osx_arm64.pp
+++ modules/roles_profiles/manifests/roles/gecko_3_b_osx_arm64.pp
@@ -14,7 +14,6 @@ class roles_profiles::roles::gecko_3_b_osx_arm64 {
   include roles_profiles::profiles::pipconf
   include roles_profiles::profiles::relops_users
   include roles_profiles::profiles::sudo
-  include roles_profiles::profiles::talos
   include roles_profiles::profiles::timezone
   include roles_profiles::profiles::users
   include roles_profiles::profiles::vnc
