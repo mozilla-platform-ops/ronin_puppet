c96fbf2d816d19cc564b90d0d80542e37872d6cd
Jonathan Moss <jwmoss88@gmail.com>
2025-07-02 11:32:21 -0400

    add test for pat

--- modules/win_shared/facts.d/facts_win_github.ps1
+++ modules/win_shared/facts.d/facts_win_github.ps1
@@ -1,2 +1,7 @@
-$Pat = Get-Content "D:\secrets\pat.txt"
+if (Test-Path "D:\secrets\pat.txt") {
+    $pat = Get-Content "D:\secrets\pat.txt"
+}
+else {
+    $pat = $null
+}
 Write-host "custom_win_github_pat=$pat"
