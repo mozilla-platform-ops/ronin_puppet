7f142a7c716636fae127a410be2f6644db92ef25
Jonathan Moss <jwmoss88@gmail.com>
2025-07-09 10:42:46 -0400

    update win10 to win11 sdk on builders and testers in azure

--- modules/roles_profiles/manifests/profiles/microsoft_tools.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_tools.pp
@@ -21,7 +21,7 @@ class roles_profiles::profiles::microsoft_tools {
           if $facts['custom_win_bootstrap_stage'] != 'complete' {
             include win_packages::dxsdk_jun10
             if $facts['custom_win_os_arch'] != 'aarch64' {
-              include win_packages::win_10_sdk
+              include win_packages::win_11_sdk
             }
             #include win_packages::win_11_sdk
           }
@@ -34,7 +34,7 @@ class roles_profiles::profiles::microsoft_tools {
           # Hardware testers don't need the windows sdk so skip installing them completely
           if $facts['custom_win_location'] == 'azure' {
             if $facts['custom_win_os_arch'] != 'aarch64' {
-              include win_packages::win_10_sdk
+              include win_packages::win_11_sdk
             }
           }
         }
