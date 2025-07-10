e0d73c9b5b8a795d5aa3d65d586bd1a90093c3d7
markcor <mcornmesser@mozilla.com>
2025-07-10 09:57:42 -0700

    FIX: Jira:RELOPS-1659 MSG: Revert HW self deploy logic (#900)
    * FIX: Jira:RELOPS-1659 MSG: Revert HW self deploy logic
    
    roles: ALL-HW-WIN
    
    * troubleshooting
    
    * revert
    
    * corrections
    
    * corrections
    
    * set idle timeout to 0

--- modules/roles_profiles/manifests/roles/win10642009hwalpha.pp
+++ modules/roles_profiles/manifests/roles/win10642009hwalpha.pp
@@ -34,5 +34,6 @@ class roles_profiles::roles::win10642009hwalpha {
   include roles_profiles::profiles::mozilla_maintenance_service
   include roles_profiles::profiles::windows_datacenter_administrator
   include roles_profiles::profiles::google_chrome
-  include roles_profiles::profiles::windows_generic_worker_standalone
+  #include roles_profiles::profiles::windows_generic_worker_standalone
+  include roles_profiles::profiles::windows_worker_runner
 }
