0650f1fc29e11b93e7283a619713dc29eaba2654
Jonathan Moss <jwmoss88@gmail.com>
2025-07-15 16:36:50 -0400

    formatting

--- modules/win_packages/manifests/sevenzip.pp
+++ modules/win_packages/manifests/sevenzip.pp
@@ -3,20 +3,20 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_packages::sevenzip {
-    case $facts['custom_win_os_arch'] {
-        'aarch64': {
-            $pkg = '7z2500-arm64.exe'
-            win_packages::win_exe_pkg  { '7-Zip 25.00 (arm64 edition)':
-                pkg             => $pkg,
-                install_options_string => ['/S'],
-            }
-        }
-        default: {
-            $pkg = '7z2500-x64.msi'
-            win_packages::win_msi_pkg  { '7-Zip 25.00 (x64 edition)':
-                pkg             => $pkg,
-                install_options_string => ['/quiet'],
-            }
-        }
+  case $facts['custom_win_os_arch'] {
+    'aarch64': {
+      $pkg = '7z2500-arm64.exe'
+      win_packages::win_exe_pkg { '7-Zip 25.00 (arm64 edition)':
+        pkg                    => $pkg,
+        install_options_string => ['/S'],
+      }
     }
+    default: {
+      $pkg = '7z2500-x64.msi'
+      win_packages::win_msi_pkg { '7-Zip 25.00 (x64 edition)':
+        pkg                    => $pkg,
+        install_options_string => ['/quiet'],
+      }
+    }
+  }
 }
