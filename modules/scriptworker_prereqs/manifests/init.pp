87c5a8800f09987e49edb0821878906a90d534ce
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2025-07-15 16:59:10 -0400

    Add support to m2 mac signers  (#806)
    * first commit
    
    * arrangement fix
    
    * duo add
    
    * format issue
    
    * bugfix
    
    * bug fix
    
    * virtualenv fix
    
    * widevine fix
    
    * reverted script worker change
    
    * added script to install openssl/duo
    
    * cache pre-compiled openssl
    
    * duo for unix bug fixes
    
    * bug fixes
    
    * add fix for integ test
    
    * reverted some changes
    
    * reverted some changes
    
    * fixes widevine clone issue
    
    * agree to xcode bug
    
    * add xcode license accept again
    
    * xcode fix and temp periodic adjustment
    
    * test
    
    * re add puppet periodic agent
    
    * roll back periodic.pp change
    
    * virtualenv fixer
    
    * revert change to duo_unix.pp
    
    * reverted duo_unix.pp formatting for easier CR review
    
    * reverted license back
    
    * reverted license back
    
    * revert periodic.pp format change
    
    * possible integ test fix
    
    * inventroy change and duo tweak
    
    * ensure backwards compatibility with duo
    
    * backwards compat check for scriptworker_prereqs
    
    * backward compat with mac_v3_signing.pp
    
    * fix macos1014 path
    
    * spacing issue
    
    * spacing issue again
    
    * hard coded scriptworker dir to test
    
    * fixed paths
    
    * reverted a few changes
    
    * disabling fw profile for testing
    
    * re-enable firewall profile
    
    * enabling for vm testing
    
    * enabling for vm testing
    
    * potential fix for old signer bug
    
    * Remove presetting /builds/scriptworker
    
    * Remove mac notarization poller
    
    * Update mac signing roles
    
    - Change hostnames
    - Adds adhoc signer config
    - Update worker types and ids
    - Update some config keys for firefox
    - Added worker_type_prefix for firefox
    - Update scriptworker base directory resolution
    - Update run-puppet.sh to use python3 instead of python
    - Update virtualenv fixer script (only updates the system pip and virtualenv packages)
    - Install python certs
    - Changed the role ordering so it installs python before trying to fix virtual envs and install the mac_v3_signing role
    - Some minor changes
    
    * Repin scriptworker dependencies
    
    * Fix scriptworker{,-scripts} pinning refreshes
    
    * add new signer roles
    
    * fix scriptworker required dirs
    
    * Change mac v4 class names
    
    * Stop using ~/Downloads on duo install
    
    * Update bootstrap_mojave.sh to current
    
    * Add debug and logging to bootstrap mojave
    
    * Unlock system keychain before adding certs
    
    * bugfix
    
    * scriptworker bug fix
    
    * Fix adhoc and vpn scriptworker roles
    
    * iScript pin requirements
    
    * Update iscript python requirements python version to 3.11 and repin
    
    * feat: Add mac_sign_pkg behaviors
    
    * Use UV to update iscript requirements
    
    ---------
    
    Co-authored-by: Heitor Neiva <hneiva@mozilla.com>

--- modules/scriptworker_prereqs/manifests/init.pp
+++ modules/scriptworker_prereqs/manifests/init.pp
@@ -1,39 +1,83 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class scriptworker_prereqs {
-    class { 'packages::python3':
-        version => '3.8.3',
+
+  # Determine macOS version
+  $mac_version = $facts['os']['release']['major']
+
+  # Select Python version based on OS
+  $python_version = $mac_version ? {
+    '18'    => '3.8.3',
+    '19'    => '3.8.3',
+    '21'    => '3.11.0',
+    '23'    => '3.11.0',
+    default => fail("Unsupported macOS version: ${mac_version}"),
+  }
+
+  # Install appropriate Python version
+  class { 'packages::python3':
+    version => $python_version,
+  }
+
+  # Install virtualenv and setup tools symlink for macOS 14+ (mac_version 21 or 23)
+  if $mac_version in ['21', '23'] {
+    exec { 'install python3 virtualenv':
+      command => '/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m pip install virtualenv',
+      unless  => '/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m virtualenv --version',
+      path    => ['/Library/Frameworks/Python.framework/Versions/3.11/bin', '/usr/bin', '/bin'],
+      user    => 'root',
+      require => Class['packages::python3'],
     }
+
+    file { '/usr/local/tools':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0755',
+    }
+
+    file { '/usr/local/tools/python3':
+      ensure  => 'link',
+      target  => '/usr/local/bin/python3',
+      require => [Class['packages::python3'], File['/usr/local/tools']],
+    }
+  }
+
+  # Legacy dirs and symlink for older macOS (macOS 10.14 and 10.15)
+  if $mac_version in ['18', '19'] {
+    file { '/tools':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0755',
+    }
+
     file { '/tools/python3':
-        ensure  => 'link',
-        target  => '/usr/local/bin/python3',
-        require => Class['packages::python3'],
+      ensure  => 'link',
+      target  => '/usr/local/bin/python3',
+      require => [Class['packages::python3'], File['/tools']],
     }
 
-    include dirs::builds
-
-    # DeveloperIDCA.cer is only required on dep, but is harmless on prod
-    file {
-        '/tmp/DeveloperIDCA.cer':
-            source => 'puppet:///modules/scriptworker_prereqs/DeveloperIDCA.cer',
+    file { '/builds':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0755',
     }
-    exec {
-        'install-developer-id-root':
-            command => '/usr/bin/security add-trusted-cert -r trustAsRoot -k /Library/Keychains/System.keychain /tmp/DeveloperIDCA.cer',
-            require => File['/tmp/DeveloperIDCA.cer'],
-            unless  => "/usr/bin/security dump-keychain /Library/Keychains/System.keychain | /usr/bin/grep 'Developer ID Certification'",
-            # This command returns an error despite actually importing
-            # the certificate correctly.
-            # For posterity, the error returned is "SecTrustSettingsSetTrustSettings: The authorization was
-            # denied since no user interaction was possible.".
-            returns => [1];
-    }
+  }
 
-    # Accept the xcode licence
-    exec {
-        'xcode_license_agree':
-            command => '/usr/bin/xcodebuild -license accept',
-    }
+  include dirs::builds
 
+  file { '/tmp/DeveloperIDCA.cer':
+    source => 'puppet:///modules/scriptworker_prereqs/DeveloperIDCA.cer',
+  }
+
+  exec { 'install-developer-id-root':
+    command => '/usr/bin/security add-trusted-cert -r trustAsRoot -k /Library/Keychains/System.keychain /tmp/DeveloperIDCA.cer',
+    require => File['/tmp/DeveloperIDCA.cer'],
+    unless  => "/usr/bin/security dump-keychain /Library/Keychains/System.keychain | /usr/bin/grep 'Developer ID Certification'",
+    returns => [1],
+  }
+
+  exec { 'xcode_license_agree':
+    command => '/usr/bin/xcodebuild -license accept',
+  }
 }
