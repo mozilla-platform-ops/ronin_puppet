fedacfd4b3fb677564e3b54d6e1d318840d59560
Heitor Neiva <hneiva@mozilla.com>
2025-07-16 08:26:41 -0700

    Fix iscript pinning + repin packages

--- modules/signing_worker/files/update-requirements.py
+++ modules/signing_worker/files/update-requirements.py
@@ -11,8 +11,8 @@ This script will update all the requirements files.
 import os.path
 import subprocess
 import sys
-import tempfile
 from pathlib import Path
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 
 import yaml
 
@@ -25,63 +25,105 @@ def run():
     with open(COMMON_YAML) as fh:
         scriptworker_config = yaml.load(fh, Loader=yaml.Loader)["scriptworker_config"]
 
-    with tempfile.TemporaryDirectory() as git_dir, tempfile.TemporaryDirectory() as req_dir:
-        print("Cloning scriptworker scripts repository")
-        subprocess.run(["git", "init"], cwd=git_dir)
-        subprocess.run(["git", "remote", "add", "origin", "https://github.com/mozilla-releng/scriptworker-scripts.git"], cwd=git_dir)
-        subprocess.run(["git", "fetch", "-a", "origin"], cwd=git_dir)
+    # with TemporaryDirectory() as ss_git_dir, TemporaryDirectory() as req_dir, TemporaryDirectory() as scriptworker_git_dir:
+    with TemporaryDirectory() as req_dir:
+        # print("Cloning scriptworker scripts repository")
+        # subprocess.run(["git", "init"], cwd=ss_git_dir)
+        # subprocess.run(["git", "remote", "add", "origin", "https://github.com/mozilla-releng/scriptworker-scripts.git"], cwd=ss_git_dir)
+        # subprocess.run(["git", "fetch", "-a", "origin"], cwd=ss_git_dir)
+
+        # print("Cloning scriptworker repository")
+        # subprocess.run(["git", "init"], cwd=scriptworker_git_dir)
+        # subprocess.run(["git", "remote", "add", "origin", "https://github.com/mozilla-releng/scriptworker.git"], cwd=scriptworker_git_dir)
+        # subprocess.run(["git", "fetch", "-a", "origin"], cwd=scriptworker_git_dir)
+
+        ss_git_dir = "/Users/hneiva/dev/scriptworker-scripts"
+        scriptworker_git_dir = "/Users/hneiva/dev/scriptworker"
 
         for name, config in scriptworker_config.items():
             print(f"Updating requirements for {name}..")
             scriptworker_revision = config["scriptworker_revision"]
             scriptworker_scripts_revision = config["scriptworker_scripts_revision"]
-            subprocess.run(["git", "reset", "--hard", scriptworker_scripts_revision], cwd=git_dir)
 
-            all_reqs = [f"https://raw.githubusercontent.com/mozilla-releng/scriptworker/{scriptworker_revision}/requirements.txt"]
+            # Checkout the specific revisions
+            subprocess.check_call(["git", "reset", "--hard", scriptworker_scripts_revision], cwd=ss_git_dir)
+            subprocess.check_call(["git", "reset", "--hard", scriptworker_revision], cwd=scriptworker_git_dir)
+
+            scriptworker_req = os.path.join(req_dir, "scriptworker.txt")
+            subprocess.check_call(
+                [
+                    "uv",
+                    "export",
+                    "--no-editable",
+                    "--no-annotate",
+                    "--no-emit-project",
+                    "--no-dev",
+                    "--python=3.11",
+                    "--quiet",
+                    "-o",
+                    scriptworker_req,
+                ],
+                cwd=scriptworker_git_dir,
+            )
+
+            # with open(scriptworker_req, "w") as fh:
+            #     fh.write(
+            #         f"git+https://github.com/mozilla-releng/scriptworker.git@{scriptworker_revision}\n"
+            #     )
+
+            all_reqs = [scriptworker_req]
+
             for package_name in ("iscript", "scriptworker-client"):
                 package_req = os.path.join(req_dir, f"{package_name}.txt")
-                subprocess.run(
+                subprocess.check_call(
                     [
                         "uv",
                         "export",
                         "--no-editable",
                         "--no-annotate",
+                        "--no-emit-project",
+                        "--no-emit-workspace",
+                        "--no-emit-package=mozbuild",
                         "--no-dev",
-                        "--python",
-                        "3.11",
+                        "--python=3.11",
+                        "--quiet",
                         "--package",
                         package_name,
                         "-o",
                         package_req,
                     ],
-                    cwd=git_dir,
+                    cwd=ss_git_dir,
                 )
                 all_reqs.append(package_req)
 
             output_file = here / f"requirements.{name}.txt"
 
-            with tempfile.NamedTemporaryFile(delete_on_close=False, suffix=".in") as fp:
+            with NamedTemporaryFile(delete_on_close=False, suffix=".in") as fp:
                 data = "-r " + "\n-r ".join(all_reqs)
                 fp.write(data.encode("utf-8"))
                 fp.seek(0)
                 fp.close()
 
-                subprocess.run(
+                subprocess.check_call(
                     [
                         "uv",
                         "pip",
                         "compile",
                         "-q",
+                        f"--overrides={req_dir}/iscript.txt",
+                        f"--overrides={req_dir}/scriptworker-client.txt",
                         "--upgrade",
-                        "--universal",
                         "--no-sources",
                         "--python-version=3.11",
+                        "--python-platform=aarch64-apple-darwin",
                         "--generate-hashes",
+                        "--no-header",
+                        "--quiet",
                         "-o",
                         output_file,
                         fp.name,
                     ],
-                    cwd=git_dir,
+                    cwd=ss_git_dir,
                 )
 
     print("Done!")
