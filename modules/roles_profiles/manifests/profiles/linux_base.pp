5bfd540a0b2f8bd35a31e30089d6feb0e5de7368
Andrew Erickson <aerickson@mozilla.com>
2025-07-18 14:51:58 -0700

    cleanup of '::operatingsystem' refs, for puppet 8?

--- modules/roles_profiles/manifests/profiles/linux_base.pp
+++ modules/roles_profiles/manifests/profiles/linux_base.pp
@@ -3,30 +3,29 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::linux_base {
+  case $facts['os']['name'] {
+    'Ubuntu': {
+      include roles_profiles::profiles::locale
+      include roles_profiles::profiles::timezone
+      include roles_profiles::profiles::ntp
+      include roles_profiles::profiles::motd
+      include roles_profiles::profiles::users
+      include roles_profiles::profiles::relops_users
+      include roles_profiles::profiles::sudo
+      include roles_profiles::profiles::securitize
 
-    case $::operatingsystem {
-        'Ubuntu': {
-            include ::roles_profiles::profiles::locale
-            include ::roles_profiles::profiles::timezone
-            include ::roles_profiles::profiles::ntp
-            include ::roles_profiles::profiles::motd
-            include ::roles_profiles::profiles::users
-            include ::roles_profiles::profiles::relops_users
-            include ::roles_profiles::profiles::sudo
-            include ::roles_profiles::profiles::securitize
+      include disable_services
+      include grub
+      # fix for ubuntu packaging bug
+      include linux_packages::testresources
 
-            include disable_services
-            include grub
-            # fix for ubuntu packaging bug
-            include linux_packages::testresources
-
-            # TODO:
-            # - add auditd
-            # - add sending of logs to log aggregator/relay
-            # - repo pinning
-        }
-        default: {
-            fail("${::operatingsystem} not supported")
-        }
+      # TODO:
+      # - add auditd
+      # - add sending of logs to log aggregator/relay
+      # - repo pinning
     }
+    default: {
+      fail("${facts['os']['name']} not supported")
+    }
+  }
 }
