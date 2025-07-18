0b911b7bf85e283462731f25db9617e8bd11706d
Andrew Erickson <aerickson@mozilla.com>
2025-07-18 15:02:45 -0700

    update Puppetfile and update inilned r10k_modules

--- r10k_modules/apt/lib/facter/apt_reboot_required.rb
+++ r10k_modules/apt/lib/facter/apt_reboot_required.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 # apt_reboot_required.rb
 Facter.add(:apt_reboot_required) do
-  confine osfamily: 'Debian'
+  confine 'os.family': 'Debian'
   setcode do
     File.file?('/var/run/reboot-required')
   end
