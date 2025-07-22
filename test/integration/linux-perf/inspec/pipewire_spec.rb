1d3b286fe06ad4e01097c777f1b0ba6b437751f8
Andrew Erickson <aerickson@mozilla.com>
2025-07-22 11:51:28 -0700

    tweak indents

--- test/integration/linux-perf/inspec/pipewire_spec.rb
+++ test/integration/linux-perf/inspec/pipewire_spec.rb
@@ -3,9 +3,9 @@ require_relative 'spec_helper'
 if os.family == 'debian' && (os.release.start_with?('18.04') or  os.release.start_with?('22.04'))
   # see pactl_spec.rb
 elsif os.family == 'debian' && os.release.start_with?('24.04')
-    describe package('pipewire') do
-        it { should be_installed }
-    end
+  describe package('pipewire') do
+    it { should be_installed }
+  end
 else
   # shouldn't be here
   # for other OS families or versions, show error
