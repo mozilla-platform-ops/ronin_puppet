51ee041710d5ea1edfd879690394b16863b15db9
Heitor Neiva <hneiva@mozilla.com>
2025-07-23 09:44:46 -0700

    feat: Install UV on mac signers

--- modules/scriptworker_prereqs/manifests/init.pp
+++ modules/scriptworker_prereqs/manifests/init.pp
@@ -39,6 +39,43 @@ class scriptworker_prereqs {
       target  => '/usr/local/bin/python3',
       require => [Class['packages::python3'], File['/usr/local/tools']],
     }
+
+    $uv_version = '0.8.2'
+    $uv_checksum = '954d24634d5f37fa26c7af75eb79893d11623fc81b4de4b82d60d1ade4bfca22'
+    # Note: This is hard-coded to aarch64 architecture - we currently don't have plans on supporting old signers
+
+    file { "/opt/tools/uv-${uv_version}":
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'wheel',
+      mode   => '0755',
+    }
+
+    archive { 'install UV':
+      path            => '/tmp/uv.tar.gz',
+      source          => "https://github.com/astral-sh/uv/releases/download/${uv_version}/uv-aarch64-apple-darwin.tar.gz",
+      checksum        => $uv_checksum,
+      checksum_type   => 'sha256',
+      extract_command => 'tar xfz %s --strip-components=1',
+      extract         => true,
+      extract_path    => "/opt/tools/uv-${uv_version}",
+      creates         => "/opt/tools/uv-${uv_version}/uv",
+      require         => File["/opt/tools/uv-${uv_version}"],
+    }
+
+    file { '/usr/local/bin/uv':
+      ensure  => 'link',
+      target  => "/opt/tools/uv-${uv_version}/uv",
+      require => Archive['install UV'],
+    }
+
+    exec { 'cleanup other UV installs':
+      command     => "find /opt/tools/ -mindepth 1 -maxdepth 1 -type d ! -name \"uv-${uv_version}\" -exec rm -rf {} +",
+      onlyif      => 'ls /opt/tools/uv-*',
+      path        => ['/usr/bin', '/bin'],
+      subscribe   => Archive['install UV'],
+      refreshonly => true,
+    }
   }
 
   # Legacy dirs and symlink for older macOS (macOS 10.14 and 10.15)
