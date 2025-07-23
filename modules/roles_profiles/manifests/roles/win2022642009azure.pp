9a5fc87f9a2271145afcc91005b252160fe445ad
Jonathan Moss <jwmoss88@gmail.com>
2025-07-23 11:55:13 -0400

    exclude gpg4win and test

--- modules/roles_profiles/manifests/roles/win2022642009azure.pp
+++ modules/roles_profiles/manifests/roles/win2022642009azure.pp
@@ -38,7 +38,7 @@ class roles_profiles::roles::win2022642009azure {
             # Worker
             include roles_profiles::profiles::google_auth
             include roles_profiles::profiles::git
-            include roles_profiles::profiles::gpg4win
+            #include roles_profiles::profiles::gpg4win
             include roles_profiles::profiles::mozilla_build
             include roles_profiles::profiles::windows_worker_runner
         }
