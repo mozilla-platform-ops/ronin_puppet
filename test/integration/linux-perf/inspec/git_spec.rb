013ee98cab6e24eff346eab5303fc8e9edb2d528
Andrew Erickson <aerickson@mozilla.com>
2025-07-30 12:15:06 -0700

    git spec test

--- test/integration/linux-perf/inspec/git_spec.rb
+++ test/integration/linux-perf/inspec/git_spec.rb
@@ -8,3 +8,15 @@ describe command('git --version') do
     its('stdout') { should eq "git version 2.42.0\n" }
     its('exit_status') { should eq 0 }
 end
+
+if os.family == 'debian' && ['18.04', '22.04'].include?(os.release)
+describe command('git --version') do
+    its('stdout') { should eq "git version 2.42.0\n" }
+    its('exit_status') { should eq 0 }
+end
+elsif os.family == 'debian' && ['24.04'].include?(os.release)
+describe command('git --version') do
+    its('stdout') { should eq "git version 2.43.0\n" }
+    its('exit_status') { should eq 0 }
+end
+end
