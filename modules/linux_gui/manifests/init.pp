abe88ec4b1c58ab26a40d8e8976dfc80b152f661
Andrew Erickson <aerickson@mozilla.com>
2025-07-30 14:37:55 -0700

    fixes

--- modules/linux_gui/manifests/init.pp
+++ modules/linux_gui/manifests/init.pp
@@ -206,8 +206,8 @@ class linux_gui (
           $screen_depth  = 32
           $refresh       = 60
           # TODO?: use puppetlabs-stdlib eventually
-          $builder_uid = generate('/usr/bin/id', '-u', $builder_user)
-          $builder_gid = generate('/usr/bin/id', '-g', $builder_user)
+          $builder_uid = generate('/usr/bin/id', '-u', $builder_user).strip
+          $builder_gid = generate('/usr/bin/id', '-g', $builder_user).strip
 
           # The new moonshot hardware GPU workers have an intel gpu.
           $use_nvidia = false
@@ -458,6 +458,9 @@ class linux_gui (
             path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin', '/usr/local/bin'],
             require => File['/usr/local/bin/enable-gnome-session-x11-service.sh'],
           }
+
+          # check on service with:
+          #   sudo -u cltbld XDG_RUNTIME_DIR=/run/user/$(id -u cltbld) systemctl --user status gnome-session-x11.service
         }
         default: {
           fail ("Cannot install on Ubuntu version ${facts['os']['release']['full']}")
