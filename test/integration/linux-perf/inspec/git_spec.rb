fefc8ff5446d39c4975fc2d02a368dcd70ef7e23
Andrew Erickson <aerickson@mozilla.com>
2025-08-04 22:34:23 -0700

    spec tests: fix 2404 git test

--- test/integration/linux-perf/inspec/git_spec.rb
+++ test/integration/linux-perf/inspec/git_spec.rb
@@ -4,11 +4,6 @@ describe package('git'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
 end
 
-describe command('git --version') do
-    its('stdout') { should eq "git version 2.42.0\n" }
-    its('exit_status') { should eq 0 }
-end
-
 if os.family == 'debian' && ['18.04', '22.04'].include?(os.release)
 describe command('git --version') do
     its('stdout') { should eq "git version 2.42.0\n" }
