ebd92d43df502a222b5d53babe2a33aa87372e69
Jonathan Moss <jwmoss88@gmail.com>
2025-08-11 17:33:58 -0400

    fix with epp template

--- modules/win_filesystem/manifests/cache_mozilla_unified.pp
+++ modules/win_filesystem/manifests/cache_mozilla_unified.pp
@@ -7,6 +7,7 @@ class win_filesystem::cache_mozilla_unified {
   $checkout_path = "${cache_drive}\\mozilla-unified"
   $mozilla_unified_url = 'https://hg.mozilla.org/mozilla-unified'
   $hg_exe_path = 'C:\\Program Files\\Mercurial\\hg.exe'
+  $clone_script = "${facts['custom_win_roninprogramdata']}\\clone_mozilla_unified.ps1"
   
   # Create the checkout directory
   file { $checkout_path:
@@ -27,13 +28,23 @@ class win_filesystem::cache_mozilla_unified {
     require => File[$checkout_path],
   }
 
+  # Create the PowerShell script from template
+  file { $clone_script:
+    ensure  => file,
+    content => epp('win_filesystem/clone_mozilla_unified.ps1.epp', {
+        'hg_exe_path'         => $hg_exe_path,
+        'mozilla_unified_url' => $mozilla_unified_url,
+        'checkout_path'       => $checkout_path,
+    }),
+  }
+
   # Perform the full hg clone using PowerShell script
   exec { 'clone_mozilla_unified':
-    command  => file('win_filesystem/clone_mozilla_unified.ps1'),
     provider => powershell,
+    command  => $clone_script,
     creates  => "${checkout_path}\\.hg",  # Only run if .hg directory doesn't exist
     timeout  => 3600,  # 1 hour timeout for large clone
-    require  => [File[$checkout_path], Acl["mozilla_unified_checkout_initial_perms"]],
+    require  => [File[$checkout_path], File[$clone_script], Acl["mozilla_unified_checkout_initial_perms"]],
   }
 
   # Ensure permissions are applied to all files after clone
