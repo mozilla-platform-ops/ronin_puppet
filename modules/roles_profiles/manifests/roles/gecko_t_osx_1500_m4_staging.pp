240a4d4b3a8f95d8d648cfbf886f02dcc5b98c15
Ryan Curran <rcurran@mozilla.com>
2025-08-26 10:14:19 -0400

    exclude cltbld user

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4_staging.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4_staging.pp
@@ -4,7 +4,7 @@
 
 class roles_profiles::roles::gecko_t_osx_1500_m4_staging {
   include macos_utils::disable_bluetooth_setup
-  include roles_profiles::profiles::cltbld_user
+  #include roles_profiles::profiles::cltbld_user
   include roles_profiles::profiles::macos_bin_signer
   include roles_profiles::profiles::macos_directory_cleaner
   include roles_profiles::profiles::macos_gw_checker
