e604ca99df13d6ce477f449709b029dc72780799
Jonathan Moss <jwmoss88@gmail.com>
2025-08-29 13:20:21 -0400

    try using dig to figure out string issue

--- modules/roles_profiles/manifests/profiles/azure_vm_file_system.pp
+++ modules/roles_profiles/manifests/profiles/azure_vm_file_system.pp
@@ -3,12 +3,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::azure_vm_file_system {
-  case facts['custom_win_vmSize'] {
-    'Standard_D32alds_v6': {
-      include win_filesystem::configure_nvme_disk
-    }
-    default: {
-      # No special file system configuration needed for this VM size
+  $vm_size_fact = facts.dig('custom_win_vmSize')
+
+  if $vm_size_fact {
+    case String($vm_size_fact) {
+      'Standard_D32alds_v6': {
+        include win_filesystem::configure_nvme_disk
+      }
+      default: {
+        # No special file system configuration needed for this VM size
+      }
     }
   }
 }
