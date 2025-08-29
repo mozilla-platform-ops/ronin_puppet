351a84508bc1a0e39d19e799ffb7f79aeeba63ab
Jonathan Moss <jwmoss88@gmail.com>
2025-08-29 13:37:38 -0400

    update formatting

--- modules/roles_profiles/manifests/profiles/azure_vm_file_system.pp
+++ modules/roles_profiles/manifests/profiles/azure_vm_file_system.pp
@@ -3,9 +3,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::azure_vm_file_system {
-  $vm_size_fact = facts.dig('custom_win_vmSize')
+  $vm_size_fact = $facts['custom_win_vmSize']
 
-  if $vm_size_fact {
+  if $vm_size_fact and $vm_size_fact != '' {
     case String($vm_size_fact) {
       'Standard_D32alds_v6': {
         include win_filesystem::configure_nvme_disk
