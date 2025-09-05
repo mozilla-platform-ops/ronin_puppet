7cc8798766daa0a8c140ed201249de16d799c5da
Jonathan Moss <jwmoss88@gmail.com>
2025-09-05 13:22:59 -0400

    remove unnecessary psm1 files

--- modules/win_disable_services/manifests/disable_onedrive.pp
+++ modules/win_disable_services/manifests/disable_onedrive.pp
@@ -3,34 +3,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_disable_services::disable_onedrive {
-  # This script will disable and remove all portions of onedrive
-  # and prevent onedrive from being setup on future user creation
-  # Using puppetlabs-powershell
-  # Script & modules are originally from https://github.com/W4RH4WK/Debloat-Windows-10
-
-  $module_dir = "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\modules"
-
-  file { "${module_dir}\\take-own":
-    ensure => directory,
-  }
-  file { "${module_dir}\\force-mkdir":
-    ensure => directory,
-  }
-
-  file { "${module_dir}\\force-mkdir\\force-mkdir.psm1":
-    content => file('win_disable_services/force-mkdir.psm1'),
-    require => File["${module_dir}\\force-mkdir"],
-  }
-  file { "${module_dir}\\take-own\\take-own.psm1":
-    content => file('win_disable_services/take-own.psm1'),
-    require => [File["${module_dir}\\force-mkdir\\force-mkdir.psm1"], File["${module_dir}\\take-own"]],
-  }
-
   exec { 'disable_onedrive':
     command  => file('win_disable_services/disable_onedrive.ps1'),
     provider => powershell,
-    require  => File["${module_dir}\\take-own\\take-own.psm1"],
-    unless   => 'Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe"',
   }
 }
 # Bug list
