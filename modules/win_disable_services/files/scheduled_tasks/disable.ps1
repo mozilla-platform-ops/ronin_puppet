308b734d9e0885e1deb518db646f6168448348f1
Jonathan Moss <jwmoss88@gmail.com>
2025-09-08 17:00:59 -0400

    disable unnecessary scheduled tasks related related to vdot

---
+++ modules/win_disable_services/files/scheduled_tasks/disable.ps1
@@ -0,0 +1,39 @@
+$SchTasksList = @(
+    "AnalyzeSystem",
+    "Cellular",
+    "Consolidator",
+    "Diagnostics",
+    "FamilySafetyMonitor",
+    "FamilySafetyRefreshTask",
+    "MapsToastTask",
+    "Microsoft Compatibility Appraiser",
+    "Microsoft Compatibility Appraiser Exp",
+    "Microsoft-Windows-DiskDiagnosticDataCollector",
+    "NotificationTask",
+    "ProcessMemoryDiagnosticEvents",
+    "Proxy",
+    "QueueReporting",
+    "RecommendedTroubleshootingScanner",
+    "RegIdleBackup",
+    "RunFullMemoryDiagnostic",
+    "ScheduledDefrag",
+    "SpeechModelDownloadTask",
+    "Sqm-Tasks",
+    "SR",
+    "StartComponentCleanup",
+    "WindowsActionDialog",
+    "WinSAT",
+    "XblGameSaveTask",
+    "UsbCeip",
+    "VerifyWinRE",
+    "Work Folders Logon Synchronization",
+    "Work Folders Maintenance Work",
+    "Restore"
+)
+
+Foreach ($Item in $SchTasksList) {
+    $TaskObject = Get-ScheduledTask $Item
+    If ($TaskObject) {
+        Disable-ScheduledTask -InputObject $TaskObject | Out-Null
+    }
+}
