2ad17a1b6db9a2eea732c6d09733f24551cbd23c
Jonathan Moss <jwmoss88@gmail.com>
2025-09-29 16:07:42 -0400

    test removing pagefile altogether

--- modules/roles_profiles/manifests/profiles/files_system_managment.pp
+++ modules/roles_profiles/manifests/profiles/files_system_managment.pp
@@ -10,13 +10,6 @@ class roles_profiles::profiles::files_system_managment {
       if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_bootstrap_stage'] == 'complete') {
         include win_filesystem::grant_cache_access
       }
-      if ($facts['custom_win_location'] == 'azure') and ($facts['custom_win_d_drive'] == 'exists') {
-        win_filesystem::set_paging_file { 'azure_paging_file':
-          location => 'D:\pagefile.sys',
-          min_size => 8192,
-          max_size => 8192,
-        }
-      }
       ## If tester then enable long path
       ## Limit long paths on hardware to rule out problem with tests failing
       if ($facts['custom_win_purpose'] == 'tester') {
