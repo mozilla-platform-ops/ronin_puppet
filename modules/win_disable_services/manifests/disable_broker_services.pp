54f7810f8f3f504310ade9463b942408078acfb6
Jonathan Moss <jwmoss88@gmail.com>
2025-10-07 16:59:06 -0400

    add cleaner code not to repeat

--- modules/win_disable_services/manifests/disable_broker_services.pp
+++ modules/win_disable_services/manifests/disable_broker_services.pp
@@ -3,49 +3,28 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_disable_services::disable_broker_services {
-  service { 'TimeBrokerSvc':
-    ensure => 'stopped',
-    enable => false,
-  }
+  $broker_services = [
+    'TimeBrokerSvc',
+    'BrokerInfrastructure',
+    'SystemEventsBroker',
+  ]
 
-  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc\Start':
-    ensure => 'present',
-    type   => dword,
-    data   => '4',
-  }
-  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc\UserServiceFlags':
-    ensure => 'present',
-    type   => dword,
-    data   => '0',
-  }
+  $broker_services.each |String $service_name| {
+    service { $service_name:
+      ensure => 'stopped',
+      enable => false,
+    }
 
-  service { 'BrokerInfrastructure':
-    ensure => 'stopped',
-    enable => false,
-  }
-  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\BrokerInfrastructure\Start':
-    ensure => 'present',
-    type   => dword,
-    data   => '4',
-  }
-  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\BrokerInfrastructure\UserServiceFlags':
-    ensure => 'present',
-    type   => dword,
-    data   => '0',
-  }
+    registry_value { "HKLM\\SYSTEM\\CurrentControlSet\\Services\\${service_name}\\Start":
+      ensure => 'present',
+      type   => dword,
+      data   => '4',
+    }
 
-  service { 'SystemEventsBroker':
-    ensure => 'stopped',
-    enable => false,
-  }
-  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\SystemEventsBroker\Start':
-    ensure => 'present',
-    type   => dword,
-    data   => '4',
-  }
-  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\SystemEventsBroker\UserServiceFlags':
-    ensure => 'present',
-    type   => dword,
-    data   => '0',
+    registry_value { "HKLM\\SYSTEM\\CurrentControlSet\\Services\\${service_name}\\UserServiceFlags":
+      ensure => 'present',
+      type   => dword,
+      data   => '0',
+    }
   }
 }
