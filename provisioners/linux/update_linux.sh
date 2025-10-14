6b6dc8c7eafe88bb697694c6a65d38e83bab7924
Andrew Erickson <aerickson@mozilla.com>
2025-10-14 15:00:59 -0400

    update-linux.sh: more cleanup

--- provisioners/linux/update_linux.sh
+++ provisioners/linux/update_linux.sh
@@ -11,7 +11,6 @@ set -e
 #
 
 # local files
-# BOOTSTRAP_FILE="bootstrap_linux.sh"
 SECRETS_FILE="vault.yaml"
 
 # remote files
@@ -27,24 +26,6 @@ if [ -z "$THE_HOST" ]; then
   echo "ERROR: Please provide a host to bootstrap (like devicepool-0.relops.mozops.net)"
   exit 1
 fi
-
-# if [ -z "$THE_ROLE" ]; then
-#   echo "ERROR: Please provide the role to assign to the host (like gecko_t_linux_talos)"
-#   exit 1
-# fi
-
-# echo "WARNING: This command will overwrite a host's role!"
-# if [ -n "$FORCE" ] && [ "$FORCE" = "force" ]; then
-#     # pass
-#     echo "INFO: force argument given, continuing"
-# else
-#     echo "ERROR: You must provide a third argument FORCE argument set to 'force' to continue."
-#     exit 1
-# fi
-
-# do stuff
-
-# set -x
 
 # sync clock
 # - we leave systemd-timesyncd.service enabled during this, seems to work...
