7c494cf8107290c2e84b53d046dbac04792343e3
Jonathan Moss <jwmoss88@gmail.com>
2025-10-16 16:43:14 -0400

    add script to suppress cmd.exe, disable oobe login window

---
+++ modules/win_scheduled_tasks/files/task_user_background_script.ps1
@@ -0,0 +1,147 @@
+function Set-WindowState {
+    <#
+    .SYNOPSIS
+    Set the state of a window.
+
+    .DESCRIPTION
+    Set the state of a window using the `ShowWindowAsync` function from `user32.dll`.
+
+    .PARAMETER InputObject
+    The process object(s) to set the state of. Can be piped from `Get-Process`.
+
+    .PARAMETER State
+    The state to set the window to. Default is 'SHOW'.
+
+    .PARAMETER SuppressErrors
+    Suppress errors when the main window handle is '0'.
+
+    .PARAMETER SetForegroundWindow
+    Set the window to the foreground
+
+    .PARAMETER ThresholdHours
+    The number of hours to keep the window handle in memory. Default is 24.
+
+    .EXAMPLE
+    Get-Process notepad | Set-WindowState -State HIDE -SuppressErrors
+
+    .EXAMPLE
+    Get-Process notepad | Set-WindowState -State SHOW -SuppressErrors
+
+    .LINK
+    https://gist.github.com/lalibi/3762289efc5805f8cfcf
+
+    .NOTES
+    Original idea from https://gist.github.com/Nora-Ballard/11240204
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = 'InputObject')]
+    param(
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+        [Object[]] $InputObject,
+
+        [Parameter(Position = 1)]
+        [ValidateSet(
+            'FORCEMINIMIZE', 'HIDE', 'MAXIMIZE', 'MINIMIZE', 'RESTORE',
+            'SHOW', 'SHOWDEFAULT', 'SHOWMAXIMIZED', 'SHOWMINIMIZED',
+            'SHOWMINNOACTIVE', 'SHOWNA', 'SHOWNOACTIVATE', 'SHOWNORMAL'
+        )]
+        [string] $State = 'SHOW',
+        [switch] $SuppressErrors = $false,
+        [switch] $SetForegroundWindow = $false,
+        [int] $ThresholdHours = 24
+    )
+
+    Begin {
+        $WindowStates = @{
+            'FORCEMINIMIZE'   = 11
+            'HIDE'            = 0
+            'MAXIMIZE'        = 3
+            'MINIMIZE'        = 6
+            'RESTORE'         = 9
+            'SHOW'            = 5
+            'SHOWDEFAULT'     = 10
+            'SHOWMAXIMIZED'   = 3
+            'SHOWMINIMIZED'   = 2
+            'SHOWMINNOACTIVE' = 7
+            'SHOWNA'          = 8
+            'SHOWNOACTIVATE'  = 4
+            'SHOWNORMAL'      = 1
+        }
+
+        $Win32ShowWindowAsync = Add-Type -MemberDefinition @'
+[DllImport("user32.dll")]
+public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool SetForegroundWindow(IntPtr hWnd);
+'@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
+
+        $global:MainWindowHandles = @{}
+
+    }
+
+    Process {
+        foreach ($process in $InputObject) {
+            $handle = $process.MainWindowHandle
+
+            if ($handle -eq 0 -and $global:MainWindowHandles.ContainsKey($process.Id)) {
+                $handle = [int] $global:MainWindowHandles[$process.Id].Handle
+            }
+
+            if ($handle -eq 0) {
+                if (-not $SuppressErrors) {
+                    Write-Error "Main Window handle is '0'"
+                }
+                else {
+                    Write-Verbose ("Skipping '{0}' with id '{1}', because Main Window handle is '0'" -f $process.ProcessName, $process.Id)
+                }
+
+                continue
+            }
+
+            Write-Verbose ("Processing '{0}' with id '{1}' and handle '{2}'" -f $process.ProcessName, $process.Id, $handle)
+
+            $global:MainWindowHandles[$process.Id] = @{
+                Handle    = $handle.ToString()
+                Timestamp = (Get-Date).ToString("o")
+            }
+
+            $Win32ShowWindowAsync::ShowWindowAsync($handle, $WindowStates[$State]) | Out-Null
+
+            if ($SetForegroundWindow) {
+                $Win32ShowWindowAsync::SetForegroundWindow($handle) | Out-Null
+            }
+
+            Write-Verbose ("» Set Window State '{1}' on '{0}'" -f $handle, $State)
+        }
+    }
+
+    End {
+        $data = [ordered] @{}
+
+        foreach ($key in $global:MainWindowHandles.Keys) {
+            if ($global:MainWindowHandles[$key].Handle -ne 0) {
+                $data["$key"] = $global:MainWindowHandles[$key]
+            }
+        }
+    }
+}
+
+## Set Cursor Position
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true) {
+    $cmdproc = Get-Process -ErrorAction SilentlyContinue | Where-Object { $_.Path -eq "C:\windows\System32\cmd.exe" }
+    
+    if ($null -ne $cmdproc) {
+        Write-Log -Message ('{0} :: Found {1} cmd.exe process(es), hiding window(s)' -f $($MyInvocation.MyCommand.Name), $cmdproc.Count) -severity 'DEBUG'
+        $cmdproc | Set-WindowState -State HIDE
+    }
+
+    $currentPos = [System.Windows.Forms.Cursor]::Position
+    if ($currentPos.X -ne 1010 -and $currentPos.Y -ne 10) {
+        Write-Log -Message ('{0} :: Moving cursor from X={1}, Y={2} to X=1010, Y=10' -f $($MyInvocation.MyCommand.Name), $currentPos.X, $currentPos.Y) -severity 'DEBUG'
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1010, 10)
+    }
+    
+    Start-Sleep -Seconds 5
+}
