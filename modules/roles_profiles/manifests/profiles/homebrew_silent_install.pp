53d21c1c7144642d24d78fa8034c148be8544677
Ryan Curran <rcurran@mozilla.com>
2025-10-17 09:30:07 -0400

    bugfix

--- modules/roles_profiles/manifests/profiles/homebrew_silent_install.pp
+++ modules/roles_profiles/manifests/profiles/homebrew_silent_install.pp
@@ -5,23 +5,38 @@
 
 class roles_profiles::profiles::homebrew_silent_install {
 
-  file { '/opt/homebrew/bin':
+  # Ensure both /opt and /opt/homebrew exist before creating /opt/homebrew/bin
+  file { ['/opt', '/opt/homebrew', '/opt/homebrew/bin']:
     ensure => directory,
-  }
-
-  # Deploy Kandji Homebrew installer script
-  file { '/opt/homebrew/bin/install_homebrew.sh':
-    ensure => file,
-    source => 'puppet:///modules/roles_profiles/profiles/homebrew/install_homebrew.sh',
     owner  => 'root',
     group  => 'wheel',
     mode   => '0755',
   }
 
-  # Execute the installer if Homebrew isn't already present
+  # Deploy Kandji Homebrew installer script
+  file { '/opt/homebrew/bin/install_homebrew.sh':
+    ensure  => file,
+    source  => 'puppet:///modules/roles_profiles/profiles/homebrew/install_homebrew.sh',
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0755',
+    require => File['/opt/homebrew/bin'],
+  }
+
+  # Run the installer only if Homebrew is missing
   exec { 'install_homebrew':
     command => '/opt/homebrew/bin/install_homebrew.sh',
-    creates => '/opt/homebrew/bin/brew',
-    path    => ['/usr/bin','/bin','/usr/sbin','/sbin'],
+    unless  => '/opt/homebrew/bin/brew --version >/dev/null 2>&1',
+    path    => ['/opt/homebrew/bin','/usr/bin','/bin','/usr/sbin','/sbin'],
+    require => File['/opt/homebrew/bin/install_homebrew.sh'],
+  }
+
+  # Add an explicit fact to verify functionality (optional future use)
+  exec { 'verify_homebrew_functionality':
+    command     => '/opt/homebrew/bin/brew --version',
+    onlyif      => '/opt/homebrew/bin/brew --version >/dev/null 2>&1',
+    path        => ['/opt/homebrew/bin','/usr/bin','/bin','/usr/sbin','/sbin'],
+    logoutput   => false,
+    refreshonly => true,
   }
 }
