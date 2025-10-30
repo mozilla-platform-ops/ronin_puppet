4dcce9955470f957cc01d2cbabc59452ce49cad0
Ryan Curran <rcurran@mozilla.com>
2025-10-30 08:13:18 -0400

    lima

--- modules/roles_profiles/manifests/profiles/colima_docker.pp
+++ modules/roles_profiles/manifests/profiles/colima_docker.pp
@@ -13,17 +13,17 @@ class roles_profiles::profiles::colima_docker (
 
   $brew_path = '/opt/homebrew/bin/brew'
 
-  # Install Docker and Colima via Homebrew under the admin user
+  # Install Docker, Lima, and Colima via Homebrew under the admin user
   exec { 'install_docker_colima':
-    command   => "/usr/bin/su - admin -c '${brew_path} install ${package_name} colima || true'",
-    unless    => "/usr/bin/su - admin -c '${brew_path} list --formula | grep -E \"(${package_name}|colima)\"'",
+    command   => "/usr/bin/su - admin -c '${brew_path} install ${package_name} lima colima || true'",
+    unless    => "/usr/bin/su - admin -c '${brew_path} list --formula | grep -E \"(${package_name}|lima|colima)\"'",
     path      => ['/opt/homebrew/bin', '/usr/local/bin', '/usr/bin', '/bin'],
     timeout   => 0,
     logoutput => true,
     require   => File['/opt/homebrew/bin'],
   }
 
-  # Verify both binaries exist - run colima version as admin user to have proper $HOME
+  # Verify all binaries exist - run colima version as admin user to have proper $HOME
   exec { 'verify_docker_colima':
     command => "/opt/homebrew/bin/docker --version && /usr/bin/su - admin -c '/opt/homebrew/bin/colima version'",
     unless  => "/opt/homebrew/bin/docker --version >/dev/null 2>&1 && /usr/bin/su - admin -c '/opt/homebrew/bin/colima version >/dev/null 2>&1'",
@@ -31,12 +31,13 @@ class roles_profiles::profiles::colima_docker (
     require => Exec['install_docker_colima'],
   }
 
-  # Start Colima
+  # Start Colima as admin user with proper PATH
   exec { 'start_colima':
-    command   => '/usr/bin/su - admin -c "/opt/homebrew/bin/colima start --arch aarch64 --vm-type vz --cpu 4 --memory 8 --network-address"',
-    unless    => '/usr/bin/su - admin -c "/opt/homebrew/bin/colima status" | grep -q "running"',
+    command   => '/usr/bin/su - admin -c "PATH=/opt/homebrew/bin:\$PATH /opt/homebrew/bin/colima start --arch aarch64 --vm-type vz --cpu 4 --memory 8 --network-address"',
+    unless    => '/usr/bin/su - admin -c "PATH=/opt/homebrew/bin:\$PATH /opt/homebrew/bin/colima status" | grep -q "running"',
     path      => ['/opt/homebrew/bin','/usr/local/bin','/usr/bin','/bin'],
     require   => Exec['verify_docker_colima'],
     logoutput => true,
+    timeout   => 300,
   }
 }
