6b8ec501c26eaf4a8648bae027bb635180a3974a
Ryan Curran <rcurran@mozilla.com>
2025-10-30 08:00:18 -0400

    bugfix

--- modules/roles_profiles/manifests/profiles/colima_docker.pp
+++ modules/roles_profiles/manifests/profiles/colima_docker.pp
@@ -1,15 +1,19 @@
+# Installs Docker and Colima using Homebrew and ensures Colima is running.
 class roles_profiles::profiles::colima_docker (
   String $package_name = lookup('docker.package_name'),
   String $service_name = lookup('docker.service_name'),
 ) {
-
+  # Install Docker and Colima via Homebrew
   package { [$package_name, 'colima']:
-    ensure => installed,
+    ensure   => installed,
+    provider => brew,
   }
 
+  # Start Colima if not already running
   exec { 'start_colima':
     command => '/opt/homebrew/bin/colima start --arch aarch64 --vm-type vz --cpu 4 --memory 8 --network-address',
-    unless  => 'pgrep -x colima',
-    path    => ['/opt/homebrew/bin','/usr/bin','/bin'],
+    unless  => '/opt/homebrew/bin/colima status | grep -q "running"',
+    path    => ['/opt/homebrew/bin','/usr/local/bin','/usr/bin','/bin'],
+    require => Package['colima'],
   }
 }
