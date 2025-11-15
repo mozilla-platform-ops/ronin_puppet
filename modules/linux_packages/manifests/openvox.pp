44b5de5a8e0bbe09db07af7b26fdd62a739e8374
Andrew Erickson <aerickson@mozilla.com>
2025-11-14 18:10:01 -0800

    openvox: tweak logic

--- modules/linux_packages/manifests/openvox.pp
+++ modules/linux_packages/manifests/openvox.pp
@@ -28,20 +28,26 @@ class linux_packages::openvox {
           # apt install openvox-agent
           # fetch and install the openvox repo deb
           $deb_name = 'openvox8-release-ubuntu24.04.deb'
-          file { 'openvox_repo_deb':
-            ensure => 'file',
-            path   => "/tmp/${deb_name}",
-            mode   => 'a+r',
-            source => "https://apt.voxpupuli.org/${deb_name}",
-            unless => 'dpkg -l | grep -q "^ii.*openvox8-release"',
+          # file { 'openvox_repo_deb':
+          #   ensure => 'file',
+          #   path   => "/tmp/${deb_name}",
+          #   mode   => 'a+r',
+          #   source => "https://apt.voxpupuli.org/${deb_name}",
+          # }
+          # use an exec and wget instead
+          exec { 'openvox_repo_deb':
+            command => "/usr/bin/wget -O /tmp/${deb_name} https://apt.voxpupuli.org/${deb_name}",
+            creates => '/etc/apt/sources.list.d/openvox8-release.list',
+            require => Package[$packages_to_purge],
           }
           package { 'openvox repo deb':
-            ensure    => installed,
-            provider  => dpkg,
-            source    => "/tmp/${deb_name}",
-            notify    => Exec['apt_update'],
-            subscribe => File['openvox_repo_deb'],
-            unless    => 'dpkg -l | grep -q "^ii.*openvox8-release"',
+            ensure      => installed,
+            provider    => dpkg,
+            source      => "/tmp/${deb_name}",
+            notify      => Exec['apt_update'],
+            # only run this if the exec above runs (which only runs if the 'creates' file is missing)
+            subscribe   => Exec['openvox_repo_deb'],
+            refreshonly => true,
           }
 
           # install openvox-agent
