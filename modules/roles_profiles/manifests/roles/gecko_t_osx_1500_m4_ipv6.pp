351130e989d364315e8ed0db4bb58855f7f01fae
Ryan Curran <rcurran@mozilla.com>
2025-11-20 11:48:53 -0500

    minimal config test

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4_ipv6.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1500_m4_ipv6.pp
@@ -3,26 +3,26 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::roles::gecko_t_osx_1500_m4_ipv6 {
-  include macos_utils::disable_bluetooth_setup
-  include roles_profiles::profiles::cltbld_user
-  include roles_profiles::profiles::macos_bin_signer
-  include roles_profiles::profiles::macos_directory_cleaner
-  include roles_profiles::profiles::macos_gw_checker
-  include roles_profiles::profiles::macos_people_remover
-  include roles_profiles::profiles::macos_run_puppet
-  include roles_profiles::profiles::macos_tcc_perms
-  include roles_profiles::profiles::macos_xcode_tools
-  include roles_profiles::profiles::motd
-  include roles_profiles::profiles::network
-  include roles_profiles::profiles::ntp
-  include roles_profiles::profiles::packages_installed
-  include roles_profiles::profiles::pipconf
-  include roles_profiles::profiles::relops_users
-  include roles_profiles::profiles::safaridriver
-  include roles_profiles::profiles::sudo
-  include roles_profiles::profiles::talos
-  include roles_profiles::profiles::timezone
-  include roles_profiles::profiles::users
-  include roles_profiles::profiles::vnc
-  include roles_profiles::profiles::worker
+  # include macos_utils::disable_bluetooth_setup
+  # include roles_profiles::profiles::cltbld_user
+  # include roles_profiles::profiles::macos_bin_signer
+  # include roles_profiles::profiles::macos_directory_cleaner
+  # include roles_profiles::profiles::macos_gw_checker
+  # include roles_profiles::profiles::macos_people_remover
+  # include roles_profiles::profiles::macos_run_puppet
+  # include roles_profiles::profiles::macos_tcc_perms
+  # include roles_profiles::profiles::macos_xcode_tools
+  # include roles_profiles::profiles::motd
+  # include roles_profiles::profiles::network
+  # include roles_profiles::profiles::ntp
+  # include roles_profiles::profiles::packages_installed
+  # include roles_profiles::profiles::pipconf
+  # include roles_profiles::profiles::relops_users
+  # include roles_profiles::profiles::safaridriver
+  # include roles_profiles::profiles::sudo
+  # include roles_profiles::profiles::talos
+  # include roles_profiles::profiles::timezone
+  # include roles_profiles::profiles::users
+  # include roles_profiles::profiles::vnc
+  # include roles_profiles::profiles::worker
 }
