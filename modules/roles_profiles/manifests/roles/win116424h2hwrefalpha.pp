ae53eab6fffacf4151f43038bdb9d6033d3232eb
Mark Cornmesser <mcornmesser@mozilla.com>
2025-11-26 12:45:06 -0800

    revert version for win hw

--- modules/roles_profiles/manifests/roles/win116424h2hwrefalpha.pp
+++ modules/roles_profiles/manifests/roles/win116424h2hwrefalpha.pp
@@ -31,6 +31,6 @@ class roles_profiles::roles::win116424h2hwrefalpha {
   include roles_profiles::profiles::mozilla_maintenance_service
   include roles_profiles::profiles::windows_datacenter_administrator
   include roles_profiles::profiles::google_chrome
-  #include roles_profiles::profiles::windows_generic_worker_standalone
-  include roles_profiles::profiles::windows_worker_runner
+  include roles_profiles::profiles::windows_generic_worker_standalone
+  #include roles_profiles::profiles::windows_worker_runner
 }
