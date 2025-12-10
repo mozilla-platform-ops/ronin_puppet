a0cb51be3f9c807fdbffedfecfe324a8b202a2da
Mark Cornmesser <mcornmesser@mozilla.com>
2025-12-10 09:32:46 -0800

    troubleshooting

--- modules/win_scheduled_tasks/files/maintainsystem-hw.ps1
+++ modules/win_scheduled_tasks/files/maintainsystem-hw.ps1
@@ -514,6 +514,7 @@ If ($bootstrap_stage -eq 'complete') {
     Get-LatestGoogleChrome
 
     StartWorkerRunner
+    Exit-PSSession
 }
 else {
     Write-Log -message  ('{0} :: Bootstrap has not completed. EXITING!' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
