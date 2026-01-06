3b1035d6e2dbbe133b89751269b6abd95ecf959f
Mark Cornmesser <mcornmesser@mozilla.com>
2026-01-06 12:49:55 -0800

    disable one drive notification

--- modules/win_scheduled_tasks/manifests/self_redeploy_check.pp
+++ modules/win_scheduled_tasks/manifests/self_redeploy_check.pp
@@ -19,8 +19,8 @@ class win_scheduled_tasks::self_redeploy_check (
     trigger   => [{
       schedule         => 'daily',
       start_time       => '00:00',
-      #minutes_interval => 120,
-      minutes_interval => 5,
+      minutes_interval => 120,
+      #minutes_interval => 5,
       #minutes_duration => 1440, # 24 hours = repeat every 2 hours all day
     }],
     user      => 'SYSTEM',
