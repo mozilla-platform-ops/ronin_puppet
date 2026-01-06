989b7a9b5462c575e0de71c6aa6a06a1971ca060
Mark Cornmesser <mcornmesser@mozilla.com>
2026-01-06 09:00:17 -0800

    disable MS edge services

---
+++ modules/win_disable_services/manifests/disable_ms_edge.pp
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class win_disable_services::disable_ms_edge {
+    win_disable_services::disable_service { 'edgeupdate':
+    }
+    win_disable_services::disable_service { 'MicrosoftEdgeElevationService':
+    }
+}
