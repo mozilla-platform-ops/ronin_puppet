e6493262045ffec2cea018702e9c49bfc7b84400
Mark Cornmesser <mcornmesser@mozilla.com>
2026-01-12 15:49:41 -0800

    disable more smart screen

--- modules/win_disable_services/manifests/disable_defender_smartscreen.pp
+++ modules/win_disable_services/manifests/disable_defender_smartscreen.pp
@@ -1,10 +1,6 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 class win_disable_services::disable_defender_smartscreen {
 
-  ## Windows Shell / Explorer SmartScreen ("Check apps and files") POLICY
+  ## 1) Shell/Explorer SmartScreen (policy)
   registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\System':
     ensure => present,
   }
@@ -19,19 +15,18 @@ class win_disable_services::disable_defender_smartscreen {
     ensure => absent,
   }
 
-  ## Explorer non-policy setting (per NinjaOne)
+  ## 2) Explorer non-policy setting (per NinjaOne)
   registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer':
     ensure => present,
   }
 
-  ## Values: "Off", "Warn", "RequireAdmin"
   registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\SmartScreenEnabled':
     ensure => present,
     type   => string,
     data   => 'Off',
   }
 
-  ## Microsoft Edge SmartScreen (Edge policy registry path)
+  ## 3) Edge SmartScreen (official Edge policy)
   registry_key { 'HKLM\SOFTWARE\Policies\Microsoft\Edge':
     ensure => present,
   }
@@ -42,8 +37,12 @@ class win_disable_services::disable_defender_smartscreen {
     data   => '0',
   }
 
-  ## disable Edge PUA reputation as well
-  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Edge\SmartScreenPuaEnabled':
+  ## 4) Store apps / AppHost web content evaluation
+  registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost':
+    ensure => present,
+  }
+
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost\EnableWebContentEvaluation':
     ensure => present,
     type   => dword,
     data   => '0',
