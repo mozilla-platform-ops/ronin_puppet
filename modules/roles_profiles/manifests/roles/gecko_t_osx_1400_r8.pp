a1a301865c070d5d09903c01d46a805f19a9b3da
Ryan Curran <rcurran@mozilla.com>
2026-01-14 14:12:42 -0500

    add saf driver back t mac14 r8s

--- modules/roles_profiles/manifests/roles/gecko_t_osx_1400_r8.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_osx_1400_r8.pp
@@ -12,6 +12,7 @@ class roles_profiles::roles::gecko_t_osx_1400_r8 {
   include roles_profiles::profiles::macos_lsdb
   include roles_profiles::profiles::macos_people_remover
   include roles_profiles::profiles::macos_run_puppet
+  include roles_profiles::profiles::macos_safari_driver
   include roles_profiles::profiles::macos_tcc_perms
   include roles_profiles::profiles::macos_xcode_tools
   include roles_profiles::profiles::motd
