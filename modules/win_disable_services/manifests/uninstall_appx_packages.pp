ef5703cc96191f517a9edbb6a403fcfb1340af0f
Mark Cornmesser <mcornmesser@mozilla.com>
2026-01-15 11:33:19 -0800

    do not run disable optional services for Azure

--- modules/win_disable_services/manifests/uninstall_appx_packages.pp
+++ modules/win_disable_services/manifests/uninstall_appx_packages.pp
@@ -1,12 +1,14 @@
 # This class is responsible for disabling AppX packages on Windows.
-class win_disable_services::uninstall_appx_packages {
+class win_disable_services::uninstall_appx_packages (
+    $apx_uninstall
+){
 
   $ronin_base  = $facts['custom_win_roninprogramdata']
   $script_path = "${ronin_base}\\win_uninstall_appx_packages.ps1"
 
   file { $script_path:
     ensure  => file,
-    content => file('win_disable_services/appxpackages/uninstall.ps1'),
+    content => file("win_disable_services/appxpackages/${apx_uninstall}"),
   }
 
   include win_disable_services::disable_ms_edge
