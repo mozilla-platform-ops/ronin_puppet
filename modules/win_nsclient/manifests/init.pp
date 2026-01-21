e0e26a87ac4f3423d5f1d58051ca9b6889e0e56a
Mark Cornmesser <mcornmesser@mozilla.com>
2026-01-21 11:21:58 -0800

    initial commit

--- modules/win_nsclient/manifests/init.pp
+++ modules/win_nsclient/manifests/init.pp
@@ -3,23 +3,47 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class win_nsclient::init (
-    String $server,
-    String $server_pw
+  String $server,
+  String $server_pw,
 ){
+  $nscp_dir    = "${facts['custom_win_programfiles']}\\NSClient++"
+  $scripts_dir = "${nscp_dir}\\scripts"
 
-    win_packages::win_msi_pkg  { 'NSClient++ (x64)':
-        pkg             => 'NSCP-0.9.15-x64.msi',
-        install_options => ['/quiet'],
-    }
-    file { "${facts['custom_win_programfiles']}\\NSClient++\\nsclient.ini":
-        content   => epp('win_nsclient/nsclient.ini.epp'),
-        show_diff => false,
-    }
-    file { "${facts['custom_win_programfiles']}\\NSClient++\\scripts\\screen_res.ps1":
-        content => file('win_nsclient/screen_res.ps1'),
-    }
-    service { 'nsclient++':
-        ensure => running,
-        enable => true,
-    }
+  win_packages::win_msi_pkg { 'NSClient++ (x64)':
+    pkg             => 'NSCP-0.9.15-x64.msi',
+    install_options => ['/quiet'],
+  }
+
+  # Barrier: don't try to drop files until the dir exists
+  file { $nscp_dir:
+    ensure  => directory,
+    require => Win_packages::Win_msi_pkg['NSClient++ (x64)'],
+  }
+
+  file { $scripts_dir:
+    ensure  => directory,
+    require => File[$nscp_dir],
+  }
+
+  file { "${nscp_dir}\\nsclient.ini":
+    content   => epp('win_nsclient/nsclient.ini.epp', {
+      'server'    => $server,
+      'server_pw' => $server_pw,
+    }),
+    show_diff => false,
+    require   => File[$nscp_dir],
+    notify    => Service['nscp'],
+  }
+
+  file { "${scripts_dir}\\screen_res.ps1":
+    content => file('win_nsclient/screen_res.ps1'),
+    require => File[$scripts_dir],
+    notify  => Service['nscp'],
+  }
+
+  service { 'nscp':
+    ensure  => running,
+    enable  => true,
+    require => Win_packages::Win_msi_pkg['NSClient++ (x64)'],
+  }
 }
