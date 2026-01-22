a4dc96c4ce621c7538eab1c1a300f9b1c8c1a160
markcor <mcornmesser@mozilla.com>
2026-01-22 13:16:04 -0800

    Do not uninstall appx on hardware

--- modules/roles_profiles/manifests/profiles/disable_services.pp
+++ modules/roles_profiles/manifests/profiles/disable_services.pp
@@ -53,6 +53,9 @@ class roles_profiles::profiles::disable_services {
                 include win_disable_services::disable_optional_services
             }
             'azure': {
+              class { 'win_disable_services::uninstall_appx_packages':
+                apx_uninstall => $apx_uninstall
+              }            
                 include win_scheduled_tasks::kill_local_clipboard
                 $apx_uninstall = 'uninstall.ps1'
             }
@@ -62,9 +65,6 @@ class roles_profiles::profiles::disable_services {
         ## Let's Uninstall Appx Packages
         ## Taken from https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
         ## Bug 1913499 https://bugzilla.mozilla.org/show_bug.cgi?id=1913499
-        class { 'win_disable_services::uninstall_appx_packages':
-            apx_uninstall => $apx_uninstall
-        }
         ## must be ran after apx uninstall
         if ($facts['custom_win_location'] == 'datacenter') {
             include win_disable_services::disable_ms_edge
