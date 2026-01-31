6dff1ad9ff81fcd3a2ddefabbd73ec6ad6c43d23
Mark Cornmesser <mcornmesser@mozilla.com>
2026-01-30 18:33:53 -0800

    sleep 60 if admin

--- modules/win_scheduled_tasks/files/maintainsystem-hw.ps1
+++ modules/win_scheduled_tasks/files/maintainsystem-hw.ps1
@@ -513,8 +513,8 @@ function Wait-ForUserInitReady {
 }
 
 Write-Log -message ('{0} :: maintained system started' -f $($MyInvocation.MyCommand.Name)) -severity 'DEBUG'
-if ($env:USERNAME -notmatch '*task*') {
-    Write-Log -message ('{0} :: Current user "{1}" does not contain "task"; sleeping 60s' -f $($MyInvocation.MyCommand.Name), $env:USERNAME) -severity 'DEBUG'
+if ($env:USERNAME -match 'administrator') {
+    Write-Log -message ('{0} :: Current user "{1}" matches "administrator"; sleeping 60s' -f $($MyInvocation.MyCommand.Name), $env:USERNAME) -severity 'DEBUG'
     Start-Sleep -Seconds 60
 }
 ## Bug https://bugzilla.mozilla.org/show_bug.cgi?id=1910123
