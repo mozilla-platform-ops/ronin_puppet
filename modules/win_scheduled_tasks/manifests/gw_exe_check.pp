2636d1c6c33d86e5a0e6aad995415eb67286d6d2
Jonathan Moss <jmoss@mozilla.com>
2026-02-05 16:34:15 -0500

    fix(scheduled_tasks): restore gw_exe_check.pp from windows branch

--- modules/win_scheduled_tasks/manifests/gw_exe_check.pp
+++ modules/win_scheduled_tasks/manifests/gw_exe_check.pp
@@ -5,22 +5,23 @@
 class win_scheduled_tasks::gw_exe_check (
 ) {
 
-    $gw_exe_check_ps1 = "${facts['custom_win_roninprogramdata']}\\gw_exe_check.ps1"
+  $gw_exe_check_ps1 = "${facts['custom_win_roninprogramdata']}\\gw_exe_check.ps1"
 
-    file { $gw_exe_check_ps1:
-        content => file('win_scheduled_tasks/gw_exe_check.ps1'),
-    }
-    # Resource from puppetlabs-scheduled_task
-    scheduled_task { 'gw_exe_check':
-        ensure    => 'present',
-        command   => "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\powershell.exe",
-        arguments => "-executionpolicy bypass -File ${gw_exe_check_ps1}",
-        enabled   => true,
-        trigger   => [{
-            'schedule'         => 'boot',
-            'minutes_interval' => '0',
-            'minutes_duration' => '0'
-        }],
-        user      => 'system',
-    }
+  file { $gw_exe_check_ps1:
+    content => file('win_scheduled_tasks/gw_exe_check.ps1'),
+  }
+
+  scheduled_task { 'gw_exe_check':
+    ensure    => 'present',
+    command   => "${facts['custom_win_system32']}\\WindowsPowerShell\\v1.0\\powershell.exe",
+    arguments => "-executionpolicy bypass -File ${gw_exe_check_ps1}",
+    enabled   => true,
+    trigger   => [{
+      schedule         => 'daily',
+      start_time       => '00:00',
+      minutes_interval => 60,
+      #minutes_duration => 1440, # 24 hours = repeat every 5 minutes all day
+    }],
+    user      => 'SYSTEM',
+  }
 }
