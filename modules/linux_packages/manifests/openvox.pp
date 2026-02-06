483076cf738cb7934029e54578930ac6d79461f3
Andrew Erickson <aerickson@mozilla.com>
2026-02-06 11:09:32 -0800

    refactor openvox class, explicitly stop puppet agent that might be running due to funk in CI

--- modules/linux_packages/manifests/openvox.pp
+++ modules/linux_packages/manifests/openvox.pp
@@ -1,146 +1,56 @@
 class linux_packages::openvox {
   case $facts['os']['name'] {
     'Ubuntu': {
-      case $facts['os']['release']['full'] {
-        '18.04':  {
-          # Purge puppet packages first
-          $packages_to_purge = [
-            # don't remove 'puppet-agent', openvox-agent defines it as a conflict and removes it
-            # - removing explicitly here breaks puppet. has to be done atomically by apt.
-            # 'puppet-agent',
-            'puppet-release',
-            'puppet5-release',
-            'puppet6-release',
-            'puppet7-release',
-            'puppet8-release',
-          ]
+      $supported_versions = ['18.04', '22.04', '24.04']
+      unless $facts['os']['release']['full'] in $supported_versions {
+        fail("linux_packages::openvox not supported on Ubuntu ${facts['os']['release']['full']}")
+      }
 
-          package { $packages_to_purge:
-            ensure => purged,
-          }
+      include apt
 
-          # fetch and install the openvox repo deb
-          # TODO: deduplicate with 24.04 code
-          $deb_name = 'openvox8-release-ubuntu18.04.deb'
-          # use an exec and wget instead
-          exec { 'get_openvox_release_deb_file':
-            command => "/usr/bin/wget -O /tmp/${deb_name} https://apt.voxpupuli.org/${deb_name}",
-            creates => '/etc/apt/sources.list.d/openvox8-release.list',
-            require => Package[$packages_to_purge],
-          }
-          exec { 'install_openvox_release_deb':
-            command     => "/usr/bin/dpkg -i /tmp/${deb_name}",
-            creates     => '/etc/apt/sources.list.d/openvox8-release.list',
-            # only run this if the exec above runs (which only runs if the 'creates' file is missing)
-            subscribe   => Exec['get_openvox_release_deb_file'],
-            notify      => Exec['apt_update'],
-            refreshonly => true,
-          }
+      # test-kitchen / CI testing:
+      #   Disable the puppet service left behind by the omnibus installer
+      service { 'puppet':
+        ensure => stopped,
+        enable => false,
+      }
 
-          # install openvox-agent
-          package { 'install openvox agent':
-            ensure    => installed,
-            name      => 'openvox-agent',
-            require   => Exec['apt_update'],
-            subscribe => Exec['install_openvox_release_deb'],
-          }
-        }
-        '22.04': {
-          # Purge puppet packages first
-          $packages_to_purge = [
-            # don't remove 'puppet-agent', openvox-agent defines it as a conflict and removes it
-            # - removing explicitly here breaks puppet. has to be done atomically by apt.
-            # 'puppet-agent',
-            'puppet-release',
-            'puppet5-release',
-            'puppet6-release',
-            'puppet7-release',
-            'puppet8-release',
-          ]
+      # Purge puppet packages first
+      # don't remove 'puppet-agent', openvox-agent defines it as a conflict and removes it
+      # - removing explicitly here breaks puppet. has to be done atomically by apt.
+      $packages_to_purge = [
+        'puppet-release',
+        'puppet5-release',
+        'puppet6-release',
+        'puppet7-release',
+        'puppet8-release',
+      ]
 
-          package { $packages_to_purge:
-            ensure => purged,
-          }
+      package { $packages_to_purge:
+        ensure => purged,
+      }
 
-          # fetch and install the openvox repo deb
-          # NOTE: you need to update the references in the 2 'creates' lines below if you change this.
-          $deb_name = 'openvox8-release-ubuntu22.04.deb'
-          # use an exec and wget instead
-          exec { 'get_openvox_release_deb_file':
-            command => "/usr/bin/wget -O /tmp/${deb_name} https://apt.voxpupuli.org/${deb_name}",
-            creates => '/etc/apt/sources.list.d/openvox8-release.list',
-            require => Package[$packages_to_purge],
-          }
-          exec { 'install_openvox_release_deb':
-            command     => "/usr/bin/dpkg -i /tmp/${deb_name}",
-            creates     => '/etc/apt/sources.list.d/openvox8-release.list',
-            # only run this if the exec above runs (which only runs if the 'creates' file is missing)
-            subscribe   => Exec['get_openvox_release_deb_file'],
-            notify      => Exec['apt_update'],
-            refreshonly => true,
-          }
+      # fetch and install the openvox repo deb
+      $deb_name = "openvox8-release-ubuntu${facts['os']['release']['full']}.deb"
+      exec { 'get_openvox_release_deb_file':
+        command => "/usr/bin/wget -O /tmp/${deb_name} https://apt.voxpupuli.org/${deb_name}",
+        creates => '/etc/apt/sources.list.d/openvox8-release.list',
+        require => Package[$packages_to_purge],
+      }
+      exec { 'install_openvox_release_deb':
+        command     => "/usr/bin/dpkg -i /tmp/${deb_name}",
+        creates     => '/etc/apt/sources.list.d/openvox8-release.list',
+        subscribe   => Exec['get_openvox_release_deb_file'],
+        notify      => Exec['apt_update'],
+        refreshonly => true,
+      }
 
-          # install openvox-agent
-          package { 'install openvox agent':
-            ensure    => installed,
-            name      => 'openvox-agent',
-            require   => Exec['apt_update'],
-            subscribe => Exec['install_openvox_release_deb'],
-          }
-        }
-        '24.04': {
-          # Purge puppet packages first
-          $packages_to_purge = [
-            # don't remove 'puppet-agent', openvox-agent defines it as a conflict and removes it
-            # - removing explicitly here breaks puppet. has to be done atomically by apt.
-            # 'puppet-agent',
-            'puppet-release',
-            'puppet5-release',
-            'puppet6-release',
-            'puppet7-release',
-            'puppet8-release',
-          ]
-
-          package { $packages_to_purge:
-            ensure => purged,
-          }
-
-          # install openvox
-          #
-          # wget https://apt.voxpupuli.org/openvox8-release-ubuntu24.04.deb
-          # dpkg -i openvox8-release-ubuntu24.04.deb
-          # apt update
-          # apt install openvox-agent
-          #
-          # fetch and install the openvox repo deb
-          # NOTE: you need to update the references in the 2 'creates' lines below if you chabge this.
-          $deb_name = 'openvox8-release-ubuntu24.04.deb'
-          # use an exec and wget instead
-          exec { 'get_openvox_release_deb_file':
-            command => "/usr/bin/wget -O /tmp/${deb_name} https://apt.voxpupuli.org/${deb_name}",
-            creates => '/etc/apt/sources.list.d/openvox8-release.list',
-            require => Package[$packages_to_purge],
-          }
-          exec { 'install_openvox_release_deb':
-            command     => "/usr/bin/dpkg -i /tmp/${deb_name}",
-            creates     => '/etc/apt/sources.list.d/openvox8-release.list',
-            # only run this if the exec above runs (which only runs if the 'creates' file is missing)
-            subscribe   => Exec['get_openvox_release_deb_file'],
-            notify      => Exec['apt_update'],
-            refreshonly => true,
-          }
-
-          # install openvox-agent
-          package { 'install openvox agent':
-            ensure    => installed,
-            name      => 'openvox-agent',
-            require   => Exec['apt_update'],
-            subscribe => Exec['install_openvox_release_deb'],
-          }
-        }
-        default: {
-          fail("linux_packages::openvox not supported on Ubuntu ${facts['os']['release']['full']}")
-        }
+      # install openvox-agent
+      package { 'install openvox agent':
+        ensure    => installed,
+        name      => 'openvox-agent',
+        require   => Exec['apt_update'],
+        subscribe => Exec['install_openvox_release_deb'],
       }
     }
     default: {
