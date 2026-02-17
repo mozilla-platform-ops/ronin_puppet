beb7843b9a7251426652dab5ca9213bbe203d78d
Jonathan Moss <jmoss@mozilla.com>
2026-02-17 16:43:01 -0500

    Fix pre-commit failures from merge conflict resolution
    - Resolve conflict markers in win_kms/manifests/set_key.pp (git rename
      confusion between set_key.pp and linux_directory_cleaner.pp)
    - Restore pipconf.pp to correct content (roles_profiles::profiles::pipconf
      wrapping macos_pipconf; windows branch had wrong win_nxlog::install content)
    - Remove misplaced profile files from roles_profiles/profiles/ that contained
      win_* class implementations with wrong class names for their file paths;
      the actual implementations live in win_* module directories

--- modules/roles_profiles/manifests/profiles/pipconf.pp
+++ modules/roles_profiles/manifests/profiles/pipconf.pp
@@ -2,16 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class win_nxlog::install {
-  case $facts['custom_win_bootstrap_stage'] {
-    'complete': {
-      notify { "${module_name} not needed since bootstrap stage is complete": }
-    }
-    default: {
-      win_packages::win_msi_pkg { 'NXLog-CE':
-        pkg             => 'nxlog-ce-2.10.2150.msi',
-        install_options => ['/quiet'],
-      }
-    }
-  }
+class roles_profiles::profiles::pipconf {
+
+    class { 'macos_pipconf':
+                enabled => true,
+            }
 }
