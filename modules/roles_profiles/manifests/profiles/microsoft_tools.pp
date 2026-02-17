b8e5c40948e141d65443b14b2c902d502b991567
Mark Cornmesser <mcornmesser@mozilla.com>
2026-02-17 14:54:28 -0800

    add xperf group the ability to system profile

--- modules/roles_profiles/manifests/profiles/microsoft_tools.pp
+++ modules/roles_profiles/manifests/profiles/microsoft_tools.pp
@@ -42,7 +42,7 @@ class roles_profiles::profiles::microsoft_tools {
               include win_packages::win_10_sdk
             }
           }
-          include win_xperf_acl
+          include xperf_profiling
         }
         default: {
           fail("${$facts['custom_win_purpose']} not supported")
