b24615b6e25fdbc77f3142c61414da584ea2acf6
Andrew Erickson <aerickson@mozilla.com>
2026-02-18 15:00:23 -0800

    add config header, tweak tests, add test todo

--- test/integration/linux-perf/inspec/snmpd_spec.rb
+++ test/integration/linux-perf/inspec/snmpd_spec.rb
@@ -7,5 +7,11 @@ end
 # check our templating worked
 describe file('/etc/snmp/snmpd.conf') do
     it { should exist }
+    # TODO: don't check community secret (so it could work on prod hosts)
     its(:content) { should match /rocommunity aaaa/ }
+
+    # check that our template is in place (and not the default)
+    its(:content) { should match /# mozilla relops snmpd.conf template/ }
+    # check that RO community is enabled
+    its(:content) { should match /^rocommunity/ }
 end
