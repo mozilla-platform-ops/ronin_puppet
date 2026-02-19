67bf83c670c2cac6c2a0bef21c46eeafa9beddfd
Ryan Curran <139882866+rcurranmoz@users.noreply.github.com>
2026-02-19 16:08:57 -0500

    more test coverage (#1056)

---
+++ test/integration/gecko_t_osx_1500_m4/serverspec/ntp_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe 'NTP configuration' do
+  describe command('sudo systemsetup -getusingnetworktime') do
+    its(:stdout) { should match(/Network Time: On/) }
+  end
+
+  describe command('sudo systemsetup -getnetworktimeserver') do
+    its(:stdout) { should match(/time\.apple\.org/) }
+  end
+end
