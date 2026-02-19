ad3d0f236e2716836bf8ec53d1f23f91871665d1
Jonathan Moss <jmoss@mozilla.com>
2026-02-19 15:12:23 -0500

    Force Basic-only auth in serverspec WinRM connection
    WinRM transport: :plaintext still registers credentials for all httpclient
    auth schemes. Without basic_auth_only: true, httpclient tries NTLM negotiate
    first when the server offers it, hitting the MD4/OpenSSL 3.x breakage.

--- test/integration/win116425h2azure/serverspec/spec_helper.rb
+++ test/integration/win116425h2azure/serverspec/spec_helper.rb
@@ -6,6 +6,7 @@ conn = WinRM::Connection.new(
   user: ENV['KITCHEN_USERNAME'],
   password: ENV['KITCHEN_PASSWORD'],
   transport: :plaintext,
+  basic_auth_only: true,
   operation_timeout: 300,
   receive_timeout: 310
 )
