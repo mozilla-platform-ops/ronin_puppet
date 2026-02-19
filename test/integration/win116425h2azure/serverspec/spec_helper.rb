ff8b813af858555add651e64f2cf9b32fb3f283d
Jonathan Moss <jmoss@mozilla.com>
2026-02-19 14:31:59 -0500

    Fix Windows serverspec WinRM connection and flatten workflow matrix
    spec_helper.rb: initialize WinRM connection using KITCHEN_HOSTNAME,
    KITCHEN_USERNAME, and KITCHEN_PASSWORD env vars set by kitchen-verifier-
    serverspec, which are required for the remote_exec: false backend.
    
    kitchen-windows.yml: remove single-entry matrix in favour of a direct
    job definition so logs are one click deep instead of two.

--- test/integration/win116425h2azure/serverspec/spec_helper.rb
+++ test/integration/win116425h2azure/serverspec/spec_helper.rb
@@ -1,5 +1,15 @@
 require 'serverspec'
 require 'winrm'
 
+conn = WinRM::Connection.new(
+  endpoint: "http://#{ENV['KITCHEN_HOSTNAME']}:5985/wsman",
+  user: ENV['KITCHEN_USERNAME'],
+  password: ENV['KITCHEN_PASSWORD'],
+  transport: :plaintext,
+  operation_timeout: 300,
+  receive_timeout: 310
+)
+
+Specinfra.configuration.winrm = conn
 set :backend, :winrm
 set :os, :family => 'windows'
