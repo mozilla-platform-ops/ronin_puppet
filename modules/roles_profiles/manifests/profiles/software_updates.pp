40f3a4777383f78012734f1132ac3ea7a2627207
Ryan Curran <rcurran@mozilla.com>
2026-02-20 13:44:46 -0500

    fix

--- modules/roles_profiles/manifests/profiles/software_updates.pp
+++ modules/roles_profiles/manifests/profiles/software_updates.pp
@@ -5,7 +5,11 @@
 class roles_profiles::profiles::software_updates {
   case $facts['os']['name'] {
     'Darwin': {
+      mac_profiles_handler::manage { 'com.github.erikberglund.ProfileCreator.7AF6CC8C-EA10-4CD9-B145-77FD3CCFEF35':
+        ensure => 'absent',
+      }
       include macos_disable_software_updates
+      Mac_profiles_handler::Manage['com.github.erikberglund.ProfileCreator.7AF6CC8C-EA10-4CD9-B145-77FD3CCFEF35'] -> Class['macos_disable_software_updates']
     }
     default: {
       fail("${facts['os']['name']} not supported")
