9223f8486746459fd3be51cc16020a35d2631108
Ryan Curran <rcurran@mozilla.com>
2026-02-20 13:05:38 -0500

    update disable scroll bars

---
+++ modules/macos_utils/manifests/disable_scroll_bars.pp
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class macos_utils::disable_scroll_bars {
+  $domain = '/Library/Preferences/.GlobalPreferences.plist'
+
+  macos_utils::defaults { 'show_scroll_bars':
+    domain   => $domain,
+    key      => 'AppleShowScrollBars',
+    value    => 'Always',
+    val_type => 'string',
+  }
+
+  macos_utils::defaults { 'no_save_to_cloud':
+    domain   => $domain,
+    key      => 'NSDocumentSaveNewDocumentsToCloud',
+    value    => '0',
+    val_type => 'bool',
+  }
+}
