5c26f6596248218deb9e4e5b203d9b7a7c48d12d
Ryan Curran <rcurran@mozilla.com>
2026-02-20 13:48:46 -0500

    fix

--- modules/roles_profiles/manifests/profiles/screensaver.pp
+++ modules/roles_profiles/manifests/profiles/screensaver.pp
@@ -5,7 +5,11 @@
 class roles_profiles::profiles::screensaver {
   case $facts['os']['name'] {
     'Darwin': {
+      mac_profiles_handler::manage { 'org.mozilla.SetDefaultScreensaver':
+        ensure => 'absent',
+      }
       include macos_disable_screensaver
+      Mac_profiles_handler::Manage['org.mozilla.SetDefaultScreensaver'] -> Class['macos_disable_screensaver']
     }
     default: {
       fail("${facts['os']['name']} not supported")
