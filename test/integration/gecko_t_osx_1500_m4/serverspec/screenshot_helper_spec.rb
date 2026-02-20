d39538ad06c2954053f333c8764461c84f417d2a
Ryan Curran <rcurran@mozilla.com>
2026-02-20 11:07:22 -0500

    fix

--- test/integration/gecko_t_osx_1500_m4/serverspec/screenshot_helper_spec.rb
+++ test/integration/gecko_t_osx_1500_m4/serverspec/screenshot_helper_spec.rb
@@ -14,11 +14,3 @@ describe file('/Users/cltbld/bin/capture-on-demand.sh') do
   it { should be_grouped_into 'staff' }
   it { should be_mode 755 }
 end
-
-describe file('/Users/cltbld/Library/LaunchAgents/com.mozilla.screencapture.plist') do
-  it { should exist }
-  it { should be_file }
-  it { should be_owned_by 'cltbld' }
-  it { should be_grouped_into 'staff' }
-  it { should be_mode 644 }
-end
