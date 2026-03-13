d5495f4b8592e873910b79d3ecfd2d2d9564be55
Jonathan Moss <jmoss@mozilla.com>
2026-03-13 16:12:07 -0400

    RELOPS-2267 Align Windows git specs with installed software check

--- test/integration/win11a6425h2azuretester/serverspec/git_spec.rb
+++ test/integration/win11a6425h2azuretester/serverspec/git_spec.rb
@@ -1,12 +1,6 @@
 require_relative 'spec_helper'
 
-git_exe = 'C:\\Program Files\\Git\\bin\\git.exe'
-
-describe file(git_exe) do
-  it { should exist }
-end
-
-describe powershell_command("& '#{git_exe}' --version") do
+describe software_property_command("$PSItem.DisplayName -match '^Git'", 'DisplayName') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/^git version /i) }
+  its(:stdout) { should match(/^Git/i) }
 end
