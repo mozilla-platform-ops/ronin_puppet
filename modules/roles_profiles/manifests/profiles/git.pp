1e7bdac224becf088f5f7c9419703af139096698
Jonathan Moss <jmoss@mozilla.com>
2026-03-13 16:45:59 -0400

    RELOPS-2267 Add Windows git troubleshooting diagnostics

--- modules/roles_profiles/manifests/profiles/git.pp
+++ modules/roles_profiles/manifests/profiles/git.pp
@@ -3,11 +3,18 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::git {
-  if ($facts['custom_win_git_version'] == '0.0.0') {
+  $detected_git_version = $facts['custom_win_git_version']
+
+  notice("DEBUG roles_profiles::profiles::git custom_win_git_version=${detected_git_version} type=${type($detected_git_version)}")
+
+  if ($detected_git_version == '0.0.0') {
+    notice('DEBUG roles_profiles::profiles::git declaring Chocolatey git package because custom_win_git_version is 0.0.0')
     include chocolatey
     case $facts['os']['name'] {
       'Windows': {
         $git_version = lookup('windows.git.version')
+
+        notice("DEBUG roles_profiles::profiles::git windows.git.version=${git_version}")
 
         package { 'git':
           ensure   => $git_version,
@@ -18,5 +25,7 @@ class roles_profiles::profiles::git {
         fail("${$facts['os']['name']} not supported")
       }
     }
+  } else {
+    notice("DEBUG roles_profiles::profiles::git skipping Git package because custom_win_git_version=${detected_git_version}")
   }
 }
