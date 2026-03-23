50020c3c7562986aeacecc70dd79cb56e34db5a5
Ryan Curran <rcurran@mozilla.com>
2026-03-23 11:33:35 -0400

    fix keyboard prompt

--- modules/macos_utils/manifests/suppress_keyboard_assistant.pp
+++ modules/macos_utils/manifests/suppress_keyboard_assistant.pp
@@ -3,9 +3,30 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class macos_utils::suppress_keyboard_assistant {
-  exec { 'suppress_keyboard_assistant':
-    command => '/usr/bin/defaults write /Library/Preferences/com.apple.keyboardtype keyboardtype -dict "4101-5341-33" -int 40',
-    unless  => '/usr/bin/defaults read /Library/Preferences/com.apple.keyboardtype keyboardtype 2>/dev/null | /usr/bin/grep -q "4101-5341-33"',
-    user    => 'root',
+  $launchagent_path = '/Users/cltbld/Library/LaunchAgents/com.mozilla.suppress-keyboard-assistant.plist'
+
+  if Integer($facts['os']['release']['major']) <= 20 {
+    $cltbld_uid = '36'
+  } else {
+    $cltbld_uid = '555'
+  }
+
+  file { $launchagent_path:
+    ensure => file,
+    owner  => 'cltbld',
+    group  => 'staff',
+    mode   => '0644',
+    source => 'puppet:///modules/macos_utils/com.mozilla.suppress-keyboard-assistant.plist',
+  }
+
+  exec { 'load or restart suppress keyboard assistant agent':
+    command     => "/bin/bash -c 'if /bin/launchctl print gui/${cltbld_uid}/com.mozilla.suppress-keyboard-assistant 2>/dev/null; then \
+                  /bin/launchctl kickstart -k gui/${cltbld_uid}/com.mozilla.suppress-keyboard-assistant; \
+                else \
+                  /bin/launchctl bootstrap gui/${cltbld_uid} \"${launchagent_path}\" 2>/dev/null; \
+                fi; exit 0'",
+    path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+    refreshonly => true,
+    subscribe   => File[$launchagent_path],
   }
 }
