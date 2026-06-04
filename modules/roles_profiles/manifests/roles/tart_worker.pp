282ecd4510dd87de5700f06d491fdbf48d0cc32c
Ryan Curran <rcurran@mozilla.com>
2026-06-04 14:01:13 -0400

    tart_worker: trim to profiles::tart only, matching deployed role (no vault deps)

--- modules/roles_profiles/manifests/roles/tart_worker.pp
+++ modules/roles_profiles/manifests/roles/tart_worker.pp
@@ -13,16 +13,5 @@
 # latter (the obfuscated admin password) cannot be delivered by a config
 # profile. Kept out of this role to avoid the admin password entering vault.
 class roles_profiles::roles::tart_worker {
-  include macos_utils::disable_bluetooth_setup
-  include roles_profiles::profiles::macos_disable_firewall
-  include roles_profiles::profiles::macos_run_puppet
-  include roles_profiles::profiles::motd
-  include roles_profiles::profiles::network
-  include roles_profiles::profiles::ntp
-  include roles_profiles::profiles::relops_users
-  include roles_profiles::profiles::sudo
-  include roles_profiles::profiles::timezone
-  include roles_profiles::profiles::users
-  include roles_profiles::profiles::vnc
   include roles_profiles::profiles::tart
 }
