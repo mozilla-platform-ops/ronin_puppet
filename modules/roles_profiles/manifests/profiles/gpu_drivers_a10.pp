7f4a29ff65e70a79efd0113c41916c4e19318876
jwmossmoz <jmoss@mozilla.com>
2026-06-09 10:30:34 -0400

    Bug 2043544 - Use staged NVIDIA GRID installers for GPU workers (#1228)
    * Bug 2043544 - Use staged NVIDIA GRID installers for GPU workers. RELOPS-2394
    
    * Bug 2043544 - Stabilize archive-backed Windows package installs. RELOPS-2394
    
    * Bug 2043544 - Keep archive fix scoped to NVIDIA GRID. RELOPS-2394
    
    * Bug 2043544 - Stabilize NVIDIA GRID offline cache test
    
    * Bug 2043544 - Use registry guard for VC++ 2022 redistributable
    
    * Bug 2043544 - Require VC++ redist before NVIDIA GRID install

--- modules/roles_profiles/manifests/profiles/gpu_drivers_a10.pp
+++ modules/roles_profiles/manifests/profiles/gpu_drivers_a10.pp
@@ -7,10 +7,13 @@ class roles_profiles::profiles::gpu_drivers_a10 {
   ## Use https://docs.nvidia.com/grid/index.html & https://github.com/Azure/azhpc-extensions/blob/master/NvidiaGPU/resources.json as reference
   case $facts['os']['name'] {
     'windows': {
+      include win_packages::vc_redist_2022_x64
+
       class { 'win_packages::drivers::nvidia_grid':
         display_name => lookup('windows.gpu_a10.display_name'),
         driver_name  => lookup('windows.gpu_a10.name'),
         srcloc       => lookup('windows.ext_pkg_src'),
+        require      => Class['win_packages::vc_redist_2022_x64'],
       }
     }
     default: {
