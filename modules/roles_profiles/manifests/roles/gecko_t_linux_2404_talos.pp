bd1cd21dec092a6f9051cc8d8f9c56516d06b3d5
Andrew Erickson <aerickson@mozilla.com>
2026-07-29 16:29:05 -0700

    linux talos: add Intel pstate performance helper

--- modules/roles_profiles/manifests/roles/gecko_t_linux_2404_talos.pp
+++ modules/roles_profiles/manifests/roles/gecko_t_linux_2404_talos.pp
@@ -24,4 +24,7 @@ class roles_profiles::roles::gecko_t_linux_2404_talos {
 
   # turbostat support
   include roles_profiles::profiles::linux_turbostat
+
+  # CPU performance limit support
+  include roles_profiles::profiles::linux_intel_pstate
 }
