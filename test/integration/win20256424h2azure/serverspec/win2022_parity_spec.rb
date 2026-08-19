290be70dbcb2ed3b23bb6b85c1ff3e50e0ffdc4d
Jonathan Moss <jmoss@mozilla.com>
2026-08-19 16:56:50 -0400

    RELOPS-2407 Drop legacy DirectX from Server 2025

--- test/integration/win20256424h2azure/serverspec/win2022_parity_spec.rb
+++ test/integration/win20256424h2azure/serverspec/win2022_parity_spec.rb
@@ -1,1 +1,1 @@
-Dir[File.expand_path('../../win2022642009azure/serverspec/*_spec.rb', __dir__)].sort.each { |spec| require spec }
+Dir[File.expand_path('../../win2022642009azure/serverspec/*_spec.rb', __dir__)].grep_v(/microsoft_tools_spec\.rb$/).sort.each { |spec| require spec }
