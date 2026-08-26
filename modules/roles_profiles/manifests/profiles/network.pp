c8211fa9bae7e6108dc5c4e19766e929294d9e13
Jonathan Moss <jmoss@mozilla.com>
2026-08-26 17:14:54 -0400

    RELOPS-2407 Simplify IPv6 exception

--- modules/roles_profiles/manifests/profiles/network.pp
+++ modules/roles_profiles/manifests/profiles/network.pp
@@ -16,9 +16,8 @@ class roles_profiles::profiles::network {
         }
       }
       # Skip disabling IPv6 for Windows 11 24H2+ x64 on Azure (requires IPv6)
-      $enable_ipv6 = $facts['custom_win_location'] == 'azure' and $facts['custom_win_os_version'] == 'win_11_2009' and
-        $facts['custom_win_display_version'] in ['24H2', '25H2'] and $facts['custom_win_os_arch'] == 'x64'
-      if !$enable_ipv6 {
+      unless $facts['custom_win_location'] == 'azure' and $facts['custom_win_os_version'] == 'win_11_2009' and
+        $facts['custom_win_display_version'] in ['24H2', '25H2'] and $facts['custom_win_os_arch'] == 'x64' {
         include win_network::disable_ipv6
       }
       if $facts['os']['release']['full'] == '2012 R2' {
