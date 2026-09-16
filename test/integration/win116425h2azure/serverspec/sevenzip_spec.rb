c3e75a8252a52335189719b4d0f55f3f589aebd1
jwmossmoz <jmoss@mozilla.com>
2026-09-16 11:50:46 -0400

    Remove dead Windows code (#1364)
    * Remove dead Windows code
    
    * Preserve Windows hardware behavior
    
    * Handle Windows roles without variant data

--- test/integration/win116425h2azure/serverspec/sevenzip_spec.rb
+++ test/integration/win116425h2azure/serverspec/sevenzip_spec.rb
@@ -1,6 +1,2 @@
 require_relative 'spec_helper'
-
-describe software_property_command("$_.DisplayName -like '*Zip*'", 'DisplayVersion') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/^25\.00/) }
-end
+require_relative '../../win10642009azure/serverspec/sevenzip_spec'
